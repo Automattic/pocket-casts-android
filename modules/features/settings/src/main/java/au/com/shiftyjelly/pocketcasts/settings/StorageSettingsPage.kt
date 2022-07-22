8727bf002ee00f1cd2f75e6cfaf17b5cbb08078c
ashiagr <agr.ash@gmail.com>
2022-07-22 06:33:15 +0530

    Add blank compose page and vm

---
+++ modules/features/settings/src/main/java/au/com/shiftyjelly/pocketcasts/settings/StorageSettingsPage.kt
@@ -0,0 +1,7 @@
+package au.com.shiftyjelly.pocketcasts.settings
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun StorageSettingsPage() {
+}
---
+++ modules/features/settings/src/main/java/au/com/shiftyjelly/pocketcasts/settings/viewmodel/StorageSettingsViewModel.kt
@@ -0,0 +1,9 @@
+package au.com.shiftyjelly.pocketcasts.settings.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class StorageSettingsViewModel
+@Inject constructor() : ViewModel()
