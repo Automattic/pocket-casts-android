cbcd59f55c69b15b315361c4fceb8bd13741cc0c
ashiagr <agr.ash@gmail.com>
2022-07-29 11:05:47 +0530

    Extract common first subscription pricing phase

--- modules/services/utils/src/main/java/au/com/shiftyjelly/pocketcasts/utils/extensions/ProductDetails.kt
+++ modules/services/utils/src/main/java/au/com/shiftyjelly/pocketcasts/utils/extensions/ProductDetails.kt
@@ -3,27 +3,19 @@ package au.com.shiftyjelly.pocketcasts.utils.extensions
 import com.android.billingclient.api.ProductDetails
 
 val ProductDetails.shortTitle: String
-    get() {
-        return title.split(" (").first()
-    }
+    get() = title.split(" (").first()
+
+val ProductDetails.firstSubscriptionPricingPhase: ProductDetails.PricingPhase?
+    get() = subscriptionOfferDetails?.firstOrNull()?.pricingPhases?.pricingPhaseList?.firstOrNull()
 
 val ProductDetails.price: String
-    get() {
-        return subscriptionOfferDetails?.firstOrNull()?.pricingPhases?.pricingPhaseList?.firstOrNull()?.formattedPrice ?: "0"
-    }
+    get() = firstSubscriptionPricingPhase?.formattedPrice ?: "0"
 
 val ProductDetails.priceDouble: Double
-    get() {
-        val priceAmountMicros = subscriptionOfferDetails?.firstOrNull()?.pricingPhases?.pricingPhaseList?.firstOrNull()?.priceAmountMicros ?: 0
-        return priceAmountMicros * 1_000_000.0
-    }
+    get() = (firstSubscriptionPricingPhase?.priceAmountMicros ?: 0) * 1_000_000.0
 
 val ProductDetails.priceCurrencyCode: String
-    get() {
-        return subscriptionOfferDetails?.firstOrNull()?.pricingPhases?.pricingPhaseList?.firstOrNull()?.priceCurrencyCode ?: ""
-    }
+    get() = firstSubscriptionPricingPhase?.priceCurrencyCode ?: ""
 
 val ProductDetails.billingPeriod: String?
-    get() {
-        return subscriptionOfferDetails?.firstOrNull()?.pricingPhases?.pricingPhaseList?.firstOrNull()?.billingPeriod
-    }
+    get() = firstSubscriptionPricingPhase?.billingPeriod
