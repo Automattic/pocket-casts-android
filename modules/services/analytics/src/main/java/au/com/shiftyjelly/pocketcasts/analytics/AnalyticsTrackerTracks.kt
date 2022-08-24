e7b889530c89b5e7fa7818fd684165b979000773
ashiagr <agr.ash@gmail.com>
2022-08-24 18:29:20 +0530

    Add basic anonymous user tracking

--- modules/services/analytics/src/main/java/au/com/shiftyjelly/pocketcasts/analytics/AnalyticsTrackerTracks.kt
+++ modules/services/analytics/src/main/java/au/com/shiftyjelly/pocketcasts/analytics/AnalyticsTrackerTracks.kt
@@ -6,14 +6,26 @@ import au.com.shiftyjelly.pocketcasts.analytics.AnalyticsTracker.PREFKEY_SEND_US
 import au.com.shiftyjelly.pocketcasts.analytics.AnalyticsTracker.sendUsageStats
 import com.automattic.android.tracks.TracksClient
 import dagger.hilt.android.qualifiers.ApplicationContext
+import org.json.JSONObject
+import java.util.Locale
 import javax.inject.Inject
 
 class AnalyticsTrackerTracks @Inject constructor(
     @ApplicationContext private val appContext: Context,
-) : Tracker {
+) : Tracker(appContext) {
     private val tracksClient: TracksClient? = TracksClient.getClient(appContext)
-    override fun track(event: AnalyticsEvent, properties: Map<String, *>?) {
-        TODO("Not yet implemented")
+    override val anonIdPrefKey: String
+        get() = TRACKS_ANON_ID
+
+    override fun track(event: AnalyticsEvent, properties: Map<String, *>) {
+        if (tracksClient == null) return
+
+        val eventName = event.name.lowercase(Locale.getDefault())
+        val user = anonID ?: generateNewAnonID()
+        val userType = TracksClient.NosaraUserType.ANON
+
+        val propertiesJson = JSONObject(properties)
+        tracksClient.track(EVENTS_PREFIX + eventName, propertiesJson, user, userType)
     }
 
     override fun flush() {
@@ -21,6 +33,7 @@ class AnalyticsTrackerTracks @Inject constructor(
     }
 
     override fun clearAllData() {
+        super.clearAllData()
         tracksClient?.clearUserProperties()
         tracksClient?.clearQueues()
     }
@@ -28,5 +41,10 @@ class AnalyticsTrackerTracks @Inject constructor(
     override fun storeUsagePref() {
         val prefs = PreferenceManager.getDefaultSharedPreferences(appContext)
         prefs.edit().putBoolean(PREFKEY_SEND_USAGE_STATS, sendUsageStats).apply()
+    }
+
+    companion object {
+        private const val TRACKS_ANON_ID = "nosara_tracks_anon_id"
+        private const val EVENTS_PREFIX = "pcandroid_"
     }
 }
--- modules/services/analytics/src/main/java/au/com/shiftyjelly/pocketcasts/analytics/Tracker.kt
+++ modules/services/analytics/src/main/java/au/com/shiftyjelly/pocketcasts/analytics/Tracker.kt
@@ -1,8 +1,48 @@
 package au.com.shiftyjelly.pocketcasts.analytics
 
-interface Tracker {
-    fun track(event: AnalyticsEvent, properties: Map<String, *>? = null)
-    fun flush()
-    fun clearAllData()
-    fun storeUsagePref()
+import android.content.Context
+import androidx.preference.PreferenceManager
+import dagger.hilt.android.qualifiers.ApplicationContext
+import java.util.UUID
+
+abstract class Tracker(@ApplicationContext private val appContext: Context) {
+    private var anonymousID: String? = null // do not access this variable directly. Use methods.
+    abstract val anonIdPrefKey: String?
+    abstract fun track(event: AnalyticsEvent, properties: Map<String, *> = emptyMap<String, String>())
+    abstract fun flush()
+    open fun clearAllData() {
+        // Reset the anon ID here
+        clearAnonID()
+    }
+
+    abstract fun storeUsagePref()
+
+    private fun clearAnonID() {
+        anonymousID = null
+        val preferences = PreferenceManager.getDefaultSharedPreferences(appContext)
+        if (preferences.contains(anonIdPrefKey)) {
+            val editor = preferences.edit()
+            editor.remove(anonIdPrefKey)
+            editor.apply()
+        }
+    }
+
+    val anonID: String?
+        get() {
+            if (anonymousID == null) {
+                val preferences = PreferenceManager.getDefaultSharedPreferences(appContext)
+                anonymousID = preferences.getString(anonIdPrefKey, null)
+            }
+            return anonymousID
+        }
+
+    fun generateNewAnonID(): String {
+        val uuid = UUID.randomUUID().toString().replace("-", "")
+        val preferences = PreferenceManager.getDefaultSharedPreferences(appContext)
+        val editor = preferences.edit()
+        editor.putString(anonIdPrefKey, uuid)
+        editor.apply()
+        anonymousID = uuid
+        return uuid
+    }
 }
