1962de64ad078d2505bd35a4b15a5c731baa6cd4
ashiagr <agr.ash@gmail.com>
2022-08-29 18:01:03 +0530

    Add tests for lifecycle events tracking

---
+++ app/src/test/java/au/com/shiftyjelly/pocketcasts/analytics/AppLifecycleAnalyticsTest.kt
@@ -0,0 +1,126 @@
+package au.com.shiftyjelly.pocketcasts.analytics
+
+import android.content.Context
+import au.com.shiftyjelly.pocketcasts.preferences.Settings
+import au.com.shiftyjelly.pocketcasts.utils.PackageUtil
+import dagger.hilt.android.qualifiers.ApplicationContext
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.any
+import org.mockito.kotlin.anyOrNull
+import org.mockito.kotlin.eq
+import org.mockito.kotlin.never
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+
+private const val VERSION_CODE_DEFAULT = 0
+private const val VERSION_CODE_AFTER_FIRST_INSTALL = 1
+private const val VERSION_CODE_AFTER_SECOND_INSTALL = 2
+
+@RunWith(MockitoJUnitRunner::class)
+class AppLifecycleAnalyticsTest {
+    @Mock
+    @ApplicationContext
+    private lateinit var context: Context
+
+    @Mock
+    private lateinit var settings: Settings
+
+    @Mock
+    private lateinit var packageUtil: PackageUtil
+
+    @Mock
+    private lateinit var analyticsTracker: AnalyticsTrackerWrapper
+    lateinit var appLifecycleAnalytics: AppLifecycleAnalytics
+
+    @Before
+    fun setUp() {
+        appLifecycleAnalytics = AppLifecycleAnalytics(
+            context,
+            settings,
+            packageUtil,
+            analyticsTracker
+        )
+    }
+
+    /* APPLICATION INSTALLED */
+
+    @Test
+    fun `given no version code in prefs, when app launched, then app installed event fired`() {
+        whenever(settings.getLastAppVersionCode()).thenReturn(VERSION_CODE_DEFAULT)
+
+        appLifecycleAnalytics.onApplicationInstalledOrUpgraded()
+
+        verify(analyticsTracker).track(AnalyticsEvent.APPLICATION_INSTALLED)
+    }
+
+    @Test
+    fun `given version code in prefs, when app launched, then app installed event not fired`() {
+        whenever(settings.getLastAppVersionCode()).thenReturn(VERSION_CODE_AFTER_FIRST_INSTALL)
+
+        appLifecycleAnalytics.onApplicationInstalledOrUpgraded()
+
+        verify(analyticsTracker, never()).track(AnalyticsEvent.APPLICATION_INSTALLED)
+    }
+
+    /* APPLICATION UPDATED */
+
+    @Test
+    fun `given no version code in prefs, when app launched, then app updated event not fired`() {
+        whenever(settings.getLastAppVersionCode()).thenReturn(VERSION_CODE_DEFAULT)
+
+        appLifecycleAnalytics.onApplicationInstalledOrUpgraded()
+
+        verify(analyticsTracker, never()).track(eq(AnalyticsEvent.APPLICATION_UPDATED), any())
+    }
+
+    @Test
+    fun `given current and last version code different, when app launched, then app updated event fired`() {
+        whenever(settings.getLastAppVersionCode()).thenReturn(VERSION_CODE_AFTER_FIRST_INSTALL)
+        whenever(packageUtil.getVersionCode(anyOrNull())).thenReturn(VERSION_CODE_AFTER_SECOND_INSTALL)
+
+        appLifecycleAnalytics.onApplicationInstalledOrUpgraded()
+
+        verify(analyticsTracker)
+            .track(
+                AnalyticsEvent.APPLICATION_UPDATED,
+                mapOf("previous_version" to VERSION_CODE_AFTER_FIRST_INSTALL)
+            )
+    }
+
+    @Test
+    fun `given current and last version code same, when app launched, then app updated event not fired`() {
+        whenever(settings.getLastAppVersionCode()).thenReturn(VERSION_CODE_AFTER_SECOND_INSTALL)
+        whenever(packageUtil.getVersionCode(anyOrNull())).thenReturn(VERSION_CODE_AFTER_SECOND_INSTALL)
+
+        appLifecycleAnalytics.onApplicationInstalledOrUpgraded()
+
+        verify(analyticsTracker, never()).track(eq(AnalyticsEvent.APPLICATION_UPDATED), any())
+    }
+
+    /* APPLICATION OPENED */
+
+    @Test
+    fun `when app is foregrounded, then app opened event fired`() {
+        appLifecycleAnalytics.onApplicationEnterForeground()
+
+        verify(analyticsTracker).track(AnalyticsEvent.APPLICATION_OPENED)
+    }
+
+    /* APPLICATION CLOSED */
+
+    @Test
+    fun `when app is backgrounded, then app closed event fired with time in app`() {
+        appLifecycleAnalytics.onApplicationEnterForeground()
+        Thread.sleep(1000)
+        appLifecycleAnalytics.onApplicationEnterBackground()
+
+        verify(analyticsTracker).track(
+            AnalyticsEvent.APPLICATION_CLOSED,
+            mapOf("time_in_app" to 1)
+        )
+    }
+}
