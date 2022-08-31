de7f2ef10280ef684c14bd1509e6947581f90107
ashiagr <agr.ash@gmail.com>
2022-08-31 17:51:49 +0530

    Track plus promotion

--- modules/services/analytics/src/main/java/au/com/shiftyjelly/pocketcasts/analytics/AnalyticsEvent.kt
+++ modules/services/analytics/src/main/java/au/com/shiftyjelly/pocketcasts/analytics/AnalyticsEvent.kt
@@ -1,8 +1,14 @@
 package au.com.shiftyjelly.pocketcasts.analytics
 
 enum class AnalyticsEvent(val key: String) {
+    /* App Lifecycle */
     APPLICATION_INSTALLED("application_installed"),
     APPLICATION_UPDATED("application_updated"),
     APPLICATION_OPENED("application_opened"),
     APPLICATION_CLOSED("application_closed"),
+
+    /* Plus Upsell */
+    PLUS_PROMOTION_SHOWN("plus_promotion_shown"),
+    PLUS_PROMOTION_DISMISSED("plus_promotion_dismissed"),
+    PLUS_PROMOTION_UPGRADE_BUTTON_TAPPED("plus_promotion_upgrade_button_tapped"),
 }
