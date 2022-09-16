d34a797b42d30f8a55950b45945e38cc0d388a39
ashiagr <agr.ash@gmail.com>
2022-09-16 17:31:24 +0530

    Display media title from PlaybackUiState

--- wear/src/main/kotlin/au/com/shiftyjelly/pocketcasts/wear/ui/component/AnimatedPlayerScreenMediaDisplay.kt
+++ wear/src/main/kotlin/au/com/shiftyjelly/pocketcasts/wear/ui/component/AnimatedPlayerScreenMediaDisplay.kt
@@ -2,25 +2,29 @@ package au.com.shiftyjelly.pocketcasts.wear.ui.component
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-
-@Suppress("UnusedParameter")
+import androidx.compose.ui.res.stringResource
+import au.com.shiftyjelly.pocketcasts.R
+import com.google.android.horologist.media.ui.components.InfoMediaDisplay
+import com.google.android.horologist.media.ui.components.LoadingMediaDisplay
+import com.google.android.horologist.media.ui.state.PlayerUiState
 @Composable
 fun AnimatedPlayerScreenMediaDisplay(
-    /*playerUiState: PlayerUiState,*/
+    playerUiState: PlayerUiState,
     modifier: Modifier = Modifier,
 ) {
-// TODO: Display UI based on player ui state media
-//    val media = playerUiState.media
-//    if (media != null) {
-    MarqueeTextMediaDisplay(
-        modifier = modifier,
-        title = "A Really Long Podcast Name", // media.title,
-        artist = "artist" // media.artist
-    )
-//    } else {
-//        InfoMediaDisplay(
-//            message = stringResource(R.string.horologist_nothing_playing),
-//            modifier = modifier
-//        )
-//    }
+    val media = playerUiState.media
+    if (!playerUiState.connected) {
+        LoadingMediaDisplay(modifier)
+    } else if (media != null) {
+        MarqueeTextMediaDisplay(
+            modifier = modifier,
+            title = media.title,
+            artist = media.artist
+        )
+    } else {
+        InfoMediaDisplay(
+            message = stringResource(R.string.nothing_playing),
+            modifier = modifier
+        )
+    }
 }
