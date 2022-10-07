bcb7a73c0fbf083a6ec3e74dc98e5679ca0dde9f
karacca <omerkaracca@gmail.com>
2022-10-07 12:45:00 +0300

    Add BasePreferenceFragment and Implement in PodcastSettingsFragment to handle loading state changes

---
+++ modules/services/views/src/main/java/au/com/shiftyjelly/pocketcasts/views/fragments/BasePreferenceFragment.kt
@@ -0,0 +1,61 @@
+package au.com.shiftyjelly.pocketcasts.views.fragments
+
+import android.os.Bundle
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.ProgressBar
+import androidx.preference.PreferenceFragmentCompat
+import androidx.recyclerview.widget.RecyclerView
+import au.com.shiftyjelly.pocketcasts.utils.extensions.dpToPx
+
+abstract class BasePreferenceFragment : PreferenceFragmentCompat() {
+
+    private var progressBar: ProgressBar? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val root = super.onCreateView(inflater, container, savedInstanceState) as ViewGroup
+        progressBar = createProgressBar()
+        root.addView(progressBar)
+        return root
+    }
+
+    override fun onCreateRecyclerView(
+        inflater: LayoutInflater,
+        parent: ViewGroup,
+        savedInstanceState: Bundle?
+    ): RecyclerView {
+        return super.onCreateRecyclerView(inflater, parent, savedInstanceState).apply {
+            // itemAnimator = null
+            // layoutAnimation = null
+        }
+    }
+
+    fun showLoading() {
+        listView.visibility = View.GONE
+        progressBar?.visibility = View.VISIBLE
+    }
+
+    fun hideLoading() {
+        listView.visibility = View.VISIBLE
+        progressBar?.visibility = View.GONE
+    }
+
+    private fun createProgressBar(): ProgressBar {
+        return ProgressBar(requireContext()).apply {
+            isIndeterminate = true
+            layoutParams = FrameLayout.LayoutParams(
+                24.dpToPx(requireContext()),
+                24.dpToPx(requireContext())
+            ).apply {
+                gravity = Gravity.CENTER
+            }
+        }
+    }
+}
