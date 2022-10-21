faea472ef68cf878ddb8504601d6bcd7e6ee4509
wtfparadox <t.t.j.mutter@gmail.com>
2022-10-21 21:55:56 +0200

    Do not replace timeskip when it is in a URL

--- modules/services/views/src/main/java/au/com/shiftyjelly/pocketcasts/views/helper/ShowNotesHelper.kt
+++ modules/services/views/src/main/java/au/com/shiftyjelly/pocketcasts/views/helper/ShowNotesHelper.kt
@@ -3,6 +3,19 @@ package au.com.shiftyjelly.pocketcasts.views.helper
 object ShowNotesHelper {
 
     fun convertTimesToLinks(html: String): String {
-        return html.replace("(\\A|\\s|>|[^a-zsA-Z_0-9/])(\\d{0,2}:?\\d{1,2}:\\d{2})(<|\\s|\\z|[^a-zsA-Z_0-9\"])".toRegex(), "$1<a href=\"http://localhost/#playerJumpTo=$2\">$2</a>$3")
+        return html.replace("(\\A|\\s|>|[^a-zsA-Z_0-9/])(\\d{0,2}:?\\d{1,2}:\\d{2})(<|\\s|\\z|[^a-zsA-Z_0-9\"])".toRegex()) { match ->
+            return@replace if (matchIsInUrl(match, html)) {
+                match.value
+            } else {
+                val (prefix, timeskip, suffix) = match.destructured
+                "$prefix<a href=\"http://localhost/#playerJumpTo=$timeskip\">$timeskip</a>$suffix"
+            }
+        }
+    }
+
+    // searches for the first closing tag that comes after a potential timeskip and compares it to url closing tag (</a>)
+    private fun matchIsInUrl(match: MatchResult, html: String): Boolean {
+        val closingTagRegex = "</\\w+>".toRegex()
+        return closingTagRegex.find(html.substring(match.range.first))?.value == "</a>"
     }
 }
