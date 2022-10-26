71d979b292a1daa9182d916c71541a0020e96cbf
ashiagr <agr.ash@gmail.com>
2022-10-26 16:55:02 +0530

    Refactor: Move to views package

--- modules/features/endofyear/src/main/java/au/com/shiftyjelly/pocketcasts/endofyear/views/stories/StoryFake1View.kt
+++ modules/features/endofyear/src/main/java/au/com/shiftyjelly/pocketcasts/endofyear/views/stories/StoryFake1View.kt
@@ -1,4 +1,4 @@
-package au.com.shiftyjelly.pocketcasts.endofyear.storyviews
+package au.com.shiftyjelly.pocketcasts.endofyear.views
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
