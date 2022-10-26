e9e7154e64a9c55260a32946d1041af9e0290f03
Matt Chowning <mchowning@gmail.com>
2022-10-26 16:08:54 -0400

    Remove comment

--- modules/services/servers/src/main/java/au/com/shiftyjelly/pocketcasts/servers/bumpstats/WpComServer.kt
+++ modules/services/servers/src/main/java/au/com/shiftyjelly/pocketcasts/servers/bumpstats/WpComServer.kt
@@ -5,9 +5,7 @@ import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface WpComServer {
-    /**
-     * Notify the that an event X occurred at Y time.
-     */
+
     @POST("/rest/v1.1/tracks/record")
     suspend fun bumpStatAnonymously(@Body request: AnonymousBumpStatsRequest): Response<String>
 }
