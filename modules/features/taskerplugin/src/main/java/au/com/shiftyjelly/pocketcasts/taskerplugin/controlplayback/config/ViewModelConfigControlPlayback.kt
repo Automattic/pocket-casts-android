58de201c57fa0d6d408a6a028a5721da4aae1ce6
João Dias <joaomgcd@gmail.com>
2022-10-28 15:27:50 +0100

    Added ability for Tasker plugin to control playback effects (speed, trim mode and volume boost) and cleaned up some code so it's easier to maintain. Also added icons to input fields.
    - When using the "Pocket Casts Control Playback" action in Tasker there are now 3 new options for the Playback Command: "Set Playback Speed", "Set Trim Silence Mode" and "Set Volume Boost"
    - Review is needed because although the above effects are correctly applied to playback, you can't see their effects on the UI, eg when you change playback speed the UI still shows the old value that was set via the UI
    - Added icons to Tasker action input fields
    - Code cleanup was made so that it's easier to add new fields to Tasker actions (you don't need to edit code in so many places as before so it's less error prone)

--- modules/features/taskerplugin/src/main/java/au/com/shiftyjelly/pocketcasts/taskerplugin/controlplayback/config/ViewModelConfigControlPlayback.kt
+++ modules/features/taskerplugin/src/main/java/au/com/shiftyjelly/pocketcasts/taskerplugin/controlplayback/config/ViewModelConfigControlPlayback.kt
@@ -1,6 +1,11 @@
 package au.com.shiftyjelly.pocketcasts.taskerplugin.controlplayback.config
 
 import android.app.Application
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import au.com.shiftyjelly.pocketcasts.images.R.drawable
+import au.com.shiftyjelly.pocketcasts.localization.R
+import au.com.shiftyjelly.pocketcasts.models.type.TrimMode
 import au.com.shiftyjelly.pocketcasts.taskerplugin.base.ViewModelBase
 import au.com.shiftyjelly.pocketcasts.taskerplugin.controlplayback.ActionHelperControlPlayback
 import au.com.shiftyjelly.pocketcasts.taskerplugin.controlplayback.InputControlPlayback
@@ -16,76 +21,53 @@ class ViewModelConfigControlPlayback @Inject constructor(
     override val helperClass get() = ActionHelperControlPlayback::class.java
 
     /**
-     * A field that only appears depending on the type of the playback command. For example, the field "Time to Skip To" will only appear if the command is "Skip To Time"
-     * @param showForCommands the types of playback commands that makes this field appear
+     * A field that only appears depending on the type of the playback command. For example, the field "Time to Skip To" will only appear if the command is "Skip To Time". A field can also always appear if it doesn't depend on a command type
+     * @param showForCommands the types of playback commands that makes this field appear. If empty, will always show field
      * @param valueGetter how to get the value of this input field from the Tasker input
      * @param valueSetter how to set a newly assigned value of this field to the Tasker input
      */
-    inner class OptionalField constructor(val showForCommands: List<InputControlPlayback.PlaybackCommand>, valueGetter: (InputControlPlayback?) -> String?, val valueSetter: (String?) -> Unit) {
-        constructor(showForCommand: InputControlPlayback.PlaybackCommand, valueGetter: (InputControlPlayback?) -> String?, valueSetter: (String?) -> Unit) : this(listOf(showForCommand), valueGetter, valueSetter)
+    private open inner class InputField<T : Any> constructor(@StringRes labelResId: Int, @DrawableRes iconResId: Int, private val showForCommands: List<InputControlPlayback.PlaybackCommand>, valueGetter: InputControlPlayback.() -> String?, valueSetter: InputControlPlayback.(String?) -> Unit) : InputFieldBase<T>(labelResId, iconResId, valueGetter, valueSetter) {
+        constructor(@StringRes labelResId: Int, @DrawableRes iconResId: Int, showForCommand: InputControlPlayback.PlaybackCommand, valueGetter: InputControlPlayback.() -> String?, valueSetter: InputControlPlayback.(String?) -> Unit) : this(labelResId, iconResId, listOf(showForCommand), valueGetter, valueSetter)
+        constructor(@StringRes labelResId: Int, @DrawableRes iconResId: Int, valueGetter: InputControlPlayback.() -> String?, valueSetter: InputControlPlayback.(String?) -> Unit) : this(labelResId, iconResId, listOf(), valueGetter, valueSetter)
 
-        private val askFor get() = showForCommands.contains(input?.commandEnum)
-        val shouldAskForState by lazy { MutableStateFlow(askFor) }
-        fun updateAskForState() {
-            shouldAskForState.tryEmit(askFor)
-        }
-
-        val valueState by lazy { MutableStateFlow(valueGetter(input)) }
-        var value: String? = null
-            set(value) {
-                valueSetter(value)
-                valueState.tryEmit(value)
-            }
+        override val askFor get() = showForCommands.isEmpty() || showForCommands.contains(input?.commandEnum)
     }
 
-    /**
-     * When adding new optional fields here make sure to add them to the [optionalFields] list below so that everything is correctly updated
-     */
-    private val optionalFieldChapterToSkipTo by lazy { OptionalField(InputControlPlayback.PlaybackCommand.SkipToChapter, { input?.chapterToSkipTo }, { input?.chapterToSkipTo = it }) }
-    private val optionalFieldTimeToSkipTo by lazy { OptionalField(InputControlPlayback.PlaybackCommand.SkipToTime, { input?.skipToSeconds }, { input?.skipToSeconds = it }) }
-    private val optionalFieldTimeToSkip by lazy { OptionalField(listOf(InputControlPlayback.PlaybackCommand.SkipForward, InputControlPlayback.PlaybackCommand.SkipBack), { input?.skipSeconds }, { input?.skipSeconds = it }) }
-    private val optionalFields = listOf(
-        optionalFieldChapterToSkipTo,
-        optionalFieldTimeToSkipTo,
-        optionalFieldTimeToSkip
+    private val inputFieldCommand = object : InputField<InputControlPlayback.PlaybackCommand>(R.string.playback_command, drawable.filter_play, { command }, { setCommand(it) }) {
+        override fun getPossibleValues() = MutableStateFlow(InputControlPlayback.PlaybackCommand.values().toList())
+        override fun getValueDescriptionSpecific(possibleValue: InputControlPlayback.PlaybackCommand?) = possibleValue?.getDescription(context)
+    }
+    override val inputFields: List<InputFieldBase<*>> = listOf(
+        inputFieldCommand,
+        InputField(R.string.chapter_to_skip_to, drawable.filter_bullet, InputControlPlayback.PlaybackCommand.SkipToChapter, { chapterToSkipTo }, { chapterToSkipTo = it }),
+        InputField(R.string.time_to_skip_to_seconds, drawable.filter_time, InputControlPlayback.PlaybackCommand.SkipToTime, { skipToSeconds }, { skipToSeconds = it }),
+        InputField(R.string.time_to_skip_seconds, drawable.filter_time, listOf(InputControlPlayback.PlaybackCommand.SkipForward, InputControlPlayback.PlaybackCommand.SkipBack), { skipSeconds }, { skipSeconds = it }),
+        object : InputField<Double>(R.string.playback_speed_between_values, au.com.shiftyjelly.pocketcasts.taskerplugin.R.drawable.speedometer, InputControlPlayback.PlaybackCommand.SetPlaybackSpeed, { playbackSpeed }, { playbackSpeed = it }) {
+            override fun getPossibleValues() = MutableStateFlow((5..30).map { it / 10.0 })
+        },
+        object : InputField<TrimMode>(R.string.trim_silence_mode, au.com.shiftyjelly.pocketcasts.taskerplugin.R.drawable.content_cut, InputControlPlayback.PlaybackCommand.SetTrimSilenceMode, { trimSilenceMode }, { trimSilenceMode = it }) {
+            override fun getPossibleValues() = MutableStateFlow(TrimMode.values().toList())
+            override fun getValueDescriptionSpecific(possibleValue: TrimMode?): String? {
+                if (possibleValue == null) return null
+
+                return context.getString(
+                    when (possibleValue) {
+                        TrimMode.OFF -> R.string.off
+                        TrimMode.LOW -> R.string.player_effects_trim_mild
+                        TrimMode.MEDIUM -> R.string.player_effects_trim_medium
+                        TrimMode.HIGH -> R.string.player_effects_trim_mad_max
+                    }
+                )
+            }
+        },
+        object : InputField<Boolean>(R.string.set_volume_boost, drawable.filter_volume, InputControlPlayback.PlaybackCommand.SetVolumeBoost, { volumeBoostEnabled }, { volumeBoostEnabled = it }) {
+            override fun getPossibleValues() = MutableStateFlow(listOf(true, false))
+        }
     )
 
-    val commandState by lazy {
-        MutableStateFlow(
-            input?.command
-        )
+    fun setCommand(value: String?) {
+        input?.command = value
+        inputFieldCommand.valueState.tryEmit(value)
+        inputFields.forEach { it.updateAskForState() }
     }
-    var command: String?
-        get() = input?.command
-        set(value) {
-            input?.command = value
-            commandState.tryEmit(value)
-            optionalFields.forEach { it.updateAskForState() }
-        }
-
-    val availableCommands get() = InputControlPlayback.PlaybackCommand.values()
-
-    /*Chapter To skip to*/
-    val shouldAskForChapter get() = optionalFieldChapterToSkipTo.shouldAskForState
-    val chapterToSkipTo get() = optionalFieldChapterToSkipTo.valueState
-    fun setChapterToSkipTo(value: String) {
-        optionalFieldChapterToSkipTo.value = value
-    }
-    /*End chapter To skip to*/
-
-    /*Time To skip to*/
-    val showAskForTimeToSkipTo get() = optionalFieldTimeToSkipTo.shouldAskForState
-    val timeToSkipTo get() = optionalFieldTimeToSkipTo.valueState
-    fun setTimeToSkipTo(value: String) {
-        optionalFieldTimeToSkipTo.value = value
-    }
-    /*End time To skip to*/
-
-    /*Time To skip */
-    val showAskForTimeToSkip get() = optionalFieldTimeToSkip.shouldAskForState
-    val timeToSkip get() = optionalFieldTimeToSkip.valueState
-    fun setTimeToSkip(value: String) {
-        optionalFieldTimeToSkip.value = value
-    }
-    /*End time To skip to*/
 }
