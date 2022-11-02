164ddf9194e3ea1b67b31b2884247872d990724f
ashiagr <agr.ash@gmail.com>
2022-11-02 12:57:30 +0530

    Add placeholder story views for listened categories and top listened podcasts

--- modules/features/endofyear/src/main/java/au/com/shiftyjelly/pocketcasts/endofyear/views/stories/StoryListenedCategoriesView.kt
+++ modules/features/endofyear/src/main/java/au/com/shiftyjelly/pocketcasts/endofyear/views/stories/StoryListenedCategoriesView.kt
@@ -1,10 +1,13 @@
 package au.com.shiftyjelly.pocketcasts.endofyear.views.stories
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import au.com.shiftyjelly.pocketcasts.compose.components.TextH30
 import au.com.shiftyjelly.pocketcasts.endofyear.stories.StoryListenedCategories
 
 @Composable
@@ -13,5 +16,21 @@ fun StoryListenedCategoriesView(
     modifier: Modifier = Modifier,
 ) {
     Column(modifier.padding(16.dp)) {
+        TextH30(
+            text = "You listened to ${story.listenedCategories.count()} different categories this year",
+            textAlign = TextAlign.Center,
+            color = story.tintColor,
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        )
+        TextH30(
+            text = "Let's take a look at some of your favourites..",
+            textAlign = TextAlign.Center,
+            color = story.tintColor,
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        )
     }
 }
