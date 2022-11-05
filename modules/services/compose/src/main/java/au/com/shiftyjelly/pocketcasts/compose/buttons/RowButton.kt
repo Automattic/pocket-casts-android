d585c60959ac1f7aa721114ef36eb4fe113c2187
Matt Chowning <mchowning@gmail.com>
2022-11-05 17:30:19 -0400

    Keep button text centered even when wrapping
    Comes up when a large display/font is used

--- modules/services/compose/src/main/java/au/com/shiftyjelly/pocketcasts/compose/buttons/RowButton.kt
+++ modules/services/compose/src/main/java/au/com/shiftyjelly/pocketcasts/compose/buttons/RowButton.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -90,6 +91,7 @@ fun RowButton(
                     modifier = Modifier
                         .padding(6.dp)
                         .align(Alignment.Center),
+                    textAlign = TextAlign.Center,
                     color = textColor
                 )
             }
