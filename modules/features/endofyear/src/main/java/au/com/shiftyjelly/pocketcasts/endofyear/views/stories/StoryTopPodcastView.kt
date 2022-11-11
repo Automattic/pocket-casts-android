7063dd41eb33f4c669ac2aecd47f4693c36e99f1
ashiagr <agr.ash@gmail.com>
2022-11-11 07:06:56 +0530

    Add top podcast story design

--- modules/features/endofyear/src/main/java/au/com/shiftyjelly/pocketcasts/endofyear/views/stories/StoryTopPodcastView.kt
+++ modules/features/endofyear/src/main/java/au/com/shiftyjelly/pocketcasts/endofyear/views/stories/StoryTopPodcastView.kt
@@ -1,84 +1,179 @@
 package au.com.shiftyjelly.pocketcasts.endofyear.views.stories
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.Surface
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import au.com.shiftyjelly.pocketcasts.compose.AppTheme
 import au.com.shiftyjelly.pocketcasts.compose.components.PodcastImage
-import au.com.shiftyjelly.pocketcasts.compose.components.TextH30
-import au.com.shiftyjelly.pocketcasts.compose.components.TextP50
-import au.com.shiftyjelly.pocketcasts.compose.preview.ThemePreviewParameterProvider
-import au.com.shiftyjelly.pocketcasts.models.db.helper.TopPodcast
+import au.com.shiftyjelly.pocketcasts.compose.components.TextH20
+import au.com.shiftyjelly.pocketcasts.compose.components.TextP40
+import au.com.shiftyjelly.pocketcasts.endofyear.util.transformPodcastCover
+import au.com.shiftyjelly.pocketcasts.localization.R
 import au.com.shiftyjelly.pocketcasts.repositories.endofyear.stories.StoryTopPodcast
 import au.com.shiftyjelly.pocketcasts.settings.stats.StatsHelper
-import au.com.shiftyjelly.pocketcasts.ui.theme.Theme
+import au.com.shiftyjelly.pocketcasts.utils.extensions.dpToPx
+import au.com.shiftyjelly.pocketcasts.utils.extensions.pxToDp
 
 @Composable
 fun StoryTopPodcastView(
     story: StoryTopPodcast,
     modifier: Modifier = Modifier,
 ) {
-    val context = LocalContext.current
-    val timeText = remember {
-        StatsHelper.secondsToFriendlyString(
-            story.topPodcast.totalPlayedTime.toLong(),
-            context.resources
-        )
-    }
+    val backgroundColor = story.topPodcast.toPodcast().getTintColor(false)
+
     Column(
-        horizontalAlignment = Alignment.CenterHorizontally
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = Color(backgroundColor))
+            .verticalScroll(rememberScrollState())
     ) {
-        PodcastImage(
-            uuid = story.topPodcast.uuid,
-            modifier = modifier
-                .size(200.dp)
-                .padding(top = 4.dp, end = 12.dp, bottom = 4.dp)
-        )
-        TextH30(
-            text = "Your top podcast was ${story.topPodcast.title} by ${story.topPodcast.author}",
-            color = story.tintColor,
-            textAlign = TextAlign.Center,
-            modifier = modifier.padding(16.dp)
-        )
-        TextP50(
-            text = "You listened to ${story.topPodcast.numberOfPlayedEpisodes} episodes for a total of $timeText",
-            color = story.tintColor,
-            textAlign = TextAlign.Center,
-            modifier = modifier.padding(16.dp)
-        )
+        Spacer(modifier = modifier.height(40.dp))
+
+        Spacer(modifier = modifier.weight(0.7f))
+
+        PodcastCoverStack(story)
+
+        Spacer(modifier = modifier.weight(0.3f))
+
+        PrimaryText(story)
+
+        Spacer(modifier = modifier.weight(0.25f))
+
+        SecondaryText(story)
+
+        Spacer(modifier = modifier.weight(0.8f))
+
+        Logo()
+
+        Spacer(modifier = modifier.height(40.dp))
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-private fun StoryTopPodcastViewPreview(
-    @PreviewParameter(ThemePreviewParameterProvider::class) themeType: Theme.ThemeType,
+private fun PodcastCoverStack(
+    story: StoryTopPodcast,
+    modifier: Modifier = Modifier,
 ) {
-    AppTheme(themeType) {
-        Surface(color = Color.Black) {
-            StoryTopPodcastView(
-                StoryTopPodcast(
-                    topPodcast = TopPodcast(
-                        uuid = "1",
-                        title = "Title",
-                        author = "Author",
-                        tintColorForLightBg = 0,
-                        numberOfPlayedEpisodes = 1,
-                        totalPlayedTime = 0.0,
+    val context = LocalContext.current
+    val currentLocalView = LocalView.current
+    val coverWidth = (currentLocalView.width.pxToDp(context).dp) / 2.5f
+    val translateBy = (coverWidth.value * .2).toInt().dpToPx(context)
+
+    Box {
+        (0..2).reversed().forEach { index ->
+            Box(
+                modifier = modifier
+                    .padding(top = (index * (coverWidth.value * .25)).dp)
+                    .transformPodcastCover()
+                    .graphicsLayer(translationX = -translateBy.toFloat())
+            ) {
+                when (index) {
+                    0 -> PodcastImage(
+                        uuid = story.topPodcast.uuid,
+                        cornerSize = 8.dp,
+                        modifier = modifier.size(coverWidth)
                     )
-                )
-            )
+
+                    1 -> {
+                        val backgroundColor =
+                            Color(story.topPodcast.toPodcast().getTintColor(false))
+                        CoverRectangle(coverWidth = coverWidth, backgroundColor = backgroundColor)
+                    }
+
+                    2 -> {
+                        val backgroundColor = Color(story.topPodcast.toPodcast().getTintColor(true))
+                        CoverRectangle(coverWidth = coverWidth, backgroundColor = backgroundColor)
+                    }
+                }
+            }
         }
+    }
+}
+
+@Composable
+private fun PrimaryText(
+    story: StoryTopPodcast,
+    modifier: Modifier = Modifier,
+) {
+    TextH20(
+        text = stringResource(
+            id = R.string.end_of_year_story_top_podcast,
+            story.topPodcast.title, story.topPodcast.author
+        ),
+        textAlign = TextAlign.Center,
+        color = story.tintColor,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 40.dp)
+    )
+}
+
+@Composable
+private fun SecondaryText(
+    story: StoryTopPodcast,
+    modifier: Modifier = Modifier,
+) {
+    val context = LocalContext.current
+    val timeText = StatsHelper.secondsToFriendlyString(
+        story.topPodcast.totalPlayedTime.toLong(),
+        context.resources
+    )
+    TextP40(
+        text = stringResource(
+            id = R.string.end_of_year_story_top_podcast_subtitle,
+            story.topPodcast.numberOfPlayedEpisodes,
+            timeText
+        ),
+        textAlign = TextAlign.Center,
+        color = story.tintColor,
+        fontWeight = FontWeight.Bold,
+        modifier = modifier
+            .fillMaxWidth()
+            .alpha(0.8f)
+            .padding(horizontal = 40.dp)
+    )
+}
+
+@Composable
+fun CoverRectangle(
+    coverWidth: Dp,
+    backgroundColor: Color,
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier = modifier.size(coverWidth)
+    ) {
+        val elevation = 4.dp
+        Card(
+            elevation = elevation,
+            shape = RoundedCornerShape(8.dp),
+            backgroundColor = backgroundColor,
+            modifier = modifier.fillMaxSize()
+        ) {}
     }
 }
