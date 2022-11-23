a95708d40d02e0ff2d65d0fb78c4ed038da03d89
Matt Chowning <mchowning@gmail.com>
2022-11-22 22:23:21 -0500

    Add search to onboarding recommendations screen

--- modules/features/account/src/main/java/au/com/shiftyjelly/pocketcasts/account/onboarding/recommendations/OnboardingRecommendationsSearch.kt
+++ modules/features/account/src/main/java/au/com/shiftyjelly/pocketcasts/account/onboarding/recommendations/OnboardingRecommendationsSearch.kt
@@ -1,32 +1,99 @@
 package au.com.shiftyjelly.pocketcasts.account.onboarding.recommendations
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Divider
+import androidx.compose.material.LinearProgressIndicator
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import au.com.shiftyjelly.pocketcasts.compose.bars.ThemedTopAppBar
+import au.com.shiftyjelly.pocketcasts.compose.components.PodcastItem
+import au.com.shiftyjelly.pocketcasts.compose.components.SearchBar
+import au.com.shiftyjelly.pocketcasts.compose.theme
 import au.com.shiftyjelly.pocketcasts.localization.R as LR
 
 @Composable
 fun OnboardingRecommendationsSearch(
     onBackPressed: () -> Unit,
 ) {
+    val viewModel = hiltViewModel<OnboardingRecommendationsSearchViewModel>()
+    val state by viewModel.state.collectAsState()
+
+    val focusRequester = remember { FocusRequester() }
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
+    }
+
     BackHandler {
         onBackPressed()
     }
 
-    Column(
-        Modifier
-            .fillMaxHeight()
-            .verticalScroll(rememberScrollState())
-    ) {
+    Column(Modifier.fillMaxHeight()) {
         ThemedTopAppBar(
             title = stringResource(LR.string.onboarding_find_podcasts),
             onNavigationClick = onBackPressed
         )
+
+        SearchBar(
+            text = state.searchQuery,
+            placeholder = stringResource(LR.string.search),
+            onTextChanged = viewModel::updateSearchQuery,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+                .focusRequester(focusRequester)
+        )
+
+        Spacer(Modifier.height(16.dp))
+        Box(Modifier.height(2.dp)) {
+            Divider(color = MaterialTheme.theme.colors.secondaryUi02)
+            if (state.loading && state.results.isNotEmpty()) {
+                // Provide a subtle loading indicator when results are displayed, but being updated
+
+                LinearProgressIndicator(
+                    color = MaterialTheme.theme.colors.secondaryUi01,
+                    backgroundColor = MaterialTheme.theme.colors.secondaryUi02,
+                    modifier = Modifier.matchParentSize()
+                )
+            }
+        }
+
+        if (state.loading && state.results.isEmpty()) {
+            // Provide an obvious loading indicator when loading and there no results yet
+            Spacer(Modifier.weight(1f))
+            CircularProgressIndicator(Modifier.align(Alignment.CenterHorizontally))
+            Spacer(Modifier.weight(5f))
+        } else {
+            LazyColumn {
+                items(state.results) {
+                    PodcastItem(
+                        podcast = it.podcast,
+                        subscribed = it.isSubscribed,
+                        showSubscribed = true,
+                        onClick = { viewModel.toggleSubscribed(it) },
+                    )
+                }
+            }
+        }
     }
 }
---
+++ modules/features/search/src/main/java/au/com/shiftyjelly/pocketcasts/search/SearchHandler.kt
@@ -0,0 +1,149 @@
+package au.com.shiftyjelly.pocketcasts.search
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.LiveDataReactiveStreams
+import au.com.shiftyjelly.pocketcasts.models.entity.Podcast
+import au.com.shiftyjelly.pocketcasts.models.to.FolderItem
+import au.com.shiftyjelly.pocketcasts.models.to.SignInState
+import au.com.shiftyjelly.pocketcasts.models.type.PodcastsSortType
+import au.com.shiftyjelly.pocketcasts.preferences.Settings
+import au.com.shiftyjelly.pocketcasts.repositories.podcast.FolderManager
+import au.com.shiftyjelly.pocketcasts.repositories.podcast.PodcastManager
+import au.com.shiftyjelly.pocketcasts.repositories.user.UserManager
+import au.com.shiftyjelly.pocketcasts.servers.ServerManager
+import au.com.shiftyjelly.pocketcasts.servers.discover.PodcastSearch
+import com.jakewharton.rxrelay2.BehaviorRelay
+import io.reactivex.BackpressureStrategy
+import io.reactivex.Observable
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.rxkotlin.Observables
+import io.reactivex.schedulers.Schedulers
+import timber.log.Timber
+import java.util.concurrent.TimeUnit
+import javax.inject.Inject
+
+class SearchHandler @Inject constructor(
+    val serverManager: ServerManager,
+    val podcastManager: PodcastManager,
+    val userManager: UserManager,
+    val settings: Settings,
+    folderManager: FolderManager
+) {
+    private val searchQuery = BehaviorRelay.create<String>().apply {
+        accept("")
+    }
+
+    private val loadingObservable = BehaviorRelay.create<Boolean>().apply {
+        accept(false)
+    }
+    private val onlySearchRemoteObservable = BehaviorRelay.create<Boolean>().apply {
+        accept(false)
+    }
+    private val signInStateObservable = userManager.getSignInState().startWith(SignInState.SignedOut()).toObservable()
+
+    private val localResults = Observable
+        .combineLatest(searchQuery, onlySearchRemoteObservable, signInStateObservable) { searchQuery, onlySearchRemoteObservable, signInState ->
+            Pair(if (onlySearchRemoteObservable) "" else searchQuery, signInState)
+        }
+        .subscribeOn(Schedulers.io())
+        .switchMap { (query, signInState) ->
+            if (query.isEmpty()) {
+                Observable.just(emptyList())
+            } else {
+                // search folders
+                val folderSearch =
+                    if (signInState.isSignedInAsPlus) {
+                        // only show folders if the user has Plus
+                        folderManager.findFoldersSingle()
+                            .subscribeOn(Schedulers.io())
+                            .flatMapObservable { Observable.fromIterable(it) }
+                            .filter { it.name.contains(query, ignoreCase = true) }
+                            .switchMapSingle { folder ->
+                                podcastManager
+                                    .findPodcastsInFolderSingle(folderUuid = folder.uuid)
+                                    .map { podcasts -> FolderItem.Folder(folder = folder, podcasts = podcasts) }
+                            }
+                            .toList()
+                    } else {
+                        Single.just(emptyList())
+                    }
+
+                // search podcasts
+                val podcastSearch = podcastManager.findSubscribedRx()
+                    .subscribeOn(Schedulers.io())
+                    .flatMapObservable { Observable.fromIterable(it) }
+                    .filter { it.title.contains(query, ignoreCase = true) || it.author.contains(query, ignoreCase = true) }
+                    .map { podcast ->
+                        podcast.isSubscribed = true
+                        FolderItem.Podcast(podcast)
+                    }
+                    .toList()
+
+                podcastSearch
+                    .zipWith(folderSearch) { podcasts, folders ->
+                        (podcasts + folders).sortedBy { PodcastsSortType.cleanStringForSort(it.title) }
+                    }
+                    .toObservable()
+            }
+        }
+
+    private val subscribedPodcastUuids = podcastManager
+        .findSubscribedRx()
+        .subscribeOn(Schedulers.io())
+        .toObservable()
+        .map { podcasts -> podcasts.map(Podcast::uuid).toHashSet() }
+
+    private val serverSearchResults = searchQuery
+        .subscribeOn(Schedulers.io())
+        .map { it.trim() }
+        .debounce { if (it.isEmpty()) Observable.empty() else Observable.timer(settings.getPodcastSearchDebounceMs(), TimeUnit.MILLISECONDS) }
+        .switchMap {
+            if (it.length <= 1) {
+                Observable.just(PodcastSearch())
+            } else {
+                loadingObservable.accept(true)
+                serverManager.searchForPodcastsRx(it).toObservable()
+                    .onErrorReturn { exception ->
+                        PodcastSearch(error = exception)
+                    }
+            }
+        }
+        .doOnNext { loadingObservable.accept(false) }
+
+    private val searchFlowable = Observables.combineLatest(searchQuery, subscribedPodcastUuids, localResults, serverSearchResults, loadingObservable) { searchTerm, subscribedPodcastUuids, localResults, serverSearchResults, loading ->
+        if (searchTerm.isBlank()) {
+            SearchState.Results(list = emptyList(), loading = loading, error = null)
+        } else {
+            // set if the podcast is subscribed so we can show a tick
+            val serverResults = serverSearchResults.searchResults.map { podcast -> FolderItem.Podcast(podcast) }
+            serverResults.forEach {
+                if (subscribedPodcastUuids.contains(it.podcast.uuid)) {
+                    it.podcast.isSubscribed = true
+                }
+            }
+            val searchResults = (localResults + serverResults).distinctBy { it.uuid }
+
+            if (serverSearchResults.searchTerm.isEmpty() || searchResults.isNotEmpty() || serverSearchResults.error != null) {
+                SearchState.Results(list = searchResults, loading = loading, error = serverSearchResults.error)
+            } else {
+                SearchState.NoResults
+            }
+        }
+    }
+        .doOnError { Timber.e(it) }
+        .onErrorReturn { exception -> SearchState.Results(list = emptyList(), loading = false, error = exception) }
+        .observeOn(AndroidSchedulers.mainThread())
+        .toFlowable(BackpressureStrategy.LATEST)
+
+    val searchResults: LiveData<SearchState> = LiveDataReactiveStreams.fromPublisher(searchFlowable)
+    val loading: LiveData<Boolean> = LiveDataReactiveStreams.fromPublisher(loadingObservable.toFlowable(BackpressureStrategy.LATEST))
+
+    fun updateSearchQuery(query: String) {
+        searchQuery.accept(query)
+    }
+
+    fun setOnlySearchRemote(remote: Boolean) {
+        onlySearchRemoteObservable.accept(remote)
+    }
+}
