166d3d6d7f6741d88fa7029463647996f2f32543
ashiagr <agr.ash@gmail.com>
2022-11-30 11:25:33 +0530

    Add pulsating heart animation + red logo on last screen

--- modules/features/endofyear/src/main/java/au/com/shiftyjelly/pocketcasts/endofyear/views/stories/StoryEpilogueView.kt
+++ modules/features/endofyear/src/main/java/au/com/shiftyjelly/pocketcasts/endofyear/views/stories/StoryEpilogueView.kt
@@ -1,7 +1,13 @@
 package au.com.shiftyjelly.pocketcasts.endofyear.views.stories
 
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -17,15 +23,16 @@ import androidx.compose.material.Icon
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import au.com.shiftyjelly.pocketcasts.compose.components.TextP40
 import au.com.shiftyjelly.pocketcasts.endofyear.R
-import au.com.shiftyjelly.pocketcasts.endofyear.components.PodcastLogoWhite
 import au.com.shiftyjelly.pocketcasts.endofyear.components.StoryPrimaryText
 import au.com.shiftyjelly.pocketcasts.endofyear.components.StorySecondaryText
 import au.com.shiftyjelly.pocketcasts.repositories.endofyear.stories.StoryEpilogue
@@ -52,7 +59,7 @@ fun StoryEpilogueView(
 
         Spacer(modifier = modifier.weight(1f))
 
-        HeartImage()
+        PulsatingHeart()
 
         Spacer(modifier = modifier.weight(0.34f))
 
@@ -68,22 +75,33 @@ fun StoryEpilogueView(
 
         Spacer(modifier = modifier.weight(1f))
 
-        PodcastLogoWhite()
+        PodcastLogo()
 
         Spacer(modifier = modifier.height(30.dp))
     }
 }
 
 @Composable
-private fun HeartImage(
+private fun PulsatingHeart(
     modifier: Modifier = Modifier,
 ) {
-    Image(
-        painter = painterResource(id = R.drawable.heart),
-        contentDescription = null,
-        modifier = modifier
-            .size(HeartImageSize)
+    val infiniteTransition = rememberInfiniteTransition()
+    val scale by infiniteTransition.animateFloat(
+        initialValue = 1f,
+        targetValue = 1.2f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(500),
+            repeatMode = RepeatMode.Reverse
+        )
     )
+    Box(modifier = Modifier.scale(scale)) {
+        Image(
+            painter = painterResource(id = R.drawable.heart),
+            contentDescription = null,
+            modifier = modifier
+                .size(HeartImageSize)
+        )
+    }
 }
 
 @Composable
@@ -129,4 +147,12 @@ private fun ReplayButton(
             modifier = modifier.padding(2.dp)
         )
     }
+}
+
+@Composable
+fun PodcastLogo() {
+    Image(
+        painter = painterResource(R.drawable.logo_red),
+        contentDescription = null,
+    )
 }
