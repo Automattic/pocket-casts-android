0db6f36f113d771ece6b5f5a62139cf4d27f0afe
Matt Chowning <mchowning@gmail.com>
2022-12-07 12:50:09 -0500

    Add trending podcasts to recommendations page

--- modules/features/account/src/main/java/au/com/shiftyjelly/pocketcasts/account/onboarding/recommendations/OnboardingRecommendationsStartPage.kt
+++ modules/features/account/src/main/java/au/com/shiftyjelly/pocketcasts/account/onboarding/recommendations/OnboardingRecommendationsStartPage.kt
@@ -1,34 +1,44 @@
 package au.com.shiftyjelly.pocketcasts.account.onboarding.recommendations
 
 import androidx.activity.compose.BackHandler
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.clearAndSetSemantics
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import au.com.shiftyjelly.pocketcasts.account.viewmodel.OnboardingRecommendationsStartPageViewModel
 import au.com.shiftyjelly.pocketcasts.compose.AppThemeWithBackground
 import au.com.shiftyjelly.pocketcasts.compose.buttons.RowButton
 import au.com.shiftyjelly.pocketcasts.compose.components.SearchBarButton
 import au.com.shiftyjelly.pocketcasts.compose.components.TextH10
+import au.com.shiftyjelly.pocketcasts.compose.components.TextH20
 import au.com.shiftyjelly.pocketcasts.compose.components.TextH30
+import au.com.shiftyjelly.pocketcasts.compose.components.TextH60
 import au.com.shiftyjelly.pocketcasts.compose.components.TextP40
+import au.com.shiftyjelly.pocketcasts.compose.components.textH60FontSize
+import au.com.shiftyjelly.pocketcasts.compose.extensions.header
+import au.com.shiftyjelly.pocketcasts.compose.podcast.PodcastSubscribeImage
 import au.com.shiftyjelly.pocketcasts.compose.preview.ThemePreviewParameterProvider
 import au.com.shiftyjelly.pocketcasts.ui.theme.Theme
 import au.com.shiftyjelly.pocketcasts.localization.R as LR
@@ -45,12 +55,10 @@ fun OnboardingRecommendationsStartPage(
     LaunchedEffect(Unit) { onShown() }
     BackHandler { onBackPressed() }
 
-    Column(
-        Modifier
-            .fillMaxHeight()
-            .verticalScroll(rememberScrollState())
-            .padding(all = 16.dp)
-    ) {
+    val viewModel = hiltViewModel<OnboardingRecommendationsStartPageViewModel>()
+    val state by viewModel.state.collectAsState()
+
+    Column {
         Row(
             horizontalArrangement = Arrangement.End,
             modifier = Modifier
@@ -66,38 +74,75 @@ fun OnboardingRecommendationsStartPage(
             )
         }
 
-        TextH10(
-            text = stringResource(LR.string.onboarding_recommendations_find_favorite_podcasts),
-            modifier = Modifier.padding(bottom = 16.dp)
-        )
-
-        TextP40(
-            text = stringResource(LR.string.onboarding_recommendations_make_pocket_casts_yours),
-            modifier = Modifier.padding(bottom = 16.dp)
-        )
-
-        SearchBarButton(
-            text = stringResource(LR.string.search),
-            onClick = onSearch,
-            modifier = Modifier.padding(bottom = 16.dp)
-        )
-
-        Box(
-            contentAlignment = Alignment.Center,
-            modifier = Modifier
-                .aspectRatio(1.2f)
-                .fillMaxWidth()
-                .padding(bottom = 16.dp)
-                .background(Color.Gray),
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(3),
+            contentPadding = PaddingValues(horizontal = 24.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalArrangement = Arrangement.spacedBy(9.dp),
+            modifier = Modifier.weight(1f)
         ) {
-            TextP40("PLACEHOLDER")
-        }
 
-        Spacer(Modifier.weight(1f))
+            header {
+                Column {
+
+                    TextH10(
+                        text = stringResource(LR.string.onboarding_recommendations_find_favorite_podcasts),
+                        modifier = Modifier.padding(bottom = 16.dp)
+                    )
+
+                    TextP40(
+                        text = stringResource(LR.string.onboarding_recommendations_make_pocket_casts_yours),
+                        modifier = Modifier.padding(bottom = 16.dp)
+                    )
+
+                    SearchBarButton(
+                        text = stringResource(LR.string.search),
+                        onClick = onSearch,
+                        modifier = Modifier.padding(bottom = 25.dp)
+                    )
+
+                    TextH20(
+                        text = stringResource(LR.string.discover_trending),
+                        modifier = Modifier.padding(bottom = 16.dp)
+                    )
+                }
+            }
+
+            items(items = state) {
+
+                // Simulate minLines = 2
+                // This is a bit of a hack based on https://stackoverflow.com/a/66401128/1910286
+                // Google is working on adding a minLines capability though: https://issuetracker.google.com/issues/122476634
+                val twoLines = with(LocalDensity.current) {
+                    val pixelsInAPoint = 4 / 3
+                    val lineHeight = textH60FontSize * pixelsInAPoint
+                    val twoLines = lineHeight * 2
+                    twoLines.toDp()
+                }
+
+                Column(Modifier.semantics(mergeDescendants = true) {}) {
+                    PodcastSubscribeImage(
+                        podcastUuid = it.uuid,
+                        podcastTitle = it.title,
+                        podcastSubscribed = it.isSubscribed,
+                        onSubscribeClick = { viewModel.updateSubscribed(it) },
+                    )
+
+                    Spacer(Modifier.height(8.dp))
+
+                    TextH60(
+                        text = it.title,
+                        maxLines = 2,
+                        modifier = Modifier
+                            .heightIn(min = twoLines)
+                            .clearAndSetSemantics {}
+                    )
+                }
+            }
+        }
 
         RowButton(
             text = stringResource(LR.string.not_now),
-            includePadding = false,
             onClick = onComplete,
         )
     }
---
+++ modules/features/account/src/main/java/au/com/shiftyjelly/pocketcasts/account/viewmodel/OnboardingRecommendationsStartPageViewModel.kt
@@ -0,0 +1,106 @@
+package au.com.shiftyjelly.pocketcasts.account.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import au.com.shiftyjelly.pocketcasts.preferences.Settings
+import au.com.shiftyjelly.pocketcasts.repositories.playback.PlaybackManager
+import au.com.shiftyjelly.pocketcasts.repositories.podcast.PodcastManager
+import au.com.shiftyjelly.pocketcasts.servers.model.transformWithRegion
+import au.com.shiftyjelly.pocketcasts.servers.server.ListRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.reactive.asFlow
+import kotlinx.coroutines.rx2.await
+import timber.log.Timber
+import javax.inject.Inject
+
+@HiltViewModel
+class OnboardingRecommendationsStartPageViewModel @Inject constructor(
+    val podcastManager: PodcastManager,
+    val playbackManager: PlaybackManager,
+    repository: ListRepository,
+    settings: Settings,
+    app: Application,
+) : AndroidViewModel(app) {
+
+    private val _state: MutableStateFlow<List<RecommendationPodcast>> = MutableStateFlow(emptyList())
+    val state: StateFlow<List<RecommendationPodcast>> = _state
+
+    init {
+        viewModelScope.launch {
+
+            val feed = repository.getDiscoverFeed().await()
+            val regionCode = settings.getDiscoveryCountryCode()
+            val region = feed.regions[regionCode]
+                ?: feed.regions[feed.defaultRegionCode]
+
+            if (region == null) {
+                val message = "Could not get region $regionCode"
+                Timber.e(message)
+                return@launch
+            }
+
+            val replacements = mapOf(
+                feed.regionCodeToken to region.code,
+                feed.regionNameToken to region.name
+            )
+
+            val updatedList = feed.layout.transformWithRegion(
+                region,
+                replacements,
+                getApplication<Application>().resources
+            ) // Update the list with the correct region substituted in where needed
+
+            val discoverPodcastList = updatedList
+                .find { it.id == "trending" } // only care about the trending list
+                ?.let { trendingRow ->
+                    repository
+                        .getListFeed(trendingRow.source).await()
+                        .podcasts
+                }
+
+            if (discoverPodcastList == null) {
+                Timber.e("Could not get trending podcast list")
+                return@launch
+            }
+
+            podcastManager
+                .observeSubscribed()
+                .asFlow()
+                .map { subscribed ->
+                    val subscribedUuids = subscribed.map { it.uuid }
+                    discoverPodcastList.map { discoverPodcast ->
+                        RecommendationPodcast(
+                            uuid = discoverPodcast.uuid,
+                            title = discoverPodcast.title ?: "",
+                            isSubscribed = discoverPodcast.uuid in subscribedUuids
+                        )
+                    }
+                }
+                .stateIn(viewModelScope)
+                .collectLatest {
+                    _state.value = it
+                }
+        }
+    }
+
+    fun updateSubscribed(podcast: RecommendationPodcast) {
+        if (podcast.isSubscribed) {
+            podcastManager.unsubscribeAsync(podcastUuid = podcast.uuid, playbackManager = playbackManager)
+        } else {
+            podcastManager.subscribeToPodcast(podcastUuid = podcast.uuid, sync = true)
+        }
+    }
+}
+
+data class RecommendationPodcast(
+    val uuid: String,
+    val title: String,
+    val isSubscribed: Boolean,
+)
