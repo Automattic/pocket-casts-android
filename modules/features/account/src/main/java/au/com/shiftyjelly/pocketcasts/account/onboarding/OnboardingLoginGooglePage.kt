f5f8fc98596ee2c713e44f5db5e47cbe92d0a68e
Philip Simpson <philz.the.gecko@gmail.com>
2023-01-12 13:53:05 +1030

    Removed unused Compose page

--- modules/features/account/src/main/java/au/com/shiftyjelly/pocketcasts/account/onboarding/OnboardingLoginGooglePage.kt
+++
@@ -1,9 +0,0 @@
-package au.com.shiftyjelly.pocketcasts.account.onboarding
-
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-
-@Composable
-internal fun OnboardingLoginGooglePage() {
-    Text("login_google")
-}
