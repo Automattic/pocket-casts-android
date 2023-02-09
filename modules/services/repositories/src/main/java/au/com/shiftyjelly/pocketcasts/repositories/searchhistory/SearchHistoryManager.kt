e3b8c2e3073712f47aacffb22e24a087811f0ee0
ashiagr <agr.ash@gmail.com>
2023-02-09 11:47:24 +0530

    Update search history manager

--- modules/services/repositories/src/main/java/au/com/shiftyjelly/pocketcasts/repositories/searchhistory/SearchHistoryManager.kt
+++ modules/services/repositories/src/main/java/au/com/shiftyjelly/pocketcasts/repositories/searchhistory/SearchHistoryManager.kt
@@ -1,3 +1,10 @@
 package au.com.shiftyjelly.pocketcasts.repositories.searchhistory
 
-interface SearchHistoryManager
+import au.com.shiftyjelly.pocketcasts.models.to.SearchHistoryEntry
+
+interface SearchHistoryManager {
+    suspend fun findAll(showFolders: Boolean, limit: Int): List<SearchHistoryEntry>
+    suspend fun add(entry: SearchHistoryEntry)
+    suspend fun remove(entry: SearchHistoryEntry)
+    suspend fun clearAll()
+}
--- modules/services/repositories/src/main/java/au/com/shiftyjelly/pocketcasts/repositories/searchhistory/SearchHistoryManagerImpl.kt
+++ modules/services/repositories/src/main/java/au/com/shiftyjelly/pocketcasts/repositories/searchhistory/SearchHistoryManagerImpl.kt
@@ -1,6 +1,7 @@
 package au.com.shiftyjelly.pocketcasts.repositories.searchhistory
 
 import au.com.shiftyjelly.pocketcasts.models.db.AppDatabase
+import au.com.shiftyjelly.pocketcasts.models.to.SearchHistoryEntry
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import javax.inject.Inject
@@ -13,4 +14,24 @@ class SearchHistoryManagerImpl @Inject constructor(
     override val coroutineContext: CoroutineContext
         get() = Dispatchers.Default
     private val searchHistoryDao = appDatabase.searchHistoryDao()
+
+    override suspend fun findAll(
+        showFolders: Boolean,
+        limit: Int,
+    ): List<SearchHistoryEntry> {
+        val items = searchHistoryDao.findAll(showFolders, limit)
+        return items.map { SearchHistoryEntry.fromSearchHistoryItem(it) }
+    }
+
+    override suspend fun add(entry: SearchHistoryEntry) {
+        searchHistoryDao.insert(entry.toSearchHistoryItem())
+    }
+
+    override suspend fun remove(entry: SearchHistoryEntry) {
+        searchHistoryDao.delete(entry.toSearchHistoryItem())
+    }
+
+    override suspend fun clearAll() {
+        searchHistoryDao.deleteAll()
+    }
 }
