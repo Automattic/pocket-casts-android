423895489ba448acc800e46cc31f433da471ffb8
ashiagr <agr.ash@gmail.com>
2023-03-30 18:24:19 +0530

    Update comment

--- modules/features/discover/src/main/java/au/com/shiftyjelly/pocketcasts/discover/view/DiscoverAdapter.kt
+++ modules/features/discover/src/main/java/au/com/shiftyjelly/pocketcasts/discover/view/DiscoverAdapter.kt
@@ -251,10 +251,10 @@ internal class DiscoverAdapter(
                 addOnWindowFocusChangeListener { hasFocus ->
                     if (!hasFocus) autoScrollHelper?.stopAutoScrollTimer()
                 }
-                /* Manage auto scroll when itemView's visibility changes on going to next screen */
+                /* Manage auto scroll when itemView's visibility changes */
                 addOnGlobalLayoutListener {
                     if (itemView.isShown) {
-                        /* Start auto scroll with a delay after coming from another screen */
+                        /* Start auto scroll with a delay when carousel item view is re-shown */
                         autoScrollHelper?.startAutoScrollTimer(delay = AutoScrollHelper.AUTO_SCROLL_DELAY)
                     } else {
                         autoScrollHelper?.stopAutoScrollTimer()
