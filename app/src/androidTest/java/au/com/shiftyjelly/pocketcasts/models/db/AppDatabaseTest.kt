4d0eb460393f5f68c80fc7fb29e1c4ccbd9f4158
ashiagr <agr.ash@gmail.com>
2023-04-04 17:10:57 +0530

    Add migration to test

--- app/src/androidTest/java/au/com/shiftyjelly/pocketcasts/models/db/AppDatabaseTest.kt
+++ app/src/androidTest/java/au/com/shiftyjelly/pocketcasts/models/db/AppDatabaseTest.kt
@@ -139,7 +139,8 @@ class AppDatabaseTest {
                 AppDatabase.MIGRATION_70_71,
                 AppDatabase.MIGRATION_71_72,
                 AppDatabase.MIGRATION_72_73,
-                AppDatabase.MIGRATION_73_74
+                AppDatabase.MIGRATION_73_74,
+                AppDatabase.MIGRATION_74_75
             )
             .build()
         // close the database and release any stream resources when the test finishes
