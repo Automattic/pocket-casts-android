8f8b20b67c5b59957144e2eeed1aaa3ba9662f5f
Gary Yund <gary.yund@gmail.com>
2023-04-14 23:25:18 -0400

    add basic unit test. Not much to cover here

---
+++ modules/features/settings/src/test/java/au/com/shiftyjelly/pocketcasts/settings/viewmodel/AdvancedSettingsViewModelTest.kt
@@ -0,0 +1,65 @@
+package au.com.shiftyjelly.pocketcasts.settings.viewmodel
+
+import android.content.Context
+import au.com.shiftyjelly.pocketcasts.analytics.AnalyticsTrackerWrapper
+import au.com.shiftyjelly.pocketcasts.preferences.Settings
+import dagger.hilt.android.qualifiers.ApplicationContext
+import junit.framework.TestCase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.setMain
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+
+@RunWith(MockitoJUnitRunner::class)
+class AdvancedSettingsViewModelTest {
+
+    @Mock
+    private lateinit var settings: Settings
+
+    @Mock
+    private lateinit var analyticsTracker: AnalyticsTrackerWrapper
+
+    @Mock
+    @ApplicationContext
+    private lateinit var context: Context
+    private lateinit var viewModel: AdvancedSettingsViewModel
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Before
+    fun setUp() {
+        Dispatchers.setMain(UnconfinedTestDispatcher())
+        whenever(settings.syncOnMeteredNetwork()).thenReturn(false)
+        viewModel = AdvancedSettingsViewModel(
+            settings,
+            analyticsTracker,
+            context
+        )
+    }
+
+    @Test
+    fun `verify settings methods gets called when they should`() {
+        startViewModelAndResumeFragment(sdkVersion = 28)
+
+        val isChecked = viewModel.state.value.backgroundSyncOnMeteredState.isChecked
+        // Just ensure it gets called
+        TestCase.assertEquals(isChecked, false)
+        verify(settings).syncOnMeteredNetwork()
+    }
+
+    private fun startViewModelAndResumeFragment(
+        sdkVersion: Int,
+    ) {
+        viewModel.start(
+            sdkVersion = sdkVersion
+        )
+        viewModel.onFragmentResume()
+    }
+}
