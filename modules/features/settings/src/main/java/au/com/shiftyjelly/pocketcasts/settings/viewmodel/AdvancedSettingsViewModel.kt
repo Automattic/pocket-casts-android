5afb947af3f7c14fab7ad830194c6b160b0b7665
Gary Yund <gary.yund@gmail.com>
2023-04-14 21:03:03 -0400

    remove dead code

--- modules/features/settings/src/main/java/au/com/shiftyjelly/pocketcasts/settings/viewmodel/AdvancedSettingsViewModel.kt
+++ modules/features/settings/src/main/java/au/com/shiftyjelly/pocketcasts/settings/viewmodel/AdvancedSettingsViewModel.kt
@@ -7,28 +7,19 @@ import androidx.lifecycle.ViewModel
 import au.com.shiftyjelly.pocketcasts.analytics.AnalyticsEvent
 import au.com.shiftyjelly.pocketcasts.analytics.AnalyticsTrackerWrapper
 import au.com.shiftyjelly.pocketcasts.preferences.Settings
-import au.com.shiftyjelly.pocketcasts.repositories.file.FileStorage
-import au.com.shiftyjelly.pocketcasts.repositories.podcast.EpisodeManager
-import au.com.shiftyjelly.pocketcasts.repositories.podcast.PodcastManager
 import au.com.shiftyjelly.pocketcasts.repositories.refresh.RefreshPodcastsTask
-import au.com.shiftyjelly.pocketcasts.utils.FileUtilWrapper
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asSharedFlow
-import java.util.*
 import javax.inject.Inject
 import au.com.shiftyjelly.pocketcasts.localization.R as LR
 
 @HiltViewModel
 class AdvancedSettingsViewModel
 @Inject constructor(
-    private val podcastManager: PodcastManager,
-    private val episodeManager: EpisodeManager,
-    private val fileStorage: FileStorage,
-    private val fileUtil: FileUtilWrapper,
     private val settings: Settings,
     private val analyticsTracker: AnalyticsTrackerWrapper,
     @ApplicationContext private val context: Context,
@@ -72,20 +63,6 @@ class AdvancedSettingsViewModel
 //        setupStorage()
     }
 
-//
-//    private fun onStorageDataWarningCheckedChange(isChecked: Boolean) {
-//        settings.setWarnOnMeteredNetwork(isChecked)
-//        updateMobileDataWarningState()
-//    }
-//
-//    private fun updateMobileDataWarningState() {
-//        mutableState.value = mutableState.value.copy(
-//            storageDataWarningState = mutableState.value.storageDataWarningState.copy(
-//                isChecked = settings.warnOnMeteredNetwork(),
-//            )
-//        )
-//    }
-
     private fun onSyncOnMeteredCheckedChange(isChecked: Boolean) {
         settings.setSyncOnMeteredNetwork(isChecked)
         updateSyncOnMeteredState()
@@ -103,26 +80,6 @@ class AdvancedSettingsViewModel
         )
     }
 
-//    private fun createAlertDialogState(
-//        title: String,
-//        @StringRes message: Int? = null,
-//    ) = AlertDialogState(
-//        title = title,
-//        message = message?.let { context.getString(message) },
-//        buttons = listOf(
-//            DialogButtonState(
-//                text = context.getString(LR.string.cancel).uppercase(
-//                    Locale.getDefault()
-//                ),
-//                onClick = {}
-//            ),
-//            DialogButtonState(
-//                text = context.getString(LR.string.ok),
-//                onClick = {}
-//            )
-//        )
-//    )
-
     fun onShown() {
         analyticsTracker.track(AnalyticsEvent.SETTINGS_ADVANCED_SHOWN)
     }
@@ -137,10 +94,4 @@ class AdvancedSettingsViewModel
             val onCheckedChange: (Boolean) -> Unit,
         )
     }
-
-//    data class AlertDialogState(
-//        val title: String,
-//        val message: String? = null,
-//        val buttons: List<DialogButtonState>,
-//    )
 }
