83328f3416e2e5c076728d58c2d73d039d98402a
Matt Chowning <mchowning@gmail.com>
2023-04-23 05:42:56 -0400

    Crossfade gravatar placeholder

--- modules/services/compose/src/main/java/au/com/shiftyjelly/pocketcasts/compose/images/GravatarProfileImage.kt
+++ modules/services/compose/src/main/java/au/com/shiftyjelly/pocketcasts/compose/images/GravatarProfileImage.kt
@@ -1,10 +1,12 @@
 package au.com.shiftyjelly.pocketcasts.compose.images
 
+import androidx.compose.animation.Crossfade
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.platform.LocalContext
 import au.com.shiftyjelly.pocketcasts.utils.Gravatar
 import coil.compose.rememberAsyncImagePainter
@@ -29,19 +31,22 @@ fun GravatarProfileImage(
             .build(),
     )
 
-    Box {
-        // Image component that attempts to load the gravatar image
+    Crossfade(
+        gravatarPainter.state.painter == null,
+        animationSpec = tween(500),
+    ) { showPlaceholder ->
         Image(
             painter = gravatarPainter,
             contentDescription = contentDescription,
-            modifier = modifier,
+            modifier = modifier
+                .alpha(if (showPlaceholder) 0f else 1f)
         )
 
         // If the gravatar image has not loaded or fails to load (because there is no gravatar image associated
-        // with this account), show the placeholder. We are doing this because setting a placeholder on an
-        // AsyncImagePainter fails to show the placeholder when there is no gravatar image associated with
-        // the account.
-        if (gravatarPainter.state.painter == null) {
+        // with this account), show the placeholder. We are settings the placeholder this way instead of
+        // setting a placeholder on an AsyncImagePainter because this approach continues showing the placeholder
+        // when there is not a gravatar image associated with the account.
+        if (showPlaceholder) {
             placeholder()
         }
     }
