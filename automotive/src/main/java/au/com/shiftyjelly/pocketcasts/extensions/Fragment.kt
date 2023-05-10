044d50fa336d4cce48312cd2366a15b3bd7809af
Philip Simpson <philz.the.gecko@gmail.com>
2023-05-10 14:34:29 +0930

    Remove QR code from Automotive

--- automotive/src/main/java/au/com/shiftyjelly/pocketcasts/extensions/Fragment.kt
+++ automotive/src/main/java/au/com/shiftyjelly/pocketcasts/extensions/Fragment.kt
@@ -3,14 +3,12 @@ package au.com.shiftyjelly.pocketcasts.extensions
 import android.content.Intent
 import android.net.Uri
 import androidx.fragment.app.Fragment
-import au.com.shiftyjelly.pocketcasts.AutomotiveLinkFragment
-import au.com.shiftyjelly.pocketcasts.ui.helper.FragmentHostListener
+import timber.log.Timber
 
 fun Fragment.openUrl(url: String) {
     try {
         this.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
     } catch (e: Exception) {
-        // fallback to showing the URL with a QR code
-        (activity as? FragmentHostListener)?.addFragment(AutomotiveLinkFragment.newInstance(url))
+        Timber.e(e)
     }
 }
