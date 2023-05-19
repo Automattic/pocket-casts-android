1adf262a094566f914e803755c8e611dfe4808d0
ashiagr <agr.ash@gmail.com>
2023-05-19 18:14:03 +0530

    Support speed update

--- wear/src/main/kotlin/au/com/shiftyjelly/pocketcasts/wear/ui/player/EffectsScreen.kt
+++ wear/src/main/kotlin/au/com/shiftyjelly/pocketcasts/wear/ui/player/EffectsScreen.kt
@@ -1,13 +1,34 @@
 package au.com.shiftyjelly.pocketcasts.wear.ui.player
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.wear.compose.material.Chip
+import androidx.wear.compose.material.ChipDefaults
+import androidx.wear.compose.material.Icon
+import androidx.wear.compose.material.MaterialTheme
+import au.com.shiftyjelly.pocketcasts.compose.components.TextH30
 import au.com.shiftyjelly.pocketcasts.localization.R
 import au.com.shiftyjelly.pocketcasts.wear.ui.component.ScreenHeaderChip
 import com.google.android.horologist.compose.layout.ScalingLazyColumn
 import com.google.android.horologist.compose.layout.ScalingLazyColumnState
+import au.com.shiftyjelly.pocketcasts.images.R as IR
+import au.com.shiftyjelly.pocketcasts.localization.R as LR
 
 object EffectsScreen {
     const val route = "effects"
@@ -18,6 +39,7 @@ fun EffectsScreen(
     columnState: ScalingLazyColumnState,
     viewModel: EffectsViewModel = hiltViewModel(),
 ) {
+    val state = viewModel.state.collectAsStateWithLifecycle().value
     ScalingLazyColumn(
         columnState = columnState,
         modifier = Modifier.fillMaxWidth()
@@ -25,5 +47,77 @@ fun EffectsScreen(
         item {
             ScreenHeaderChip(text = R.string.effects)
         }
+        when (state) {
+            EffectsViewModel.State.Loading -> Unit
+            is EffectsViewModel.State.Loaded -> {
+                item {
+                    SpeedChip(
+                        state = state,
+                        onPlusClicked = { viewModel.increasePlaybackSpeed() },
+                        onMinusClicked = { viewModel.decreasePlaybackSpeed() },
+                    )
+                }
+            }
+        }
     }
+}
+
+@Composable
+private fun SpeedChip(
+    state: EffectsViewModel.State.Loaded,
+    modifier: Modifier = Modifier,
+    onPlusClicked: () -> Unit,
+    onMinusClicked: () -> Unit,
+) {
+    Chip(
+        onClick = { },
+        colors = ChipDefaults.secondaryChipColors(),
+        shape = MaterialTheme.shapes.large,
+        modifier = modifier
+            .height(96.dp)
+            .fillMaxWidth(),
+        label = {
+            Column(
+                verticalArrangement = Arrangement.Center,
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 8.dp),
+                ) {
+                    Icon(
+                        painter = painterResource(IR.drawable.ic_speed),
+                        contentDescription = null,
+                        modifier = Modifier.size(20.dp)
+                    )
+                    Text(
+                        text = stringResource(id = LR.string.player_effects_speed),
+                        modifier = Modifier.padding(start = 8.dp),
+                        style = MaterialTheme.typography.button.copy(
+                            color = Color.White
+                        ),
+                    )
+                }
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    IconButton(onClick = onMinusClicked) {
+                        Icon(
+                            painter = painterResource(IR.drawable.minus_simple),
+                            contentDescription = stringResource(LR.string.player_effects_speed_up),
+                        )
+                    }
+                    TextH30(text = String.format("%.1fx", state.playbackEffects.playbackSpeed))
+                    IconButton(onClick = onPlusClicked) {
+                        Icon(
+                            painter = painterResource(IR.drawable.plus_simple),
+                            contentDescription = stringResource(LR.string.player_effects_speed_down),
+                        )
+                    }
+                }
+            }
+        },
+    )
 }
--- wear/src/main/kotlin/au/com/shiftyjelly/pocketcasts/wear/ui/player/EffectsViewModel.kt
+++ wear/src/main/kotlin/au/com/shiftyjelly/pocketcasts/wear/ui/player/EffectsViewModel.kt
@@ -1,9 +1,76 @@
 package au.com.shiftyjelly.pocketcasts.wear.ui.player
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import au.com.shiftyjelly.pocketcasts.models.to.PlaybackEffects
+import au.com.shiftyjelly.pocketcasts.preferences.Settings
+import au.com.shiftyjelly.pocketcasts.repositories.extensions.saveToGlobalSettings
+import au.com.shiftyjelly.pocketcasts.repositories.playback.PlaybackManager
+import au.com.shiftyjelly.pocketcasts.utils.extensions.clipToRange
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.rx2.asFlow
 import javax.inject.Inject
+import kotlin.math.round
 
 @HiltViewModel
 class EffectsViewModel
-@Inject constructor() : ViewModel()
+@Inject constructor(
+    private val playbackManager: PlaybackManager,
+    private val settings: Settings,
+) : ViewModel() {
+
+    val state: StateFlow<State> =
+        playbackManager.playbackStateRelay
+            .asFlow()
+            .map {
+                State.Loaded(settings.getGlobalPlaybackEffects())
+            }
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(stopTimeoutMillis = 5_000),
+                initialValue = State.Loading
+            )
+
+    fun increasePlaybackSpeed() {
+        val currentState = state.value as? State.Loaded ?: return
+        viewModelScope.launch {
+            changePlaybackSpeed(currentState.playbackEffects.playbackSpeed + 0.1)
+        }
+    }
+
+    fun decreasePlaybackSpeed() {
+        val currentState = state.value as? State.Loaded ?: return
+        viewModelScope.launch {
+            changePlaybackSpeed(currentState.playbackEffects.playbackSpeed - 0.1)
+        }
+    }
+
+    private fun changePlaybackSpeed(speed: Double) {
+        val currentState = state.value as? State.Loaded ?: return
+        val effects = currentState.playbackEffects
+        val clippedToRangeSpeed = speed.clipToRange(0.5, 3.0)
+        // to stop the issue 1.2000000000000002
+        val roundedSpeed = round(clippedToRangeSpeed * 10.0) / 10.0
+        effects.playbackSpeed = roundedSpeed
+        saveEffects(effects)
+    }
+
+    private fun saveEffects(effects: PlaybackEffects) {
+        viewModelScope.launch {
+            playbackManager.updatePlayerEffects(effects)
+            effects.saveToGlobalSettings(settings)
+        }
+    }
+
+    sealed class State {
+        data class Loaded(
+            val playbackEffects: PlaybackEffects,
+        ) : State()
+        object Loading : State()
+    }
+}
