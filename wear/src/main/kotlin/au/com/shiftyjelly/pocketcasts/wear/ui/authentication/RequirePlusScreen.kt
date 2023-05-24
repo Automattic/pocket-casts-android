fa895918c7ad5e9d39c1ff57ccf4140b65f09621
Matt Chowning <mchowning@gmail.com>
2023-05-24 17:43:16 -0400

    Update UI for RequirePlusScreen

--- wear/src/main/kotlin/au/com/shiftyjelly/pocketcasts/wear/ui/authentication/RequirePlusScreen.kt
+++ wear/src/main/kotlin/au/com/shiftyjelly/pocketcasts/wear/ui/authentication/RequirePlusScreen.kt
@@ -1,11 +1,28 @@
 package au.com.shiftyjelly.pocketcasts.wear.ui.authentication
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
 import androidx.wear.compose.material.Text
+import au.com.shiftyjelly.pocketcasts.account.onboarding.components.SubscriptionBadge
+import au.com.shiftyjelly.pocketcasts.account.onboarding.upgrade.UpgradeFeatureCard
+import au.com.shiftyjelly.pocketcasts.wear.ui.component.WatchListChip
 import com.google.android.horologist.compose.layout.ScalingLazyColumn
 import com.google.android.horologist.compose.layout.ScalingLazyColumnState
+import au.com.shiftyjelly.pocketcasts.images.R as IR
+import au.com.shiftyjelly.pocketcasts.localization.R as LR
+import au.com.shiftyjelly.pocketcasts.ui.R as UR
 
 object RequirePlusScreen {
     const val route = "requirePlus"
@@ -21,9 +38,38 @@ fun RequirePlusScreen(
         columnState = columnState,
     ) {
         item {
+            SubscriptionBadge(
+                iconRes = UpgradeFeatureCard.PLUS.iconRes,
+                shortNameRes = UpgradeFeatureCard.PLUS.shortNameRes,
+                iconColor = Color.Black,
+                backgroundColor = colorResource(UR.color.plus_gold),
+                textColor = Color.Black
+            )
+        }
+
+        item { Spacer(Modifier.height(16.dp)) }
+
+        item {
             Text(
-                text = "[INSERT BEAUTIFUL UI HERE]\nPlease sign in with a Plus account to use Pocket Casts on your watch",
+                text = buildAnnotatedString {
+                    append(stringResource(LR.string.log_in_watch_requires_plus))
+                    append(" ")
+                    withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
+                        append("play.pocketcasts.com")
+                    }
+                },
+                textAlign = TextAlign.Center,
                 modifier = Modifier.clickable { onContinueToLogin() }
+            )
+        }
+
+        item { Spacer(Modifier.height(16.dp)) }
+
+        item {
+            WatchListChip(
+                title = stringResource(LR.string.log_in),
+                iconRes = IR.drawable.signin,
+                onClick = onContinueToLogin
             )
         }
     }
