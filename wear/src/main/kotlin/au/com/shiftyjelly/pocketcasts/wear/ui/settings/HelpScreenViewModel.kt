3e8a5d99fd6f21e26df11f51ad719618ad821eb0
Matt Chowning <mchowning@gmail.com>
2023-06-15 15:36:04 -0400

    Add delay before toast after message queued
    It can take a few seconds for the message to be received by the phone,
    so this helps avoid having the toast direct the user to their phone too
    quickly and having it seem for a few seconds like it's not working.

--- wear/src/main/kotlin/au/com/shiftyjelly/pocketcasts/wear/ui/settings/HelpScreenViewModel.kt
+++ wear/src/main/kotlin/au/com/shiftyjelly/pocketcasts/wear/ui/settings/HelpScreenViewModel.kt
@@ -8,12 +8,14 @@ import au.com.shiftyjelly.pocketcasts.shared.WatchMessageSendState
 import au.com.shiftyjelly.pocketcasts.shared.WatchPhoneCommunication
 import au.com.shiftyjelly.pocketcasts.shared.WatchPhoneCommunicationState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+import kotlin.time.Duration.Companion.seconds
 import au.com.shiftyjelly.pocketcasts.localization.R as LR
 
 @HiltViewModel
@@ -36,6 +38,11 @@ class HelpScreenViewModel @Inject constructor(
                 WatchMessageSendState.FAILED_TO_QUEUE -> LR.string.settings_help_phone_unavailable_message
             }
 
+            if (result == WatchMessageSendState.QUEUED) {
+                // There is a bit of delay between an item being queued and it being received on the phone,
+                // so add a short delay before directing the user to their phone.
+                delay(2.seconds)
+            }
             Toast.makeText(context, message, Toast.LENGTH_LONG).show()
         }
     }
@@ -46,6 +53,12 @@ class HelpScreenViewModel @Inject constructor(
             val message = when (result) {
                 WatchMessageSendState.QUEUED -> LR.string.settings_help_logs_sent_to_phone_check_phone
                 WatchMessageSendState.FAILED_TO_QUEUE -> LR.string.settings_help_phone_unavailable_message
+            }
+
+            if (result == WatchMessageSendState.QUEUED) {
+                // There is a bit of delay between an item being queued and it being received on the phone,
+                // so add a short delay before directing the user to their phone.
+                delay(2.seconds)
             }
             Toast.makeText(context, message, Toast.LENGTH_LONG).show()
         }
