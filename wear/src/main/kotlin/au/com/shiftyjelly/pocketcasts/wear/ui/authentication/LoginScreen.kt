75432e728f6c0a6552625428d2442e1679398bb8
Matt Chowning <mchowning@gmail.com>
2023-06-21 14:21:14 -0400

    Simplify login screen

--- wear/src/main/kotlin/au/com/shiftyjelly/pocketcasts/wear/ui/authentication/LoginScreen.kt
+++ wear/src/main/kotlin/au/com/shiftyjelly/pocketcasts/wear/ui/authentication/LoginScreen.kt
@@ -1,16 +1,8 @@
 package au.com.shiftyjelly.pocketcasts.wear.ui.authentication
 
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.wear.compose.material.ChipDefaults
-import androidx.wear.compose.material.MaterialTheme
 import au.com.shiftyjelly.pocketcasts.compose.CallOnce
 import com.google.android.horologist.compose.layout.ScalingLazyColumn
 import com.google.android.horologist.compose.layout.ScalingLazyColumnState
@@ -35,28 +27,6 @@ fun LoginScreen(
     ScalingLazyColumn(
         columnState = columnState,
     ) {
-        item {
-            Text(
-                text = stringResource(LR.string.log_in),
-                textAlign = TextAlign.Center,
-                color = MaterialTheme.colors.onPrimary,
-                style = MaterialTheme.typography.title2,
-            )
-        }
-
-        item {
-            Text(
-                text = stringResource(LR.string.log_in_subtitle),
-                textAlign = TextAlign.Center,
-                color = MaterialTheme.colors.onPrimary,
-                style = MaterialTheme.typography.body1,
-            )
-        }
-
-        item {
-            Spacer(modifier = Modifier.height(12.dp))
-        }
-
         item {
             Chip(
                 labelId = LR.string.log_in_with_google,
