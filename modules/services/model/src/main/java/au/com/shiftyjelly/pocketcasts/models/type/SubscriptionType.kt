0ceac30567fdc1124282328681bd43cc2e516311
ashiagr <agr.ash@gmail.com>
2023-06-23 15:48:21 +0530

    Add comment

--- modules/services/model/src/main/java/au/com/shiftyjelly/pocketcasts/models/type/SubscriptionType.kt
+++ modules/services/model/src/main/java/au/com/shiftyjelly/pocketcasts/models/type/SubscriptionType.kt
@@ -1,5 +1,10 @@
 package au.com.shiftyjelly.pocketcasts.models.type
 
+ /* pdeCcb-2fL-p2#comment-2074
+The SubscriptionType field will always be Plus.
+It is being replaced by SubscriptionTier with Plus and Patron tiers but currently not supported in production.
+This can be removed once SubscriptionTier is supported.
+*/
 enum class SubscriptionType(val label: String) {
     NONE("none"),
     PLUS("plus");
