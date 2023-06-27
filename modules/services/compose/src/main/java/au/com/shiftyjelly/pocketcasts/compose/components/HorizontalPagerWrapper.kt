aa2db532cc58bffe2b277be398ded21ed5a5d8e1
ashiagr <agr.ash@gmail.com>
2023-06-27 11:37:11 +0530

    Fix pager bottom margin when there is no page indicator

--- modules/services/compose/src/main/java/au/com/shiftyjelly/pocketcasts/compose/components/HorizontalPagerWrapper.kt
+++ modules/services/compose/src/main/java/au/com/shiftyjelly/pocketcasts/compose/components/HorizontalPagerWrapper.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -91,6 +92,8 @@ fun HorizontalPagerWrapper(
                 pagerState = pagerState,
                 color = pageIndicatorColor,
             )
+        } else {
+            Spacer(modifier = Modifier.height(16.dp))
         }
     }
 }
