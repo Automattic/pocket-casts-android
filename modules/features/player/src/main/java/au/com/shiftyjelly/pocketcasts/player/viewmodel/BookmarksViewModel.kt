d20702445856a7ee6f39c4f38366e08a6c7038f3
ashiagr <agr.ash@gmail.com>
2023-07-04 10:52:57 +0530

    Add blank bookmarks viewmodel

---
+++ modules/features/player/src/main/java/au/com/shiftyjelly/pocketcasts/player/viewmodel/BookmarksViewModel.kt
@@ -0,0 +1,9 @@
+package au.com.shiftyjelly.pocketcasts.player.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class BookmarksViewModel
+@Inject constructor() : ViewModel()
