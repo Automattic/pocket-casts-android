67d463ac9f41d768eeb63cf391663952ce857507
Caleb Langat <95022986+Mzazi25@users.noreply.github.com>
2023-07-17 13:28:42 +0300

    Added codeAnalysis script to run spotless

---
+++ codeAnalysis.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+./gradlew spotlessApply
+
+# Before use it, in the first time, you must guarantee some running permissions:
+# chmod +x codeAnalysis.sh
+#
+# After that, you just need to run:
+# ./codeAnalysis.sh
