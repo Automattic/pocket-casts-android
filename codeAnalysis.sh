85b78a079fcd41dce1772377b2477af1fe16fae5
Caleb Langat <95022986+Mzazi25@users.noreply.github.com>
2023-07-22 09:50:40 +0300

    removed shell script

--- codeAnalysis.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-./gradlew spotlessApply
-
-# Before use it, in the first time, you must guarantee some running permissions:
-# chmod +x codeAnalysis.sh
-#
-# After that, you just need to run:
-# ./codeAnalysis.sh
