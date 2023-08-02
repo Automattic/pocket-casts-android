98bb91b9882c8900833456eb41dcc9b597723baa
ashiagr <agr.ash@gmail.com>
2023-08-02 13:59:44 +0530

    Add multiselect

--- modules/features/podcasts/src/main/java/au/com/shiftyjelly/pocketcasts/podcasts/view/episode/EpisodeContainerFragment.kt
+++ modules/features/podcasts/src/main/java/au/com/shiftyjelly/pocketcasts/podcasts/view/episode/EpisodeContainerFragment.kt
@@ -21,6 +21,7 @@ import androidx.viewpager2.widget.ViewPager2
 import au.com.shiftyjelly.pocketcasts.analytics.SourceView
 import au.com.shiftyjelly.pocketcasts.featureflag.Feature
 import au.com.shiftyjelly.pocketcasts.featureflag.FeatureFlag
+import au.com.shiftyjelly.pocketcasts.images.R
 import au.com.shiftyjelly.pocketcasts.models.entity.PodcastEpisode
 import au.com.shiftyjelly.pocketcasts.models.type.EpisodeViewSource
 import au.com.shiftyjelly.pocketcasts.player.view.BookmarksFragment
@@ -30,16 +31,20 @@ import au.com.shiftyjelly.pocketcasts.ui.helper.StatusBarColor
 import au.com.shiftyjelly.pocketcasts.ui.theme.Theme
 import au.com.shiftyjelly.pocketcasts.ui.theme.ThemeColor
 import au.com.shiftyjelly.pocketcasts.views.fragments.BaseDialogFragment
+import au.com.shiftyjelly.pocketcasts.views.multiselect.MultiSelectBookmarksHelper
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.tabs.TabLayoutMediator
 import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
+import javax.inject.Inject
 import au.com.shiftyjelly.pocketcasts.localization.R as LR
 import au.com.shiftyjelly.pocketcasts.ui.R as UR
 
 @AndroidEntryPoint
-class EpisodeContainerFragment : BaseDialogFragment(), EpisodeFragment.EpisodeLoadedListener {
+class EpisodeContainerFragment :
+    BaseDialogFragment(),
+    EpisodeFragment.EpisodeLoadedListener {
     companion object {
         const val ARG_EPISODE_UUID = "episodeUUID"
         const val ARG_EPISODE_VIEW_SOURCE = "episode_view_source"
@@ -113,6 +118,8 @@ class EpisodeContainerFragment : BaseDialogFragment(), EpisodeFragment.EpisodeLo
         get() = if (forceDarkTheme && theme.isLightTheme) Theme.ThemeType.DARK else theme.activeTheme
 
     private lateinit var adapter: ViewPagerAdapter
+    @Inject
+    lateinit var multiSelectHelper: MultiSelectBookmarksHelper
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         if (!forceDarkTheme || theme.isDarkTheme) {
@@ -153,6 +160,8 @@ class EpisodeContainerFragment : BaseDialogFragment(), EpisodeFragment.EpisodeLo
 
         binding.setupViewPager()
 
+        binding.setupMultiSelectHelper()
+
         binding.btnClose.setOnClickListener { dismiss() }
     }
 
@@ -180,6 +189,11 @@ class EpisodeContainerFragment : BaseDialogFragment(), EpisodeFragment.EpisodeLo
         }
 
         viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageScrollStateChanged(state: Int) {
+                super.onPageScrollStateChanged(state)
+                viewPager.isUserInputEnabled = !multiSelectHelper.isMultiSelecting
+            }
+
             override fun onPageSelected(position: Int) {
                 super.onPageSelected(position)
                 btnFav.isVisible = adapter.isDetailsTab(position)
@@ -188,9 +202,25 @@ class EpisodeContainerFragment : BaseDialogFragment(), EpisodeFragment.EpisodeLo
         })
     }
 
+    private fun FragmentEpisodeContainerBinding.setupMultiSelectHelper() {
+        multiSelectHelper.isMultiSelectingLive.observe(viewLifecycleOwner) { isMultiSelecting ->
+            multiSelectToolbar.isVisible = isMultiSelecting
+            multiSelectToolbar.setNavigationIcon(R.drawable.ic_arrow_back)
+        }
+        multiSelectHelper.context = context
+        multiSelectToolbar.setup(
+            lifecycleOwner = viewLifecycleOwner,
+            multiSelectHelper = multiSelectHelper,
+            menuRes = null,
+            fragmentManager = parentFragmentManager,
+        )
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         binding = null
+        multiSelectHelper.isMultiSelecting = false
+        multiSelectHelper.context = null
     }
 
     private class ViewPagerAdapter(
