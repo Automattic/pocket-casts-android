2767f55d32961a2516b758513b0fcb73e0bc120e
Matt Chowning <mchowning@gmail.com>
2023-08-02 21:31:43 -0400

    Return to mocking settings in PodcastManagerTest

--- app/src/androidTest/java/au/com/shiftyjelly/pocketcasts/models/db/PodcastManagerTest.kt
+++ app/src/androidTest/java/au/com/shiftyjelly/pocketcasts/models/db/PodcastManagerTest.kt
@@ -1,12 +1,14 @@
 package au.com.shiftyjelly.pocketcasts.models.db
 
-import androidx.preference.PreferenceManager
+import android.content.SharedPreferences
 import androidx.room.Room
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import androidx.test.platform.app.InstrumentationRegistry
 import au.com.shiftyjelly.pocketcasts.models.db.dao.PodcastDao
 import au.com.shiftyjelly.pocketcasts.models.entity.Podcast
-import au.com.shiftyjelly.pocketcasts.preferences.SettingsImpl
+import au.com.shiftyjelly.pocketcasts.models.to.PodcastGrouping
+import au.com.shiftyjelly.pocketcasts.preferences.Settings
+import au.com.shiftyjelly.pocketcasts.preferences.UserSetting
 import au.com.shiftyjelly.pocketcasts.repositories.playback.PlaybackManager
 import au.com.shiftyjelly.pocketcasts.repositories.podcast.EpisodeManager
 import au.com.shiftyjelly.pocketcasts.repositories.podcast.PlaylistManager
@@ -18,7 +20,6 @@ import au.com.shiftyjelly.pocketcasts.servers.cdn.StaticServerManager
 import au.com.shiftyjelly.pocketcasts.servers.podcast.PodcastCacheServerManager
 import au.com.shiftyjelly.pocketcasts.servers.refresh.RefreshServerManager
 import au.com.shiftyjelly.pocketcasts.utils.Optional
-import com.squareup.moshi.Moshi
 import io.reactivex.Single
 import org.junit.After
 import org.junit.Assert.assertTrue
@@ -45,13 +46,10 @@ class PodcastManagerTest {
         val episodeManager = mock<EpisodeManager>()
         val playlistManager = mock<PlaylistManager>()
 
-        val settings = SettingsImpl(
-            sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context),
-            privatePreferences = PreferenceManager.getDefaultSharedPreferences(context),
-            context = context,
-            firebaseRemoteConfig = mock(),
-            moshi = Moshi.Builder().build(),
-        )
+        val settings = mock<Settings> {
+            on { podcastGroupingDefault } doReturn MockUserSetting(PodcastGrouping.None)
+            on { showArchivedDefault } doReturn MockUserSetting(false)
+        }
 
         val syncManagerSignedOut = mock<SyncManager> {
             on { isLoggedIn() } doReturn false
@@ -113,5 +111,19 @@ class PodcastManagerTest {
         podcastManagerSignedIn.unsubscribe(uuid, playbackManager)
         val daoPodcast = podcastDao.findByUuid(uuid)
         assertTrue("Podcast should be unsubscribed", daoPodcast?.isSubscribed == false)
+    }
+
+    // This manual mock is needed to avoid problems when accessing a lazily initialized UserSetting::flow
+    // from a mocked Settings class
+    private class MockUserSetting<T>(
+        private val initialValue: T,
+        sharedPrefKey: String = "a_shared_pref_key",
+        sharedPrefs: SharedPreferences = mock(),
+    ) : UserSetting<T>(
+        sharedPrefKey = sharedPrefKey,
+        sharedPrefs = sharedPrefs,
+    ) {
+        override fun get(): T = initialValue
+        override fun persist(value: T, commit: Boolean) {}
     }
 }
