fef7c65fa956d9a125af99f5f708752f82871bb7
Matt Chowning <mchowning@gmail.com>
2023-08-09 13:09:17 -0400

    Make discover country code a UserSetting

--- modules/features/account/src/main/java/au/com/shiftyjelly/pocketcasts/account/viewmodel/OnboardingRecommendationsStartPageViewModel.kt
+++ modules/features/account/src/main/java/au/com/shiftyjelly/pocketcasts/account/viewmodel/OnboardingRecommendationsStartPageViewModel.kt
@@ -135,7 +135,7 @@ class OnboardingRecommendationsStartPageViewModel @Inject constructor(
                 return@launch
             }
 
-            val regionCode = settings.getDiscoveryCountryCode()
+            val regionCode = settings.discoverCountryCode.flow.value
             val region = feed.regions[regionCode]
                 ?: feed.regions[feed.defaultRegionCode]
                     .let {
