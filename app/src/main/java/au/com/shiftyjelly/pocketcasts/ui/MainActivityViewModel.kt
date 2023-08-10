66316c53bf0a5257dbabddefae260a7866d2c1a7
ashiagr <agr.ash@gmail.com>
2023-08-10 17:13:58 +0530

    Handle bookmark intents

--- app/src/main/java/au/com/shiftyjelly/pocketcasts/ui/MainActivityViewModel.kt
+++ app/src/main/java/au/com/shiftyjelly/pocketcasts/ui/MainActivityViewModel.kt
@@ -6,11 +6,15 @@ import androidx.lifecycle.toLiveData
 import androidx.lifecycle.viewModelScope
 import au.com.shiftyjelly.pocketcasts.models.to.SignInState
 import au.com.shiftyjelly.pocketcasts.models.to.SubscriptionStatus
+import au.com.shiftyjelly.pocketcasts.player.view.bookmark.BookmarkArguments
 import au.com.shiftyjelly.pocketcasts.preferences.Settings
+import au.com.shiftyjelly.pocketcasts.repositories.bookmark.BookmarkManager
 import au.com.shiftyjelly.pocketcasts.repositories.endofyear.EndOfYearManager
 import au.com.shiftyjelly.pocketcasts.repositories.playback.PlaybackManager
 import au.com.shiftyjelly.pocketcasts.repositories.playback.PlaybackState
+import au.com.shiftyjelly.pocketcasts.repositories.podcast.PodcastManager
 import au.com.shiftyjelly.pocketcasts.repositories.user.UserManager
+import au.com.shiftyjelly.pocketcasts.ui.theme.Theme
 import au.com.shiftyjelly.pocketcasts.views.multiselect.MultiSelectBookmarksHelper
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.reactivex.BackpressureStrategy
@@ -23,11 +27,14 @@ import javax.inject.Inject
 @HiltViewModel
 class MainActivityViewModel
 @Inject constructor(
-    playbackManager: PlaybackManager,
+    private val playbackManager: PlaybackManager,
     userManager: UserManager,
     val settings: Settings,
     private val endOfYearManager: EndOfYearManager,
     private val multiSelectBookmarksHelper: MultiSelectBookmarksHelper,
+    private val podcastManager: PodcastManager,
+    private val bookmarkManager: BookmarkManager,
+    private val theme: Theme,
 ) : ViewModel() {
 
     var isPlayerOpen: Boolean = false
@@ -73,5 +80,34 @@ class MainActivityViewModel
 
     fun closeMultiSelect() {
         multiSelectBookmarksHelper.closeMultiSelect()
+    }
+
+    fun buildBookmarkArguments(onSuccess: (BookmarkArguments) -> Unit) {
+        viewModelScope.launch {
+            val episode = playbackManager.getCurrentEpisode() ?: return@launch
+            val timeInSecs = playbackManager.getCurrentTimeMs(episode) / 1000
+
+            // load the existing bookmark
+            val bookmark = bookmarkManager.findByEpisodeTime(
+                episode = episode,
+                timeSecs = timeInSecs
+            )
+
+            val podcast =
+                bookmark?.let { podcastManager.findPodcastByUuidSuspend(bookmark.podcastUuid) }
+            val backgroundColor =
+                if (podcast == null) 0xFF000000.toInt() else theme.playerBackgroundColor(podcast)
+            val tintColor =
+                if (podcast == null) 0xFFFFFFFF.toInt() else theme.playerHighlightColor(podcast)
+
+            val arguments = BookmarkArguments(
+                bookmarkUuid = bookmark?.uuid,
+                episodeUuid = episode.uuid,
+                timeSecs = timeInSecs,
+                backgroundColor = backgroundColor,
+                tintColor = tintColor,
+            )
+            onSuccess(arguments)
+        }
     }
 }
