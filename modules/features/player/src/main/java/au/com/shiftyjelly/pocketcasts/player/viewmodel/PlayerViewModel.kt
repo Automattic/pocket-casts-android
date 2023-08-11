9d01525a3c9c327f8603ac10bc33fb340681186a
ashiagr <agr.ash@gmail.com>
2023-08-11 14:01:57 +0530

    Guard shelf item

--- modules/features/player/src/main/java/au/com/shiftyjelly/pocketcasts/player/viewmodel/PlayerViewModel.kt
+++ modules/features/player/src/main/java/au/com/shiftyjelly/pocketcasts/player/viewmodel/PlayerViewModel.kt
@@ -19,6 +19,7 @@ import au.com.shiftyjelly.pocketcasts.models.to.Chapter
 import au.com.shiftyjelly.pocketcasts.models.to.Chapters
 import au.com.shiftyjelly.pocketcasts.models.to.PlaybackEffects
 import au.com.shiftyjelly.pocketcasts.models.type.EpisodeStatusEnum
+import au.com.shiftyjelly.pocketcasts.models.type.SubscriptionTier
 import au.com.shiftyjelly.pocketcasts.player.R
 import au.com.shiftyjelly.pocketcasts.player.view.ShelfItem
 import au.com.shiftyjelly.pocketcasts.player.view.ShelfItems
@@ -208,7 +209,11 @@ class PlayerViewModel @Inject constructor(
     }
         .toFlowable(BackpressureStrategy.LATEST)
         .combineLatest(userManager.getSignInState()).map { (shelfItems, signInState) ->
-            shelfItems.filter { item -> !item.isPlus || signInState.isSignedInAsPlusOrPatron }
+            shelfItems.filter { item ->
+                (item.tier == SubscriptionTier.NONE) ||
+                    (item.tier == SubscriptionTier.PLUS && signInState.isSignedInAsPlusOrPatron) ||
+                    (item.tier == SubscriptionTier.PATRON && signInState.isSignedInAsPatron)
+            }
         }
 
     private val shelfUpNext = upNextStateObservable.distinctUntilChanged { t1, t2 ->
