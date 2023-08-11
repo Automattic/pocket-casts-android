97b9cf916d2a2f70e3efabf589ba942dbd40f005
ashiagr <agr.ash@gmail.com>
2023-08-11 17:58:48 +0530

    Update upsell badge

--- modules/features/player/src/main/java/au/com/shiftyjelly/pocketcasts/player/view/bookmark/components/PlusUpsellView.kt
+++ modules/features/player/src/main/java/au/com/shiftyjelly/pocketcasts/player/view/bookmark/components/PlusUpsellView.kt
@@ -1,17 +1,25 @@
 package au.com.shiftyjelly.pocketcasts.player.view.bookmark.components
 
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
 import au.com.shiftyjelly.pocketcasts.compose.AppTheme
+import au.com.shiftyjelly.pocketcasts.compose.components.TextH20
+import au.com.shiftyjelly.pocketcasts.compose.images.SubscriptionBadge
 import au.com.shiftyjelly.pocketcasts.compose.preview.ThemePreviewParameterProvider
 import au.com.shiftyjelly.pocketcasts.ui.theme.Theme
 import au.com.shiftyjelly.pocketcasts.images.R as IR
 import au.com.shiftyjelly.pocketcasts.localization.R as LR
+import au.com.shiftyjelly.pocketcasts.ui.R as UR
 
 @Composable
 fun PlusUpsellView(
@@ -21,10 +29,19 @@ fun PlusUpsellView(
 ) {
     MessageView(
         titleView = {
-            Image(
-                painter = painterResource(IR.drawable.pocket_casts_plus_logo),
-                contentDescription = stringResource(LR.string.pocket_casts),
-            )
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                TextH20(text = stringResource(LR.string.pocket_casts))
+                Spacer(modifier = Modifier.width(8.dp))
+                SubscriptionBadge(
+                    iconRes = IR.drawable.ic_patron,
+                    shortNameRes = LR.string.pocket_casts_patron_short,
+                    iconColor = Color.White,
+                    textColor = Color.White,
+                    backgroundColor = colorResource(UR.color.patron_purple),
+                )
+            }
         },
         buttonTitleRes = LR.string.subscribe, // TODO: Bookmarks update upsell button title based on subscription status
         buttonAction = onClick,
