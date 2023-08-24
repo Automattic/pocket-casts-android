52363d2b97c2c31038024fd3e048f872fc553ef1
ashiagr <agr.ash@gmail.com>
2023-08-24 15:18:46 +0530

    Save requested dates in settings

--- modules/services/preferences/src/main/java/au/com/shiftyjelly/pocketcasts/preferences/Settings.kt
+++ modules/services/preferences/src/main/java/au/com/shiftyjelly/pocketcasts/preferences/Settings.kt
@@ -143,6 +143,8 @@ interface Settings {
         const val LOG_TAG_AUTO = "PocketCastsAuto"
 
         const val NOTIFICATIONS_DISABLED_MESSAGE_SHOWN = "notificationsDisabledMessageShown"
+
+        const val APP_REVIEW_REQUESTED_DATES = "in_app_review_requested_dates"
     }
 
     enum class NotificationChannel(val id: String) {
@@ -659,4 +661,7 @@ interface Settings {
     fun <T> setBookmarksSortType(sortType: T)
     fun getBookmarksSortTypeForPlayer(): BookmarksSortTypeForPlayer
     fun getBookmarksSortTypeForPodcast(): BookmarksSortTypeForPodcast
+
+    fun addReviewRequestedDate()
+    fun getReviewRequestedDates(): List<String>
 }
