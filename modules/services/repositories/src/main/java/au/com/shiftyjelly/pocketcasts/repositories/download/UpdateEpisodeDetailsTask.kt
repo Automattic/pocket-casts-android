a549402c5f2ebb5ac04b137ea4a69384cf0f7b24
Philip Simpson <philz.the.gecko@gmail.com>
2023-09-20 12:13:17 +0930

    Adding more logging to the update episode details worker

--- modules/services/repositories/src/main/java/au/com/shiftyjelly/pocketcasts/repositories/download/UpdateEpisodeDetailsTask.kt
+++ modules/services/repositories/src/main/java/au/com/shiftyjelly/pocketcasts/repositories/download/UpdateEpisodeDetailsTask.kt
@@ -17,11 +17,12 @@ import au.com.shiftyjelly.pocketcasts.utils.extensions.await
 import au.com.shiftyjelly.pocketcasts.utils.log.LogBuffer
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
-import io.sentry.Sentry
 import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import timber.log.Timber
+import java.io.IOException
+import java.util.concurrent.TimeUnit
 
 @HiltWorker
 class UpdateEpisodeDetailsTask @AssistedInject constructor(
@@ -30,16 +31,19 @@ class UpdateEpisodeDetailsTask @AssistedInject constructor(
     var episodeManager: EpisodeManager,
 ) : CoroutineWorker(context, params) {
     companion object {
-        const val TASK_NAME = "UpdateEpisodeDetailsTask"
-        const val INPUT_EPISODE_UUIDS = "episode_uuids"
+        private const val TASK_NAME = "UpdateEpisodeDetailsTask"
+        private const val INPUT_EPISODE_UUIDS = "episode_uuids"
+        private const val REQUEST_TIMEOUT_SECS = 20L
 
         fun enqueue(episodes: List<PodcastEpisode>, context: Context) {
             if (episodes.isEmpty()) {
                 return
             }
-            val episodeUuids = episodes.map { it.uuid }.toTypedArray()
+
+            val episodeUuids = episodes.map { it.uuid }
+            LogBuffer.i(LogBuffer.TAG_BACKGROUND_TASKS, "$TASK_NAME - enqueued ${episodeUuids.joinToString()}")
             val workData = Data.Builder()
-                .putStringArray(INPUT_EPISODE_UUIDS, episodeUuids)
+                .putStringArray(INPUT_EPISODE_UUIDS, episodeUuids.toTypedArray())
                 .build()
             val workRequest = OneTimeWorkRequestBuilder<UpdateEpisodeDetailsTask>()
                 .setConstraints(Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build())
@@ -61,63 +65,68 @@ class UpdateEpisodeDetailsTask @AssistedInject constructor(
         }
 
         val startTime = SystemClock.elapsedRealtime()
-        LogBuffer.i(LogBuffer.TAG_BACKGROUND_TASKS, "UpdateEpisodeDetailsJob - onStartJob")
+        info("Worker started - episodes: ${episodeUuids.joinToString()}}")
 
         try {
-            Timber.i("Downloading Meta Data for ${episodeUuids.size} episodes")
-
-            val client = OkHttpClient.Builder().build()
+            val client = OkHttpClient.Builder().callTimeout(REQUEST_TIMEOUT_SECS, TimeUnit.SECONDS).build()
 
             for (episodeUuid in episodeUuids) {
                 val episode = episodeManager.findByUuid(episodeUuid) ?: continue
                 val downloadUrl = episode.downloadUrl?.toHttpUrlOrNull() ?: continue
-
                 val request = Request.Builder()
                     .url(downloadUrl)
                     .addHeader("User-Agent", "Pocket Casts")
-                    .head()
                     .build()
 
                 if (isStopped) {
                     return Result.retry()
                 }
 
-                Sentry.addBreadcrumb("UpdateEpisodeDetailsJob - calling ${episode.uuid} - $downloadUrl")
-                val response = client.newCall(request).await()
+                try {
+                    val response = client.newCall(request).await()
 
-                val contentType = response.header("Content-Type")
-                if (contentType != null && contentType.isNotBlank()) {
-                    if ((episode.fileType.isNullOrBlank() && (contentType.startsWith("audio") || contentType.startsWith("video"))) || contentType.startsWith("video")) {
-                        episodeManager.updateFileType(episode, contentType)
-                        episode.fileType = contentType
-                    }
-                }
-
-                val contentLengthHeader = response.header("Content-Length")
-                if (!contentLengthHeader.isNullOrBlank()) {
-                    try {
-                        val contentLength = java.lang.Long.parseLong(contentLengthHeader)
-                        val sizeInBytes = episode.sizeInBytes
-                        if ((sizeInBytes == 0L || sizeInBytes != contentLength) && contentLength > 153600) {
-                            episodeManager.updateSizeInBytes(episode, contentLength)
-                            episode.sizeInBytes = contentLength
+                    val contentType = response.header("Content-Type")
+                    if (!contentType.isNullOrBlank()) {
+                        if ((episode.fileType.isNullOrBlank() && (contentType.startsWith("audio") || contentType.startsWith("video"))) || contentType.startsWith("video")) {
+                            episodeManager.updateFileType(episode, contentType)
+                            episode.fileType = contentType
                         }
-                    } catch (nfe: NumberFormatException) {
-                        Timber.e(nfe, "Unable to read content length.")
                     }
+
+                    val contentLengthHeader = response.header("Content-Length")
+                    if (!contentLengthHeader.isNullOrBlank()) {
+                        try {
+                            val contentLength = java.lang.Long.parseLong(contentLengthHeader)
+                            val sizeInBytes = episode.sizeInBytes
+                            if ((sizeInBytes == 0L || sizeInBytes != contentLength) && contentLength > 153600) {
+                                episodeManager.updateSizeInBytes(episode, contentLength)
+                                episode.sizeInBytes = contentLength
+                            }
+                        } catch (nfe: NumberFormatException) {
+                            Timber.i(nfe, "Unable to read content length.")
+                        }
+                    }
+                } catch (ioException: IOException) {
+                    // don't report IO issues such as timeouts to Sentry
+                    info("Unable to check episode file details with a head request for $episodeUuid. Error: ${ioException.message}")
                 }
             }
-        } catch (t: Throwable) {
-            LogBuffer.e(LogBuffer.TAG_BACKGROUND_TASKS, t, "Unable to check episode file details with a head request.")
-            return if (runAttemptCount < 3) {
-                LogBuffer.i(LogBuffer.TAG_BACKGROUND_TASKS, "UpdateEpisodeDetailsJob - onTaskCompleted - retry ${runAttemptCount + 1}")
-                Result.retry()
-            } else {
-                Result.failure()
-            }
-        }
 
-        LogBuffer.i(LogBuffer.TAG_BACKGROUND_TASKS, "UpdateEpisodeDetailsJob - onTaskCompleted - ${String.format("%d ms", SystemClock.elapsedRealtime() - startTime)}")
-        return Result.success()
+            info("Worker completed - took ${SystemClock.elapsedRealtime() - startTime} ms")
+            return Result.success()
+        } catch (t: Throwable) {
+            info("Worker failed - took ${SystemClock.elapsedRealtime() - startTime} ms")
+            // report the error to Sentry and to the log buffer for support
+            LogBuffer.e(LogBuffer.TAG_BACKGROUND_TASKS, t, "Unable to check episode file details with a head request.")
+            // mark the worker as a success or it will won't process all the chain tasks
+            return Result.success()
+        }
+    }
+
+    /**
+     * Log a message to the log buffer for support and to the console for debugging.
+     */
+    private fun info(message: String) {
+        LogBuffer.i(LogBuffer.TAG_BACKGROUND_TASKS, "$TASK_NAME (Worker ID: $id) - $message")
     }
 }
