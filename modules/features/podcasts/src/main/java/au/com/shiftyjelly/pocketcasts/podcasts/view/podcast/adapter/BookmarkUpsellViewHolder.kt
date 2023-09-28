d5b6099d001c93e9f91aded78440b50222666e6f
ashiagr <agr.ash@gmail.com>
2023-09-28 12:52:55 +0530

    Use feature tier in start upsell flow

--- modules/features/podcasts/src/main/java/au/com/shiftyjelly/pocketcasts/podcasts/view/podcast/adapter/BookmarkUpsellViewHolder.kt
+++ modules/features/podcasts/src/main/java/au/com/shiftyjelly/pocketcasts/podcasts/view/podcast/adapter/BookmarkUpsellViewHolder.kt
@@ -9,6 +9,8 @@ import androidx.recyclerview.widget.RecyclerView
 import au.com.shiftyjelly.pocketcasts.analytics.SourceView
 import au.com.shiftyjelly.pocketcasts.compose.AppTheme
 import au.com.shiftyjelly.pocketcasts.compose.theme
+import au.com.shiftyjelly.pocketcasts.featureflag.Feature
+import au.com.shiftyjelly.pocketcasts.featureflag.FeatureTier
 import au.com.shiftyjelly.pocketcasts.player.view.bookmark.components.MessageViewColors
 import au.com.shiftyjelly.pocketcasts.player.view.bookmark.components.UpsellView
 import au.com.shiftyjelly.pocketcasts.settings.onboarding.OnboardingFlow
@@ -31,7 +33,10 @@ class BookmarkUpsellViewHolder(
                     sourceView = sourceView,
                     onClick = {
                         val source = OnboardingUpgradeSource.BOOKMARKS
-                        val onboardingFlow = OnboardingFlow.Upsell(source, true)
+                        val onboardingFlow = OnboardingFlow.Upsell(
+                            source = source,
+                            showPatronOnly = Feature.BOOKMARKS_ENABLED.tier == FeatureTier.Patron,
+                        )
                         OnboardingLauncher.openOnboardingFlow(context.getActivity(), onboardingFlow)
                     },
                     modifier = Modifier
