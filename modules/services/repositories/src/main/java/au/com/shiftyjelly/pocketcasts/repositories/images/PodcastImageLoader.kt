334ef2d01c0a0ac433fee05fff051c7d2e8b4368
joenjogu <joenjogu97@gmail.com>
2023-10-06 16:16:29 +0300

    Add podcast/episode title and thumbnail to android share sheet preview (#1372)
    Co-authored-by: Matt Chowning <mchowning@gmail.com>

--- modules/services/repositories/src/main/java/au/com/shiftyjelly/pocketcasts/repositories/images/PodcastImageLoader.kt
+++ modules/services/repositories/src/main/java/au/com/shiftyjelly/pocketcasts/repositories/images/PodcastImageLoader.kt
@@ -33,27 +33,27 @@ open class PodcastImageLoader(
     var radiusPx: Int = 0
     var shouldScale: Boolean = true
 
-    fun getBitmap(userEpisode: UserEpisode, size: Int): Bitmap? {
-        return runBlocking {
-            try {
-                val request = load(userEpisode).size(size, size).build()
-                return@runBlocking context.imageLoader.execute(request).drawable!!.toBitmap()
-            } catch (e: Exception) {
-                val request = loadNoPodcastCoil().size(size, size).build()
-                return@runBlocking context.imageLoader.execute(request).drawable?.toBitmap()
-            }
-        }
+    fun getBitmap(userEpisode: UserEpisode, size: Int): Bitmap? = runBlocking {
+        val request = load(userEpisode).size(size, size).build()
+        executeImageRequest(request, size)
     }
 
-    fun getBitmap(podcast: Podcast, size: Int): Bitmap? {
-        return runBlocking {
-            try {
-                val request = load(podcast).size(size, size).build()
-                return@runBlocking context.imageLoader.execute(request).drawable!!.toBitmap()
-            } catch (e: Exception) {
-                val request = loadNoPodcastCoil().size(size, size).build()
-                return@runBlocking context.imageLoader.execute(request).drawable?.toBitmap()
-            }
+    fun getBitmap(podcast: Podcast, size: Int): Bitmap? = runBlocking {
+        getBitmapSuspend(podcast, size)
+    }
+
+    suspend fun getBitmapSuspend(podcast: Podcast, size: Int): Bitmap? {
+        val request = load(podcast).size(size, size).build()
+        return executeImageRequest(request, size)
+    }
+
+    private suspend fun executeImageRequest(request: ImageRequest, size: Int): Bitmap? {
+        val drawable = context.imageLoader.execute(request).drawable
+        return if (drawable != null) {
+            drawable.toBitmap()
+        } else {
+            val fallbackRequest = loadNoPodcastCoil().size(size, size).build()
+            context.imageLoader.execute(fallbackRequest).drawable?.toBitmap()
         }
     }
 
