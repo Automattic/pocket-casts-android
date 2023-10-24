c09d8d5ac85abe494ddc8da82578536ccaeda405
Matt Chowning <mchowning@gmail.com>
2023-10-24 16:31:14 +0200

    Remove translation for Pocket Casts Playback

--- modules/features/endofyear/src/main/java/au/com/shiftyjelly/pocketcasts/endofyear/views/stories/StoryIntroView.kt
+++ modules/features/endofyear/src/main/java/au/com/shiftyjelly/pocketcasts/endofyear/views/stories/StoryIntroView.kt
@@ -11,7 +11,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
@@ -22,7 +21,6 @@ import au.com.shiftyjelly.pocketcasts.compose.preview.ThemePreviewParameterProvi
 import au.com.shiftyjelly.pocketcasts.endofyear.R
 import au.com.shiftyjelly.pocketcasts.endofyear.components.PodcastLogoWhite
 import au.com.shiftyjelly.pocketcasts.ui.theme.Theme
-import au.com.shiftyjelly.pocketcasts.localization.R as LR
 
 @Composable
 fun StoryIntroView(
@@ -128,7 +126,7 @@ private fun TitleView(
         content = { parallaxModifier, biasAlignment ->
             Image(
                 painter = painterResource(R.drawable.pocket_casts_playback),
-                contentDescription = stringResource(LR.string.end_of_year_pocket_casts_playback),
+                contentDescription = "Pocket Casts Playback", // Intentionally not translated
                 contentScale = ContentScale.FillBounds,
                 modifier = parallaxModifier,
                 alignment = biasAlignment,
