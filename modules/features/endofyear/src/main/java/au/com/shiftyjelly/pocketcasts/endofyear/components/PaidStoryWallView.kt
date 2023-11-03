aa537f0bc05b860d1014cbd23f3f1320d9b95d2d
ashiagr <agr.ash@gmail.com>
2023-11-03 10:36:21 +0530

    Add paid story wall view

---
+++ modules/features/endofyear/src/main/java/au/com/shiftyjelly/pocketcasts/endofyear/components/PaidStoryWallView.kt
@@ -0,0 +1,102 @@
+package au.com.shiftyjelly.pocketcasts.endofyear.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import au.com.shiftyjelly.pocketcasts.compose.AppTheme
+import au.com.shiftyjelly.pocketcasts.compose.buttons.RowButton
+import au.com.shiftyjelly.pocketcasts.compose.images.SubscriptionBadgeDisplayMode
+import au.com.shiftyjelly.pocketcasts.compose.images.SubscriptionBadgeForTier
+import au.com.shiftyjelly.pocketcasts.models.type.Subscription.SubscriptionTier
+import au.com.shiftyjelly.pocketcasts.repositories.endofyear.stories.Story
+import au.com.shiftyjelly.pocketcasts.ui.theme.Theme
+import au.com.shiftyjelly.pocketcasts.localization.R as LR
+
+@Composable
+fun PaidStoryWallView(
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color.White.copy(alpha = 0.3f))
+            .verticalScroll(rememberScrollState())
+            .padding(vertical = 40.dp)
+    ) {
+        Spacer(modifier = modifier.weight(0.2f))
+
+        SubscriptionBadgeForTier(
+            tier = SubscriptionTier.PLUS,
+            displayMode = SubscriptionBadgeDisplayMode.Colored,
+        )
+
+        Spacer(modifier = modifier.height(14.dp))
+
+        PrimaryText()
+
+        Spacer(modifier = modifier.height(14.dp))
+
+        SecondaryText()
+
+        Spacer(modifier = modifier.height(14.dp))
+
+        UpgradeButton()
+
+        Spacer(modifier = modifier.weight(1f))
+    }
+}
+
+@Composable
+private fun PrimaryText(
+    modifier: Modifier = Modifier,
+) {
+    val text = stringResource(LR.string.end_of_year_stories_theres_more)
+    StoryPrimaryText(text = text, color = Story.TintColor, modifier = modifier)
+}
+
+@Composable
+private fun SecondaryText(
+    modifier: Modifier = Modifier,
+) {
+    val text = stringResource(LR.string.end_of_year_stories_subscribe_to_plus)
+    StorySecondaryText(text = text, color = Story.SubtitleColor, modifier = modifier)
+}
+
+@Composable
+private fun UpgradeButton(
+    modifier: Modifier = Modifier,
+) {
+    val text = stringResource(LR.string.trial_start)
+    RowButton(
+        text = text,
+        colors = ButtonDefaults.buttonColors(backgroundColor = Color.White),
+        textColor = Color.Black,
+        onClick = {},
+        modifier = modifier
+            .fillMaxSize(.65f)
+    )
+}
+
+@Preview
+@Composable
+fun PaidStoryWallViewPreview() {
+    AppTheme(Theme.ThemeType.DARK) {
+        PaidStoryWallView()
+    }
+}
