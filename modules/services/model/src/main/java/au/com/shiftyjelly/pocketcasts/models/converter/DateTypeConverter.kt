f9eea33a880b480da12154a4a926ec99613131b2
Matt Chowning <mchowning@gmail.com>
2023-12-01 08:32:32 -0500

    Revert "Use type converter that cannot return null for date to avoid crash (#1573)"
    This reverts commit 0df5efec5504ca46f7ef80f7b58f057e9712a2e1.

--- modules/services/model/src/main/java/au/com/shiftyjelly/pocketcasts/models/converter/DateTypeConverter.kt
+++ modules/services/model/src/main/java/au/com/shiftyjelly/pocketcasts/models/converter/DateTypeConverter.kt
@@ -1,9 +1,6 @@
 package au.com.shiftyjelly.pocketcasts.models.converter
 
 import androidx.room.TypeConverter
-import io.sentry.Sentry
-import timber.log.Timber
-import java.time.Instant
 import java.util.Date
 
 class DateTypeConverter {
@@ -16,31 +13,5 @@ class DateTypeConverter {
     @TypeConverter
     fun toLong(value: Date?): Long? {
         return value?.time
-    }
-}
-
-typealias SafeDate = Date
-
-// Type converter for dates that will not return null even if a null parameter is passed in.
-class SafeDateTypeConverter {
-
-    @TypeConverter
-    fun toDate(value: Long?): SafeDate {
-        return if (value == null) {
-            "ShouldNotBeNullDateTypeConverter::toDate called with null parameter. Returning epoch date.".let {
-                Timber.w(it)
-                Sentry.addBreadcrumb(it)
-            }
-            EPOCH
-        } else {
-            Date(value)
-        }
-    }
-
-    @TypeConverter
-    fun toLong(value: SafeDate?): Long = value?.time ?: 0L
-
-    companion object {
-        private val EPOCH = Date(Instant.EPOCH.toEpochMilli())
     }
 }
