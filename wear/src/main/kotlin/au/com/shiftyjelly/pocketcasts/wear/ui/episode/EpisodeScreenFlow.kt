05681baabcba90306a72fd2d020d62d181cf9f69
Matt Chowning <mchowning@gmail.com>
2023-12-21 15:03:27 -0500

    Revert "Prevent wear chips from getting cut off by round watch edges (#1636)"
    This reverts commit 3462a80ed061294d0e4d6bc9f7bec36199d8f956.

--- wear/src/main/kotlin/au/com/shiftyjelly/pocketcasts/wear/ui/episode/EpisodeScreenFlow.kt
+++ wear/src/main/kotlin/au/com/shiftyjelly/pocketcasts/wear/ui/episode/EpisodeScreenFlow.kt
@@ -17,7 +17,6 @@ import androidx.navigation.NavType
 import androidx.navigation.compose.navigation
 import androidx.navigation.navArgument
 import androidx.wear.compose.material.SwipeToDismissBoxState
-import au.com.shiftyjelly.pocketcasts.wear.extensions.responsive
 import au.com.shiftyjelly.pocketcasts.wear.ui.component.NotificationScreen
 import au.com.shiftyjelly.pocketcasts.wear.ui.component.NowPlayingPager
 import au.com.shiftyjelly.pocketcasts.wear.ui.component.ObtainConfirmationScreen
@@ -59,8 +58,7 @@ object EpisodeScreenFlow {
         ) {
             scrollable(
                 route = episodeScreen,
-                columnStateFactory = ScalingLazyColumnDefaults.responsive(
-                    firstItemIsFullWidth = false,
+                columnStateFactory = ScalingLazyColumnDefaults.belowTimeText(
                     verticalArrangement = Arrangement.spacedBy(0.dp, Alignment.Top)
                 )
             ) {
@@ -110,10 +108,7 @@ object EpisodeScreenFlow {
                 }
             }
 
-            scrollable(
-                route = upNextOptionsScreen,
-                columnStateFactory = ScalingLazyColumnDefaults.responsive(),
-            ) {
+            scrollable(upNextOptionsScreen) {
                 it.viewModel.timeTextMode = NavScaffoldViewModel.TimeTextMode.Off
                 val episodeScreenBackStackEntry = remember(it.backStackEntry) {
                     navController.getBackStackEntry(episodeScreen)
