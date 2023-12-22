d3d0b4cbf40dd8560c9adba20c4fb72770189bd2
ashiagr <agr.ash@gmail.com>
2023-12-22 15:02:50 +0530

    Revert "media3 - fix setAudioCapabilities deprecated error by removing it"
    This reverts commit 32653eef8ca04e8665203f3a384c8785775f0706.

--- modules/services/repositories/src/main/java/au/com/shiftyjelly/pocketcasts/repositories/playback/ShiftyRenderersFactory.kt
+++ modules/services/repositories/src/main/java/au/com/shiftyjelly/pocketcasts/repositories/playback/ShiftyRenderersFactory.kt
@@ -44,6 +44,7 @@ class ShiftyRenderersFactory(context: Context?, statsManager: StatsManager, priv
     override fun buildAudioSink(context: Context, enableFloatOutput: Boolean, enableAudioTrackPlaybackParams: Boolean): AudioSink {
         processorChain = ShiftyAudioProcessorChain(customAudio)
         return DefaultAudioSink.Builder(context)
+            .setAudioCapabilities(AudioCapabilities.getCapabilities(context))
             .setAudioProcessorChain(processorChain!!)
             .setEnableFloatOutput(enableFloatOutput)
             .setEnableAudioTrackPlaybackParams(enableAudioTrackPlaybackParams)
