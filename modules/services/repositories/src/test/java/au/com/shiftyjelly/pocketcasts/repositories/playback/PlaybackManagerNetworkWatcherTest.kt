469f6006d9961666d04bfd223256b148e22e7ffc
Michał Sikora <me@mehow.io>
2024-01-30 10:42:10 +0100

    Redesign PlaybackManagerNetworkWatcher

--- modules/services/repositories/src/test/java/au/com/shiftyjelly/pocketcasts/repositories/playback/PlaybackManagerNetworkWatcherTest.kt
+++ modules/services/repositories/src/test/java/au/com/shiftyjelly/pocketcasts/repositories/playback/PlaybackManagerNetworkWatcherTest.kt
@@ -3,45 +3,44 @@
 package au.com.shiftyjelly.pocketcasts.repositories.playback
 
 import android.net.NetworkCapabilities
-import au.com.shiftyjelly.pocketcasts.sharedtest.MainCoroutineRule
 import junit.framework.TestCase.assertEquals
-import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.emitAll
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.test.TestCoroutineScheduler
-import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.TestScope
 import kotlinx.coroutines.test.runTest
-import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.mockito.Mock
-import org.mockito.junit.MockitoJUnitRunner
 import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
-import org.mockito.kotlin.whenever
 
-@RunWith(MockitoJUnitRunner::class)
+@ExperimentalCoroutinesApi
 class PlaybackManagerNetworkWatcherTest {
+    private val inputNetworkCapabilities = MutableStateFlow<NetworkCapabilities?>(null)
+    private var numTimesCalledOnSwitchToMeteredConnection = 0
 
-    @get:Rule
-    val coroutineRule = MainCoroutineRule()
-
-    @Mock private lateinit var networkConnectionWatcher: NetworkConnectionWatcher
-    private lateinit var playbackManagerNetworkWatcher: PlaybackManagerNetworkWatcher
+    private val networkConnectionWatcher = object : NetworkConnectionWatcher {
+        override val networkCapabilities = inputNetworkCapabilities
+    }
+    private val playbackManagerNetworkWatcher = PlaybackManagerNetworkWatcher(
+        networkConnectionWatcher = networkConnectionWatcher,
+        onSwitchToMeteredConnection = { numTimesCalledOnSwitchToMeteredConnection++ },
+    )
 
     @Test
     fun `handles switch to metered connection`() = runTest {
         val isMeteredCapabilities = listOf(false, true)
-        val timesCalled = getSwitchToMeteredCalls(backgroundScope, testScheduler, isMeteredCapabilities)
-        assertEquals(1, timesCalled)
+        runNetworkMetering(isMeteredCapabilities)
+        assertEquals(1, numTimesCalledOnSwitchToMeteredConnection)
     }
 
     @Test
     fun `does not record switch if always on metered`() = runTest {
         val isMeteredCapabilities = listOf(true, true, true)
-        val timesCalled = getSwitchToMeteredCalls(backgroundScope, testScheduler, isMeteredCapabilities)
-        assertEquals(0, timesCalled)
+        runNetworkMetering(isMeteredCapabilities)
+        assertEquals(0, numTimesCalledOnSwitchToMeteredConnection)
     }
 
     @Test
@@ -51,37 +50,16 @@ class PlaybackManagerNetworkWatcherTest {
             true, true, true, false, false, true, // second switch to metered
             true, // not a switch to metered because was already metered
         )
-        val timesCalled = getSwitchToMeteredCalls(backgroundScope, testScheduler, isMeteredCapabilities)
-        assertEquals(2, timesCalled)
+        runNetworkMetering(isMeteredCapabilities)
+        assertEquals(2, numTimesCalledOnSwitchToMeteredConnection)
     }
 
-    private fun getSwitchToMeteredCalls(
-        backgroundScope: CoroutineScope,
-        testScheduler: TestCoroutineScheduler,
-        list: List<Boolean>,
-    ): Int {
-        // initialize NetworkConnectionWatcher mock
-        val mutableNetworkCapabilitiesFlow = MutableStateFlow<NetworkCapabilities?>(null)
-        whenever(networkConnectionWatcher.networkCapabilities)
-            .thenReturn(mutableNetworkCapabilitiesFlow)
-
-        playbackManagerNetworkWatcher = PlaybackManagerNetworkWatcher(
-            applicationScope = CoroutineScope(UnconfinedTestDispatcher(testScheduler)),
-            networkConnectionWatcher = networkConnectionWatcher,
-        )
-
-        var numTimesCalledOnSwitchToMeteredConnection = 0
-        playbackManagerNetworkWatcher.initialize(
-            onSwitchToMeteredConnection = { numTimesCalledOnSwitchToMeteredConnection++ },
-        )
-
-        list.forEach { isMetered ->
-            backgroundScope.launch(UnconfinedTestDispatcher(testScheduler)) {
-                mutableNetworkCapabilitiesFlow.value = networkCapabilities(isMetered)
-            }
+    private suspend fun TestScope.runNetworkMetering(meteredChanges: List<Boolean>) {
+        backgroundScope.launch(Dispatchers.Unconfined) {
+            playbackManagerNetworkWatcher.observeConnection()
         }
 
-        return numTimesCalledOnSwitchToMeteredConnection
+        inputNetworkCapabilities.emitAll(meteredChanges.map(::networkCapabilities).asFlow())
     }
 
     private fun networkCapabilities(isMetered: Boolean) = mock<NetworkCapabilities> {
