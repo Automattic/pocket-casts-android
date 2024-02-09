67973053ab676211969b23ef835bae20b3cc2368
Michał Sikora <me@mehow.io>
2024-02-09 10:42:50 +0100

    Fix setOffloadMode() removed

--- modules/services/repositories/src/main/java/au/com/shiftyjelly/pocketcasts/repositories/playback/ShiftyRenderersFactory.kt
+++ modules/services/repositories/src/main/java/au/com/shiftyjelly/pocketcasts/repositories/playback/ShiftyRenderersFactory.kt
@@ -48,7 +48,6 @@ class ShiftyRenderersFactory(context: Context?, statsManager: StatsManager, priv
             .setAudioProcessorChain(processorChain!!)
             .setEnableFloatOutput(enableFloatOutput)
             .setEnableAudioTrackPlaybackParams(enableAudioTrackPlaybackParams)
-            .setOffloadMode(if (enableOffload) DefaultAudioSink.OFFLOAD_MODE_ENABLED_GAPLESS_REQUIRED else DefaultAudioSink.OFFLOAD_MODE_DISABLED)
             .build()
     }
 
