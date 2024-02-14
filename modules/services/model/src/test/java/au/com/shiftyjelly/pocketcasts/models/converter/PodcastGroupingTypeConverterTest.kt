efa8c7878e37486898397886c29d07138b1b11dd
Michał Sikora <me@mehow.io>
2024-02-14 11:25:58 +0100

    Add tests for podcast grouping

---
+++ modules/services/model/src/test/java/au/com/shiftyjelly/pocketcasts/models/converter/PodcastGroupingTypeConverterTest.kt
@@ -0,0 +1,62 @@
+package au.com.shiftyjelly.pocketcasts.models.converter
+
+import au.com.shiftyjelly.pocketcasts.models.to.PodcastGrouping
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class PodcastGroupingTypeConverterTest {
+    private val converter = PodcastGroupingTypeConverter()
+
+    @Test
+    fun `podcast groupings are encoded correctly`() {
+        val expected = mapOf(
+            PodcastGrouping.None to 0,
+            PodcastGrouping.Downloaded to 1,
+            PodcastGrouping.Unplayed to 2,
+            PodcastGrouping.Season to 3,
+            PodcastGrouping.Starred to 4,
+        )
+
+        val databaseValues = PodcastGrouping.All.associateWith { grouping ->
+            converter.toInt(grouping)
+        }
+
+        assertEquals(expected, databaseValues)
+    }
+
+    @Test
+    fun `podcast groupings are decoded correctly`() {
+        val expected = mapOf(
+            0 to PodcastGrouping.None,
+            1 to PodcastGrouping.Downloaded,
+            2 to PodcastGrouping.Unplayed,
+            3 to PodcastGrouping.Season,
+            4 to PodcastGrouping.Starred,
+        )
+
+        val databaseValues = List(5) { it to converter.toPodcastGrouping(it) }.toMap()
+
+        assertEquals(expected, databaseValues)
+    }
+
+    @Test
+    fun `encode null value`() {
+        val databaseValue = converter.toInt(null)
+
+        assertEquals(0, databaseValue)
+    }
+
+    @Test
+    fun `decode null value`() {
+        val grouping = converter.toPodcastGrouping(null)
+
+        assertEquals(PodcastGrouping.None, grouping)
+    }
+
+    @Test
+    fun `decode unknown value`() {
+        val grouping = converter.toPodcastGrouping(Int.MIN_VALUE)
+
+        assertEquals(PodcastGrouping.None, grouping)
+    }
+}
