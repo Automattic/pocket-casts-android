619920adfb7365e5fb5eb77acf8f15723b9af1ca
Michał Sikora <me@mehow.io>
2024-03-04 17:01:43 +0100

    Set RxJava schedulers inside coroutines rule

--- modules/services/sharedtest/src/main/java/au/com/shiftyjelly/pocketcasts/sharedtest/MainCoroutineRule.kt
+++ modules/services/sharedtest/src/main/java/au/com/shiftyjelly/pocketcasts/sharedtest/MainCoroutineRule.kt
@@ -1,5 +1,11 @@
 package au.com.shiftyjelly.pocketcasts.sharedtest
 
+import io.reactivex.Scheduler
+import io.reactivex.android.plugins.RxAndroidPlugins
+import io.reactivex.functions.Function
+import io.reactivex.plugins.RxJavaPlugins
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.Callable
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.TestDispatcher
@@ -13,12 +19,22 @@ import org.junit.runner.Description
 class MainCoroutineRule(
     val testDispatcher: TestDispatcher = UnconfinedTestDispatcher(),
 ) : TestWatcher() {
+    // This is needed due to RxJava and Coroutines interop
+    private val immediateScheduler = Schedulers.trampoline()
+    private val androidSchedulerHandler = Function<Callable<Scheduler>, Scheduler> { immediateScheduler }
+    private val schedulerHandler = Function<Scheduler, Scheduler> { immediateScheduler }
 
     override fun starting(description: Description) {
+        RxJavaPlugins.setIoSchedulerHandler(schedulerHandler)
+        RxJavaPlugins.setComputationSchedulerHandler(schedulerHandler)
+        RxJavaPlugins.setNewThreadSchedulerHandler(schedulerHandler)
+        RxAndroidPlugins.setInitMainThreadSchedulerHandler(androidSchedulerHandler)
         Dispatchers.setMain(testDispatcher)
     }
 
     override fun finished(description: Description) {
         Dispatchers.resetMain()
+        RxAndroidPlugins.reset()
+        RxJavaPlugins.reset()
     }
 }
