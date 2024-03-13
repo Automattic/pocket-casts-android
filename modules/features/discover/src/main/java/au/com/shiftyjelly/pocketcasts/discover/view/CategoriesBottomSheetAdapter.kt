3c8348970ce607e7718fa156b09cb877e6da011d
Eduarda Barbosa <meduarda.qb@gmail.com>
2024-03-13 08:21:39 -0300

    refactor CategoriesBottomSheetAdapter to use binding and improve performance when setting click listener

--- modules/features/discover/src/main/java/au/com/shiftyjelly/pocketcasts/discover/view/CategoriesBottomSheetAdapter.kt
+++ modules/features/discover/src/main/java/au/com/shiftyjelly/pocketcasts/discover/view/CategoriesBottomSheetAdapter.kt
@@ -1,36 +1,44 @@
 package au.com.shiftyjelly.pocketcasts.discover.view
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import au.com.shiftyjelly.pocketcasts.discover.R
+import au.com.shiftyjelly.pocketcasts.discover.databinding.ItemCategoryBinding
 import au.com.shiftyjelly.pocketcasts.servers.model.DiscoverCategory
 import au.com.shiftyjelly.pocketcasts.servers.model.NetworkLoadableList
 import coil.load
 
-class CategoriesBottomSheetAdapter(val onCategoryClick: (NetworkLoadableList) -> Unit) : ListAdapter<DiscoverCategory, CategoriesBottomSheetAdapter.CategoryViewHolder>(CATEGORY_DIFF) {
-    class CategoryViewHolder(val itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val categoryIcon: ImageView = itemView.findViewById(R.id.imageView)
-        val categoryName: TextView = itemView.findViewById(R.id.lblTitle)
-        val itemCategoryLayout: LinearLayout = itemView.findViewById(R.id.itemCategoryLinear)
+class CategoriesBottomSheetAdapter(
+    private val onCategoryClick: (NetworkLoadableList) -> Unit,
+) : ListAdapter<DiscoverCategory, CategoriesBottomSheetAdapter.CategoryViewHolder>(CATEGORY_DIFF) {
+    class CategoryViewHolder(
+        val binding: ItemCategoryBinding,
+        onItemClicked: (Int) -> Unit,
+    ) : RecyclerView.ViewHolder(binding.root) {
+        init {
+            binding.itemCategoryLinear.setOnClickListener {
+                onItemClicked(bindingAdapterPosition)
+            }
+        }
+
+        fun bind(category: DiscoverCategory) {
+            binding.lblTitle.text = category.name
+            binding.lblTitle.contentDescription = category.name
+            binding.imageView.load(category.icon)
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val itemView = inflater.inflate(R.layout.item_category, parent, false)
-        return CategoryViewHolder(itemView)
+        val binding = ItemCategoryBinding.inflate(inflater, parent, false)
+
+        return CategoryViewHolder(binding) {
+            onCategoryClick(getItem(it))
+        }
     }
 
     override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
-        val category = getItem(position)
-        holder.categoryName.text = category.name
-        holder.categoryName.contentDescription = category.name
-        holder.categoryIcon.load(category.icon)
-        holder.itemCategoryLayout.setOnClickListener { onCategoryClick(category) }
+        holder.bind(getItem(position))
     }
 }
