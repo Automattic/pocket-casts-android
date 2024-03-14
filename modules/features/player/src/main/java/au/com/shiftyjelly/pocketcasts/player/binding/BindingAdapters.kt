852047a0a6d5dd3d9e9e87a1f0d0b041a5152d27
Wojtek Zieba <wojtek.zieba@automattic.com>
2024-03-14 11:54:41 +0100

    chore: remove unused binding adapters

--- modules/features/player/src/main/java/au/com/shiftyjelly/pocketcasts/player/binding/BindingAdapters.kt
+++ modules/features/player/src/main/java/au/com/shiftyjelly/pocketcasts/player/binding/BindingAdapters.kt
@@ -1,12 +1,6 @@
 package au.com.shiftyjelly.pocketcasts.player.binding
 
-import android.content.res.ColorStateList
 import android.view.View
-import android.widget.ImageView
-import androidx.core.view.ViewCompat
-import androidx.core.widget.ImageViewCompat
-import androidx.databinding.BindingAdapter
-import androidx.databinding.BindingConversion
 import au.com.shiftyjelly.pocketcasts.player.view.PlayerSeekBar
 import au.com.shiftyjelly.pocketcasts.repositories.playback.PlaybackState
 import au.com.shiftyjelly.pocketcasts.ui.theme.Theme
@@ -15,38 +9,8 @@ import okhttp3.HttpUrl
 
 object BindingAdapters {
 
-    @BindingAdapter("show")
-    @JvmStatic
-    fun setShow(view: View, visible: Boolean) {
-        view.visibility = if (visible) View.VISIBLE else View.GONE
-    }
-
     fun View.showIfPresent(url: HttpUrl?) {
         visibility = if (url?.toString().isNullOrEmpty()) View.GONE else View.VISIBLE
-    }
-
-    @BindingAdapter("backgroundTint")
-    @JvmStatic
-    fun setBackgroundTint(view: View, color: Int) {
-        if (color == 0) {
-            return
-        }
-        ViewCompat.setBackgroundTintList(view, ColorStateList.valueOf(color))
-    }
-
-    @BindingAdapter("tint")
-    @JvmStatic
-    fun setTint(imageView: ImageView, color: Int) {
-        if (color == 0) {
-            return
-        }
-        ImageViewCompat.setImageTintList(imageView, ColorStateList.valueOf(color))
-    }
-
-    @BindingConversion
-    @JvmStatic
-    fun convertBooleanToVisibility(visible: Boolean): Int {
-        return if (visible) View.VISIBLE else View.GONE
     }
 
     fun PlayerSeekBar.setPlaybackState(playbackState: PlaybackState?) {
@@ -64,19 +28,6 @@ object BindingAdapters {
         setTintColor(tintColor, theme)
         this.isBuffering = isBuffering
         bufferedUpToInSecs = bufferedUpTo / 1000
-    }
-
-    @BindingAdapter("play")
-    @JvmStatic
-    fun setLottieAnimationViewPlay(view: LottieAnimationView, play: Boolean?) {
-        if (play != null && play) {
-            if (!view.isAnimating) {
-                view.playAnimation()
-            }
-        } else {
-            view.pauseAnimation()
-            view.progress = 0.5f
-        }
     }
 
     fun LottieAnimationView.playIfTrue(play: Boolean?) {
