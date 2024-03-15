ad920c280a6de4e7cf36354760fe78b7006c6648
Wojtek Zieba <wojtek.zieba@automattic.com>
2024-03-15 14:56:30 +0100

    style: remove outer object from view extensions in player module

--- modules/features/player/src/main/java/au/com/shiftyjelly/pocketcasts/player/binding/ViewExtensions.kt
+++ modules/features/player/src/main/java/au/com/shiftyjelly/pocketcasts/player/binding/ViewExtensions.kt
@@ -7,37 +7,34 @@ import au.com.shiftyjelly.pocketcasts.ui.theme.Theme
 import com.airbnb.lottie.LottieAnimationView
 import okhttp3.HttpUrl
 
-object ViewExtensions {
+fun View.showIfPresent(url: HttpUrl?) {
+    visibility = if (url?.toString().isNullOrEmpty()) View.GONE else View.VISIBLE
+}
 
-    fun View.showIfPresent(url: HttpUrl?) {
-        visibility = if (url?.toString().isNullOrEmpty()) View.GONE else View.VISIBLE
+fun PlayerSeekBar.setPlaybackState(playbackState: PlaybackState?) {
+    if (playbackState == null) {
+        return
     }
+    setDurationMs(playbackState.durationMs)
+    setCurrentTimeMs(playbackState.positionMs)
+    setTintColor(playbackState.podcast?.tintColorForDarkBg, Theme.ThemeType.DARK)
+}
 
-    fun PlayerSeekBar.setPlaybackState(playbackState: PlaybackState?) {
-        if (playbackState == null) {
-            return
+fun PlayerSeekBar.setSeekBarState(durationMs: Int, positionMs: Int, tintColor: Int, bufferedUpTo: Int, isBuffering: Boolean, theme: Theme.ThemeType) {
+    setDurationMs(durationMs)
+    setCurrentTimeMs(positionMs)
+    setTintColor(tintColor, theme)
+    this.isBuffering = isBuffering
+    bufferedUpToInSecs = bufferedUpTo / 1000
+}
+
+fun LottieAnimationView.playIfTrue(play: Boolean?) {
+    if (play != null && play) {
+        if (!isAnimating) {
+            playAnimation()
         }
-        setDurationMs(playbackState.durationMs)
-        setCurrentTimeMs(playbackState.positionMs)
-        setTintColor(playbackState.podcast?.tintColorForDarkBg, Theme.ThemeType.DARK)
-    }
-
-    fun PlayerSeekBar.setSeekBarState(durationMs: Int, positionMs: Int, tintColor: Int, bufferedUpTo: Int, isBuffering: Boolean, theme: Theme.ThemeType) {
-        setDurationMs(durationMs)
-        setCurrentTimeMs(positionMs)
-        setTintColor(tintColor, theme)
-        this.isBuffering = isBuffering
-        bufferedUpToInSecs = bufferedUpTo / 1000
-    }
-
-    fun LottieAnimationView.playIfTrue(play: Boolean?) {
-        if (play != null && play) {
-            if (!isAnimating) {
-                playAnimation()
-            }
-        } else {
-            pauseAnimation()
-            progress = 0.5f
-        }
+    } else {
+        pauseAnimation()
+        progress = 0.5f
     }
 }
