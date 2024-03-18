d4fa418c2a50b76c0608200d9e5a8b359187ea25
Wojtek Zieba <wojtek.zieba@automattic.com>
2024-03-18 12:05:50 +0100

    fix: remove unused binding adapters

--- modules/features/podcasts/src/main/java/au/com/shiftyjelly/pocketcasts/podcasts/helper/BindingAdapters.kt
+++ modules/features/podcasts/src/main/java/au/com/shiftyjelly/pocketcasts/podcasts/helper/BindingAdapters.kt
@@ -13,40 +13,7 @@ import au.com.shiftyjelly.pocketcasts.podcasts.view.components.PlayButton
 import au.com.shiftyjelly.pocketcasts.podcasts.view.components.PlayButtonType
 
 object BindingAdapters {
-
-    @BindingAdapter("show")
-    @JvmStatic
-    fun setShow(view: View, visible: Boolean) {
-        view.visibility = if (visible) View.VISIBLE else View.GONE
-    }
-
-    @BindingAdapter("showIfPresent")
-    @JvmStatic
-    fun setShowIfPresent(view: View, string: String?) {
-        view.visibility = if (string.isNullOrBlank()) View.GONE else View.VISIBLE
-    }
-
-    @BindingAdapter(value = ["episode", "buttonType", "color", "fromListUuid"])
-    @JvmStatic
-    fun setupPlayButton(button: PlayButton, episode: BaseEpisode, buttonType: PlayButtonType, color: Int, fromListUuid: String?) {
-        button.setButtonType(episode, buttonType, color, fromListUuid)
-    }
-
-    @BindingAdapter("readMore")
-    @JvmStatic
-    fun setReadMore(textView: TextView, collapsedLines: Int) {
-        textView.maxLines = collapsedLines
-        textView.ellipsize = TextUtils.TruncateAt.END
-        textView.setOnClickListener {
-            val transition = ChangeBounds().apply {
-                duration = 200
-                interpolator = FastOutSlowInInterpolator()
-            }
-            TransitionManager.beginDelayedTransition(textView.parent as ViewGroup, transition)
-            textView.maxLines = if (textView.maxLines > collapsedLines) collapsedLines else Int.MAX_VALUE
-        }
-    }
-
+    
     fun TextView.readMore(collapsedLines: Int) {
         maxLines = collapsedLines
         ellipsize = TextUtils.TruncateAt.END
