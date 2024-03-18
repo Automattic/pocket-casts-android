acacd562d6339f9f160a7dca61eda8ebcfe9c270
Wojtek Zieba <wojtek.zieba@automattic.com>
2024-03-18 12:07:35 +0100

    chore: remove BindingAdapters object

--- modules/features/podcasts/src/main/java/au/com/shiftyjelly/pocketcasts/podcasts/helper/BindingAdapters.kt
+++ modules/features/podcasts/src/main/java/au/com/shiftyjelly/pocketcasts/podcasts/helper/BindingAdapters.kt
@@ -1,29 +1,21 @@
 package au.com.shiftyjelly.pocketcasts.podcasts.helper
 
 import android.text.TextUtils
-import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.databinding.BindingAdapter
 import androidx.interpolator.view.animation.FastOutSlowInInterpolator
 import androidx.transition.ChangeBounds
 import androidx.transition.TransitionManager
-import au.com.shiftyjelly.pocketcasts.models.entity.BaseEpisode
-import au.com.shiftyjelly.pocketcasts.podcasts.view.components.PlayButton
-import au.com.shiftyjelly.pocketcasts.podcasts.view.components.PlayButtonType
 
-object BindingAdapters {
-    
-    fun TextView.readMore(collapsedLines: Int) {
-        maxLines = collapsedLines
-        ellipsize = TextUtils.TruncateAt.END
-        setOnClickListener {
-            val transition = ChangeBounds().apply {
-                duration = 200
-                interpolator = FastOutSlowInInterpolator()
-            }
-            TransitionManager.beginDelayedTransition(parent as ViewGroup, transition)
-            maxLines = if (maxLines > collapsedLines) collapsedLines else Int.MAX_VALUE
+fun TextView.readMore(collapsedLines: Int) {
+    maxLines = collapsedLines
+    ellipsize = TextUtils.TruncateAt.END
+    setOnClickListener {
+        val transition = ChangeBounds().apply {
+            duration = 200
+            interpolator = FastOutSlowInInterpolator()
         }
+        TransitionManager.beginDelayedTransition(parent as ViewGroup, transition)
+        maxLines = if (maxLines > collapsedLines) collapsedLines else Int.MAX_VALUE
     }
 }
