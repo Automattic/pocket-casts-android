b08db7a13f004fd884bfe4de2c4f174f36b2e104
Michał Sikora <me@mehow.io>
2024-03-19 14:32:24 +0100

    Remove field qualifier from Json annotation

--- modules/services/servers/src/main/java/au/com/shiftyjelly/pocketcasts/servers/podcast/ShowNotesResponse.kt
+++ modules/services/servers/src/main/java/au/com/shiftyjelly/pocketcasts/servers/podcast/ShowNotesResponse.kt
@@ -5,7 +5,7 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class ShowNotesResponse(
-    @field:Json(name = "podcast") val podcast: ShowNotesPodcast?,
+    @Json(name = "podcast") val podcast: ShowNotesPodcast?,
 ) {
     fun findEpisode(episodeUuid: String): ShowNotesEpisode? {
         return podcast?.episodes?.find { it.uuid == episodeUuid }
@@ -14,13 +14,13 @@ data class ShowNotesResponse(
 
 @JsonClass(generateAdapter = true)
 data class ShowNotesPodcast(
-    @field:Json(name = "uuid") val uuid: String,
-    @field:Json(name = "episodes") val episodes: List<ShowNotesEpisode>?,
+    @Json(name = "uuid") val uuid: String,
+    @Json(name = "episodes") val episodes: List<ShowNotesEpisode>?,
 )
 
 @JsonClass(generateAdapter = true)
 data class ShowNotesEpisode(
-    @field:Json(name = "uuid") val uuid: String,
-    @field:Json(name = "show_notes") val showNotes: String?,
-    @field:Json(name = "image") val image: String?,
+    @Json(name = "uuid") val uuid: String,
+    @Json(name = "show_notes") val showNotes: String?,
+    @Json(name = "image") val image: String?,
 )
