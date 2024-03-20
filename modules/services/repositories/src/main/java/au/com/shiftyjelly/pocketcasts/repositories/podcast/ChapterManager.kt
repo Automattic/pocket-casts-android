1fb9979c9a3bde2045dcfa21f2a294c9f6b9a0d8
Michał Sikora <me@mehow.io>
2024-03-20 11:27:54 +0100

    Map database chapters to chapters

--- modules/services/repositories/src/main/java/au/com/shiftyjelly/pocketcasts/repositories/podcast/ChapterManager.kt
+++ modules/services/repositories/src/main/java/au/com/shiftyjelly/pocketcasts/repositories/podcast/ChapterManager.kt
@@ -1,7 +1,11 @@
 package au.com.shiftyjelly.pocketcasts.repositories.podcast
 
-import au.com.shiftyjelly.pocketcasts.models.to.DbChapter as Chapter
+import au.com.shiftyjelly.pocketcasts.models.to.Chapters
+import au.com.shiftyjelly.pocketcasts.models.to.DbChapter
+import kotlinx.coroutines.flow.Flow
 
 interface ChapterManager {
-    suspend fun updateChapters(episodeUuid: String, chapters: List<Chapter>)
+    suspend fun updateChapters(episodeUuid: String, chapters: List<DbChapter>)
+
+    fun observerChaptersForEpisode(episodeUuid: String): Flow<Chapters>
 }
