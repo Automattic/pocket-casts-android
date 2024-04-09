242a82256cb17d380d696523d9a54a3af1310289
Michał Sikora <me@mehow.io>
2024-04-09 12:27:06 +0200

    Add widget installation analytics (#2020)

--- modules/features/widgets/src/main/kotlin/au/com/shiftyjelly/pocketcasts/core/ui/widget/PodcastWidget.kt
+++ modules/features/widgets/src/main/kotlin/au/com/shiftyjelly/pocketcasts/core/ui/widget/PodcastWidget.kt
@@ -1,10 +1,10 @@
 package au.com.shiftyjelly.pocketcasts.core.ui.widget
 
-import androidx.glance.appwidget.GlanceAppWidget
-import androidx.glance.appwidget.GlanceAppWidgetReceiver
 import au.com.shiftyjelly.pocketcasts.widget.MediumPlayerWidget
+import au.com.shiftyjelly.pocketcasts.widget.PlayerWidgetReceiver
 
 //  This receiver has to be kept in this package due to legacy reasons. Old widget was defined here.
-internal class PodcastWidget : GlanceAppWidgetReceiver() {
-    override val glanceAppWidget: GlanceAppWidget = MediumPlayerWidget()
+internal class PodcastWidget : PlayerWidgetReceiver() {
+    override val glanceAppWidget = MediumPlayerWidget()
+    override val widgetTypeAnalyticsValue = "player_medium"
 }
