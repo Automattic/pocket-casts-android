0c185010e5af8fe427dbe0416a6de470bfc16014
Michał Sikora <me@mehow.io>
2024-04-17 10:35:20 +0200

    Do not clip playback controls in the large player header

--- modules/features/widgets/src/main/kotlin/au/com/shiftyjelly/pocketcasts/widget/ui/LargePlayerHeader.kt
+++ modules/features/widgets/src/main/kotlin/au/com/shiftyjelly/pocketcasts/widget/ui/LargePlayerHeader.kt
@@ -7,9 +7,11 @@ import androidx.glance.GlanceTheme
 import androidx.glance.LocalContext
 import androidx.glance.background
 import androidx.glance.layout.Alignment
+import androidx.glance.layout.Box
 import androidx.glance.layout.Column
 import androidx.glance.layout.Row
 import androidx.glance.layout.Spacer
+import androidx.glance.layout.fillMaxSize
 import androidx.glance.layout.fillMaxWidth
 import androidx.glance.layout.height
 import androidx.glance.layout.padding
@@ -25,58 +27,63 @@ internal fun LargePlayerHeader(
 ) {
     val episode = state.currentEpisode
 
-    Row(
-        verticalAlignment = Alignment.Top,
+    Box(
+        contentAlignment = Alignment.TopEnd,
         modifier = modifier
             .fillMaxWidth()
             .height(116.dp)
             .background(GlanceTheme.colors.primaryContainer),
-
     ) {
-        EpisodeImage(
-            episode = episode,
-            useEpisodeArtwork = state.useEpisodeArtwork,
-            size = 116.dp,
-        )
-        Spacer(
-            modifier = GlanceModifier.width(12.dp),
-        )
-        Column(
-            verticalAlignment = Alignment.Vertical.Top,
-            modifier = GlanceModifier.defaultWeight().height(116.dp),
+        Row(
+            verticalAlignment = Alignment.Top,
+            modifier = modifier.fillMaxSize(),
         ) {
-            Spacer(
-                modifier = GlanceModifier.height(4.dp),
-            )
-            NonScalingText(
-                text = LocalContext.current.getString(LR.string.player_tab_playing_wide),
-                textSize = 13.dp,
-                useDynamicColors = state.useDynamicColors,
-                alpha = 0.8,
-            )
-            NonScalingText(
-                text = episode?.title ?: LocalContext.current.getString(LR.string.widget_no_episode_playing),
-                textSize = 16.dp,
-                useDynamicColors = state.useDynamicColors,
-                isBold = true,
-                modifier = GlanceModifier.padding(end = 16.dp),
+            EpisodeImage(
+                episode = episode,
+                useEpisodeArtwork = state.useEpisodeArtwork,
+                size = 116.dp,
             )
             Spacer(
-                modifier = GlanceModifier.height(4.dp),
+                modifier = GlanceModifier.width(12.dp),
             )
-            NonScalingText(
-                text = episode?.getTimeLeft(LocalContext.current) ?: " ",
-                textSize = 13.dp,
-                useDynamicColors = state.useDynamicColors,
-                alpha = 0.8,
-            )
-            PlaybackControls(
-                isPlaying = state.isPlaying,
-                modifier = GlanceModifier.defaultWeight(),
-            )
-            Spacer(
-                modifier = GlanceModifier.height(4.dp),
-            )
+            Column(
+                verticalAlignment = Alignment.Vertical.Top,
+                modifier = GlanceModifier.defaultWeight().height(116.dp),
+            ) {
+                Spacer(
+                    modifier = GlanceModifier.height(4.dp),
+                )
+                NonScalingText(
+                    text = LocalContext.current.getString(LR.string.player_tab_playing_wide),
+                    textSize = 13.dp,
+                    useDynamicColors = state.useDynamicColors,
+                    alpha = 0.8,
+                    modifier = GlanceModifier.padding(end = 32.dp),
+                )
+                NonScalingText(
+                    text = episode?.title ?: LocalContext.current.getString(LR.string.widget_no_episode_playing),
+                    textSize = 16.dp,
+                    useDynamicColors = state.useDynamicColors,
+                    isBold = true,
+                    modifier = GlanceModifier.padding(end = 32.dp),
+                )
+                Spacer(
+                    modifier = GlanceModifier.height(4.dp),
+                )
+                NonScalingText(
+                    text = episode?.getTimeLeft(LocalContext.current) ?: " ",
+                    textSize = 13.dp,
+                    useDynamicColors = state.useDynamicColors,
+                    alpha = 0.8,
+                )
+                PlaybackControls(
+                    isPlaying = state.isPlaying,
+                    modifier = GlanceModifier.defaultWeight(),
+                )
+                Spacer(
+                    modifier = GlanceModifier.height(4.dp),
+                )
+            }
         }
         PocketCastsLogo()
     }
