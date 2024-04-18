2fee586abe4787c3fa1724282049d21f356c4b66
ashiagr <agr.ash@gmail.com>
2024-04-18 15:54:00 +0530

    Update parceler

--- modules/services/utils/src/main/java/au/com/shiftyjelly/pocketcasts/utils/parceler/DurationParceler.kt
+++ modules/services/utils/src/main/java/au/com/shiftyjelly/pocketcasts/utils/parceler/DurationParceler.kt
@@ -1,21 +1,20 @@
 package au.com.shiftyjelly.pocketcasts.utils.parceler
 
 import android.os.Parcel
-import kotlinx.parcelize.Parceler
 import kotlin.time.Duration
-import kotlin.time.Duration.Companion.seconds
+import kotlin.time.Duration.Companion.milliseconds
+import kotlinx.parcelize.Parceler
 
 object DurationParceler : Parceler<Duration?> {
-    override fun create(parcel: Parcel) =
-        if (parcel.dataSize() > 0) {
-            parcel.readLong().seconds
-        } else {
-            null
-        }
+    override fun create(parcel: Parcel): Duration? {
+        val isPresent = parcel.readByte() == 0.toByte()
+        return if (isPresent) parcel.readLong().milliseconds else null
+    }
 
     override fun Duration?.write(parcel: Parcel, flags: Int) {
+        parcel.writeByte(if (this != null) 1.toByte() else 0.toByte())
         if (this != null) {
-            parcel.writeLong(inWholeSeconds)
+            parcel.writeLong(inWholeMilliseconds)
         }
     }
 }
