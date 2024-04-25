068ccecc14676dd583002f42001dd330b2fe2aea
Michał Sikora <me@mehow.io>
2024-04-25 14:38:38 +0200

    Control granular downloads artwork

--- wear/src/main/kotlin/au/com/shiftyjelly/pocketcasts/wear/ui/downloads/DownloadsScreen.kt
+++ wear/src/main/kotlin/au/com/shiftyjelly/pocketcasts/wear/ui/downloads/DownloadsScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.wear.compose.foundation.lazy.items
 import au.com.shiftyjelly.pocketcasts.models.entity.PodcastEpisode
+import au.com.shiftyjelly.pocketcasts.preferences.model.ArtworkConfiguration.Element
 import au.com.shiftyjelly.pocketcasts.wear.theme.WearAppTheme
 import au.com.shiftyjelly.pocketcasts.wear.ui.component.EpisodeChip
 import au.com.shiftyjelly.pocketcasts.wear.ui.component.ScreenHeaderChip
@@ -29,7 +30,7 @@ fun DownloadsScreen(
     val state by viewModel.stateFlow.collectAsState()
     val artworkConfiguration by viewModel.artworkConfiguration.collectAsState()
 
-    Content(columnState, state, artworkConfiguration.useEpisodeArtwork, onItemClick)
+    Content(columnState, state, artworkConfiguration.useEpisodeArtwork(Element.Downloads), onItemClick)
 }
 
 @Composable
