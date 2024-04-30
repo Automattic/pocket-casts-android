b0bb30eb0c2396dd9916f5075b9b027650b9d5b8
Michał Sikora <me@mehow.io>
2024-04-30 19:14:39 +0200

    Fetch new episodes for Nova Launcher

--- modules/services/repositories/src/main/java/au/com/shiftyjelly/pocketcasts/repositories/nova/NovaLauncherManager.kt
+++ modules/services/repositories/src/main/java/au/com/shiftyjelly/pocketcasts/repositories/nova/NovaLauncherManager.kt
@@ -1,7 +1,9 @@
 package au.com.shiftyjelly.pocketcasts.repositories.nova
 
+import au.com.shiftyjelly.pocketcasts.models.entity.NovaLauncherNewEpisode
 import au.com.shiftyjelly.pocketcasts.models.entity.NovaLauncherSubscribedPodcast
 
 interface NovaLauncherManager {
     suspend fun getSubscribedPodcasts(): List<NovaLauncherSubscribedPodcast>
+    suspend fun getNewEpisodes(): List<NovaLauncherNewEpisode>
 }
