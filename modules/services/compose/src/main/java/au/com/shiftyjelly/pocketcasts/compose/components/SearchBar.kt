c754dd2b92d631c8c2cd8bc1e2754f1a9eac76e1
ashiagr <agr.ash@gmail.com>
2024-05-01 14:01:07 +0530

    Changes to reduce search bar height

--- modules/services/compose/src/main/java/au/com/shiftyjelly/pocketcasts/compose/components/SearchBar.kt
+++ modules/services/compose/src/main/java/au/com/shiftyjelly/pocketcasts/compose/components/SearchBar.kt
@@ -1,21 +1,26 @@
 package au.com.shiftyjelly.pocketcasts.compose.components
 
 import android.view.KeyEvent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.key.onKeyEvent
@@ -28,6 +33,7 @@ import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.onClick
 import androidx.compose.ui.semantics.role
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -38,6 +44,7 @@ import au.com.shiftyjelly.pocketcasts.utils.extensions.removeNewLines
 import au.com.shiftyjelly.pocketcasts.images.R as IR
 import au.com.shiftyjelly.pocketcasts.localization.R as LR
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun SearchBar(
     text: String,
@@ -48,34 +55,27 @@ fun SearchBar(
     enabled: Boolean = true,
 ) {
     val focusManager = LocalFocusManager.current
-    OutlinedTextField(
+    val colors = TextFieldDefaults.outlinedTextFieldColors(
+        cursorColor = MaterialTheme.theme.colors.primaryText02,
+        textColor = MaterialTheme.theme.colors.primaryText01,
+        disabledTextColor = MaterialTheme.theme.colors.primaryText01,
+        placeholderColor = MaterialTheme.theme.colors.primaryText02,
+        disabledPlaceholderColor = MaterialTheme.theme.colors.primaryText02,
+        leadingIconColor = MaterialTheme.theme.colors.primaryIcon02,
+        disabledLeadingIconColor = MaterialTheme.theme.colors.primaryIcon02,
+        trailingIconColor = MaterialTheme.theme.colors.primaryIcon02,
+        disabledTrailingIconColor = MaterialTheme.theme.colors.primaryIcon02,
+        backgroundColor = MaterialTheme.theme.colors.primaryField01,
+        focusedBorderColor = Color.Transparent,
+        unfocusedBorderColor = Color.Transparent,
+        disabledBorderColor = Color.Transparent,
+    )
+    val shape = RoundedCornerShape(10.dp)
+    BasicTextField(
         value = text,
         onValueChange = {
             onTextChanged(it.removeNewLines())
         },
-        placeholder = {
-            Text(
-                placeholder,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis,
-            )
-        },
-        colors = TextFieldDefaults.outlinedTextFieldColors(
-            cursorColor = MaterialTheme.theme.colors.primaryText02,
-            textColor = MaterialTheme.theme.colors.primaryText01,
-            disabledTextColor = MaterialTheme.theme.colors.primaryText01,
-            placeholderColor = MaterialTheme.theme.colors.primaryText02,
-            disabledPlaceholderColor = MaterialTheme.theme.colors.primaryText02,
-            leadingIconColor = MaterialTheme.theme.colors.primaryIcon02,
-            disabledLeadingIconColor = MaterialTheme.theme.colors.primaryIcon02,
-            trailingIconColor = MaterialTheme.theme.colors.primaryIcon02,
-            disabledTrailingIconColor = MaterialTheme.theme.colors.primaryIcon02,
-            backgroundColor = MaterialTheme.theme.colors.primaryField01,
-            focusedBorderColor = Color.Transparent,
-            unfocusedBorderColor = Color.Transparent,
-            disabledBorderColor = Color.Transparent,
-        ),
-        shape = RoundedCornerShape(10.dp),
         keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
         keyboardActions = KeyboardActions(
             onSearch = {
@@ -84,36 +84,64 @@ fun SearchBar(
             },
         ),
         maxLines = 1,
-        leadingIcon = {
-            Icon(
-                painter = painterResource(IR.drawable.ic_search),
-                contentDescription = null,
-            )
-        },
-        trailingIcon = {
-            if (text.isNotEmpty()) {
-                IconButton(
-                    onClick = {
-                        onTextChanged("")
-                        focusManager.clearFocus()
-                    },
-                ) {
-                    Icon(
-                        painter = painterResource(IR.drawable.ic_cancel),
-                        contentDescription = stringResource(LR.string.cancel),
-                    )
+        enabled = enabled,
+        modifier = modifier
+            .onKeyEvent {
+                // close the keyboard on enter
+                if (it.nativeKeyEvent.keyCode == KeyEvent.KEYCODE_ENTER) {
+                    focusManager.clearFocus()
+                    true
+                } else {
+                    false
                 }
             }
-        },
-        enabled = enabled,
-        modifier = modifier.onKeyEvent {
-            // close the keyboard on enter
-            if (it.nativeKeyEvent.keyCode == KeyEvent.KEYCODE_ENTER) {
-                focusManager.clearFocus()
-                true
-            } else {
-                false
-            }
+            .background(colors.backgroundColor(enabled).value, shape)
+            .defaultMinSize(
+                minWidth = TextFieldDefaults.MinWidth,
+                minHeight = 50.dp,
+            ),
+        decorationBox = @Composable { innerTextField ->
+            TextFieldDefaults.OutlinedTextFieldDecorationBox(
+                value = text,
+                innerTextField = innerTextField,
+                placeholder = {
+                    Text(
+                        placeholder,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                    )
+                },
+                leadingIcon = {
+                    Icon(
+                        painter = painterResource(IR.drawable.ic_search),
+                        contentDescription = null,
+                    )
+                },
+                trailingIcon = {
+                    if (text.isNotEmpty()) {
+                        IconButton(
+                            onClick = {
+                                onTextChanged("")
+                                focusManager.clearFocus()
+                            },
+                        ) {
+                            Icon(
+                                painter = painterResource(IR.drawable.ic_cancel),
+                                contentDescription = stringResource(LR.string.cancel),
+                            )
+                        }
+                    }
+                },
+                enabled = enabled,
+                colors = colors,
+                singleLine = true,
+                interactionSource = remember { MutableInteractionSource() },
+                visualTransformation = VisualTransformation.None,
+                contentPadding = TextFieldDefaults.textFieldWithoutLabelPadding(
+                    top = 0.dp,
+                    bottom = 0.dp,
+                ),
+            )
         },
     )
 }
