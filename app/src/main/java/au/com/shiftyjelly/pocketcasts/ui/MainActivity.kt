ba27c088dc2045b23396b9a54fbff2e1b9a0d2bb
Ashita Agrawal <agr.ash@gmail.com>
2024-05-01 18:03:08 +0530

    Add empty state mini player (#2143)

--- app/src/main/java/au/com/shiftyjelly/pocketcasts/ui/MainActivity.kt
+++ app/src/main/java/au/com/shiftyjelly/pocketcasts/ui/MainActivity.kt
@@ -394,7 +394,11 @@ class MainActivity :
             activity = this,
         )
 
-        val showMiniPlayerImmediately = savedInstanceState?.getBoolean(SAVEDSTATE_MINIPLAYER_SHOWN, false) ?: false
+        val showMiniPlayerImmediately = if (!FeatureFlag.isEnabled(Feature.UPNEXT_IN_TAB_BAR)) {
+            savedInstanceState?.getBoolean(SAVEDSTATE_MINIPLAYER_SHOWN, false) ?: false
+        } else {
+            true
+        }
         binding.playerBottomSheet.isVisible = showMiniPlayerImmediately
 
         setupPlayerViews(showMiniPlayerImmediately)
@@ -532,7 +536,9 @@ class MainActivity :
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         outState.putBoolean(SAVEDSTATE_PLAYER_OPEN, binding.playerBottomSheet.isPlayerOpen)
-        outState.putBoolean(SAVEDSTATE_MINIPLAYER_SHOWN, binding.playerBottomSheet.isShown)
+        if (!FeatureFlag.isEnabled(Feature.UPNEXT_IN_TAB_BAR)) {
+            outState.putBoolean(SAVEDSTATE_MINIPLAYER_SHOWN, binding.playerBottomSheet.isShown)
+        }
     }
 
     override fun overrideNextRefreshTimer() {
@@ -917,8 +923,9 @@ class MainActivity :
                     supportFragmentManager.findFragmentByTag(bottomSheetTag)?.let {
                         removeBottomSheetFragment(it)
                     }
-
-                    binding.playerBottomSheet.isDragEnabled = true
+                    if (!FeatureFlag.isEnabled(Feature.UPNEXT_IN_TAB_BAR) || !playbackManager.upNextQueue.isEmpty) {
+                        binding.playerBottomSheet.isDragEnabled = true
+                    }
 
                     updateNavAndStatusColors(playerOpen = viewModel.isPlayerOpen, viewModel.lastPlaybackState?.podcast)
                 } else {
@@ -979,9 +986,11 @@ class MainActivity :
 
         // Handle up next shortcut
         if (intent.getStringExtra(INTENT_EXTRA_PAGE) == "upnext") {
-            intent.putExtra(INTENT_EXTRA_PAGE, null as String?)
-            binding.playerBottomSheet.openPlayer()
-            showUpNextFragment(UpNextSource.UP_NEXT_SHORTCUT)
+            if (!playbackManager.upNextQueue.isEmpty) {
+                intent.putExtra(INTENT_EXTRA_PAGE, null as String?)
+                binding.playerBottomSheet.openPlayer()
+                showUpNextFragment(UpNextSource.UP_NEXT_SHORTCUT)
+            }
         }
     }
 
