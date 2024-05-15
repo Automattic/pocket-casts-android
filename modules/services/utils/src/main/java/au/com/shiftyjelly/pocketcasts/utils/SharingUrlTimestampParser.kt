9768fe10985043986f5bca2cb9768e2ebdb04c3d
Eduarda Barbosa <meduarda.qb@gmail.com>
2024-05-15 12:10:43 -0300

    parse hours minutes and seconds timestamp pattern

--- modules/services/utils/src/main/java/au/com/shiftyjelly/pocketcasts/utils/SharingUrlTimestampParser.kt
+++ modules/services/utils/src/main/java/au/com/shiftyjelly/pocketcasts/utils/SharingUrlTimestampParser.kt
@@ -6,6 +6,15 @@ import kotlin.time.Duration.Companion.seconds
 class SharingUrlTimestampParser {
     fun parseTimestamp(t: String): Pair<Duration?, Duration?> {
         val intervalPattern = Regex("""(\d*\.?\d*)?,?(\d*\.?\d*)?""") // t=10,20 or t=,20 or t=10
+        val hmsPattern = Regex("""(?:(\d+)h)?(?:(\d+)m)?(?:(\d+(?:\.\d+)?)s)?""") // t=10h20m30s or t=10m30s or or 10m or t=10s
+
+        hmsPattern.matchEntire(t)?.let { match ->
+            val hours = match.groups[1]?.value?.toInt() ?: 0
+            val minutes = match.groups[2]?.value?.toInt() ?: 0
+            val seconds = match.groups[3]?.value?.toDouble() ?: 0.0
+            val totalSeconds = ((hours * 3600) + (minutes * 60) + seconds)
+            return Pair(totalSeconds.seconds, null)
+        }
 
         intervalPattern.find(t)?.let { match ->
             val startTime = match.groups[1]?.value?.takeIf { it.isNotEmpty() }?.toDouble()?.toInt() ?: 0
--- modules/services/utils/src/test/java/au/com/shiftyjelly/pocketcasts/utils/SharingUrlTimestampParserTest.kt
+++ modules/services/utils/src/test/java/au/com/shiftyjelly/pocketcasts/utils/SharingUrlTimestampParserTest.kt
@@ -14,4 +14,13 @@ class SharingUrlTimestampParserTest {
         assertEquals(Pair(0.seconds, 20.seconds), parser.parseTimestamp(",20"))
         assertEquals(Pair(10.seconds, 0.seconds), parser.parseTimestamp("10"))
     }
+
+    @Test
+    fun testParseTimestamp_hms() {
+        assertEquals(Pair(454070.seconds, null), parser.parseTimestamp("125h67m50s"))
+        assertEquals(Pair(3600.seconds, null), parser.parseTimestamp("1h"))
+        assertEquals(Pair(60.seconds, null), parser.parseTimestamp("1m"))
+        assertEquals(Pair(1.seconds, null), parser.parseTimestamp("1s"))
+        assertEquals(Pair(3661.seconds, null), parser.parseTimestamp("1h1m1s"))
+    }
 }
