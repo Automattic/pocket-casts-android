437797db67a2c3e88dc17d17d68f86a10d828477
Michał Sikora <me@mehow.io>
2024-05-21 08:10:45 +0200

    Refactor timestamp parser

--- modules/services/utils/src/main/java/au/com/shiftyjelly/pocketcasts/utils/SharingUrlTimestampParser.kt
+++ modules/services/utils/src/main/java/au/com/shiftyjelly/pocketcasts/utils/SharingUrlTimestampParser.kt
@@ -1,58 +1,57 @@
 package au.com.shiftyjelly.pocketcasts.utils
 
 import kotlin.time.Duration
+import kotlin.time.Duration.Companion.hours
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.minutes
 import kotlin.time.Duration.Companion.seconds
 
 class SharingUrlTimestampParser {
     companion object {
-        val intervalPattern = Regex("""(\d*\.?\d*)?,?(\d*\.?\d*)?""")
-        val hmsPattern = Regex("""(?:(\d+)h)?(?:(0?[0-5]?\d)m)?(?:(0?[0-5]?\d)s)?""")
-        val hhmmssFractionPattern = Regex("""(?:(\d+):)?(0[0-9]|[1-5][0-9]):(0[0-9]|[1-5][0-9])(?:\.(\d+))?""")
+        val intervalPattern = Regex("""^(\d+\.?\d*)$""")
+        val hmsPattern = Regex("""^(?:(\d+)h)?(?:(0?[0-5]?\d)m)?(?:(0?[0-5]?\d)s)?$""")
+        val hhmmssFractionPattern = Regex("""^(?:(\d+):)?(0[0-9]|[1-5][0-9]):(0[0-9]|[1-5][0-9])(?:\.(\d+))?$""")
     }
 
-    fun parseTimestamp(timestamp: String): Pair<Duration?, Duration?> {
-        if (timestamp.contains(",")) {
-            val (part1, part2) = timestamp.split(",")
-            val startParsedTime: Pair<Double?, Double?> = parseTime(part1)
-            val endParsedTime: Pair<Double?, Double?> = parseTime(part2)
-            return Pair(extractTimeFrom(startParsedTime)?.seconds, extractTimeFrom(endParsedTime)?.seconds)
+    fun parseTimestamp(timestamp: String): Pair<Duration?, Duration?>? {
+        val splitTimestamps = timestamp.split(",")
+        return when (splitTimestamps.size) {
+            1 -> timestamp.toDuration() to null
+            2 -> {
+                val (rawStart, rawEnd) = timestamp.split(",")
+                val start = if (rawStart.isBlank()) {
+                    Duration.ZERO
+                } else {
+                    rawStart.toDuration()
+                }
+                val end = rawEnd.toDuration()
+                start to end
+            }
+            else -> null
         }
-        val result = parseTime(timestamp)
-        return Pair(result.first?.seconds, result.second?.seconds)
     }
 
-    private fun parseTime(t: String): Pair<Double?, Double?> {
-        hmsPattern.matchEntire(t)?.let { match ->
-            val hours = match.groups[1]?.value?.toInt() ?: 0
-            val minutes = match.groups[2]?.value?.toInt() ?: 0
-            val seconds = match.groups[3]?.value?.toDouble() ?: 0.0
-            val totalSeconds = ((hours * 3600) + (minutes * 60) + seconds)
-            return Pair(totalSeconds, null)
+    private fun String.toDuration(): Duration? {
+        hmsPattern.matchEntire(this)?.let { match ->
+            val h = match.groups[1]?.value?.toLongOrNull() ?: 0
+            val m = match.groups[2]?.value?.toLongOrNull() ?: 0
+            val s = match.groups[3]?.value?.toDoubleOrNull() ?: 0.0
+            return h.hours + m.minutes + s.seconds
         }
 
-        hhmmssFractionPattern.matchEntire(t)?.let { match ->
-            val hours = match.groups[1]?.value?.toLong() ?: 0
-            val minutes = match.groups[2]?.value?.toLong() ?: 0
-            val seconds = match.groups[3]?.value?.toLong() ?: 0
-            val fractionInMilliseconds = match.groups[4]?.value?.take(3)?.toDouble() ?: 0.0 // Extract first three digits
-            val totalSeconds = (hours * 3600) + (minutes * 60) + seconds + (fractionInMilliseconds / 1000.0)
-            return Pair(totalSeconds, null)
+        hhmmssFractionPattern.matchEntire(this)?.let { match ->
+            val hh = match.groups[1]?.value?.toLongOrNull() ?: 0
+            val mm = match.groups[2]?.value?.toLongOrNull() ?: 0
+            val ss = match.groups[3]?.value?.toLongOrNull() ?: 0
+            val fraction = match.groups[4]?.value?.take(3)?.toDoubleOrNull() ?: 0.0
+            return hh.hours + mm.minutes + ss.seconds + fraction.milliseconds
         }
 
-        intervalPattern.find(t)?.let { match ->
-            val startTime = match.groups[1]?.value?.takeIf { it.isNotEmpty() }?.toDouble()?.toInt()?.takeIf { it != 0 }
-            val endTime = match.groups[2]?.value?.takeIf { it.isNotEmpty() }?.toDouble()?.toInt()?.takeIf { it != 0 }
-            return Pair(startTime?.toDouble(), endTime?.toDouble())
+        intervalPattern.matchEntire(this)?.let { match ->
+            val interval = match.groups[1]?.value?.toDoubleOrNull() ?: 0.0
+            return interval.seconds
         }
 
-        return Pair(null, null)
-    }
-
-    private fun extractTimeFrom(startParsedTime: Pair<Double?, Double?>) = if (startParsedTime.first == null && startParsedTime.second == null) {
-        null
-    } else if (startParsedTime.first != null) {
-        startParsedTime.first
-    } else {
-        startParsedTime.second
+        return null
     }
 }
