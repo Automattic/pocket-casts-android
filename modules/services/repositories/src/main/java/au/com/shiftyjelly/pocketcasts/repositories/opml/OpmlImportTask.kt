351b936c9849fc6c06a5c9b79ab0948978f06850
Eduarda Barbosa <meduarda.qb@gmail.com>
2024-05-24 07:32:21 -0300

    add unit tests for pre process opml file task

--- modules/services/repositories/src/main/java/au/com/shiftyjelly/pocketcasts/repositories/opml/OpmlImportTask.kt
+++ modules/services/repositories/src/main/java/au/com/shiftyjelly/pocketcasts/repositories/opml/OpmlImportTask.kt
@@ -28,14 +28,7 @@ import au.com.shiftyjelly.pocketcasts.servers.refresh.RefreshServerManager
 import au.com.shiftyjelly.pocketcasts.utils.log.LogBuffer
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
-import java.io.BufferedReader
-import java.io.BufferedWriter
-import java.io.File
-import java.io.FileInputStream
-import java.io.FileOutputStream
 import java.io.InputStream
-import java.io.InputStreamReader
-import java.io.OutputStreamWriter
 import java.io.StringReader
 import java.net.URL
 import java.util.Scanner
@@ -235,7 +228,7 @@ class OpmlImportTask @AssistedInject constructor(
         val resolver = applicationContext.contentResolver
         try {
             resolver.openInputStream(uri)?.use { inputStream ->
-                val modifiedInputStream = replaceInvalidXmlCharacter(inputStream)
+                val modifiedInputStream = PreProcessOpmlFile().replaceInvalidXmlCharacter(inputStream)
                 urls = readOpmlUrlsSax(modifiedInputStream)
             }
         } catch (e: SAXException) {
@@ -246,37 +239,6 @@ class OpmlImportTask @AssistedInject constructor(
 
         processUrls(urls)
         return urls.size
-    }
-
-    private fun replaceInvalidXmlCharacter(inputStream: InputStream): InputStream {
-        val tempFile = File.createTempFile("output", ".xml")
-        tempFile.deleteOnExit()
-
-        BufferedReader(InputStreamReader(inputStream, Charsets.UTF_8)).use { reader ->
-            BufferedWriter(OutputStreamWriter(FileOutputStream(tempFile), Charsets.UTF_8)).use { writer ->
-                reader.lineSequence().forEach { line ->
-                    val modifiedLine = line
-                        .replace("<outline", "\n<outline")
-                        .replace("&(?!amp;|quot;|gt;|lt;)".toRegex(), "&amp;")
-                        .replace("’", "&apos;")
-
-                    val textRegex = """(?<=text=")(.*?)(?="\s*(?:/>|\s+xmlUrl|\s+type))""".toRegex()
-                    val finalLine = textRegex.replace(modifiedLine) { matchResult ->
-                        val text = matchResult.groupValues[1]
-                        val fixedText = text
-                            .replace("\"", "&quot;")
-                            .replace(">", "&gt;")
-                            .replace("<", "&lt;")
-                        matchResult.value.replace(text, fixedText)
-                    }
-
-                    writer.write(finalLine)
-                    writer.newLine()
-                }
-            }
-        }
-
-        return FileInputStream(tempFile)
     }
 
     private suspend fun processUrls(urls: List<String>) {
