eb44f25f273584ce0f0d994eecccfb2ae731580a
Eduarda Barbosa <meduarda.qb@gmail.com>
2024-06-05 13:03:12 -0300

    migrate ChangePwdFragment to jetpack compose

--- modules/features/account/src/main/java/au/com/shiftyjelly/pocketcasts/account/ChangePwdFragment.kt
+++ modules/features/account/src/main/java/au/com/shiftyjelly/pocketcasts/account/ChangePwdFragment.kt
@@ -1,40 +1,26 @@
 package au.com.shiftyjelly.pocketcasts.account
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.WindowManager
-import android.view.inputmethod.EditorInfo
-import androidx.core.view.isVisible
-import androidx.core.view.updatePadding
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
-import au.com.shiftyjelly.pocketcasts.account.AccountActivity.AccountUpdatedSource
-import au.com.shiftyjelly.pocketcasts.account.databinding.FragmentChangePwdBinding
-import au.com.shiftyjelly.pocketcasts.account.viewmodel.ChangePasswordError
-import au.com.shiftyjelly.pocketcasts.account.viewmodel.ChangePasswordState
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import au.com.shiftyjelly.pocketcasts.account.viewmodel.ChangePwdViewModel
 import au.com.shiftyjelly.pocketcasts.account.viewmodel.DoneViewModel
+import au.com.shiftyjelly.pocketcasts.compose.AppThemeWithBackground
 import au.com.shiftyjelly.pocketcasts.preferences.Settings
-import au.com.shiftyjelly.pocketcasts.ui.extensions.getThemeColor
-import au.com.shiftyjelly.pocketcasts.ui.extensions.getTintedDrawable
 import au.com.shiftyjelly.pocketcasts.ui.helper.FragmentHostListener
-import au.com.shiftyjelly.pocketcasts.utils.extensions.dpToPx
-import au.com.shiftyjelly.pocketcasts.views.extensions.addOnTextChanged
+import au.com.shiftyjelly.pocketcasts.utils.extensions.pxToDp
 import au.com.shiftyjelly.pocketcasts.views.fragments.BaseFragment
-import au.com.shiftyjelly.pocketcasts.views.helper.NavigationIcon
-import au.com.shiftyjelly.pocketcasts.views.helper.UiUtil
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
-import kotlinx.coroutines.launch
-import au.com.shiftyjelly.pocketcasts.images.R as IR
 import au.com.shiftyjelly.pocketcasts.localization.R as LR
-import au.com.shiftyjelly.pocketcasts.ui.R as UR
 
 @AndroidEntryPoint
 class ChangePwdFragment : BaseFragment() {
@@ -44,166 +30,44 @@ class ChangePwdFragment : BaseFragment() {
         }
     }
 
-    @Inject lateinit var settings: Settings
+    @Inject
+    lateinit var settings: Settings
 
-    private val viewModel: ChangePwdViewModel by viewModels()
     private val doneViewModel: DoneViewModel by activityViewModels()
-    private var binding: FragmentChangePwdBinding? = null
+    private val viewModel: ChangePwdViewModel by viewModels()
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        binding = FragmentChangePwdBinding.inflate(inflater, container, false)
-        return binding?.root
-    }
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                val bottomOffset by settings.bottomInset.collectAsStateWithLifecycle(initialValue = 0)
+                AppThemeWithBackground(theme.activeTheme) {
+                    ChangePasswordPage(
+                        viewModel = viewModel,
+                        onBackPressed = {
+                            @Suppress("DEPRECATION")
+                            activity?.onBackPressed()
+                        },
+                        changePassword = {
+                            viewModel.changePassword()
+                        },
+                        onSuccess = {
+                            doneViewModel.setChangedPasswordState(detail = getString(LR.string.profile_password_changed_successful))
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        binding = null
-    }
+                            doneViewModel.trackShown(AccountActivity.AccountUpdatedSource.CHANGE_PASSWORD)
 
-    @Suppress("DEPRECATION")
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        // hack: enable scrolling upon keyboard
-        activity?.window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
-    }
-
-    override fun onDetach() {
-        super.onDetach()
-        // hack: enable scrolling upon keyboard
-        activity?.window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN)
+                            val fragment = ChangeDoneFragment.newInstance(closeParent = true)
+                            (activity as FragmentHostListener).addFragment(fragment)
+                        },
+                        bottomOffset = bottomOffset.pxToDp(LocalContext.current).dp,
+                    )
+                }
+            }
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        val binding = binding ?: return
-        val toolbar = binding.toolbar ?: return
-        val progress = binding.progress
-        val txtPwdCurrent = binding.txtPwdCurrent
-        val txtPwdNew = binding.txtPwdNew
-        val txtPwdConfirm = binding.txtPwdConfirm
-        val txtError = binding.txtError
-        val btnConfirm = binding.btnConfirm
-
-        setupToolbarAndStatusBar(
-            toolbar = toolbar,
-            title = getString(LR.string.profile_change_password_title),
-            navigationIcon = NavigationIcon.BackArrow,
-        )
-
-        progress.isVisible = false
-        txtPwdCurrent.requestFocus()
-
         viewModel.clearValues()
-
-        txtPwdCurrent.setText(viewModel.pwdCurrent.value?.toString())
-        txtPwdCurrent.setOnEditorActionListener { _, actionId, _ ->
-            if (actionId == EditorInfo.IME_ACTION_DONE) {
-                txtPwdNew.requestFocus()
-                return@setOnEditorActionListener true
-            }
-            false
-        }
-
-        txtPwdNew.setText(viewModel.pwdCurrent.value?.toString())
-        txtPwdNew.setOnEditorActionListener { _, actionId, _ ->
-            if (actionId == EditorInfo.IME_ACTION_DONE) {
-                txtPwdConfirm.requestFocus()
-                return@setOnEditorActionListener true
-            }
-            false
-        }
-
-        txtPwdConfirm.setText(viewModel.pwdCurrent.value?.toString())
-
-        txtPwdCurrent.addOnTextChanged {
-            viewModel.updatePwdCurrent(it)
-        }
-
-        txtPwdNew.addOnTextChanged {
-            viewModel.updatePwdNew(it)
-        }
-
-        txtPwdConfirm.addOnTextChanged {
-            viewModel.updatePwdConfirm(it)
-        }
-
-        viewModel.changePasswordState.observe(viewLifecycleOwner) {
-            when (it) {
-                is ChangePasswordState.Empty -> {
-                    updateForm(invalidPwdCurrent = false, invalidPwdNew = false, invalidPwdConfirm = false)
-                }
-                is ChangePasswordState.Failure -> {
-                    progress.isVisible = false
-
-                    val invalidPwdCurrent = it.errors.contains(ChangePasswordError.INVALID_PASSWORD_CURRENT)
-                    val invalidPwdNew = it.errors.contains(ChangePasswordError.INVALID_PASSWORD_NEW)
-                    val invalidPwdConfirm = it.errors.contains(ChangePasswordError.INVALID_PASSWORD_CONFIRM)
-                    val serverFail = it.errors.contains(ChangePasswordError.SERVER)
-
-                    updateForm(invalidPwdCurrent, invalidPwdNew, invalidPwdConfirm)
-
-                    if (serverFail) {
-                        txtError.text = getString(LR.string.server_login_password_change_failed)
-                        viewModel.clearServerError()
-                    }
-                }
-                is ChangePasswordState.Loading -> {
-                    txtError.text = ""
-                    progress.isVisible = true
-                }
-                is ChangePasswordState.Success -> {
-                    progress.isVisible = false
-
-                    doneViewModel.setChangedPasswordState(detail = getString(LR.string.profile_password_changed_successful))
-
-                    doneViewModel.trackShown(AccountUpdatedSource.CHANGE_PASSWORD)
-
-                    val fragment = ChangeDoneFragment.newInstance(closeParent = true)
-                    (activity as FragmentHostListener).addFragment(fragment)
-                }
-            }
-        }
-
-        btnConfirm.setOnClickListener {
-            progress.isVisible = true
-            UiUtil.hideKeyboard(txtPwdConfirm)
-            viewModel.changePassword()
-        }
-
-        viewLifecycleOwner.lifecycleScope.launch {
-            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                settings.bottomInset.collect { bottomInset ->
-                    view.updatePadding(bottom = bottomInset)
-                }
-            }
-        }
-    }
-
-    override fun onPause() {
-        super.onPause()
-
-        val binding = binding ?: return
-        UiUtil.hideKeyboard(binding.root)
-    }
-
-    private fun updateForm(invalidPwdCurrent: Boolean, invalidPwdNew: Boolean, invalidPwdConfirm: Boolean) {
-        val binding = binding ?: return
-
-        val view = binding.root
-        val context = view.context
-
-        val allMatch = !invalidPwdCurrent && !invalidPwdNew && !invalidPwdConfirm
-        binding.btnConfirm.isEnabled = allMatch
-        binding.btnConfirm.alpha = if (allMatch) 1.0f else 0.2f
-
-        val tintColor = context.getThemeColor(UR.attr.primary_interactive_01)
-        val passwordDrawable = context.getTintedDrawable(IR.drawable.ic_password, tintColor)
-        val iconSize = 32.dpToPx(context)
-        passwordDrawable?.setBounds(0, 0, iconSize, iconSize)
-
-        binding.txtPwdCurrent.setCompoundDrawablesRelative(passwordDrawable, null, null, null)
-        binding.txtPwdNew.setCompoundDrawablesRelative(passwordDrawable, null, null, null)
-        binding.txtPwdConfirm.setCompoundDrawablesRelative(passwordDrawable, null, null, null)
+        viewModel.clearServerError()
     }
 }
