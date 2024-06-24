abaca2e78dfcaf3e450358129fa93693dcefa4e4
ashiagr <agr.ash@gmail.com>
2024-06-24 13:11:04 +0530

    Update feature default value to true
    It is still guarded by advanced setting so its safe to enable the feature.

--- modules/services/utils/src/main/java/au/com/shiftyjelly/pocketcasts/utils/featureflag/Feature.kt
+++ modules/services/utils/src/main/java/au/com/shiftyjelly/pocketcasts/utils/featureflag/Feature.kt
@@ -118,7 +118,7 @@ enum class Feature(
     CACHE_ENTIRE_PLAYING_EPISODE(
         key = "cache_entire_playing_episode",
         title = "Cache entire playing episode",
-        defaultValue = BuildConfig.DEBUG,
+        defaultValue = true,
         tier = FeatureTier.Free,
         hasFirebaseRemoteFlag = true,
         hasDevToggle = true,
