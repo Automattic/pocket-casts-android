7b667c2a683367d0aa4a2b9237dbbd44db60de23
Michał Sikora <me@mehow.io>
2024-06-27 15:30:49 +0200

    Merge/release 7.67 into main (#2408)
    Co-authored-by: Ashita Agrawal <agr.ash@gmail.com>
    Co-authored-by: Eduarda Barbosa <42220351+mebarbosa@users.noreply.github.com>

--- modules/services/utils/src/main/java/au/com/shiftyjelly/pocketcasts/utils/config/FirebaseConfig.kt
+++ modules/services/utils/src/main/java/au/com/shiftyjelly/pocketcasts/utils/config/FirebaseConfig.kt
@@ -15,7 +15,9 @@ object FirebaseConfig {
     const val REFRESH_PODCASTS_BATCH_SIZE = "refresh_podcasts_batch_size"
     const val EXOPLAYER_CACHE_SIZE_IN_MB = "exoplayer_cache_size_in_mb"
     const val EXOPLAYER_CACHE_ENTIRE_PLAYING_EPISODE_SIZE_IN_MB = "exoplayer_cache_entire_playing_episode_size_in_mb"
+    const val EXOPLAYER_CACHE_ENTIRE_PLAYING_EPISODE_SETTING_DEFAULT = "exoplayer_cache_entire_playing_episode_setting_default"
     const val PLAYBACK_EPISODE_POSITION_CHANGED_ON_SYNC_THRESHOLD_SECS = "playback_episode_position_changed_on_sync_threshold_secs"
+
     val defaults = mapOf(
         PERIODIC_SAVE_TIME_MS to 60000L,
         PLAYER_RELEASE_TIME_OUT_MS to 500L,
@@ -26,6 +28,7 @@ object FirebaseConfig {
         REFRESH_PODCASTS_BATCH_SIZE to 200L,
         EXOPLAYER_CACHE_SIZE_IN_MB to if (BuildConfig.DEBUG) 100L else 0L,
         EXOPLAYER_CACHE_ENTIRE_PLAYING_EPISODE_SIZE_IN_MB to if (BuildConfig.DEBUG) 500L else 0L,
+        EXOPLAYER_CACHE_ENTIRE_PLAYING_EPISODE_SETTING_DEFAULT to true,
         PLAYBACK_EPISODE_POSITION_CHANGED_ON_SYNC_THRESHOLD_SECS to 5L,
     ) + Feature.values()
         .filter { it.hasFirebaseRemoteFlag }
