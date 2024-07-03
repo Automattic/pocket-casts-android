70bcda97469697053f5867cba385bd3779bcba4c
Ashita Agrawal <agr.ash@gmail.com>
2024-07-03 09:48:53 +0530

    Transcripts -  Integrate (temporary) UI container to show transcript in the Player (#2425)

--- modules/services/model/src/main/java/au/com/shiftyjelly/pocketcasts/models/db/dao/TranscriptDao.kt
+++ modules/services/model/src/main/java/au/com/shiftyjelly/pocketcasts/models/db/dao/TranscriptDao.kt
@@ -5,11 +5,15 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import au.com.shiftyjelly.pocketcasts.models.to.Transcript
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 abstract class TranscriptDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     abstract fun insert(transcript: Transcript)
+
+    @Query("SELECT * FROM episode_transcript WHERE episode_uuid IS :episodeUuid")
+    abstract fun observerTranscriptForEpisode(episodeUuid: String): Flow<Transcript?>
 
     @Query("DELETE FROM episode_transcript WHERE episode_uuid IS :episodeUuid")
     abstract suspend fun deleteForEpisode(episodeUuid: String)
