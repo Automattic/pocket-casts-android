74b4d94d6dea0a3991b0e17a43de125977e5c040
Héctor Abraham <hector.abraham.morillo.prieto@automattic.com>
2024-07-10 09:57:08 +0200

    Add Gravatar cache buster comments

--- modules/services/utils/src/main/java/au/com/shiftyjelly/pocketcasts/utils/Gravatar.kt
+++ modules/services/utils/src/main/java/au/com/shiftyjelly/pocketcasts/utils/Gravatar.kt
@@ -8,11 +8,17 @@ object Gravatar {
     fun getGravatarChangeAvatarUrl(email: String): String =
         "https://gravatar.com/profile?is_quick_editor=true&email=${URLEncoder.encode(email, "UTF-8")}&scope=avatars"
 
+    /**
+     * The timestamp is used to force a refresh of the gravatar image. We use it as a cache buster.
+     * When a user updates their avatar, it takes a few seconds to be updated everywhere, so if we reload the same previous URL,
+     * we'll get the old avatar. We'll get the recently uploaded avatar using the cache buster (which can be any random string).
+     */
     private var lastTimeStamp = System.currentTimeMillis()
 
     /**
      * d=404: display no image if there is not one associated with the requested email hash
      * s=400: size of the image
+     * _=lastTimeStamp: cache buster
      * https://en.gravatar.com/site/implement/images/
      */
     fun getUrl(email: String): String? =
