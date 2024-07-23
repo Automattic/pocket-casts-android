27bfd3859c34ffc16a0389ec8025c2857affd696
Michał Sikora <me@mehow.io>
2024-07-23 13:56:33 +0200

    Detect installed social platforms

--- modules/features/sharing/src/main/kotlin/au/com/shiftyjelly/pocketcasts/sharing/social/PlatfromItem.kt
+++ modules/features/sharing/src/main/kotlin/au/com/shiftyjelly/pocketcasts/sharing/social/PlatfromItem.kt
@@ -22,6 +22,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
 import au.com.shiftyjelly.pocketcasts.compose.components.TextH70
 import au.com.shiftyjelly.pocketcasts.sharing.ui.ShareColors
@@ -82,4 +83,8 @@ private fun PlatformItemPreview(
             onClick = {},
         )
     }
+}
+
+private class SocialPlatformProvider : PreviewParameterProvider<SocialPlatform> {
+    override val values get() = SocialPlatform.entries.asSequence()
 }
--- modules/features/sharing/src/main/kotlin/au/com/shiftyjelly/pocketcasts/sharing/social/SocialPlatform.kt
+++ modules/features/sharing/src/main/kotlin/au/com/shiftyjelly/pocketcasts/sharing/social/SocialPlatform.kt
@@ -1,44 +1,61 @@
 package au.com.shiftyjelly.pocketcasts.sharing.social
 
+import android.content.Context
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
-import androidx.compose.ui.tooling.preview.PreviewParameterProvider
-import au.com.shiftyjelly.pocketcasts.images.R
+import au.com.shiftyjelly.pocketcasts.utils.getPackageInfo
+import au.com.shiftyjelly.pocketcasts.images.R as IR
+import au.com.shiftyjelly.pocketcasts.localization.R as LR
 
 internal enum class SocialPlatform(
     @DrawableRes val logoId: Int,
     @StringRes val nameId: Int,
+    val packageId: String?,
 ) {
     Instagram(
-        logoId = R.drawable.ic_share_logo_instagram,
-        nameId = au.com.shiftyjelly.pocketcasts.localization.R.string.share_label_instagram_stories,
+        logoId = IR.drawable.ic_share_logo_instagram,
+        nameId = LR.string.share_label_instagram_stories,
+        packageId = "com.instagram.android",
     ),
     WhatsApp(
-        logoId = R.drawable.ic_share_logo_whats_app,
-        nameId = au.com.shiftyjelly.pocketcasts.localization.R.string.share_label_whats_app,
+        logoId = IR.drawable.ic_share_logo_whats_app,
+        nameId = LR.string.share_label_whats_app,
+        packageId = "com.whatsapp",
     ),
     Telegram(
-        logoId = R.drawable.ic_share_logo_telegram,
-        nameId = au.com.shiftyjelly.pocketcasts.localization.R.string.share_label_telegram,
+        logoId = IR.drawable.ic_share_logo_telegram,
+        nameId = LR.string.share_label_telegram,
+        packageId = "org.telegram.messenger",
     ),
     X(
-        logoId = R.drawable.ic_share_logo_x,
-        nameId = au.com.shiftyjelly.pocketcasts.localization.R.string.share_label_x,
+        logoId = IR.drawable.ic_share_logo_x,
+        nameId = LR.string.share_label_x,
+        packageId = "com.twitter.android",
     ),
     Tumblr(
-        logoId = R.drawable.ic_share_logo_tumblr,
-        nameId = au.com.shiftyjelly.pocketcasts.localization.R.string.share_label_tumblr,
+        logoId = IR.drawable.ic_share_logo_tumblr,
+        nameId = LR.string.share_label_tumblr,
+        packageId = "com.tumblr",
     ),
     PocketCasts(
-        logoId = R.drawable.ic_share_logo_pocket_casts,
-        nameId = au.com.shiftyjelly.pocketcasts.localization.R.string.share_label_copy_link,
+        logoId = IR.drawable.ic_share_logo_pocket_casts,
+        nameId = LR.string.share_label_copy_link,
+        packageId = null,
     ),
     More(
-        logoId = R.drawable.ic_share_logo_more,
-        nameId = au.com.shiftyjelly.pocketcasts.localization.R.string.share_label_more,
+        logoId = IR.drawable.ic_share_logo_more,
+        nameId = LR.string.share_label_more,
+        packageId = null,
     ),
-}
+    ;
 
-internal class SocialPlatformProvider : PreviewParameterProvider<SocialPlatform> {
-    override val values get() = SocialPlatform.entries.asSequence()
+    companion object {
+        fun getAvailablePlatforms(context: Context): Set<SocialPlatform> = buildSet {
+            SocialPlatform.entries.forEach { platform ->
+                if (platform.packageId?.let(context::getPackageInfo) != null) {
+                    add(platform)
+                }
+            }
+        }
+    }
 }
