445d62c22af4b21bcfd02f660ec9a2303778493e
Michał Sikora <me@mehow.io>
2024-07-23 13:50:07 +0200

    Refactor package info querying

--- modules/services/utils/src/main/java/au/com/shiftyjelly/pocketcasts/utils/PackageUtil.kt
+++ modules/services/utils/src/main/java/au/com/shiftyjelly/pocketcasts/utils/PackageUtil.kt
@@ -1,31 +1,20 @@
 package au.com.shiftyjelly.pocketcasts.utils
 
 import android.content.Context
-import android.content.pm.PackageInfo
 import android.content.pm.PackageManager
 import android.os.Build
 import androidx.core.content.pm.PackageInfoCompat
-import javax.inject.Inject
 
-class PackageUtil @Inject constructor() {
-    fun getVersionCode(context: Context): Int {
-        val packageInfo = getPackageInfo(context)
-        return packageInfo?.let {
-            PackageInfoCompat.getLongVersionCode(it).toInt()
-        } ?: 0
-    }
+fun Context.getVersionCode() = getPackageInfo(packageName)?.let {
+    PackageInfoCompat.getLongVersionCode(it).toInt()
+} ?: 0
 
-    private fun getPackageInfo(context: Context): PackageInfo? {
-        return try {
-            val manager = context.packageManager
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                manager.getPackageInfo(context.packageName, PackageManager.PackageInfoFlags.of(0))
-            } else {
-                @Suppress("DEPRECATION")
-                manager.getPackageInfo(context.packageName, 0)
-            }
-        } catch (e: PackageManager.NameNotFoundException) {
-            null
-        }
+fun Context.getPackageInfo(packageName: String) = try {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        packageManager.getPackageInfo(packageName, PackageManager.PackageInfoFlags.of(0))
+    } else {
+        packageManager.getPackageInfo(packageName, 0)
     }
+} catch (e: PackageManager.NameNotFoundException) {
+    null
 }
