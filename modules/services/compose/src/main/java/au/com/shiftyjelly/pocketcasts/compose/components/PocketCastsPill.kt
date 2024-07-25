6239ca294153bd92f108e214524799adbd1e55db
Michał Sikora <me@mehow.io>
2024-07-25 08:56:15 +0200

    Use string resource for the PC name

--- modules/services/compose/src/main/java/au/com/shiftyjelly/pocketcasts/compose/components/PocketCastsPill.kt
+++ modules/services/compose/src/main/java/au/com/shiftyjelly/pocketcasts/compose/components/PocketCastsPill.kt
@@ -16,9 +16,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import au.com.shiftyjelly.pocketcasts.images.R as IR
+import au.com.shiftyjelly.pocketcasts.localization.R as LR
 
 @Composable
 fun PocketCastsPill(
@@ -39,7 +41,7 @@ fun PocketCastsPill(
         modifier = Modifier.width(8.dp),
     )
     TextH70(
-        text = "Pocket Casts",
+        text = stringResource(id = LR.string.pocket_casts),
         color = Color.White,
     )
 }
