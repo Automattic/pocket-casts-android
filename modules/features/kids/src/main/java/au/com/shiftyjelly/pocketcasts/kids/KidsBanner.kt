7087149cca08d0930a5ef71dcbeb9f5c61c1a488
Eduarda Barbosa <meduarda.qb@gmail.com>
2024-07-26 08:37:39 -0300

    use alias for strings and drawables

--- modules/features/kids/src/main/java/au/com/shiftyjelly/pocketcasts/kids/KidsBanner.kt
+++ modules/features/kids/src/main/java/au/com/shiftyjelly/pocketcasts/kids/KidsBanner.kt
@@ -1,6 +1,5 @@
 package au.com.shiftyjelly.pocketcasts.kids
 
-import android.content.res.Configuration
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -22,8 +21,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.contentDescription
@@ -41,19 +38,15 @@ import au.com.shiftyjelly.pocketcasts.compose.components.TextH40
 import au.com.shiftyjelly.pocketcasts.compose.components.TextH70
 import au.com.shiftyjelly.pocketcasts.compose.preview.ThemePreviewParameterProvider
 import au.com.shiftyjelly.pocketcasts.compose.theme
-import au.com.shiftyjelly.pocketcasts.images.R.drawable.ic_close
-import au.com.shiftyjelly.pocketcasts.images.R.drawable.kids_profile_face
-import au.com.shiftyjelly.pocketcasts.localization.R.string
-import au.com.shiftyjelly.pocketcasts.localization.R.string.close
 import au.com.shiftyjelly.pocketcasts.ui.theme.Theme
+import au.com.shiftyjelly.pocketcasts.images.R as IR
+import au.com.shiftyjelly.pocketcasts.localization.R as LR
 
 @Composable
 fun KidsProfileCard(
     onDismiss: () -> Unit,
 ) {
-    val configuration = LocalConfiguration.current
-    val isLandscape = configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
-    val bannerContentDescription = LocalContext.current.getString(string.kids_profile_banner)
+    val bannerContentDescription = stringResource(LR.string.kids_profile_banner)
 
     Box(
         modifier = Modifier
@@ -64,8 +57,8 @@ fun KidsProfileCard(
     ) {
         Image(
             colorFilter = ColorFilter.tint(MaterialTheme.theme.colors.primaryUi05Selected),
-            painter = painterResource(id = ic_close),
-            contentDescription = stringResource(id = close),
+            painter = painterResource(IR.drawable.ic_close),
+            contentDescription = stringResource(LR.string.close),
             modifier = Modifier
                 .padding(11.dp)
                 .size(22.dp)
@@ -85,7 +78,7 @@ fun KidsProfileCard(
                     modifier = Modifier.padding(start = 24.dp, top = 16.dp, bottom = 8.dp),
                 ) {
                     TextH40(
-                        text = stringResource(string.kids_profile),
+                        text = stringResource(LR.string.kids_profile),
                         fontWeight = FontWeight.W500,
                     )
                     Box(
@@ -96,7 +89,7 @@ fun KidsProfileCard(
                             .height(24.dp),
                     ) {
                         Text(
-                            text = stringResource(string.soon_label),
+                            text = stringResource(LR.string.soon_label),
                             color = MaterialTheme.theme.colors.primaryUi05Selected,
                             fontSize = 11.sp,
                             fontWeight = FontWeight.W600,
@@ -104,16 +97,15 @@ fun KidsProfileCard(
                     }
                 }
                 TextH70(
-                    text = stringResource(string.kids_profile_banner_description),
+                    text = stringResource(LR.string.kids_profile_banner_description),
                     color = MaterialTheme.theme.colors.primaryUi05Selected,
                     modifier = Modifier
-                        .padding(start = 24.dp, bottom = 8.dp)
-                        .then(if (isLandscape) Modifier else Modifier.width(200.dp)),
+                        .padding(start = 24.dp, end = 116.dp, bottom = 8.dp),
                     fontWeight = FontWeight.W600,
                 )
 
                 ClickableText(
-                    text = AnnotatedString(text = stringResource(string.request_early_access)),
+                    text = AnnotatedString(text = stringResource(LR.string.request_early_access)),
                     onClick = { },
                     modifier = Modifier.padding(start = 24.dp, bottom = 14.dp),
                     style = TextStyle(
@@ -127,8 +119,8 @@ fun KidsProfileCard(
         }
 
         Image(
-            painter = painterResource(id = kids_profile_face),
-            contentDescription = stringResource(id = string.kids_profile_face_image),
+            painter = painterResource(IR.drawable.kids_profile_face),
+            contentDescription = stringResource(LR.string.kids_profile_face_image),
             modifier = Modifier
                 .size(116.dp)
                 .padding(top = 36.dp)
