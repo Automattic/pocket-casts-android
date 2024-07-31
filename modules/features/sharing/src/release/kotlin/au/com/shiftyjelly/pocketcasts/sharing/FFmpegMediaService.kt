de6db3cf9f859932e4339ef8eae79e63de005447
Michał Sikora <me@mehow.io>
2024-07-31 17:01:53 +0200

    Fix release build MediaService implementation

--- modules/features/sharing/src/release/kotlin/au/com/shiftyjelly/pocketcasts/sharing/FFmpegMediaService.kt
+++ modules/features/sharing/src/release/kotlin/au/com/shiftyjelly/pocketcasts/sharing/FFmpegMediaService.kt
@@ -4,10 +4,11 @@ import android.content.Context
 import au.com.shiftyjelly.pocketcasts.models.entity.Podcast
 import au.com.shiftyjelly.pocketcasts.models.entity.PodcastEpisode
 import au.com.shiftyjelly.pocketcasts.sharing.clip.Clip
+import java.io.File
 
 internal class FFmpegMediaService(
     private val context: Context,
-) : MediaExtractor {
+) : MediaService {
     override suspend fun clipAudio(podcast: Podcast, episode: PodcastEpisode, clipRange: Clip.Range): Result<File> {
         throw AssertionError("Not supported in release")
     }
