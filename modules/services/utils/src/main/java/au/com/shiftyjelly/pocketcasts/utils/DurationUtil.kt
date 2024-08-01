b982224a851677083591d8950ecf8a6d4d20853e
Michał Sikora <me@mehow.io>
2024-08-01 12:35:45 +0200

    Remove unnecessary fqcns

--- modules/services/utils/src/main/java/au/com/shiftyjelly/pocketcasts/utils/DurationUtil.kt
+++ modules/services/utils/src/main/java/au/com/shiftyjelly/pocketcasts/utils/DurationUtil.kt
@@ -5,8 +5,8 @@ import kotlin.time.Duration
 
 fun Duration.toHhMmSs() = toComponents { hours, minutes, seconds, _ ->
     if (hours == 0L) {
-        kotlin.String.format(Locale.ROOT, "%02d:%02d", minutes, seconds)
+        String.format(Locale.ROOT, "%02d:%02d", minutes, seconds)
     } else {
-        kotlin.String.format(Locale.ROOT, "%02d:%02d:%02d", hours, minutes, seconds)
+        String.format(Locale.ROOT, "%02d:%02d:%02d", hours, minutes, seconds)
     }
 }
