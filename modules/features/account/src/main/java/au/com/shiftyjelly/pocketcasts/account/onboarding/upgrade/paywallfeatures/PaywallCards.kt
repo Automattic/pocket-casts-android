7e381a4b41e28f497b07824a29a3cb864a982697
Eduarda Barbosa <meduarda.qb@gmail.com>
2024-08-15 15:52:52 -0300

    create generic component for cards

--- modules/features/account/src/main/java/au/com/shiftyjelly/pocketcasts/account/onboarding/upgrade/paywallfeatures/PaywallCards.kt
+++ modules/features/account/src/main/java/au/com/shiftyjelly/pocketcasts/account/onboarding/upgrade/paywallfeatures/PaywallCards.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import au.com.shiftyjelly.pocketcasts.compose.AppTheme
 import au.com.shiftyjelly.pocketcasts.compose.components.TextH30
@@ -29,8 +30,16 @@ import au.com.shiftyjelly.pocketcasts.images.R as IR
 import au.com.shiftyjelly.pocketcasts.localization.R as LR
 import au.com.shiftyjelly.pocketcasts.ui.R as UR
 
+data class CardData(
+    val imageResId: Int,
+    val contentDescription: String,
+    val title: String,
+    val description: String,
+    val imageHeight: Dp,
+)
+
 @Composable
-fun BookmarksCard() {
+fun CardItem(cardData: CardData) {
     Card(
         shape = RoundedCornerShape(8.dp),
         modifier = Modifier
@@ -44,17 +53,17 @@ fun BookmarksCard() {
                 .background(colorResource(UR.color.coolgrey_90)),
         ) {
             Image(
-                painter = painterResource(IR.drawable.bookmarks),
-                contentDescription = stringResource(LR.string.paywall_bookmarks_content_description),
+                painter = painterResource(cardData.imageResId),
+                contentDescription = cardData.contentDescription,
                 modifier = Modifier
-                    .height(224.dp)
+                    .height(cardData.imageHeight)
                     .padding(top = 20.dp, start = 2.dp, end = 2.dp)
                     .align(Alignment.CenterHorizontally),
                 contentScale = ContentScale.Fit,
             )
 
             TextH30(
-                text = stringResource(LR.string.paywall_bookmarks_title),
+                text = cardData.title,
                 textAlign = TextAlign.Start,
                 fontWeight = FontWeight.W600,
                 color = Color.White,
@@ -63,7 +72,7 @@ fun BookmarksCard() {
                     .align(Alignment.Start),
             )
             TextP50(
-                text = stringResource(LR.string.paywall_bookmarks_description),
+                text = cardData.description,
                 textAlign = TextAlign.Start,
                 fontWeight = FontWeight.W400,
                 color = colorResource(UR.color.coolgrey_50),
@@ -73,191 +82,66 @@ fun BookmarksCard() {
             )
         }
     }
+}
+
+@Composable
+fun BookmarksCard() {
+    val bookmarksCardData = CardData(
+        imageResId = IR.drawable.bookmarks,
+        contentDescription = stringResource(LR.string.paywall_bookmarks_content_description),
+        title = stringResource(LR.string.paywall_bookmarks_title),
+        description = stringResource(LR.string.paywall_bookmarks_description),
+        imageHeight = 224.dp,
+    )
+    CardItem(cardData = bookmarksCardData)
 }
 
 @Composable
 fun FoldersCard() {
-    Card(
-        shape = RoundedCornerShape(8.dp),
-        modifier = Modifier
-            .height(394.dp)
-            .width(313.dp)
-            .background(Color.Transparent),
-    ) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(colorResource(UR.color.coolgrey_90)),
-        ) {
-            Image(
-                painter = painterResource(IR.drawable.folders),
-                contentDescription = stringResource(LR.string.paywall_folders_content_description),
-                modifier = Modifier
-                    .height(231.dp)
-                    .padding(top = 20.dp, start = 2.dp, end = 2.dp)
-                    .align(Alignment.CenterHorizontally),
-                contentScale = ContentScale.Fit,
-            )
-
-            TextH30(
-                text = stringResource(LR.string.paywall_folders_title),
-                textAlign = TextAlign.Start,
-                fontWeight = FontWeight.W600,
-                color = Color.White,
-                modifier = Modifier
-                    .padding(top = 20.dp, start = 24.dp, end = 24.dp)
-                    .align(Alignment.Start),
-            )
-            TextP50(
-                text = stringResource(LR.string.paywall_folders_description),
-                textAlign = TextAlign.Start,
-                fontWeight = FontWeight.W400,
-                color = colorResource(UR.color.coolgrey_50),
-                modifier = Modifier
-                    .padding(top = 4.dp, start = 24.dp, end = 24.dp, bottom = 48.dp)
-                    .align(Alignment.Start),
-            )
-        }
-    }
+    val foldersCardData = CardData(
+        imageResId = IR.drawable.folders,
+        contentDescription = stringResource(LR.string.paywall_folders_content_description),
+        title = stringResource(LR.string.paywall_folders_title),
+        description = stringResource(LR.string.paywall_folders_description),
+        imageHeight = 231.dp,
+    )
+    CardItem(cardData = foldersCardData)
 }
 
 @Composable
 fun DesktopWebAppCard() {
-    Card(
-        shape = RoundedCornerShape(8.dp),
-        modifier = Modifier
-            .height(394.dp)
-            .width(313.dp)
-            .background(Color.Transparent),
-    ) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(colorResource(UR.color.coolgrey_90)),
-        ) {
-            Image(
-                painter = painterResource(IR.drawable.desktop),
-                contentDescription = stringResource(LR.string.paywall_desktop_and_web_content_description),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 20.dp)
-                    .height(186.dp)
-                    .align(Alignment.CenterHorizontally),
-                contentScale = ContentScale.Fit,
-            )
-
-            TextH30(
-                text = stringResource(LR.string.paywall_desktop_and_web_title),
-                textAlign = TextAlign.Start,
-                fontWeight = FontWeight.W600,
-                color = Color.White,
-                modifier = Modifier
-                    .padding(top = 20.dp, start = 24.dp, end = 24.dp)
-                    .align(Alignment.Start),
-            )
-            TextP50(
-                text = stringResource(LR.string.paywall_desktop_and_web_description),
-                textAlign = TextAlign.Start,
-                fontWeight = FontWeight.W400,
-                color = colorResource(UR.color.coolgrey_50),
-                modifier = Modifier
-                    .padding(top = 4.dp, start = 24.dp, end = 24.dp, bottom = 48.dp)
-                    .align(Alignment.Start),
-            )
-        }
-    }
+    val desktopCardData = CardData(
+        imageResId = IR.drawable.desktop,
+        contentDescription = stringResource(LR.string.paywall_desktop_and_web_content_description),
+        title = stringResource(LR.string.paywall_desktop_and_web_title),
+        description = stringResource(LR.string.paywall_desktop_and_web_description),
+        imageHeight = 186.dp,
+    )
+    CardItem(cardData = desktopCardData)
 }
 
 @Composable
 fun WatchCard() {
-    Card(
-        shape = RoundedCornerShape(8.dp),
-        modifier = Modifier
-            .height(394.dp)
-            .width(313.dp)
-            .background(Color.Transparent),
-    ) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(colorResource(UR.color.coolgrey_90)),
-        ) {
-            Image(
-                painter = painterResource(IR.drawable.watch),
-                contentDescription = stringResource(LR.string.paywall_watch_content_description),
-                modifier = Modifier
-                    .height(231.dp)
-                    .padding(top = 20.dp, start = 2.dp, end = 2.dp)
-                    .align(Alignment.CenterHorizontally),
-                contentScale = ContentScale.Fit,
-            )
-
-            TextH30(
-                text = stringResource(LR.string.paywall_watch_title),
-                textAlign = TextAlign.Start,
-                fontWeight = FontWeight.W600,
-                color = Color.White,
-                modifier = Modifier
-                    .padding(top = 20.dp, start = 24.dp, end = 24.dp)
-                    .align(Alignment.Start),
-            )
-            TextP50(
-                text = stringResource(LR.string.paywall_watch_description),
-                textAlign = TextAlign.Start,
-                fontWeight = FontWeight.W400,
-                color = colorResource(UR.color.coolgrey_50),
-                modifier = Modifier
-                    .padding(top = 4.dp, start = 24.dp, end = 24.dp, bottom = 48.dp)
-                    .align(Alignment.Start),
-            )
-        }
-    }
+    val watchCardData = CardData(
+        imageResId = IR.drawable.watch,
+        contentDescription = stringResource(LR.string.paywall_watch_content_description),
+        title = stringResource(LR.string.paywall_watch_title),
+        description = stringResource(LR.string.paywall_watch_description),
+        imageHeight = 231.dp,
+    )
+    CardItem(cardData = watchCardData)
 }
 
 @Composable
 fun SlumberStudiosCard() {
-    Card(
-        shape = RoundedCornerShape(8.dp),
-        modifier = Modifier
-            .height(394.dp)
-            .width(313.dp)
-            .background(Color.Transparent),
-    ) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(colorResource(UR.color.coolgrey_90)),
-        ) {
-            Image(
-                painter = painterResource(IR.drawable.slumber),
-                contentDescription = stringResource(LR.string.paywall_slumber_studios_content_description),
-                modifier = Modifier
-                    .height(195.dp)
-                    .padding(top = 20.dp, start = 2.dp, end = 2.dp)
-                    .align(Alignment.CenterHorizontally),
-                contentScale = ContentScale.Fit,
-            )
-
-            TextH30(
-                text = stringResource(LR.string.paywall_slumber_studios_title),
-                textAlign = TextAlign.Start,
-                fontWeight = FontWeight.W600,
-                color = Color.White,
-                modifier = Modifier
-                    .padding(top = 20.dp, start = 24.dp, end = 24.dp)
-                    .align(Alignment.Start),
-            )
-            TextP50(
-                text = stringResource(LR.string.paywall_slumber_studios_description),
-                textAlign = TextAlign.Start,
-                fontWeight = FontWeight.W400,
-                color = colorResource(UR.color.coolgrey_50),
-                modifier = Modifier
-                    .padding(top = 4.dp, start = 24.dp, end = 24.dp, bottom = 48.dp)
-                    .align(Alignment.Start),
-            )
-        }
-    }
+    val slumberCardData = CardData(
+        imageResId = IR.drawable.slumber,
+        contentDescription = stringResource(LR.string.paywall_slumber_studios_content_description),
+        title = stringResource(LR.string.paywall_slumber_studios_title),
+        description = stringResource(LR.string.paywall_slumber_studios_description),
+        imageHeight = 195.dp,
+    )
+    CardItem(cardData = slumberCardData)
 }
 
 @Preview
