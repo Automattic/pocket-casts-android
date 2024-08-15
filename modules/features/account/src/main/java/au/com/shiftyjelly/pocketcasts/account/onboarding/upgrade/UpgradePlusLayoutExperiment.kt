d2e9da3886874fb53effdb0e943b47abcd074db1
Eduarda Barbosa <meduarda.qb@gmail.com>
2024-08-15 11:15:40 -0300

    add preview for subscribe button

--- modules/features/account/src/main/java/au/com/shiftyjelly/pocketcasts/account/onboarding/upgrade/UpgradePlusLayoutExperiment.kt
+++ modules/features/account/src/main/java/au/com/shiftyjelly/pocketcasts/account/onboarding/upgrade/UpgradePlusLayoutExperiment.kt
@@ -29,6 +29,7 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import au.com.shiftyjelly.pocketcasts.account.viewmodel.OnboardingUpgradeFeaturesState
@@ -173,4 +174,10 @@ private fun SubscribeButton(
             )
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SubscribeButtonPreview() {
+    SubscribeButton(onClickSubscribe = { })
 }
