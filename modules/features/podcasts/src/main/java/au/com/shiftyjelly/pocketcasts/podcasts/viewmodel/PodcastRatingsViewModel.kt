d03277bf88d3d7d898bbcb18aa9f2a1f75e8550d
Eduarda Barbosa <42220351+mebarbosa@users.noreply.github.com>
2024-08-20 08:19:52 -0300

    [Give ratings] Catch all exceptions when refreshing ratings (#2670)

--- modules/features/podcasts/src/main/java/au/com/shiftyjelly/pocketcasts/podcasts/viewmodel/PodcastRatingsViewModel.kt
+++ modules/features/podcasts/src/main/java/au/com/shiftyjelly/pocketcasts/podcasts/viewmodel/PodcastRatingsViewModel.kt
@@ -27,7 +27,6 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import retrofit2.HttpException
 import timber.log.Timber
 
 private const val MAX_STARS = 5
@@ -61,17 +60,7 @@ class PodcastRatingsViewModel
 
     fun refreshPodcastRatings(uuid: String) {
         launch(Dispatchers.IO) {
-            try {
-                ratingsManager.refreshPodcastRatings(podcastUuid = uuid, useCache = true)
-            } catch (e: Exception) {
-                val message = "Failed to refresh podcast ratings"
-                // don't report missing rating or network errors to Sentry
-                if (e is HttpException || e is IOException) {
-                    Timber.i(e, message)
-                } else {
-                    Timber.e(e, message)
-                }
-            }
+            ratingsManager.refreshPodcastRatings(podcastUuid = uuid, useCache = true)
         }
     }
 
