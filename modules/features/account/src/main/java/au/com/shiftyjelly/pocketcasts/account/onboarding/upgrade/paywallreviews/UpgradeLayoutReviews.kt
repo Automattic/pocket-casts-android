df6e05583d67614628243fca9b733b251e24e0a1
Eduarda Barbosa <meduarda.qb@gmail.com>
2024-08-28 09:19:08 -0300

    update layout to prepare for having reviews

--- modules/features/account/src/main/java/au/com/shiftyjelly/pocketcasts/account/onboarding/upgrade/paywallreviews/UpgradeLayoutReviews.kt
+++ modules/features/account/src/main/java/au/com/shiftyjelly/pocketcasts/account/onboarding/upgrade/paywallreviews/UpgradeLayoutReviews.kt
@@ -1,28 +1,21 @@
 package au.com.shiftyjelly.pocketcasts.account.onboarding.upgrade.paywallreviews
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBars
-import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -39,7 +32,6 @@ import au.com.shiftyjelly.pocketcasts.account.onboarding.components.UpgradeFeatu
 import au.com.shiftyjelly.pocketcasts.account.onboarding.upgrade.OnboardingUpgradeHelper.plusGradientBrush
 import au.com.shiftyjelly.pocketcasts.account.onboarding.upgrade.PlusUpgradeLayoutReviewsItem
 import au.com.shiftyjelly.pocketcasts.account.onboarding.upgrade.SubscribeButton
-import au.com.shiftyjelly.pocketcasts.account.onboarding.upgrade.calculateMinimumHeightWithInsets
 import au.com.shiftyjelly.pocketcasts.account.viewmodel.OnboardingUpgradeFeaturesState
 import au.com.shiftyjelly.pocketcasts.compose.AppTheme
 import au.com.shiftyjelly.pocketcasts.compose.buttons.RowTextButton
@@ -47,7 +39,6 @@ import au.com.shiftyjelly.pocketcasts.compose.components.TextH20
 import au.com.shiftyjelly.pocketcasts.compose.components.TextH40
 import au.com.shiftyjelly.pocketcasts.compose.components.TextP50
 import au.com.shiftyjelly.pocketcasts.compose.images.SubscriptionBadge
-import au.com.shiftyjelly.pocketcasts.compose.theme
 import au.com.shiftyjelly.pocketcasts.localization.R
 import au.com.shiftyjelly.pocketcasts.ui.theme.Theme
 import au.com.shiftyjelly.pocketcasts.images.R as IR
@@ -57,7 +48,6 @@ import au.com.shiftyjelly.pocketcasts.ui.R as UR
 @Composable
 fun UpgradeLayoutReviews(
     state: OnboardingUpgradeFeaturesState.Loaded,
-    scrollState: ScrollState,
     onNotNowPressed: () -> Unit,
     onClickSubscribe: () -> Unit,
     canUpgrade: Boolean,
@@ -65,26 +55,25 @@ fun UpgradeLayoutReviews(
 ) {
     AppTheme(Theme.ThemeType.DARK) {
         Box(
-            modifier = modifier.fillMaxHeight(),
+            modifier = modifier
+                .fillMaxHeight()
+                .background(color = Color.Black),
             contentAlignment = Alignment.BottomCenter,
         ) {
-            BoxWithConstraints(
-                Modifier
-                    .fillMaxHeight()
-                    .background(color = MaterialTheme.theme.colors.primaryUi02),
+            Column(
+                modifier = Modifier
+                    .fillMaxHeight(),
+                horizontalAlignment = Alignment.CenterHorizontally,
             ) {
-                Box(modifier = Modifier.verticalScroll(scrollState)) {
-                    Column(
-                        Modifier
-                            .windowInsetsPadding(WindowInsets.statusBars)
-                            .windowInsetsPadding(WindowInsets.navigationBars)
-                            .heightIn(min = this@BoxWithConstraints.calculateMinimumHeightWithInsets())
-                            .padding(bottom = 100.dp),
-                    ) {
+                LazyColumn(
+                    modifier = Modifier.weight(1f),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    item {
                         Row(
                             modifier = Modifier
                                 .fillMaxWidth()
-                                .padding(top = 8.dp, bottom = 40.dp),
+                                .padding(bottom = 40.dp, top = 16.dp),
                             horizontalArrangement = Arrangement.End,
                         ) {
                             RowTextButton(
@@ -99,47 +88,51 @@ fun UpgradeLayoutReviews(
                                 includePadding = false,
                             )
                         }
+                    }
 
-                        Column {
-                            TextH20(
-                                text = stringResource(LR.string.paywall_layout_reviews_title),
-                                color = Color.White,
-                                textAlign = TextAlign.Center,
-                                modifier = Modifier
-                                    .padding(horizontal = 20.dp)
-                                    .padding(bottom = 8.dp)
-                                    .fillMaxWidth(),
-                            )
+                    item {
+                        TextH20(
+                            text = stringResource(LR.string.paywall_layout_reviews_title),
+                            color = Color.White,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier
+                                .padding(horizontal = 20.dp)
+                                .padding(bottom = 8.dp)
+                                .fillMaxWidth(),
+                        )
 
-                            TextP50(
-                                text = stringResource(LR.string.paywall_layout_reviews_subtitle),
-                                fontWeight = FontWeight.W400,
-                                color = colorResource(UR.color.coolgrey_50),
-                                textAlign = TextAlign.Center,
-                                modifier = Modifier
-                                    .padding(horizontal = 40.dp)
-                                    .padding(bottom = 16.dp)
-                                    .fillMaxWidth(),
-                            )
+                        TextP50(
+                            text = stringResource(LR.string.paywall_layout_reviews_subtitle),
+                            fontWeight = FontWeight.W400,
+                            color = colorResource(UR.color.coolgrey_50),
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier
+                                .padding(horizontal = 40.dp)
+                                .padding(bottom = 16.dp)
+                                .fillMaxWidth(),
+                        )
+                    }
 
-                            PlusBenefits(
-                                modifier = Modifier
-                                    .padding(horizontal = 20.dp)
-                                    .padding(bottom = 24.dp),
-                            )
+                    item {
+                        PlusBenefits(
+                            modifier = Modifier
+                                .padding(horizontal = 20.dp)
+                                .padding(bottom = 24.dp),
+                        )
+                    }
 
-                            Stars(
-                                modifier = Modifier
-                                    .padding(horizontal = 20.dp)
-                                    .padding(bottom = 24.dp),
-                            )
-                        }
+                    item {
+                        Stars(
+                            modifier = Modifier
+                                .padding(horizontal = 20.dp)
+                                .padding(bottom = 24.dp),
+                        )
                     }
                 }
-            }
 
-            if (canUpgrade) {
-                SubscribeButton(state.currentSubscription, onClickSubscribe)
+                if (canUpgrade) {
+                    SubscribeButton(state.currentSubscription, onClickSubscribe)
+                }
             }
         }
     }
@@ -205,7 +198,9 @@ fun Stars(modifier: Modifier = Modifier) {
         Image(
             painter = painterResource(IR.drawable.stars),
             contentDescription = stringResource(LR.string.paywall_layout_reviews_stars_content_description),
-            modifier = Modifier.padding(bottom = 8.dp),
+            modifier = Modifier
+                .padding(bottom = 8.dp)
+                .size(width = 150.dp, height = 26.dp),
         )
 
         TextH40(
