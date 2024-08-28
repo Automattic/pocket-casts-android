03de43a6aea56fa4a881924c7b97eac47467548f
Michał Sikora <me@mehow.io>
2024-08-28 15:06:58 +0200

    Fix formatting

--- modules/features/settings/src/main/java/au/com/shiftyjelly/pocketcasts/settings/whatsnew/SharingHeader.kt
+++ modules/features/settings/src/main/java/au/com/shiftyjelly/pocketcasts/settings/whatsnew/SharingHeader.kt
@@ -26,7 +26,6 @@ import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
