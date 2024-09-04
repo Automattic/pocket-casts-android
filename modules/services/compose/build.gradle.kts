6f2f32c762c80c341fb4a1ba98917b6b559f2142
Michał Sikora <me@mehow.io>
2024-09-04 11:20:31 +0200

    Optimize compose module config

--- modules/services/compose/build.gradle.kts
+++ modules/services/compose/build.gradle.kts
@@ -2,11 +2,7 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.ksp)
-    alias(libs.plugins.hilt)
-    alias(libs.plugins.kotlin.parcelize)
 }
-
-apply(from = "${project.rootDir}/base.gradle")
 
 android {
     namespace = "au.com.shiftyjelly.pocketcasts.compose"
@@ -17,11 +13,26 @@ android {
 }
 
 dependencies {
+    implementation(libs.compose.activity)
+    implementation(libs.compose.animation)
+    implementation(libs.compose.icons)
+    implementation(libs.compose.material)
+    implementation(libs.compose.ui)
+    implementation(libs.compose.ui.tooling.preview)
+    implementation(libs.compose.ui.util)
+    implementation(libs.lottie)
+    implementation(libs.lottie.compose)
+    implementation(libs.coil.compose)
+    api(libs.showkase)
+    implementation(platform(libs.compose.bom))
+
+    ksp(libs.showkase.processor)
+
     implementation(project(":modules:services:images"))
     implementation(project(":modules:services:localization"))
-    implementation(project(":modules:services:model"))
-    implementation(project(":modules:services:preferences"))
-    implementation(project(":modules:services:repositories"))
-    implementation(project(":modules:services:ui"))
+    api(project(":modules:services:model"))
+    api(project(":modules:services:preferences"))
+    api(project(":modules:services:repositories"))
+    api(project(":modules:services:ui"))
     implementation(project(":modules:services:utils"))
 }
