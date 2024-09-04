fa7388788199e21774f8559160c056132f8ed9cb
Michał Sikora <me@mehow.io>
2024-09-04 11:20:30 +0200

    Optimize utils module config

--- modules/services/utils/build.gradle.kts
+++ modules/services/utils/build.gradle.kts
@@ -6,8 +6,6 @@ plugins {
     alias(libs.plugins.kotlin.parcelize)
 }
 
-apply(from = "${project.rootDir}/base.gradle")
-
 // utils should have no other module dependencies
 
 android {
@@ -15,4 +13,38 @@ android {
     buildFeatures {
         buildConfig = true
     }
+}
+
+dependencies {
+    api("io.coil-kt:coil-base:2.5.0")
+    implementation("androidx.lifecycle:lifecycle-livedata:2.8.4")
+    testImplementation("org.mockito:mockito-core:5.7.0")
+
+    api(libs.appcompat)
+    implementation(libs.auth)
+    implementation(libs.coroutines.core)
+    implementation(libs.firebase.analytics)
+    implementation(libs.firebase.config)
+    api(libs.hilt.android)
+    api(libs.okhttp)
+    implementation(libs.rxandroid)
+    api(libs.rxjava)
+    implementation(libs.car)
+    implementation(libs.annotation)
+    implementation(libs.cast)
+    implementation("androidx.compose.ui:ui-graphics:1.6.2")
+    implementation(libs.core.ktx)
+    api(libs.material)
+    implementation(libs.oss.licenses)
+    implementation(libs.play.services.wearable)
+    api(libs.timber)
+    implementation(platform(libs.compose.bom))
+    implementation(platform(libs.firebase.bom))
+
+    testImplementation(libs.coroutines.test)
+    testImplementation(libs.junit) { exclude(group = "org.hamcrest") }
+    testImplementation(libs.mockito.kotlin)
+
+    ksp(libs.dagger.hilt.compiler)
+    ksp(libs.hilt.compiler)
 }
