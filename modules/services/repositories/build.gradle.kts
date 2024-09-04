a138b84e5fc9d82299c6916d014ab13292429684
Michał Sikora <me@mehow.io>
2024-09-04 11:20:32 +0200

    Optimize repositories module config

--- modules/services/repositories/build.gradle.kts
+++ modules/services/repositories/build.gradle.kts
@@ -6,8 +6,6 @@ plugins {
     alias(libs.plugins.kotlin.parcelize)
 }
 
-apply(from = "${project.rootDir}/base.gradle")
-
 android {
     namespace = "au.com.shiftyjelly.pocketcasts.repositories"
     buildFeatures {
@@ -16,16 +14,65 @@ android {
 }
 
 dependencies {
+    implementation(libs.appcompat)
+    implementation(libs.auth)
+    api(libs.billing)
+    api(libs.billing.ktx)
+    implementation(libs.coroutines.core)
+    implementation(libs.coroutines.rx2)
+    api(libs.hilt.android)
+    api(libs.hilt.work)
+    implementation(libs.lifecycle.process)
+    api(libs.media3.datasource)
+    implementation(libs.media3.datasource.okhttp)
+    api(libs.media3.exoplayer)
+    implementation(libs.media3.exoplayer.hls)
+    api(libs.media3.extractor)
+    implementation(libs.media3.ui)
+    api(libs.moshi)
+    api(libs.okhttp)
+    implementation(libs.protobuf.kotlin.lite)
+    implementation(libs.protobuf.javalite)
+    implementation(libs.retrofit)
     implementation(libs.room.ktx)
-    implementation(project(":modules:services:analytics"))
-    implementation(project(":modules:services:crashlogging"))
+    implementation(libs.rxandroid)
+    api(libs.rxjava)
+    api(libs.rxrelay)
+    implementation(libs.rxkotlin)
+    api(libs.work.runtime)
+    api(libs.work.rxjava2)
+    implementation(libs.cast)
+    implementation(libs.lifecycle.reactivestreams.java)
+    implementation(libs.coil)
+    implementation("androidx.compose.ui:ui-graphics:1.6.2")
+    implementation(libs.core.ktx)
+    implementation(libs.device.names)
+    implementation(libs.guava)
+    implementation(libs.material)
+    implementation(libs.oss.licenses)
+    implementation(libs.play.services.wearable)
+    implementation(libs.preference)
+    implementation(libs.timber)
+
+    testImplementation(libs.coroutines.test)
+    testImplementation(libs.junit) { exclude(group = "org.hamcrest") }
+    testImplementation(libs.mockito.kotlin)
+    testImplementation(libs.turbine)
+    testImplementation("org.mockito:mockito-core:5.7.0")
+
+    ksp(libs.dagger.hilt.compiler)
+    ksp(libs.hilt.compiler)
+
+    implementation(libs.room.ktx)
+    api(project(":modules:services:analytics"))
+    api(project(":modules:services:crashlogging"))
     implementation(project(":modules:services:deeplink"))
     implementation(project(":modules:services:images"))
-    implementation(project(":modules:services:localization"))
-    implementation(project(":modules:services:model"))
-    implementation(project(":modules:services:preferences"))
-    implementation(project(":modules:services:protobuf"))
-    implementation(project(":modules:services:servers"))
-    implementation(project(":modules:services:utils"))
+    api(project(":modules:services:localization"))
+    api(project(":modules:services:model"))
+    api(project(":modules:services:preferences"))
+    api(project(":modules:services:protobuf"))
+    api(project(":modules:services:servers"))
+    api(project(":modules:services:utils"))
     testImplementation(project(":modules:services:sharedtest"))
 }
