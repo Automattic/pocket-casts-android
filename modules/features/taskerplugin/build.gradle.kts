d950bbf51b96881c56d142ae0149cf7fd133e5ce
Michał Sikora <me@mehow.io>
2024-09-04 11:20:32 +0200

    Optimize taskerplugin module config

--- modules/features/taskerplugin/build.gradle.kts
+++ modules/features/taskerplugin/build.gradle.kts
@@ -3,30 +3,39 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.ksp)
     alias(libs.plugins.hilt)
-    alias(libs.plugins.kotlin.parcelize)
 }
-
-apply(from = "${project.rootDir}/base.gradle")
 
 android {
     namespace = "au.com.shiftyjelly.pocketcasts.taskerplugin"
     buildFeatures {
         buildConfig = true
-        viewBinding = true
+        viewBinding = false
         compose = true
     }
 }
 
 dependencies {
+    implementation(libs.compose.activity)
+    implementation(libs.compose.material)
+    implementation(libs.compose.ui)
+    implementation(libs.compose.ui.tooling.preview)
+    implementation(libs.compose.ui.util)
+    api(libs.hilt.android)
+    implementation(libs.play.services.wearable)
+    implementation(platform(libs.compose.bom))
+
+    ksp(libs.dagger.hilt.compiler)
+    ksp(libs.hilt.compiler)
+
+
     implementation(project(":modules:services:analytics"))
     implementation(project(":modules:services:localization"))
-    implementation(project(":modules:services:ui"))
+    api(project(":modules:services:ui"))
     implementation(project(":modules:services:compose"))
-    implementation(project(":modules:services:repositories"))
-    implementation(project(":modules:services:model"))
-    implementation(project(":modules:services:views"))
+    api(project(":modules:services:repositories"))
+    api(project(":modules:services:model"))
     implementation(project(":modules:services:images"))
-    implementation(project(":modules:services:preferences"))
+    api(project(":modules:services:preferences"))
     implementation(project(":modules:services:utils"))
-    implementation(libs.tasker)
+    api(libs.tasker)
 }
