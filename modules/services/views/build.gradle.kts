816963341812e194471bdbf96b014cac72ea654e
Michał Sikora <me@mehow.io>
2024-09-04 11:20:32 +0200

    Optimize views module config

--- modules/services/views/build.gradle.kts
+++ modules/services/views/build.gradle.kts
@@ -6,8 +6,6 @@ plugins {
     alias(libs.plugins.kotlin.parcelize)
 }
 
-apply(from = "${project.rootDir}/base.gradle")
-
 android {
     namespace = "au.com.shiftyjelly.pocketcasts.views"
     buildFeatures {
@@ -18,15 +16,51 @@ android {
 }
 
 dependencies {
-    implementation(project(":modules:services:analytics"))
-    implementation(project(":modules:services:compose"))
-    implementation(project(":modules:services:crashlogging"))
+    api(libs.appcompat)
+    api(libs.compose.material)
+    implementation(libs.compose.ui)
+    implementation(libs.coroutines.core)
+    implementation(libs.compose.ui.tooling.preview)
+    implementation(libs.fragment.ktx)
+    api(libs.hilt.android)
+    api(libs.lottie)
+    implementation(libs.okhttp)
+    implementation(libs.rxandroid)
+    api(libs.rxjava)
+    api(libs.rxrelay)
+    implementation(libs.rxkotlin)
+    implementation(libs.cardview)
+    implementation(libs.cast)
+    api(libs.constraintlayout)
+    implementation(libs.core.ktx)
+    api(libs.material)
+    api(libs.mediarouter)
+    api(libs.preference)
+    api(libs.recyclerview)
+    implementation(libs.timber)
+    api(libs.navigation.runtime)
+    implementation(libs.lifecycle.reactivestreams.java)
+    implementation(libs.viewpager)
+    api("com.google.android.play:review:2.0.1")
+    implementation(platform(libs.compose.bom))
+    api(libs.crashlogging)
+
+    testImplementation(libs.junit) { exclude(group = "org.hamcrest") }
+    testImplementation("org.mockito:mockito-core:5.7.0")
+    testImplementation(libs.mockito.kotlin)
+
+    ksp(libs.dagger.hilt.compiler)
+    ksp(libs.hilt.compiler)
+
+
+    api(project(":modules:services:analytics"))
+    api(project(":modules:services:compose"))
     implementation(project(":modules:services:images"))
     implementation(project(":modules:services:localization"))
-    implementation(project(":modules:services:model"))
-    implementation(project(":modules:services:preferences"))
-    implementation(project(":modules:services:repositories"))
-    implementation(project(":modules:services:servers"))
-    implementation(project(":modules:services:ui"))
-    implementation(project(":modules:services:utils"))
+    api(project(":modules:services:model"))
+    api(project(":modules:services:preferences"))
+    api(project(":modules:services:repositories"))
+    api(project(":modules:services:servers"))
+    api(project(":modules:services:ui"))
+    api(project(":modules:services:utils"))
 }
