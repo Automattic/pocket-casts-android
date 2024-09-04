5ed51042309b8205f144be69e773102123f9c5a8
Michał Sikora <me@mehow.io>
2024-09-04 11:20:30 +0200

    Optimize localization module config

--- modules/services/localization/build.gradle.kts
+++ modules/services/localization/build.gradle.kts
@@ -1,16 +1,15 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.ksp)
-    alias(libs.plugins.hilt)
-    alias(libs.plugins.kotlin.parcelize)
 }
-
-apply(from = "${project.rootDir}/base.gradle")
 
 android {
     namespace = "au.com.shiftyjelly.pocketcasts.localization"
     buildFeatures {
         buildConfig = true
     }
+}
+
+dependencies {
+    compileOnly(libs.annotation)
 }
