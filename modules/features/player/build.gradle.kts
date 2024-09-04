bbda39ee59ed98da2400746b70db3809be816ad2
Michał Sikora <me@mehow.io>
2024-09-04 11:20:33 +0200

    Optimize player module config

--- modules/features/player/build.gradle.kts
+++ modules/features/player/build.gradle.kts
@@ -6,8 +6,6 @@ plugins {
     alias(libs.plugins.kotlin.parcelize)
 }
 
-apply(from = "${project.rootDir}/base.gradle")
-
 android {
     namespace = "au.com.shiftyjelly.pocketcasts.player"
     buildFeatures {
@@ -18,20 +16,68 @@ android {
 }
 
 dependencies {
+    api(libs.appcompat)
+    implementation(libs.compose.animation)
+    implementation(libs.compose.material)
+    implementation(libs.compose.ui)
+    implementation(libs.compose.ui.tooling.preview)
+    implementation(libs.coroutines.core)
+    implementation(libs.coroutines.rx2)
+    implementation(libs.fragment.ktx)
+    implementation(libs.fragment.compose)
+    api(libs.hilt.android)
+    implementation(libs.lifecycle.reactivestreams.java)
+    implementation(libs.hilt.navigation.compose)
+    implementation(libs.lifecycle.runtime.compose)
+    implementation(libs.lifecycle.viewmodel.compose)
+    api(libs.lottie)
+    implementation(libs.media3.exoplayer)
+    implementation(libs.media3.extractor)
+    api(libs.media3.ui)
+    api(libs.okhttp)
+    implementation(libs.rxandroid)
+    api(libs.rxjava)
+    implementation(libs.rxrelay)
+    implementation(libs.rxkotlin)
+    api(libs.cardview)
+    implementation(libs.cast)
+    api(libs.constraintlayout)
+    implementation(libs.core.ktx)
+    api(libs.material)
+    api(libs.material.progressbar)
+    api(libs.mediarouter)
+    implementation(libs.preference)
+    api(libs.recyclerview)
+    api(libs.showkase)
+    implementation(libs.timber)
+    api(libs.viewpager)
+    implementation(platform(libs.compose.bom))
+
+    testImplementation(libs.arch.core)
+    testImplementation(libs.coroutines.test)
+    testImplementation(libs.junit) { exclude(group = "org.hamcrest") }
+    testImplementation(libs.mockito.kotlin)
+    testImplementation(libs.turbine)
+    testImplementation("org.mockito:mockito-core:5.7.0")
+
+    ksp(libs.dagger.hilt.compiler)
+    ksp(libs.hilt.compiler)
+    ksp(libs.showkase.processor)
+
     // features
-    implementation(project(":modules:features:settings"))
+    api(project(":modules:features:settings"))
     implementation(project(":modules:features:reimagine"))
     // services
-    implementation(project(":modules:services:analytics"))
-    implementation(project(":modules:services:compose"))
+    api(project(":modules:services:analytics"))
+    api(project(":modules:services:compose"))
     implementation(project(":modules:services:images"))
-    implementation(project(":modules:services:localization"))
-    implementation(project(":modules:services:model"))
-    implementation(project(":modules:services:preferences"))
-    implementation(project(":modules:services:repositories"))
-    implementation(project(":modules:services:servers"))
-    implementation(project(":modules:services:utils"))
-    implementation(project(":modules:services:ui"))
-    implementation(project(":modules:services:views"))
+    api(project(":modules:services:localization"))
+    api(project(":modules:services:model"))
+    api(project(":modules:services:preferences"))
+    api(project(":modules:services:repositories"))
+    api(project(":modules:services:servers"))
+    api(project(":modules:services:utils"))
+    api(project(":modules:services:ui"))
+    api(project(":modules:services:views"))
     testImplementation(project(":modules:services:sharedtest"))
 }
