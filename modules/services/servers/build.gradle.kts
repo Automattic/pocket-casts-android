95e6ea1e2881d006e366bc64b0e1e08e60703419
Michał Sikora <me@mehow.io>
2024-09-04 11:20:32 +0200

    Optimize severs module config

--- modules/services/servers/build.gradle.kts
+++ modules/services/servers/build.gradle.kts
@@ -6,8 +6,6 @@ plugins {
     alias(libs.plugins.kotlin.parcelize)
 }
 
-apply(from = "${project.rootDir}/base.gradle")
-
 android {
     namespace = "au.com.shiftyjelly.pocketcasts.servers"
     buildFeatures {
@@ -16,11 +14,34 @@ android {
 }
 
 dependencies {
-    implementation(project(":modules:services:analytics"))
-    implementation(project(":modules:services:crashlogging"))
+    implementation(libs.coroutines.core)
+    api(libs.hilt.android)
+    api(libs.moshi)
+    implementation(libs.moshi.adapters)
+    api(libs.okhttp)
+    implementation(libs.okHttp.logging)
+    api(libs.protobuf.javalite)
+    api(libs.retrofit)
+    implementation(libs.retrofit.moshi)
+    implementation(libs.retrofit.rxjava2)
+    api(libs.rxjava)
+    implementation(libs.rxandroid)
+    api(libs.work.runtime)
+    implementation(libs.retrofit.protobuf) { exclude(group = "com.google.protobuf", module = "protobuf-java") }
+    implementation(libs.timber)
+
+    testImplementation(libs.coroutines.test)
+    testImplementation(libs.junit) { exclude(group = "org.hamcrest") }
+
+    ksp(libs.dagger.hilt.compiler)
+    ksp(libs.hilt.compiler)
+    ksp(libs.moshi.kotlin.codegen)
+    implementation(libs.crashlogging)
+
+
     implementation(project(":modules:services:localization"))
-    implementation(project(":modules:services:model"))
-    implementation(project(":modules:services:preferences"))
-    implementation(project(":modules:services:protobuf"))
+    api(project(":modules:services:model"))
+    api(project(":modules:services:preferences"))
+    api(project(":modules:services:protobuf"))
     implementation(project(":modules:services:utils"))
 }
