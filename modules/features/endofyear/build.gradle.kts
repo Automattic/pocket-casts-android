fc62f7eec7d6b76bdda158bf0ffabb1d207f1e83
Michał Sikora <me@mehow.io>
2024-09-04 11:20:34 +0200

    Optimize endofyear module config

--- modules/features/endofyear/build.gradle.kts
+++ modules/features/endofyear/build.gradle.kts
@@ -3,35 +3,54 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.ksp)
     alias(libs.plugins.hilt)
-    alias(libs.plugins.kotlin.parcelize)
 }
-
-apply(from = "${project.rootDir}/base.gradle")
 
 android {
     namespace = "au.com.shiftyjelly.pocketcasts.endofyear"
     buildFeatures {
         buildConfig = true
-        viewBinding = true
+        viewBinding = false
         compose = true
     }
 }
 
 dependencies {
+    implementation(libs.appcompat)
+    implementation(libs.compose.activity)
+    implementation(libs.compose.material)
+    implementation(libs.compose.constraint)
+    implementation(libs.compose.ui)
+    implementation(libs.compose.ui.tooling.preview)
+    implementation(libs.coroutines.core)
+    api(libs.hilt.android)
+    implementation(libs.core.ktx)
+    api(libs.showkase)
+    implementation(libs.timber)
+    api(libs.crashlogging)
+    implementation(platform(libs.compose.bom))
+
+    testImplementation(libs.coroutines.test)
+    testImplementation(libs.junit) { exclude(group = "org.hamcrest") }
+    testImplementation(libs.mockito.kotlin)
+    testImplementation("org.mockito:mockito-core:5.7.0")
+
+    ksp(libs.dagger.hilt.compiler)
+    ksp(libs.hilt.compiler)
+    ksp(libs.showkase.processor)
+
     // features
     implementation(project(":modules:features:settings"))
     // services
-    implementation(project(":modules:services:analytics"))
+    api(project(":modules:services:analytics"))
     implementation(project(":modules:services:compose"))
-    implementation(project(":modules:services:crashlogging"))
     implementation(project(":modules:services:images"))
     implementation(project(":modules:services:localization"))
-    implementation(project(":modules:services:model"))
-    implementation(project(":modules:services:preferences"))
-    implementation(project(":modules:services:repositories"))
-    implementation(project(":modules:services:servers"))
-    implementation(project(":modules:services:ui"))
-    implementation(project(":modules:services:utils"))
-    implementation(project(":modules:services:views"))
+    api(project(":modules:services:model"))
+    api(project(":modules:services:preferences"))
+    api(project(":modules:services:repositories"))
+    api(project(":modules:services:servers"))
+    api(project(":modules:services:ui"))
+    api(project(":modules:services:utils"))
+    api(project(":modules:services:views"))
     testImplementation(project(":modules:services:sharedtest"))
 }
