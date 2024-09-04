9d74f9955096b912ddd3fb52978549a50153bb98
Michał Sikora <me@mehow.io>
2024-09-04 11:20:34 +0200

    Optimize kids module config

--- modules/features/kids/build.gradle.kts
+++ modules/features/kids/build.gradle.kts
@@ -5,8 +5,6 @@ plugins {
     alias(libs.plugins.hilt)
 }
 
-apply(from = "${project.rootDir}/base.gradle")
-
 android {
     namespace = "au.com.shiftyjelly.pocketcasts.kids"
     buildFeatures {
@@ -16,13 +14,33 @@ android {
 }
 
 dependencies {
+    implementation(libs.compose.animation)
+    implementation(libs.compose.material)
+    implementation(libs.compose.ui)
+    implementation(libs.compose.ui.tooling.preview)
+    implementation(libs.fragment.ktx)
+    api(libs.hilt.android)
+    implementation(libs.core.ktx)
+    api(libs.material)
+    api(libs.showkase)
+    implementation(platform(libs.compose.bom))
+
+    testImplementation(libs.coroutines.test)
+    testImplementation(libs.junit) { exclude(group = "org.hamcrest") }
+    testImplementation(libs.mockito.kotlin)
+    testImplementation("org.mockito:mockito-core:5.7.0")
+
+    ksp(libs.dagger.hilt.compiler)
+    ksp(libs.hilt.compiler)
+    ksp(libs.showkase.processor)
+
+
     implementation(project(":modules:services:compose"))
     implementation(project(":modules:services:localization"))
     implementation(project(":modules:services:images"))
-    implementation(project(":modules:services:ui"))
-    implementation(project(":modules:services:repositories"))
-    implementation(project(":modules:services:servers"))
-    implementation(project(":modules:services:analytics"))
+    api(project(":modules:services:ui"))
+    api(project(":modules:services:repositories"))
+    api(project(":modules:services:analytics"))
 
     testImplementation(project(":modules:services:sharedtest"))
 }
