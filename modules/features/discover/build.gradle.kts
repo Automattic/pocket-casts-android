8633f7bc0ffbcf9a21bc2bb88860f538320a6918
Michał Sikora <me@mehow.io>
2024-09-04 11:20:35 +0200

    Optimize discover module config

--- modules/features/discover/build.gradle.kts
+++ modules/features/discover/build.gradle.kts
@@ -3,37 +3,66 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.ksp)
     alias(libs.plugins.hilt)
-    alias(libs.plugins.kotlin.parcelize)
 }
-
-apply(from = "${project.rootDir}/base.gradle")
 
 android {
     namespace = "au.com.shiftyjelly.pocketcasts.discover"
     buildFeatures {
         buildConfig = true
         viewBinding = true
-        compose = true
+        compose = false
     }
 }
 
 dependencies {
+    api(libs.appcompat)
+    implementation(libs.coroutines.core)
+    implementation(libs.coroutines.rx2)
+    implementation(libs.fragment.ktx)
+    api(libs.hilt.android)
+    api(libs.hilt.work)
+    testImplementation(libs.moshi)
+    testImplementation(libs.okhttp)
+    testImplementation(libs.retrofit)
+    testImplementation(libs.retrofit.moshi)
+    implementation(libs.rxandroid)
+    api(libs.rxjava)
+    implementation(libs.rxrelay)
+    implementation(libs.rxkotlin)
+    api(libs.work.runtime)
+    api(libs.cardview)
+    implementation(libs.coil)
+    api(libs.constraintlayout)
+    implementation(libs.core.ktx)
+    api(libs.material)
+    implementation(libs.material.dialogs)
+    implementation(libs.material.progressbar)
+    implementation(libs.preference)
+    api(libs.recyclerview)
+    implementation(libs.timber)
+    api(libs.crashlogging)
+    implementation(platform(libs.compose.bom))
+
+    testImplementation(libs.coroutines.test)
+    testImplementation(libs.junit) { exclude(group = "org.hamcrest") }
+    testImplementation(libs.okHttp.mockwebserver)
+
+    ksp(libs.dagger.hilt.compiler)
+    ksp(libs.hilt.compiler)
+
     // features
-    implementation(project(":modules:features:account"))
     implementation(project(":modules:features:podcasts"))
     implementation(project(":modules:features:search"))
 
     // services
-    implementation(project(":modules:services:analytics"))
-    implementation(project(":modules:services:compose"))
-    implementation(project(":modules:services:crashlogging"))
+    api(project(":modules:services:analytics"))
     implementation(project(":modules:services:localization"))
     implementation(project(":modules:services:images"))
-    implementation(project(":modules:services:model"))
-    implementation(project(":modules:services:preferences"))
-    implementation(project(":modules:services:repositories"))
-    implementation(project(":modules:services:servers"))
+    api(project(":modules:services:model"))
+    api(project(":modules:services:preferences"))
+    api(project(":modules:services:repositories"))
+    api(project(":modules:services:servers"))
     implementation(project(":modules:services:utils"))
-    implementation(project(":modules:services:ui"))
-    implementation(project(":modules:services:views"))
+    api(project(":modules:services:ui"))
+    api(project(":modules:services:views"))
 }
