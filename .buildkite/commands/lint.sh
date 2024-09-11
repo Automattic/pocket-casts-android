091e60f5e2101a717293cdd019bd567aedc80c80
Wojtek Zieba <wojtek.zieba@automattic.com>
2024-09-11 18:23:01 +0200

    Look only for sarif files when uploading sarif files

--- .buildkite/commands/lint.sh
+++ .buildkite/commands/lint.sh
@@ -3,4 +3,4 @@
 echo "--- 🧹 Linting"
 ./gradlew lintDebug
 
-find . -type f -path "**/build/reports/lint-results*.*" -exec upload_sarif_to_github '{}' 'Automattic' 'pocket-casts-android' \;
+find . -type f -path "**/build/reports/lint-results*.sarif" -exec upload_sarif_to_github '{}' 'Automattic' 'pocket-casts-android' \;
