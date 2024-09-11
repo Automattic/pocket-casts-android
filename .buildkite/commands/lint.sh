d69078e4d56c9ce4bd109b5504c2ca02dbb20b58
Wojtek Zieba <wojtek.zieba@automattic.com>
2024-09-11 19:07:38 +0200

    Send sarif reports even if lint task fails

--- .buildkite/commands/lint.sh
+++ .buildkite/commands/lint.sh
@@ -1,6 +1,9 @@
-#!/bin/bash -eu
+#!/bin/bash -u
 
 echo "--- 🧹 Linting"
 ./gradlew lintDebug
+lint_exit_code=$?
 
 find . -type f -path "**/build/reports/lint-results*.sarif" -exec upload_sarif_to_github '{}' 'Automattic' 'pocket-casts-android' \;
+
+exit $lint_exit_code
