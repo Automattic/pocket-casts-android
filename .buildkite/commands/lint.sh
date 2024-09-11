b7a19ef89901ac087eae13a0b3293b4793e81b4a
Wojtek Zieba <wojtek.zieba@automattic.com>
2024-09-11 19:33:48 +0200

    Remove leftover

--- .buildkite/commands/lint.sh
+++ .buildkite/commands/lint.sh
@@ -4,7 +4,6 @@ echo "--- 🧹 Linting"
 ./gradlew lintDebug
 lint_exit_code=$?
 
- "**/build/reports/lint-results*.sarif" -exec
 upload_sarif_to_github 'app/build/reports/lint-results-debug.sarif' 'Automattic' 'pocket-casts-android'
 
 exit $lint_exit_code
