4a72db4bbce61777a67266ab04bf003ad7f65c1a
Wojtek Zieba <wojtek.zieba@automattic.com>
2024-09-12 09:58:42 +0200

    Update lint task run on CI
    As we use checkDependencies now, we don't want to run lint for each module, but only for application modules.
    
    The `debug` suffix was redundant, as lint runs on default variant only by default.

--- .buildkite/commands/lint.sh
+++ .buildkite/commands/lint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -u
 
 echo "--- 🧹 Linting"
-./gradlew lintDebug
+./gradlew :app:lint :automotive:lint :wear:lint
 lint_exit_code=$?
 
 upload_sarif_to_github 'app/build/reports/lint-results-debug.sarif' 'Automattic' 'pocket-casts-android'
