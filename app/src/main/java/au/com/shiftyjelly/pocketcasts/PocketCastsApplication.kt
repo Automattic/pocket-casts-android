d4dd5ab926a3f1e9dfcc4b7cca622dbe57594b40
Eduarda Barbosa <meduarda.qb@gmail.com>
2024-09-13 10:05:16 -0300

    initialize explat when open app

--- app/src/main/java/au/com/shiftyjelly/pocketcasts/PocketCastsApplication.kt
+++ app/src/main/java/au/com/shiftyjelly/pocketcasts/PocketCastsApplication.kt
@@ -6,6 +6,7 @@ import android.os.StrictMode
 import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
 import au.com.shiftyjelly.pocketcasts.analytics.AnalyticsTracker
+import au.com.shiftyjelly.pocketcasts.analytics.experiments.ExperimentsProvider
 import au.com.shiftyjelly.pocketcasts.crashlogging.InitializeRemoteLogging
 import au.com.shiftyjelly.pocketcasts.discover.worker.CuratedPodcastsSyncWorker
 import au.com.shiftyjelly.pocketcasts.engage.EngageSdkBridge
@@ -121,6 +122,8 @@ class PocketCastsApplication : Application(), Configuration.Provider {
     @Inject lateinit var databaseExportHelper: DatabaseExportHelper
 
     @Inject lateinit var engageSdkBridge: EngageSdkBridge
+
+    @Inject lateinit var experimentsProvider: ExperimentsProvider
 
     override fun onCreate() {
         if (BuildConfig.DEBUG) {
@@ -264,6 +267,7 @@ class PocketCastsApplication : Application(), Configuration.Provider {
         novaLauncherBridge.monitorNovaLauncherIntegration()
         CuratedPodcastsSyncWorker.enqueuPeriodicWork(this)
         engageSdkBridge.registerIntegration()
+        experimentsProvider.initialize()
 
         settings.useDynamicColorsForWidget.flow
             .onEach { widgetManager.updateWidgetFromSettings(playbackManager) }
