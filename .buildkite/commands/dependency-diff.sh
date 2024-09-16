7e3e325b506d20e747aae1f4b5ea7109b61dbb55
Wojtek Zieba <wojtek.zieba@automattic.com>
2024-09-16 12:19:39 +0200

    Update dependency diff step

--- .buildkite/commands/dependency-diff.sh
+++ .buildkite/commands/dependency-diff.sh
@@ -1,9 +1,4 @@
 #!/bin/bash -u
 
-echo "--- :rubygems: Setting up Gems"
-install_gems
-
-echo "--- :closed_lock_with_key: Installing Secrets"
-bundle exec fastlane run configure_apply
-
-comment_with_dependency_diff 'app' 'releaseRuntimeClasspath' 'Automattic' 'pocket-casts-android'
+echo "--- :memo: Commenting on the PR with the dependency diff"
+comment_with_dependency_diff 'app' 'releaseRuntimeClasspath'
