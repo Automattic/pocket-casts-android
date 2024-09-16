a27c0db46954faf6c597d0765aa812472227197c
Eduarda Barbosa <meduarda.qb@gmail.com>
2024-09-16 12:08:22 -0300

    delete unused code

--- modules/services/analytics/src/main/java/au/com/shiftyjelly/pocketcasts/analytics/experiments/Variation.kt
+++ modules/services/analytics/src/main/java/au/com/shiftyjelly/pocketcasts/analytics/experiments/Variation.kt
@@ -3,12 +3,4 @@ package au.com.shiftyjelly.pocketcasts.analytics.experiments
 sealed class Variation {
     data object Control : Variation()
     data class Treatment(val name: String? = null) : Variation()
-
-    companion object {
-        private const val TREATMENT = "treatment"
-        fun fromName(name: String) = when (name) {
-            TREATMENT -> Treatment()
-            else -> Control
-        }
-    }
 }
