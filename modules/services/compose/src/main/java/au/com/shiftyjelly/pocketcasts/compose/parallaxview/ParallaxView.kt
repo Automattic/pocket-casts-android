2229afba10e2887d6b92106ec19abaad0794ff97
Michał Sikora <me@mehow.io>
2024-09-18 00:17:58 -0700

    Add gyroscope animation to referral card (#2875)

--- modules/services/compose/src/main/java/au/com/shiftyjelly/pocketcasts/compose/parallaxview/ParallaxView.kt
+++ modules/services/compose/src/main/java/au/com/shiftyjelly/pocketcasts/compose/parallaxview/ParallaxView.kt
@@ -7,24 +7,22 @@ import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.offset
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.BiasAlignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
+import au.com.shiftyjelly.pocketcasts.utils.DeviceOrientationDetector
+import au.com.shiftyjelly.pocketcasts.utils.OrientationData
 import java.util.LinkedList
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.flow.transform
-import kotlinx.coroutines.launch
 
 // https://rb.gy/pwz9r
 @Composable
@@ -34,9 +32,8 @@ fun ParallaxView(
     content: @Composable (Modifier, BiasAlignment) -> Unit,
 ) {
     val context = LocalContext.current
-    val scope = rememberCoroutineScope()
-
-    var data by remember { mutableStateOf<SensorData?>(null) }
+    val orientationFlow = remember { DeviceOrientationDetector.create(context)?.orientationData()?.runningAverage(3) ?: emptyFlow() }
+    val data by orientationFlow.collectAsState(OrientationData(0f, 0f))
 
     Content(
         modifier = modifier,
@@ -44,30 +41,13 @@ fun ParallaxView(
         depthMultiplier = depthMultiplier,
         content = content,
     )
-
-    DisposableEffect(Unit) {
-        val dataManager = SensorDataManager(context)
-        dataManager.init()
-
-        val job = scope.launch {
-            dataManager.data
-                .receiveAsFlow()
-                .runningAverage(3)
-                .collect { data = it }
-        }
-
-        onDispose {
-            dataManager.cancel()
-            job.cancel()
-        }
-    }
 }
 
 @SuppressLint("UnrememberedMutableState")
 @Composable
 private fun Content(
     modifier: Modifier = Modifier,
-    data: SensorData?,
+    data: OrientationData?,
     depthMultiplier: Int,
     content: @Composable (Modifier, BiasAlignment) -> Unit,
 ) {
@@ -110,29 +90,29 @@ private fun Content(
     }
 }
 
-private fun Flow<SensorData>.runningAverage(windowSize: Int): Flow<SensorData> {
+private fun Flow<OrientationData>.runningAverage(windowSize: Int): Flow<OrientationData> {
     require(windowSize > 0) { "Window size must be positive" }
     return transform {
-        val buffer = LinkedList<SensorData>()
+        val buffer = LinkedList<OrientationData>()
         collect { value ->
             if (buffer.size == windowSize) {
                 buffer.removeFirst()
             }
             buffer.add(value)
-            val bufferAverage = averageSensorData(buffer)
+            val bufferAverage = averageOrientationData(buffer)
             emit(bufferAverage)
         }
     }
 }
 
-private fun averageSensorData(sensorDataList: List<SensorData>): SensorData {
-    require(sensorDataList.isNotEmpty()) { "Sensor data list must not be empty" }
+private fun averageOrientationData(orientationDataList: List<OrientationData>): OrientationData {
+    require(orientationDataList.isNotEmpty()) { "Sensor data list must not be empty" }
 
-    val totalRoll = sensorDataList.sumOf { it.roll.toDouble() }
-    val totalPitch = sensorDataList.sumOf { it.pitch.toDouble() }
+    val totalRoll = orientationDataList.sumOf { it.roll.toDouble() }
+    val totalPitch = orientationDataList.sumOf { it.pitch.toDouble() }
 
-    val averageRoll = (totalRoll / sensorDataList.size).toFloat()
-    val averagePitch = (totalPitch / sensorDataList.size).toFloat()
+    val averageRoll = (totalRoll / orientationDataList.size).toFloat()
+    val averagePitch = (totalPitch / orientationDataList.size).toFloat()
 
-    return SensorData(averageRoll, averagePitch)
+    return OrientationData(averageRoll, averagePitch)
 }
