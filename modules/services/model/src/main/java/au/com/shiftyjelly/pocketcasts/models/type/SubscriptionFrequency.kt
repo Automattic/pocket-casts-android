1727b6461ec8681948603215f3341f6119f25498
Michał Sikora <me@mehow.io>
2024-09-18 12:44:06 +0200

    Clean up R8 configuration

--- modules/services/model/src/main/java/au/com/shiftyjelly/pocketcasts/models/type/SubscriptionFrequency.kt
+++ modules/services/model/src/main/java/au/com/shiftyjelly/pocketcasts/models/type/SubscriptionFrequency.kt
@@ -1,8 +1,10 @@
 package au.com.shiftyjelly.pocketcasts.models.type
 
 import androidx.annotation.StringRes
+import com.squareup.moshi.JsonClass
 import au.com.shiftyjelly.pocketcasts.localization.R as LR
 
+@JsonClass(generateAdapter = false)
 enum class SubscriptionFrequency(val label: String, @StringRes val localisedLabelRes: Int) {
     NONE("none", LR.string.none),
     MONTHLY("monthly", LR.string.plus_monthly),
