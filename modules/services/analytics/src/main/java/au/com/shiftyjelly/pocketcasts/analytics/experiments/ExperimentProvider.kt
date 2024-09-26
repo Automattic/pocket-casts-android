3eb0332ef095201f376917903581945cbf5d5098
Eduarda Barbosa <42220351+mebarbosa@users.noreply.github.com>
2024-09-26 07:41:12 -0300

    [Paywall Experiments] Implement ExPlat (#2871)
    Co-authored-by: Wojtek Zieba <wojtek.zieba@automattic.com>

--- modules/services/analytics/src/main/java/au/com/shiftyjelly/pocketcasts/analytics/experiments/ExperimentProvider.kt
+++ modules/services/analytics/src/main/java/au/com/shiftyjelly/pocketcasts/analytics/experiments/ExperimentProvider.kt
@@ -1,18 +1,50 @@
 package au.com.shiftyjelly.pocketcasts.analytics.experiments
 
+import au.com.shiftyjelly.pocketcasts.analytics.AccountStatusInfo
+import au.com.shiftyjelly.pocketcasts.utils.featureflag.Feature
+import au.com.shiftyjelly.pocketcasts.utils.featureflag.FeatureFlag
 import au.com.shiftyjelly.pocketcasts.utils.log.LogBuffer
-import com.google.firebase.remoteconfig.FirebaseRemoteConfig
-import com.google.firebase.remoteconfig.ktx.get
+import com.automattic.android.experimentation.Experiment
+import com.automattic.android.experimentation.VariationsRepository
+import com.automattic.android.experimentation.domain.Variation.Control
+import com.automattic.android.experimentation.domain.Variation.Treatment
+import java.util.UUID
 import javax.inject.Inject
+import au.com.shiftyjelly.pocketcasts.analytics.experiments.Experiment as ExperimentModel
 
 class ExperimentProvider @Inject constructor(
-    private val firebaseRemoteConfig: FirebaseRemoteConfig,
+    private val accountStatusInfo: AccountStatusInfo,
+    private val repository: VariationsRepository,
 ) {
+    companion object {
+        const val TAG = "ExperimentsProvider"
+        const val PLATFORM = "pocketcasts"
+    }
 
-    fun getVariation(experiment: Experiment): Variation {
-        val name = firebaseRemoteConfig.getString(experiment.identifier)
-        val variation = Variation.fromName(name)
-        LogBuffer.i(LogBuffer.TAG_BACKGROUND_TASKS, "Experiment $experiment, variation ${if (name.isBlank()) "not set" else variation.toString()}")
-        return variation
+    fun initialize() {
+        if (FeatureFlag.isEnabled(Feature.EXPLAT_EXPERIMENT)) {
+            val uuid = accountStatusInfo.getUuid() ?: UUID.randomUUID().toString().replace("-", "")
+
+            LogBuffer.i(TAG, "Initializing experiments with uuid: $uuid")
+
+            repository.initialize(anonymousId = uuid, oAuthToken = null)
+        }
+    }
+
+    fun clear() {
+        if (FeatureFlag.isEnabled(Feature.EXPLAT_EXPERIMENT)) {
+            LogBuffer.i(TAG, "Clearing experiments")
+            repository.clear()
+        }
+    }
+
+    fun getVariation(experiment: ExperimentModel): Variation? {
+        if (!FeatureFlag.isEnabled(Feature.EXPLAT_EXPERIMENT)) return null
+
+        return when (val variation = repository.getVariation(Experiment(experiment.identifier))) {
+            is Control -> Variation.Control
+            is Treatment -> Variation.Treatment(variation.name)
+            else -> null
+        }
     }
 }
