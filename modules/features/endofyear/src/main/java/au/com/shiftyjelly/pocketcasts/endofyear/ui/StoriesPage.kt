1bb47339129e0f2347e627872299eb79cf4546dc
Michał Sikora <me@mehow.io>
2024-10-14 15:57:00 +0200

    Limit stories size

--- modules/features/endofyear/src/main/java/au/com/shiftyjelly/pocketcasts/endofyear/ui/StoriesPage.kt
+++ modules/features/endofyear/src/main/java/au/com/shiftyjelly/pocketcasts/endofyear/ui/StoriesPage.kt
@@ -1,12 +1,15 @@
 package au.com.shiftyjelly.pocketcasts.endofyear.ui
 
+import android.content.Context
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.BoxWithConstraintsScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.LinearProgressIndicator
@@ -16,68 +19,72 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import au.com.shiftyjelly.pocketcasts.compose.components.TextH10
 import au.com.shiftyjelly.pocketcasts.compose.components.TextP50
 import au.com.shiftyjelly.pocketcasts.endofyear.Story
 import au.com.shiftyjelly.pocketcasts.endofyear.UiState
+import au.com.shiftyjelly.pocketcasts.utils.Util
 import kotlinx.coroutines.launch
 
 @Composable
 internal fun StoriesPage(
     state: UiState,
 ) {
-    when (state) {
-        is UiState.Synced -> Stories(state.stories)
-        is UiState.Syncing -> LoadingIndicator()
-        is UiState.Failure -> ErrorMessage()
+    val size = getSizeLimit(LocalContext.current)?.let(Modifier::size) ?: Modifier.fillMaxSize()
+    BoxWithConstraints(
+        modifier = Modifier.then(size),
+    ) {
+        when (state) {
+            is UiState.Synced -> Stories(state.stories)
+            is UiState.Syncing -> LoadingIndicator()
+            is UiState.Failure -> ErrorMessage()
+        }
     }
 }
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-private fun Stories(
+private fun BoxWithConstraintsScope.Stories(
     stories: List<Story>,
 ) {
-    BoxWithConstraints(
-        modifier = Modifier.fillMaxSize(),
-    ) {
-        val pagerState = rememberPagerState(pageCount = { stories.size })
-        val coroutineScope = rememberCoroutineScope()
-        val widthPx = LocalDensity.current.run { maxWidth.toPx() }
+    val pagerState = rememberPagerState(pageCount = { stories.size })
+    val coroutineScope = rememberCoroutineScope()
+    val widthPx = LocalDensity.current.run { maxWidth.toPx() }
 
-        HorizontalPager(
-            state = pagerState,
-            userScrollEnabled = false,
-            modifier = Modifier.pointerInput(Unit) {
-                detectTapGestures { offset ->
-                    if (!pagerState.isScrollInProgress) {
-                        coroutineScope.launch {
-                            val nextPage = if (offset.x > widthPx / 2) {
-                                pagerState.currentPage + 1
-                            } else {
-                                pagerState.currentPage - 1
-                            }
-                            pagerState.scrollToPage(nextPage)
+    HorizontalPager(
+        state = pagerState,
+        userScrollEnabled = false,
+        modifier = Modifier.pointerInput(Unit) {
+            detectTapGestures { offset ->
+                if (!pagerState.isScrollInProgress) {
+                    coroutineScope.launch {
+                        val nextPage = if (offset.x > widthPx / 2) {
+                            pagerState.currentPage + 1
+                        } else {
+                            pagerState.currentPage - 1
                         }
+                        pagerState.scrollToPage(nextPage)
                     }
                 }
-            },
-        ) { index ->
-            when (val story = stories[index]) {
-                is Story.Cover -> StoryPlaceholder(story)
-                is Story.NumberOfShows -> StoryPlaceholder(story)
-                is Story.TopShow -> StoryPlaceholder(story)
-                is Story.TopShows -> StoryPlaceholder(story)
-                is Story.Ratings -> StoryPlaceholder(story)
-                is Story.TotalTime -> StoryPlaceholder(story)
-                is Story.LongestEpisode -> StoryPlaceholder(story)
-                is Story.PlusInterstitial -> StoryPlaceholder(story)
-                is Story.YearVsYear -> StoryPlaceholder(story)
-                is Story.CompletionRate -> StoryPlaceholder(story)
-                is Story.Ending -> StoryPlaceholder(story)
             }
+        },
+    ) { index ->
+        when (val story = stories[index]) {
+            is Story.Cover -> StoryPlaceholder(story)
+            is Story.NumberOfShows -> StoryPlaceholder(story)
+            is Story.TopShow -> StoryPlaceholder(story)
+            is Story.TopShows -> StoryPlaceholder(story)
+            is Story.Ratings -> StoryPlaceholder(story)
+            is Story.TotalTime -> StoryPlaceholder(story)
+            is Story.LongestEpisode -> StoryPlaceholder(story)
+            is Story.PlusInterstitial -> StoryPlaceholder(story)
+            is Story.YearVsYear -> StoryPlaceholder(story)
+            is Story.CompletionRate -> StoryPlaceholder(story)
+            is Story.Ending -> StoryPlaceholder(story)
         }
     }
 }
@@ -106,6 +113,18 @@ private val Story.backgroundColor get() = when (this) {
     is Story.YearVsYear -> Color(0xFFEEB1F4)
     is Story.CompletionRate -> Color(0xFFE0EFAD)
     is Story.Ending -> Color(0xFFEE661C)
+}
+
+private fun getSizeLimit(context: Context): DpSize? {
+    return if (Util.isTablet(context)) {
+        val configuration = context.resources.configuration
+        val screenHeightInDp = configuration.screenHeightDp
+        val dialogHeight = (screenHeightInDp * 0.9f).coerceAtMost(700.dp.value)
+        val dialogWidth = dialogHeight / 2f
+        DpSize(dialogWidth.dp, dialogHeight.dp)
+    } else {
+        null
+    }
 }
 
 @Composable
