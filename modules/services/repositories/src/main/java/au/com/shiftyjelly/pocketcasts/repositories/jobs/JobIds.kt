e406ef30e933bffea2d35d766ba6e599ead58b7c
ashiagr <agr.ash@gmail.com>
2024-10-18 10:45:58 +0530

    Remove unused job ids

--- modules/services/repositories/src/main/java/au/com/shiftyjelly/pocketcasts/repositories/jobs/JobIds.kt
+++ modules/services/repositories/src/main/java/au/com/shiftyjelly/pocketcasts/repositories/jobs/JobIds.kt
@@ -2,6 +2,4 @@ package au.com.shiftyjelly.pocketcasts.repositories.jobs
 
 object JobIds {
     const val REFRESH_PODCASTS_JOB_ID = 51111
-    const val EPISODE_METADATA_JOB_ID = 62222
-    const val VERSION_MIGRATION_JOB_ID = 70000
 }
