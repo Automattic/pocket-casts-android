f38fc32cebcee2e6ff81023c80b4115751be08b7
Michał Sikora <me@mehow.io>
2024-10-21 13:33:53 +0200

    Make sure that close button is in the top right corner

--- modules/features/endofyear/src/main/java/au/com/shiftyjelly/pocketcasts/endofyear/ui/StoriesPage.kt
+++ modules/features/endofyear/src/main/java/au/com/shiftyjelly/pocketcasts/endofyear/ui/StoriesPage.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -168,6 +169,7 @@ internal fun BoxScope.TopControls(
         horizontalAlignment = Alignment.End,
         modifier = Modifier
             .align(Alignment.TopCenter)
+            .fillMaxWidth()
             .padding(top = 8.dp, start = 16.dp, end = 16.dp),
     ) {
         PagerProgressingIndicator(
