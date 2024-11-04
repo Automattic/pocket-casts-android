136d6eaa4346929fc8739b3a4b29f2301c72c9dd
Eduarda Barbosa <meduarda.qb@gmail.com>
2024-11-04 08:44:55 -0300

    simplify layout

--- modules/services/views/src/main/java/au/com/shiftyjelly/pocketcasts/views/lowstorage/LowStorageDialog.kt
+++ modules/services/views/src/main/java/au/com/shiftyjelly/pocketcasts/views/lowstorage/LowStorageDialog.kt
@@ -1,7 +1,6 @@
 package au.com.shiftyjelly.pocketcasts.views.lowstorage
 
 import android.view.ViewGroup
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -85,70 +84,64 @@ internal fun LowStorageDialog(
 ) {
     val formattedTotalDownloadSize = Util.formattedBytes(bytes = totalDownloadSize, context = LocalContext.current)
 
-    Box(
-        modifier = modifier
-            .fillMaxWidth(),
-        contentAlignment = Alignment.Center,
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.fillMaxWidth(),
     ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = modifier.fillMaxWidth(),
-        ) {
-            Icon(
-                painter = painterResource(IR.drawable.swipe_affordance),
-                contentDescription = stringResource(LR.string.drag_down_to_dismiss_content_description),
-                tint = MaterialTheme.theme.colors.primaryIcon02,
-                modifier = Modifier
-                    .width(56.dp)
-                    .padding(top = 8.dp, bottom = 24.dp),
-            )
+        Icon(
+            painter = painterResource(IR.drawable.swipe_affordance),
+            contentDescription = stringResource(LR.string.drag_down_to_dismiss_content_description),
+            tint = MaterialTheme.theme.colors.primaryIcon02,
+            modifier = Modifier
+                .width(56.dp)
+                .padding(top = 8.dp, bottom = 24.dp),
+        )
 
-            Icon(
-                painterResource(IR.drawable.pencil_cleanup),
-                contentDescription = stringResource(LR.string.pencil_clean_up_icon_content_description),
-                modifier = Modifier
-                    .padding(bottom = 16.dp)
-                    .size(40.dp),
-                tint = MaterialTheme.theme.colors.primaryInteractive01,
-            )
+        Icon(
+            painterResource(IR.drawable.pencil_cleanup),
+            contentDescription = stringResource(LR.string.pencil_clean_up_icon_content_description),
+            modifier = Modifier
+                .padding(bottom = 16.dp)
+                .size(40.dp),
+            tint = MaterialTheme.theme.colors.primaryInteractive01,
+        )
 
-            TextH20(
-                text = stringResource(LR.string.need_to_free_up_space),
-                textAlign = TextAlign.Center,
-                modifier = modifier.padding(bottom = 10.dp, start = 21.dp, end = 21.dp),
-            )
+        TextH20(
+            text = stringResource(LR.string.need_to_free_up_space),
+            textAlign = TextAlign.Center,
+            modifier = modifier.padding(bottom = 10.dp, start = 21.dp, end = 21.dp),
+        )
 
-            TextH50(
-                text = stringResource(LR.string.save_space_by_managing_downloaded_episodes, formattedTotalDownloadSize),
-                fontWeight = FontWeight.W500,
-                textAlign = TextAlign.Center,
-                modifier = modifier.padding(bottom = 57.dp, start = 21.dp, end = 21.dp),
-            )
+        TextH50(
+            text = stringResource(LR.string.save_space_by_managing_downloaded_episodes, formattedTotalDownloadSize),
+            fontWeight = FontWeight.W500,
+            textAlign = TextAlign.Center,
+            modifier = modifier.padding(bottom = 57.dp, start = 21.dp, end = 21.dp),
+        )
 
-            RowButton(
-                text = stringResource(LR.string.manage_downloads),
-                onClick = { onManageDownloadsClick.invoke() },
-                includePadding = false,
-                textColor = MaterialTheme.theme.colors.primaryInteractive02,
-                colors = ButtonDefaults.buttonColors(
-                    backgroundColor = MaterialTheme.theme.colors.primaryInteractive01,
-                ),
-                modifier = modifier
-                    .padding(horizontal = 21.dp)
-                    .padding(bottom = 12.dp),
-            )
+        RowButton(
+            text = stringResource(LR.string.manage_downloads),
+            onClick = { onManageDownloadsClick.invoke() },
+            includePadding = false,
+            textColor = MaterialTheme.theme.colors.primaryInteractive02,
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = MaterialTheme.theme.colors.primaryInteractive01,
+            ),
+            modifier = modifier
+                .padding(horizontal = 21.dp)
+                .padding(bottom = 12.dp),
+        )
 
-            RowOutlinedButton(
-                text = stringResource(LR.string.maybe_later),
-                onClick = { onMaybeLaterClick.invoke() },
-                includePadding = false,
-                modifier = modifier
-                    .padding(bottom = 12.dp)
-                    .padding(horizontal = 21.dp),
-                border = null,
-                colors = ButtonDefaults.outlinedButtonColors(contentColor = MaterialTheme.theme.colors.primaryText01),
-            )
-        }
+        RowOutlinedButton(
+            text = stringResource(LR.string.maybe_later),
+            onClick = { onMaybeLaterClick.invoke() },
+            includePadding = false,
+            modifier = modifier
+                .padding(bottom = 12.dp)
+                .padding(horizontal = 21.dp),
+            border = null,
+            colors = ButtonDefaults.outlinedButtonColors(contentColor = MaterialTheme.theme.colors.primaryText01),
+        )
     }
 }
 
