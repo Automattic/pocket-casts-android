597bd818cbb5a22b7c79346dafd0090745b29fe8
Michał Sikora <me@mehow.io>
2024-11-05 16:21:11 +0100

    Add cleaning interceptor

---
+++ modules/services/servers/src/main/java/au/com/shiftyjelly/pocketcasts/servers/CleanAndRetryInterceptor.kt
@@ -0,0 +1,21 @@
+package au.com.shiftyjelly.pocketcasts.servers
+
+import okhttp3.Interceptor
+import okhttp3.Response
+
+internal class CleanAndRetryInterceptor(
+    private val headersToRemove: List<String>,
+) : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalRequest = chain.request()
+        val response = chain.proceed(originalRequest)
+        return if (response.isSuccessful || response.isRedirect) {
+            response
+        } else {
+            val cleanRequest = originalRequest.newBuilder()
+                .let { builder -> headersToRemove.fold(builder) { acc, header -> acc.removeHeader(header) } }
+                .build()
+            chain.proceed(cleanRequest)
+        }
+    }
+}
