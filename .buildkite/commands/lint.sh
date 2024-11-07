b390380f56a3ec05502b46323cbfc4fca88530fd
Wojciech Zięba <wojtek.zieba@automattic.com>
2024-11-07 12:37:20 +0100

    Run lint in release variant (#2868)

--- .buildkite/commands/lint.sh
+++ .buildkite/commands/lint.sh
@@ -1,11 +1,17 @@
 #!/bin/bash -u
 
+echo "--- :rubygems: Setting up Gems"
+install_gems
+
+echo "--- :closed_lock_with_key: Installing Secrets"
+bundle exec fastlane run configure_apply
+
 echo "--- 🧹 Linting"
 # Run lint on app module first, to make sure that the lint-results-debug.sarif file is always generated
-./gradlew :app:lint
+./gradlew :app:lintRelease
 app_lint_exit_code=$?
 
-./gradlew :automotive:lint :wear:lint
+./gradlew :automotive:lintRelease :wear:lintRelease
 automotive_wear_lint_exit_code=$?
 
 if [ $app_lint_exit_code -ne 0 ] || [ $automotive_wear_lint_exit_code -ne 0 ]; then
@@ -14,6 +20,6 @@ else
   lint_exit_code=0
 fi
 
-upload_sarif_to_github 'app/build/reports/lint-results-debug.sarif'
+upload_sarif_to_github 'app/build/reports/lint-results-release.sarif'
 
 exit $lint_exit_code
