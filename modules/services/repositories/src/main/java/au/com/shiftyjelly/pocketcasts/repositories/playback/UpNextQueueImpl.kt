7068161e471f1759d84ba3bf07d504dc004e03cc
Philip Simpson <philip.simpson@automattic.com>
2024-11-18 13:31:53 +1030

    Fix test issue

--- modules/services/repositories/src/main/java/au/com/shiftyjelly/pocketcasts/repositories/playback/UpNextQueueImpl.kt
+++ modules/services/repositories/src/main/java/au/com/shiftyjelly/pocketcasts/repositories/playback/UpNextQueueImpl.kt
@@ -205,8 +205,9 @@ class UpNextQueueImpl @Inject constructor(
 
         val mutableList = episodes.toMutableList()
         if (isEmpty) {
-            playNextNow(mutableList.first(), downloadManager, null)
-            mutableList.removeFirst()
+            val firstEpisode = mutableList.first()
+            playNextNow(firstEpisode, downloadManager, null)
+            mutableList.remove(firstEpisode)
         }
 
         mutableList.asReversed().forEach {
