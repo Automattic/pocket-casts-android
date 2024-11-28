a02b17d3b779c27576e4d15d1b41f3abf5b796fa
Michał Sikora <me@mehow.io>
2024-11-28 11:54:35 +0100

    Move Pocket Casts red color to a constant

--- modules/services/compose/src/main/java/au/com/shiftyjelly/pocketcasts/compose/PocketCastsColors.kt
+++ modules/services/compose/src/main/java/au/com/shiftyjelly/pocketcasts/compose/PocketCastsColors.kt
@@ -3,6 +3,8 @@ package au.com.shiftyjelly.pocketcasts.compose
 import androidx.compose.ui.graphics.Color
 
 object PocketCastsColors {
+    val pocketRed = Color(0xFFF43E37)
+
     val plusGold = Color(0xFFFFD846)
     val plusGoldLight = plusGold
     val plusGoldDark = Color(0xFFFEB525)
