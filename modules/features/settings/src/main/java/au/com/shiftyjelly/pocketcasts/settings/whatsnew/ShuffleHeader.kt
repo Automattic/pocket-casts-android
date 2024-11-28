4ff23d15ee1e91e64afa4e1b183b24b557d0fbbf
Eduarda Barbosa <meduarda.qb@gmail.com>
2024-11-28 08:17:56 -0300

    update shuffle image size

--- modules/features/settings/src/main/java/au/com/shiftyjelly/pocketcasts/settings/whatsnew/ShuffleHeader.kt
+++ modules/features/settings/src/main/java/au/com/shiftyjelly/pocketcasts/settings/whatsnew/ShuffleHeader.kt
@@ -1,5 +1,6 @@
 package au.com.shiftyjelly.pocketcasts.settings.whatsnew
 
+import android.content.res.Configuration
 import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
@@ -13,6 +14,7 @@ import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -38,6 +40,7 @@ fun ShuffleHeader(modifier: Modifier = Modifier) {
         painter = painterResource(id = IR.drawable.shuffle),
         contentDescription = stringResource(LR.string.up_next_shuffle_button_content_description),
         tint = MaterialTheme.colors.primary.copy(alpha = animatedAlpha),
-        modifier = modifier.size(120.dp),
+        modifier = modifier
+            .size(if (LocalConfiguration.current.orientation == Configuration.ORIENTATION_PORTRAIT) 120.dp else 80.dp),
     )
 }
