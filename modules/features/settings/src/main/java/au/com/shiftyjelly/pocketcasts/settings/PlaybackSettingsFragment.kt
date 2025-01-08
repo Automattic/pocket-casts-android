36e1af37cc0585ed738645dc95ef197ccfa0cec4
Eduarda Barbosa <42220351+mebarbosa@users.noreply.github.com>
2025-01-08 00:32:37 -0300

    Tracks Audit: Add General defaults events (#3411)

--- modules/features/settings/src/main/java/au/com/shiftyjelly/pocketcasts/settings/PlaybackSettingsFragment.kt
+++ modules/features/settings/src/main/java/au/com/shiftyjelly/pocketcasts/settings/PlaybackSettingsFragment.kt
@@ -692,7 +692,11 @@ class PlaybackSettingsFragment : BaseFragment() {
                 ),
             )
             .setIconId(R.drawable.ic_podcasts)
+            .setOnSecondary {
+                analyticsTracker.track(AnalyticsEvent.SETTINGS_GENERAL_EPISODE_GROUPING_DO_NOT_APPLY_TO_EXISTING)
+            }
             .setOnConfirm {
+                analyticsTracker.track(AnalyticsEvent.SETTINGS_GENERAL_EPISODE_GROUPING_APPLY_TO_EXISTING)
                 applicationScope.launch {
                     podcastManager.updateGroupingForAllBlocking(grouping)
                 }
@@ -707,7 +711,11 @@ class PlaybackSettingsFragment : BaseFragment() {
             .setTitle(getString(LR.string.settings_apply_to_existing_podcasts))
             .setSummary(getString(if (shouldShow) LR.string.settings_apply_archived_show else LR.string.settings_apply_archived_hide))
             .setIconId(R.drawable.ic_podcasts)
+            .setOnSecondary {
+                analyticsTracker.track(AnalyticsEvent.SETTINGS_GENERAL_ARCHIVED_EPISODES_DO_NOT_APPLY_TO_EXISTING)
+            }
             .setOnConfirm {
+                analyticsTracker.track(AnalyticsEvent.SETTINGS_GENERAL_ARCHIVED_EPISODES_APPLY_TO_EXISTING)
                 applicationScope.launch {
                     podcastManager.updateAllShowArchived(shouldShow)
                 }
