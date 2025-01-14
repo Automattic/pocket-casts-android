967a093b4e514acbaf9f10e2164a4d4b58dbedff
Eduarda Barbosa <meduarda.qb@gmail.com>
2025-01-14 10:13:47 -0300

    ignore media session stop event when casting

--- modules/services/repositories/src/main/java/au/com/shiftyjelly/pocketcasts/repositories/playback/MediaSessionManager.kt
+++ modules/services/repositories/src/main/java/au/com/shiftyjelly/pocketcasts/repositories/playback/MediaSessionManager.kt
@@ -687,6 +687,15 @@ class MediaSessionManager(
                 .subscribeBy(onError = { Timber.e(it) })
         }
 
+        // note: the stop event is called from cars when they only want to pause, this is less destructive and doesn't cause issues if they try to play again
+        override fun onStop() {
+            // This event is causing issues during casting, so as a temporary solution, we are going to ignore it for now
+            if (playbackManager.player !is CastPlayer) {
+                logEvent("stop")
+                enqueueCommand("stop") { playbackManager.pauseSuspend(sourceView = source) }
+            }
+        }
+
         override fun onSkipToPrevious() {
             onRewind()
         }
