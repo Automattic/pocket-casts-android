7d6941ee4c4f08845d60d0f541fe0ff043b27c68
Michał Sikora <me@mehow.io>
2025-01-29 11:32:08 +0100

    Revert "Bump androidx.core:core-ktx from 1.13.1 to 1.15.0 (#3207)"
    This reverts commit f4efbe43491e94fda12d6c58931e7002478173b8.

--- modules/features/podcasts/src/main/java/au/com/shiftyjelly/pocketcasts/podcasts/view/podcast/PodcastAdapter.kt
+++ modules/features/podcasts/src/main/java/au/com/shiftyjelly/pocketcasts/podcasts/view/podcast/PodcastAdapter.kt
@@ -685,7 +685,7 @@ class PodcastAdapter(
         podcast.podcastUrl?.let { url ->
             if (url.isNotBlank()) {
                 try {
-                    context.startActivity(webUrlToIntent(url), null)
+                    startActivity(context, webUrlToIntent(url), null)
                 } catch (e: Exception) {
                     Timber.e(e, "Failed to open podcast web page.")
                 }
