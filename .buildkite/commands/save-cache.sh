f456e705399965d71e49dd6ff4b96f2927429701
Petros Paraskevopoulos <9729923+ParaskP7@users.noreply.github.com>
2025-01-30 11:13:05 +0200

    [Dependency Cache] Use Custom Init Gradle Task to Download Dependencies (#3512)

--- .buildkite/commands/save-cache.sh
+++ .buildkite/commands/save-cache.sh
@@ -2,33 +2,8 @@
 
 set -euo pipefail
 
-echo "--- :rubygems: Setting up Gems"
-install_gems
-
-echo "--- :closed_lock_with_key: Installing Secrets"
-bundle exec fastlane run configure_apply
-
-# .buildkite/pipeline.yml -> ./gradlew assembleRelease
-# .buildkite/commands/prototype-build.sh -> build_and_upload_prototype_build
-# -> prototype_build_type = 'debugProd'
-echo "--- 🛠 Download Mobile App Dependencies [Assemble Apps]"
-./gradlew assembleDebug
-echo ""
-
-# .buildkite/commands/lint.sh -> ./gradlew :app:lintRelease + ./gradlew :automotive:lintRelease :wear:lintRelease
-echo "--- 🧹 Download Lint Dependencies [Lint Apps]"
-./gradlew :app:lintDebug :automotive:lintDebug :wear:lintDebug
-echo ""
-
-# .buildkite/pipeline.yml -> ./gradlew testDebugUnitTest
-echo "--- 🧪 Download Unit Test Dependencies [Assemble Unit Tests]"
-./gradlew assembleDebugUnitTest
-echo ""
-
-# .buildkite/pipeline.yml -> build_and_instrumented_test
-# -> gradle(tasks: %w[assembleDebug assembleDebugAndroidTest])
-echo "--- 🧪 Download Android Test Dependencies [Assemble Android Tests]"
-./gradlew assembleDebugAndroidTest
+echo "--- 📦 Download Dependencies"
+./gradlew downloadDependencies
 echo ""
 
 echo "--- 💾 Save Cache"
