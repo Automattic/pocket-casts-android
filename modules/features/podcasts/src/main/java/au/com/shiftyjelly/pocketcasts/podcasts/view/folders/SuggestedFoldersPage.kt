4eab0c27b9af770b38ef9bb88ee44b9ed6e91d24
Eduarda Barbosa <42220351+mebarbosa@users.noreply.github.com>
2025-02-28 10:46:07 -0300

    [Suggested Folders] Add How it Works (#3668)

--- modules/features/podcasts/src/main/java/au/com/shiftyjelly/pocketcasts/podcasts/view/folders/SuggestedFoldersPage.kt
+++ modules/features/podcasts/src/main/java/au/com/shiftyjelly/pocketcasts/podcasts/view/folders/SuggestedFoldersPage.kt
@@ -14,6 +14,7 @@ import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -21,7 +22,12 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.clearAndSetSemantics
 import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.text.LinkAnnotation
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextLinkStyles
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
@@ -31,7 +37,6 @@ import au.com.shiftyjelly.pocketcasts.compose.CallOnce
 import au.com.shiftyjelly.pocketcasts.compose.buttons.RowButton
 import au.com.shiftyjelly.pocketcasts.compose.buttons.RowOutlinedButton
 import au.com.shiftyjelly.pocketcasts.compose.components.TextH10
-import au.com.shiftyjelly.pocketcasts.compose.components.TextP40
 import au.com.shiftyjelly.pocketcasts.compose.folder.FolderImage
 import au.com.shiftyjelly.pocketcasts.compose.preview.ThemePreviewParameterProvider
 import au.com.shiftyjelly.pocketcasts.compose.theme
@@ -42,10 +47,12 @@ import au.com.shiftyjelly.pocketcasts.localization.R as LR
 @Composable
 fun SuggestedFoldersPage(
     folders: List<Folder>,
+    useWhiteColorForHowItWorks: Boolean,
     onShown: () -> Unit,
     onDismiss: () -> Unit,
     onUseTheseFolders: () -> Unit,
     onCreateCustomFolders: () -> Unit,
+    onHowItWorks: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
     CallOnce {
@@ -84,11 +91,13 @@ fun SuggestedFoldersPage(
                 )
             }
             item(span = { GridItemSpan(maxLineSpan) }) {
-                TextP40(
-                    text = stringResource(LR.string.suggested_folders_subtitle),
-                    color = MaterialTheme.theme.colors.primaryText02,
+                SuggestedFoldersDescription(
+                    textColor = MaterialTheme.theme.colors.primaryText02,
+                    secondTextColor = if (useWhiteColorForHowItWorks) Color.White else MaterialTheme.theme.colors.primaryInteractive01,
                     modifier = Modifier.padding(bottom = 10.dp),
-                )
+                ) {
+                    onHowItWorks.invoke()
+                }
             }
             items(
                 count = folders.size,
@@ -144,6 +153,44 @@ fun FolderItem(folderName: String, folderColor: Color, podcastUuids: List<String
     )
 }
 
+@Composable
+private fun SuggestedFoldersDescription(
+    textColor: Color,
+    secondTextColor: Color,
+    modifier: Modifier = Modifier,
+    onSecondTextClick: () -> Unit,
+) {
+    val firstString = stringResource(LR.string.suggested_folders_subtitle)
+    val secondString = stringResource(LR.string.suggested_folders_how_it_works)
+
+    val annotatedString = buildAnnotatedString {
+        append(firstString)
+        append(" ")
+        addLink(
+            url = LinkAnnotation.Url(
+                url = "",
+                styles = TextLinkStyles(style = SpanStyle(color = secondTextColor)),
+            ) {
+                onSecondTextClick.invoke()
+            },
+            start = firstString.length + 1,
+            end = firstString.length + 1 + secondString.length,
+        )
+
+        withStyle(style = SpanStyle(color = secondTextColor, fontWeight = FontWeight.W700)) {
+            append(secondString)
+        }
+    }
+
+    Text(
+        text = annotatedString,
+        color = textColor,
+        modifier = modifier,
+        fontSize = 16.sp,
+        lineHeight = 22.sp,
+    )
+}
+
 @Preview(showBackground = true)
 @Composable
 private fun SuggestedFoldersPagePreview(@PreviewParameter(ThemePreviewParameterProvider::class) themeType: Theme.ThemeType) {
@@ -153,6 +200,8 @@ private fun SuggestedFoldersPagePreview(@PreviewParameter(ThemePreviewParameterP
             onUseTheseFolders = {},
             onCreateCustomFolders = {},
             onShown = {},
+            onHowItWorks = {},
+            useWhiteColorForHowItWorks = false,
             folders = listOf(
                 Folder("Folder 1", listOf("2e61ba20-50a9-0135-902b-63f4b61a9224", "2e61ba20-50a9-0135-902b-63f4b61a9224"), 1),
                 Folder("Folder 2", listOf("2e61ba20-50a9-0135-902b-63f4b61a9224", "2e61ba20-50a9-0135-902b-63f4b61a9224"), 2),
