76a2abc468a68d6cffbd26ad3e7955c627d25e1e
Michał Sikora <me@mehow.io>
2025-03-04 22:11:55 -0800

    Remove obsolete suggested folders code (#3701)

--- modules/features/podcasts/src/main/java/au/com/shiftyjelly/pocketcasts/podcasts/view/folders/SuggestedFoldersViewModel.kt
+++ modules/features/podcasts/src/main/java/au/com/shiftyjelly/pocketcasts/podcasts/view/folders/SuggestedFoldersViewModel.kt
@@ -2,7 +2,6 @@ package au.com.shiftyjelly.pocketcasts.podcasts.view.folders
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import au.com.shiftyjelly.pocketcasts.analytics.AnalyticsEvent
 import au.com.shiftyjelly.pocketcasts.analytics.AnalyticsTracker
 import au.com.shiftyjelly.pocketcasts.models.entity.SuggestedFolderDetails
 import au.com.shiftyjelly.pocketcasts.models.type.PodcastsSortType
@@ -30,25 +29,11 @@ class SuggestedFoldersViewModel @Inject constructor(
     private val _state = MutableStateFlow<FoldersState>(FoldersState.Idle)
     val state: StateFlow<FoldersState> = _state
 
-    fun onShown() {
-        analyticsTracker.track(AnalyticsEvent.SUGGESTED_FOLDERS_MODAL_SHOWN)
-    }
-
-    fun onDismissed() {
-        analyticsTracker.track(AnalyticsEvent.SUGGESTED_FOLDERS_MODAL_DISMISSED)
-    }
-
     fun onReplaceExistingFoldersShown() {
-        analyticsTracker.track(AnalyticsEvent.SUGGESTED_FOLDERS_REPLACE_EXISTING_FOLDERS_MODAL_SHOWN)
         _state.value = FoldersState.Idle
     }
 
-    fun onReplaceExistingFoldersTapped() {
-        analyticsTracker.track(AnalyticsEvent.SUGGESTED_FOLDERS_REPLACE_FOLDERS_TAPPED)
-    }
-
     fun onUseTheseFolders(folders: List<Folder>) {
-        analyticsTracker.track(AnalyticsEvent.SUGGESTED_FOLDERS_MODAL_USE_THESE_FOLDERS_TAPPED)
         viewModelScope.launch {
             val currentFoldersCount = folderManager.countFolders()
             if (currentFoldersCount > 0) {
@@ -57,10 +42,6 @@ class SuggestedFoldersViewModel @Inject constructor(
                 overrideFoldersWithSuggested(folders)
             }
         }
-    }
-
-    fun onCreateCustomFolders() {
-        analyticsTracker.track(AnalyticsEvent.SUGGESTED_FOLDERS_MODAL_CREATE_CUSTOM_FOLDERS_TAPPED)
     }
 
     fun overrideFoldersWithSuggested(folders: List<Folder>) {
@@ -81,14 +62,6 @@ class SuggestedFoldersViewModel @Inject constructor(
             suggestedFoldersManager.deleteSuggestedFolders(folders.toSuggestedFolders())
             _state.value = FoldersState.Created
         }
-    }
-
-    fun onHowItWorksTapped() {
-        analyticsTracker.track(AnalyticsEvent.SUGGESTED_FOLDERS_HOW_IT_WORKS_TAPPED)
-    }
-
-    fun onHowItWorksGotItTapped() {
-        analyticsTracker.track(AnalyticsEvent.SUGGESTED_FOLDERS_HOW_IT_WORKS_GOT_IT_TAPPED)
     }
 
     sealed class FoldersState {
