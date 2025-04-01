b412622965a12c19747e19ec78b09b9a2244613b
Wojtek Zieba <wojtek.zieba@automattic.com>
2025-04-01 20:29:21 +0200

    Update secrets and logic to support public Gradle cache node

--- build-logic/src/main/kotlin/com/pocketcasts/PocketCastsBuildCachePlugin.kt
+++ build-logic/src/main/kotlin/com/pocketcasts/PocketCastsBuildCachePlugin.kt
@@ -1,6 +1,8 @@
 package com.pocketcasts
 
+import java.io.File
 import java.net.URI
+import java.util.Properties
 import org.gradle.api.Plugin
 import org.gradle.api.initialization.Settings
 import org.gradle.caching.http.HttpBuildCache
@@ -9,15 +11,46 @@ import org.gradle.kotlin.dsl.remote
 class PocketCastsBuildCachePlugin : Plugin<Settings> {
 
     override fun apply(target: Settings) {
-        if (System.getenv("CI")?.toBoolean() == true) {
-            target.buildCache {
+
+        val developerProperties = loadPropertiesFromFile(File("${target.rootDir.path}/developer.properties"))
+        val secretProperties = loadPropertiesFromFile(File("${target.rootDir.path}/secret.properties"))
+
+        target.buildCache {
+            if (System.getenv("CI")?.toBoolean() == true) {
                 remote<HttpBuildCache> {
-                    url = URI.create("http://10.0.2.215:5071/cache/")
+                    url = URI.create("http://10.0.2.214:5071/cache/")
                     isAllowUntrustedServer = true
                     isAllowInsecureProtocol = true
                     isPush = true
+                    credentials {
+                        username = "ci-user"
+                        password = System.getenv("GRADLE_CACHE_NODE_PASSWORD")
+                    }
+                }
+            } else if (developerProperties.getProperty(USE_REMOTE_BUILD_CACHE_LOCALLY).toBoolean()) {
+                remote<HttpBuildCache> {
+                    url = URI.create(secretProperties.getProperty("gradleCacheNodeUrl").ifEmpty { throw IllegalArgumentException("Gradle Cache Node URL is missing. Make sure to apply secrets `bundle exec fastlane run configure_apply`.") })
+                    isPush = false
+                    credentials {
+                        username = "developer"
+                        password = secretProperties.getProperty("gradleCacheNodePassword")
+                    }
                 }
             }
         }
+    }
+
+    private fun loadPropertiesFromFile(file: File): Properties {
+        val properties = Properties()
+        if (file.exists()) {
+            file.inputStream().use { stream ->
+                properties.load(stream)
+            }
+        }
+        return properties
+    }
+
+    companion object {
+        const val USE_REMOTE_BUILD_CACHE_LOCALLY = "use_remote_build_cache_locally"
     }
 }
