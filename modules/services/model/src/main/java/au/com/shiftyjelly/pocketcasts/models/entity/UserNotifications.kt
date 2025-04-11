56cd2f0319c802f7d4f015cd95665e668686fff1
Eduarda Barbosa <meduarda.qb@gmail.com>
2025-04-11 09:56:45 -0300

    simplify notification db

--- modules/services/model/src/main/java/au/com/shiftyjelly/pocketcasts/models/entity/UserNotifications.kt
+++ modules/services/model/src/main/java/au/com/shiftyjelly/pocketcasts/models/entity/UserNotifications.kt
@@ -6,9 +6,8 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "user_notifications")
 data class UserNotifications(
-    @PrimaryKey @ColumnInfo(name = "user_id") var userId: String,
-    @ColumnInfo(name = "notification_id") var notificationId: Int,
-    @ColumnInfo(name = "sent_this_week") var sentThisWeek: Int,
-    @ColumnInfo(name = "last_sent_at") var lastSentAt: Long,
+    @PrimaryKey @ColumnInfo(name = "notification_id") var notificationId: Int,
+    @ColumnInfo(name = "sent_this_week") var sentThisWeek: Int = 0,
+    @ColumnInfo(name = "last_sent_at") var lastSentAt: Long = 0,
     @ColumnInfo(name = "interacted_at") var interactedAt: Long? = null,
 )
--- modules/services/model/src/main/java/au/com/shiftyjelly/pocketcasts/models/type/NotificationCategory.kt
+++
@@ -1,8 +0,0 @@
-package au.com.shiftyjelly.pocketcasts.models.type
-
-enum class NotificationCategory {
-    ONBOARDING,
-    RECOMMENDATION,
-    RE_ENGAGEMENT,
-    NEW_FEATURE,
-}
