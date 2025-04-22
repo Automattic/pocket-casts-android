cf8ba72a2829566a20856401fe6860c9f56301bf
Michał Sikora <me@mehow.io>
2025-04-22 15:05:11 +0200

    Add data types to the payment module

---
+++ modules/services/payment/src/test/kotlin/com/android/billingclient/api/ProductDetails.kt
@@ -0,0 +1,73 @@
+package com.android.billingclient.api
+
+import com.android.billingclient.api.ProductDetails.PricingPhase
+import com.android.billingclient.api.ProductDetails.SubscriptionOfferDetails
+import org.json.JSONArray
+import org.json.JSONObject
+
+fun GoogleProductDetails(
+    productId: String = "product-id",
+    type: String = "subs",
+    title: String = "title",
+    name: String = "name",
+    skuDetailsToken: String = "sku-details-token",
+    subscriptionOfferDetails: List<SubscriptionOfferDetails>? = listOf(GoogleOfferDetails()),
+): ProductDetails {
+    val json = JSONObject()
+        .put("productId", productId)
+        .put("type", type)
+        .put("title", title)
+        .put("name", name)
+        .put("skuDetailsToken", skuDetailsToken)
+        .putOpt("subscriptionOfferDetails", subscriptionOfferDetails?.map(SubscriptionOfferDetails::toJson)?.let(::JSONArray))
+    return ProductDetails(json.toString())
+}
+
+fun GoogleOfferDetails(
+    basePlanId: String = "base-plan-id",
+    offerId: String? = null,
+    offerIdToken: String = "offer-id-token",
+    pricingPhases: List<PricingPhase> = listOf(GooglePricingPhase()),
+    offerTags: List<String> = emptyList(),
+): SubscriptionOfferDetails {
+    val json = JSONObject()
+        .put("basePlanId", basePlanId)
+        .putOpt("offerId", offerId)
+        .put("offerIdToken", offerIdToken)
+        .put("pricingPhases", JSONArray(pricingPhases.map(PricingPhase::toJson)))
+        .put("offerTags", JSONArray(offerTags))
+    return SubscriptionOfferDetails(json)
+}
+
+fun GooglePricingPhase(
+    priceAmountMicros: Long = 100_000_000,
+    priceCurrencyCode: String = "USD",
+    formattedPrice: String = "$100.00",
+    billingPeriod: String = "P1M",
+    recurrenceMode: Int = 1,
+    billingCycleCount: Int = 0,
+): PricingPhase {
+    val json = JSONObject()
+        .put("priceAmountMicros", priceAmountMicros)
+        .put("priceCurrencyCode", priceCurrencyCode)
+        .put("formattedPrice", formattedPrice)
+        .put("billingPeriod", billingPeriod)
+        .put("recurrenceMode", recurrenceMode)
+        .put("billingCycleCount", billingCycleCount)
+    return PricingPhase(json)
+}
+
+private fun PricingPhase.toJson() = JSONObject()
+    .put("priceAmountMicros", priceAmountMicros)
+    .put("priceCurrencyCode", priceCurrencyCode)
+    .put("formattedPrice", formattedPrice)
+    .put("billingPeriod", billingPeriod)
+    .put("recurrenceMode", recurrenceMode)
+    .put("billingCycleCount", billingCycleCount)
+
+private fun SubscriptionOfferDetails.toJson() = JSONObject()
+    .put("basePlanId", basePlanId)
+    .putOpt("offerId", offerId)
+    .put("offerIdToken", offerToken)
+    .put("pricingPhases", JSONArray(pricingPhases.pricingPhaseList.map(PricingPhase::toJson)))
+    .put("offerTags", JSONArray(offerTags))
