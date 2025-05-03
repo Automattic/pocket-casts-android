d319057fc978cf9af2712424cca0aebf64365941
Philip Simpson <philip.simpson@automattic.com>
2025-05-04 07:31:04 +0930

    Talkback improvement to Podroll header

--- modules/features/podcasts/src/main/java/au/com/shiftyjelly/pocketcasts/podcasts/view/components/PodrollHeaderRow.kt
+++ modules/features/podcasts/src/main/java/au/com/shiftyjelly/pocketcasts/podcasts/view/components/PodrollHeaderRow.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
@@ -31,7 +32,9 @@ fun PodrollHeader(onClick: () -> Unit) {
         verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier
             .fillMaxWidth()
-            .clickable { onClick() }
+            .clickable(role = Role.Button) {
+                onClick()
+            }
             .padding(horizontal = 16.dp, vertical = 10.dp),
     ) {
         Icon(
