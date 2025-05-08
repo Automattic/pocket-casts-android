b3d5e7283469f014ff5283c40df7f9f457c714c0
Tamas Szelezsan <szelezsant@gmail.com>
2025-05-08 12:16:21 +0200

    Add VM and repository to interact with preferences

---
+++ modules/features/settings/src/main/java/au/com/shiftyjelly/pocketcasts/settings/notifications/model/NotificationPreferenceCategory.kt
@@ -0,0 +1,6 @@
+package au.com.shiftyjelly.pocketcasts.settings.notifications.model
+
+internal data class NotificationPreferenceCategory(
+    val title: String,
+    val preferences: List<NotificationPreference>
+)
