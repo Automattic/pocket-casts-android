5283ec01dee9bed92c0acbf02a00b415cb4c80db
Philip Simpson <philip.simpson@automattic.com>
2025-05-14 16:25:21 +0930

    Adds Podroll and recommendations separator title

--- modules/features/podcasts/src/main/java/au/com/shiftyjelly/pocketcasts/podcasts/view/podcast/adapter/DividerSubTitleViewHolder.kt
+++ modules/features/podcasts/src/main/java/au/com/shiftyjelly/pocketcasts/podcasts/view/podcast/adapter/DividerSubTitleViewHolder.kt
@@ -1,23 +1,25 @@
 package au.com.shiftyjelly.pocketcasts.podcasts.view.podcast.adapter
 
-import androidx.compose.foundation.layout.padding
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.unit.dp
 import androidx.recyclerview.widget.RecyclerView
 import au.com.shiftyjelly.pocketcasts.compose.AppTheme
-import au.com.shiftyjelly.pocketcasts.compose.components.HorizontalDivider
+import au.com.shiftyjelly.pocketcasts.podcasts.view.components.DividerSubTitle
+import au.com.shiftyjelly.pocketcasts.podcasts.view.podcast.PodcastAdapter
 import au.com.shiftyjelly.pocketcasts.ui.theme.Theme
 
-class DividerLineViewHolder(
+class DividerSubTitleViewHolder(
     private val composeView: ComposeView,
     private val theme: Theme,
 ) : RecyclerView.ViewHolder(composeView) {
 
-    fun bind() {
+    fun bind(data: PodcastAdapter.DividerSubTitleRow) {
         composeView.setContent {
             AppTheme(theme.activeTheme) {
-                HorizontalDivider(Modifier.padding(horizontal = 16.dp))
+                DividerSubTitle(
+                    title = data.title,
+                    icon = data.icon,
+                    onClick = data.onClick,
+                )
             }
         }
     }
