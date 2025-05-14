be19587f3f4c857a19ada439731fc4899f9dae87
Philip Simpson <philz.the.gecko@gmail.com>
2025-05-14 20:45:14 +0930

    Recommendations: Add similar shows section header when Podroll exists (#3998)

--- modules/features/podcasts/src/main/java/au/com/shiftyjelly/pocketcasts/podcasts/view/components/DividerSubTitle.kt
+++ modules/features/podcasts/src/main/java/au/com/shiftyjelly/pocketcasts/podcasts/view/components/DividerSubTitle.kt
@@ -1,5 +1,6 @@
 package au.com.shiftyjelly.pocketcasts.podcasts.view.components
 
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,7 +14,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
@@ -22,30 +22,37 @@ import au.com.shiftyjelly.pocketcasts.compose.AppThemeWithBackground
 import au.com.shiftyjelly.pocketcasts.compose.components.TextH60
 import au.com.shiftyjelly.pocketcasts.compose.preview.ThemePreviewParameterProvider
 import au.com.shiftyjelly.pocketcasts.compose.theme
-import au.com.shiftyjelly.pocketcasts.podcasts.R
 import au.com.shiftyjelly.pocketcasts.ui.theme.Theme
-import au.com.shiftyjelly.pocketcasts.localization.R as LR
+import au.com.shiftyjelly.pocketcasts.images.R as IR
 
 @Composable
-fun PodrollHeader(onClick: () -> Unit) {
+fun DividerSubTitle(
+    @DrawableRes icon: Int,
+    title: String,
+    onClick: (() -> Unit)?,
+) {
     Row(
         verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier
             .fillMaxWidth()
-            .clickable(role = Role.Button) {
-                onClick()
-            }
+            .then(
+                if (onClick != null) {
+                    Modifier.clickable(role = Role.Button, onClick = onClick)
+                } else {
+                    Modifier
+                },
+            )
             .padding(horizontal = 16.dp, vertical = 10.dp),
     ) {
         Icon(
-            painter = painterResource(R.drawable.ic_author),
+            painter = painterResource(icon),
             contentDescription = null,
             tint = MaterialTheme.theme.colors.primaryIcon03,
             modifier = Modifier.size(16.dp),
         )
         Spacer(Modifier.width(8.dp))
         TextH60(
-            text = stringResource(LR.string.recommended_by_creator),
+            text = title,
             color = MaterialTheme.theme.colors.primaryText02,
         )
     }
@@ -53,10 +60,10 @@ fun PodrollHeader(onClick: () -> Unit) {
 
 @Preview
 @Composable
-fun PodrollHeaderPreview(
+fun DividerSubTitlePreview(
     @PreviewParameter(ThemePreviewParameterProvider::class) theme: Theme.ThemeType,
 ) {
     AppThemeWithBackground(theme) {
-        PodrollHeader(onClick = {})
+        DividerSubTitle(icon = IR.drawable.ic_author_small, title = "Recommended shows by the creator", onClick = {})
     }
 }
