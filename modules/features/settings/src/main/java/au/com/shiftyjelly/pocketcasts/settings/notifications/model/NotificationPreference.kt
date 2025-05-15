1806756ff86aa826d1e53c1e4269bdf17159e533
Tamas Szelezsan <szelezsant@gmail.com>
2025-05-15 11:21:37 +0200

    Add docs for domain model definitions

--- modules/features/settings/src/main/java/au/com/shiftyjelly/pocketcasts/settings/notifications/model/NotificationPreference.kt
+++ modules/features/settings/src/main/java/au/com/shiftyjelly/pocketcasts/settings/notifications/model/NotificationPreference.kt
@@ -1,5 +1,8 @@
 package au.com.shiftyjelly.pocketcasts.settings.notifications.model
 
+/**
+ * This enum defines the preferences we keep record on
+ */
 internal enum class NotificationPreferences {
     NEW_EPISODES_NOTIFY_ME,
     NEW_EPISODES_CHOOSE_PODCASTS,
@@ -11,23 +14,35 @@ internal enum class NotificationPreferences {
     SETTINGS_HIDE_NOTIFICATION_ON_PAUSE
 }
 
+/**
+ * Generic interface that describes the common preference properties and constrain subclassing via its sealed nature.
+ */
 internal sealed interface NotificationPreference<T> {
     val title: String
     val preference: NotificationPreferences
     val value: T
 
+    /**
+     * Simple preference that stores a boolean value that can either be on/off.
+     */
     data class SwitchPreference(
         override val title: String,
         override val value: Boolean,
         override val preference: NotificationPreferences
     ) : NotificationPreference<Boolean>
 
+    /**
+     * Simple preference that represents a preference with a human-readable value.
+     */
     data class TextPreference(
         override val title: String,
         override val value: String?,
         override val preference: NotificationPreferences,
     ): NotificationPreference<String?>
 
+    /**
+     * Preference that holds the generic raw value of the underlying setting and also provides a human-readable [displayValue].
+     */
     data class ValueHolderPreference<T>(
         override val title: String,
         override val value: T,
@@ -35,6 +50,9 @@ internal sealed interface NotificationPreference<T> {
         override val preference: NotificationPreferences,
     ): NotificationPreference<T>
 
+    /**
+     * Preference that lets you select form the provided [options] and keeps track of the current selection. Also provides a human-readable [displayText] that represents the current [value].
+     */
     data class RadioGroupPreference<T>(
         override val title: String,
         override val value: T,
@@ -43,12 +61,15 @@ internal sealed interface NotificationPreference<T> {
         val displayText: String?
     ) : NotificationPreference<T>
 
+    /**
+     * Preference that lets you select at most [maxNumberOfSelectableOptions] options from the provided [options] and keeps track of the current selection. Also human-readable [displayText] that represents the current [value].
+     */
     data class MultiSelectPreference<T>(
         override val title: String,
         override val value: List<T>,
         override val preference: NotificationPreferences,
         val options: List<T>,
         val displayText: String?,
-        val numberOfItemToSelect: Int
+        val maxNumberOfSelectableOptions: Int
     ) : NotificationPreference<List<T>>
 }
