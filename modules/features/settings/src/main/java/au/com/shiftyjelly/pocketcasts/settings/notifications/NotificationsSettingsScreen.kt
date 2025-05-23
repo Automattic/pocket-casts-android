0776760f449d01a86979cf73218fc6cb8ddbb264
Tamas Szelezsan <szelezsant@gmail.com>
2025-05-23 13:33:41 +0200

    Favor existing `Banner` component

--- modules/features/settings/src/main/java/au/com/shiftyjelly/pocketcasts/settings/notifications/NotificationsSettingsScreen.kt
+++ modules/features/settings/src/main/java/au/com/shiftyjelly/pocketcasts/settings/notifications/NotificationsSettingsScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
@@ -18,15 +19,17 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import au.com.shiftyjelly.pocketcasts.compose.AppTheme
 import au.com.shiftyjelly.pocketcasts.compose.bars.ThemedTopAppBar
+import au.com.shiftyjelly.pocketcasts.compose.components.Banner
 import au.com.shiftyjelly.pocketcasts.compose.preview.ThemePreviewParameterProvider
 import au.com.shiftyjelly.pocketcasts.compose.theme
 import au.com.shiftyjelly.pocketcasts.localization.R
 import au.com.shiftyjelly.pocketcasts.preferences.model.PlayOverNotificationSetting
 import au.com.shiftyjelly.pocketcasts.settings.notifications.components.NotificationPreferenceCategory
-import au.com.shiftyjelly.pocketcasts.settings.notifications.components.NotificationSettingsBanner
 import au.com.shiftyjelly.pocketcasts.settings.notifications.model.NotificationPreferenceType
 import au.com.shiftyjelly.pocketcasts.settings.util.TextResource
 import au.com.shiftyjelly.pocketcasts.ui.theme.Theme
+import au.com.shiftyjelly.pocketcasts.images.R as IR
+import au.com.shiftyjelly.pocketcasts.localization.R as LR
 import au.com.shiftyjelly.pocketcasts.settings.notifications.model.NotificationPreferenceCategory as CategoryModel
 
 @OptIn(ExperimentalFoundationApi::class)
@@ -57,11 +60,16 @@ internal fun NotificationsSettingsScreen(
         ) {
             if (!state.areSystemNotificationsEnabled) {
                 stickyHeader {
-                    NotificationSettingsBanner(
-                        onSettingsClicked = onSystemNotificationsSettingsClicked,
+                    Banner(
                         modifier = Modifier.fillMaxWidth()
                             .padding(horizontal = 16.dp)
                             .padding(top = 16.dp),
+                        title = stringResource(LR.string.notifications_settings_turn_on_push_title),
+                        description = stringResource(LR.string.notifications_settings_turn_on_push_message),
+                        actionLabel = stringResource(LR.string.notifications_settings_turn_on_push_button),
+                        icon = painterResource(IR.drawable.ic_notifications),
+                        onActionClick = onSystemNotificationsSettingsClicked,
+                        onDismiss = null
                     )
                 }
             }
