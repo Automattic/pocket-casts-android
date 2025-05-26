d72a586e10e4feb8a18f5404f424890c620e477b
Tamas Szelezsan <szelezsant@gmail.com>
2025-05-26 14:21:51 +0200

    Don't fire smart folders notification when the user has no podcasts

--- modules/services/repositories/src/main/java/au/com/shiftyjelly/pocketcasts/repositories/notification/NotificationWorker.kt
+++ modules/services/repositories/src/main/java/au/com/shiftyjelly/pocketcasts/repositories/notification/NotificationWorker.kt
@@ -15,6 +15,7 @@ import au.com.shiftyjelly.pocketcasts.preferences.Settings
 import au.com.shiftyjelly.pocketcasts.repositories.R
 import au.com.shiftyjelly.pocketcasts.repositories.notification.NotificationSchedulerImpl.Companion.DOWNLOADED_EPISODES
 import au.com.shiftyjelly.pocketcasts.repositories.notification.NotificationSchedulerImpl.Companion.SUBCATEGORY
+import au.com.shiftyjelly.pocketcasts.repositories.podcast.PodcastManager
 import au.com.shiftyjelly.pocketcasts.utils.featureflag.Feature
 import au.com.shiftyjelly.pocketcasts.utils.featureflag.FeatureFlag
 import dagger.assisted.Assisted
@@ -28,6 +29,7 @@ class NotificationWorker @AssistedInject constructor(
     private val settings: Settings,
     private val notificationHelper: NotificationHelper,
     private val notificationManager: NotificationManager,
+    private val podcastManager: PodcastManager,
 ) : CoroutineWorker(context, params) {
     override suspend fun doWork(): Result {
         val subcategory = inputData.getString(SUBCATEGORY) ?: return Result.failure()
@@ -38,7 +40,7 @@ class NotificationWorker @AssistedInject constructor(
             return Result.failure()
         }
 
-        if (notificationManager.hasUserInteractedWithFeature(type)) {
+        if (notificationManager.hasUserInteractedWithFeature(type) && shouldSchedule(type)) {
             return Result.failure()
         }
 
@@ -52,6 +54,13 @@ class NotificationWorker @AssistedInject constructor(
         return Result.success()
     }
 
+    private fun shouldSchedule(type: NotificationType): Boolean {
+        return when (type) {
+            is NewFeaturesAndTipsNotificationType.SmartFolders -> podcastManager.getSubscribedPodcastUuidsRxSingle().blockingGet().isNotEmpty()
+            else -> true
+        }
+    }
+
     private fun getNotificationBuilder(type: NotificationType): NotificationCompat.Builder {
         val downloadedEpisodes = inputData.getInt(DOWNLOADED_EPISODES, 0)
 
@@ -59,9 +68,11 @@ class NotificationWorker @AssistedInject constructor(
             is TrendingAndRecommendationsNotificationType -> {
                 notificationHelper.trendingAndRecommendationsChannelBuilder()
             }
+
             is NewFeaturesAndTipsNotificationType -> {
                 notificationHelper.featuresAndTipsChannelBuilder()
             }
+
             else -> notificationHelper.dailyRemindersChannelBuilder()
         }
 
