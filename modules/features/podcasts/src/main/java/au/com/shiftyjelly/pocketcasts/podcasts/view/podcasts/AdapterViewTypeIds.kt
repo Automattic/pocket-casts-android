c0dd538fad56a5456ab7369f38e5770f4f8b2a3c
Michał Sikora <me@mehow.io>
2025-06-04 09:34:53 +0200

    Introduce view type IDs object

---
+++ modules/features/podcasts/src/main/java/au/com/shiftyjelly/pocketcasts/podcasts/view/podcasts/AdapterViewTypeIds.kt
@@ -0,0 +1,7 @@
+package au.com.shiftyjelly.pocketcasts.podcasts.view.podcasts
+
+internal object AdapterViewTypeIds {
+    const val PodcastId = 1
+    const val FolderId = PodcastId + 1
+    const val BannerAdId = FolderId + 1
+}
