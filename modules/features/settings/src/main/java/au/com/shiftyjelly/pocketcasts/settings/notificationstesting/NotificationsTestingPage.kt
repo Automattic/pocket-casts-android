a4fd98d9d19f2d4596441079c89a21395263e136
Tamas Szelezsan <szelezsant@gmail.com>
2025-06-25 16:21:08 +0200

    Change delay picker component

--- modules/features/settings/src/main/java/au/com/shiftyjelly/pocketcasts/settings/notificationstesting/NotificationsTestingPage.kt
+++ modules/features/settings/src/main/java/au/com/shiftyjelly/pocketcasts/settings/notificationstesting/NotificationsTestingPage.kt
@@ -1,29 +1,37 @@
 package au.com.shiftyjelly.pocketcasts.settings.notificationstesting
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
@@ -31,15 +39,21 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import au.com.shiftyjelly.pocketcasts.compose.AppThemeWithBackground
 import au.com.shiftyjelly.pocketcasts.compose.bars.ThemedTopAppBar
-import au.com.shiftyjelly.pocketcasts.compose.components.FormField
+import au.com.shiftyjelly.pocketcasts.compose.components.DialogButtonState
+import au.com.shiftyjelly.pocketcasts.compose.components.DialogFrame
 import au.com.shiftyjelly.pocketcasts.compose.components.HorizontalDivider
 import au.com.shiftyjelly.pocketcasts.compose.components.SettingRadioDialogRow
+import au.com.shiftyjelly.pocketcasts.compose.components.SettingRow
 import au.com.shiftyjelly.pocketcasts.compose.components.TextP40
 import au.com.shiftyjelly.pocketcasts.compose.components.TextP60
 import au.com.shiftyjelly.pocketcasts.compose.preview.ThemePreviewParameterProvider
 import au.com.shiftyjelly.pocketcasts.compose.theme
+import au.com.shiftyjelly.pocketcasts.localization.R
 import au.com.shiftyjelly.pocketcasts.ui.theme.Theme.ThemeType
+import au.com.shiftyjelly.pocketcasts.utils.extensions.isPositive
+import java.util.Locale
 import kotlin.time.Duration.Companion.seconds
+import kotlinx.coroutines.delay
 
 @Composable
 internal fun NotificationsTestingPage(
@@ -159,25 +173,12 @@ private fun SettingNotifications(
             onSave = { selectedCategory = it },
             indent = false,
         )
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(
-                    horizontal = 24.dp,
-                ),
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            TextP40(
-                text = "Select consecutive delay (seconds)",
-            )
-            Spacer(modifier = Modifier.width(8.dp))
-            FormField(
-                value = selectedDelay.toString(),
-                placeholder = "Enter seconds here",
-                onValueChange = { selectedDelay = it.toIntOrNull() ?: 0 },
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-            )
-        }
+        DelayPicker(
+            primaryText = "Select consecutive delay (seconds)",
+            saved = selectedDelay,
+            onSave = { selectedDelay = it },
+            modifier = Modifier.fillMaxWidth(),
+        )
         TextP60(
             modifier = Modifier.padding(
                 horizontal = 24.dp,
@@ -233,25 +234,11 @@ private fun UniqueNotificationSchedulerContent(
             onSave = { selectedType = it },
             indent = false,
         )
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(
-                    horizontal = 24.dp,
-                ),
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            TextP40(
-                text = "Select delay seconds",
-            )
-            Spacer(modifier = Modifier.width(8.dp))
-            FormField(
-                value = selectedDelay.toString(),
-                placeholder = "Enter seconds here",
-                onValueChange = { selectedDelay = it.toIntOrNull() ?: 0 },
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-            )
-        }
+        DelayPicker(
+            primaryText = "Select delay (seconds)",
+            saved = selectedDelay,
+            onSave = { selectedDelay = it },
+        )
         TextP60(
             modifier = Modifier.padding(
                 horizontal = 24.dp,
@@ -270,6 +257,101 @@ private fun UniqueNotificationSchedulerContent(
         ) {
             TextP40(text = if (notificationTrigger?.triggerType is NotificationsTestingViewModel.NotificationTriggerType.Delayed) "Schedule" else "Fire now")
         }
+    }
+}
+
+@Composable
+private fun DelayPicker(
+    primaryText: String,
+    saved: Int,
+    onSave: (Int) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    var showDialog by remember { mutableStateOf(false) }
+
+    SettingRow(
+        primaryText = primaryText,
+        secondaryText = stringResource(R.string.seconds_plural, saved),
+        modifier = modifier.clickable { showDialog = true },
+        indent = false,
+    ) {
+        if (showDialog) {
+            val focusRequester = remember { FocusRequester() }
+            LaunchedEffect(Unit) {
+                // delay apparently needed to ensure the soft keyboard opens
+                delay(100)
+                focusRequester.requestFocus()
+            }
+
+            var value by remember {
+                mutableStateOf(
+                    TextFieldValue(
+                        text = saved.toString(),
+                        selection = TextRange(0, saved.toString().length),
+                    ),
+                )
+            }
+
+            val onFinish = {
+                val saveableValue = value.text.toPositiveNumberOrNull()
+                if (saveableValue != null) {
+                    onSave(saveableValue)
+                    showDialog = false
+                }
+            }
+
+            DialogFrame(
+                title = primaryText,
+                buttons = listOf(
+                    DialogButtonState(
+                        text = stringResource(au.com.shiftyjelly.pocketcasts.localization.R.string.cancel).uppercase(
+                            Locale.getDefault(),
+                        ),
+                        onClick = { showDialog = false },
+                    ),
+                    DialogButtonState(
+                        text = stringResource(au.com.shiftyjelly.pocketcasts.localization.R.string.ok),
+                        onClick = onFinish,
+                        enabled = value.text.toPositiveNumberOrNull() != null,
+                    ),
+                ),
+                onDismissRequest = { showDialog = false },
+            ) {
+                OutlinedTextField(
+                    value = value,
+                    onValueChange = {
+                        if (it.text.isEmpty()) {
+                            value = it
+                        } else {
+                            val positiveNumber = it.text.toPositiveNumberOrNull()
+                            if (positiveNumber != null) {
+                                value = it.copy(text = positiveNumber.toString())
+                            }
+                        }
+                    },
+                    colors = TextFieldDefaults.textFieldColors(
+                        textColor = MaterialTheme.theme.colors.primaryText01,
+                        placeholderColor = MaterialTheme.theme.colors.primaryText02,
+                        backgroundColor = MaterialTheme.theme.colors.primaryUi01,
+                    ),
+                    label = {
+                        Text(stringResource(R.string.seconds_label))
+                    },
+                    singleLine = true,
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                    keyboardActions = KeyboardActions { onFinish() },
+                    modifier = Modifier
+                        .padding(horizontal = 24.dp)
+                        .focusRequester(focusRequester),
+                )
+            }
+        }
+    }
+}
+
+private fun String?.toPositiveNumberOrNull(): Int? {
+    return this?.toIntOrNull().let { int ->
+        if (int.isPositive()) int else null
     }
 }
 
