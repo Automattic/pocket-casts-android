14ce1e78cf9710c16f9fe7afeaa99731c6df30df
Michał Sikora <me@mehow.io>
2025-07-17 09:29:53 +0200

    Show Playlists Onboarding fragment

--- modules/features/filters/src/main/java/au/com/shiftyjelly/pocketcasts/playlists/PlaylistsFragment.kt
+++ modules/features/filters/src/main/java/au/com/shiftyjelly/pocketcasts/playlists/PlaylistsFragment.kt
@@ -7,14 +7,19 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import au.com.shiftyjelly.pocketcasts.compose.AppThemeWithBackground
 import au.com.shiftyjelly.pocketcasts.compose.extensions.contentWithoutConsumedInsets
@@ -30,6 +35,7 @@ class PlaylistsFragment :
     BaseFragment(),
     TopScrollable {
     private val scrollToTopSignal = MutableSharedFlow<Unit>()
+    private val viewModel by viewModels<PlaylistsViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -37,6 +43,7 @@ class PlaylistsFragment :
         savedInstanceState: Bundle?,
     ) = contentWithoutConsumedInsets {
         val listState = rememberLazyListState()
+        val uiState by viewModel.uiState.collectAsState()
 
         AppThemeWithBackground(theme.activeTheme) {
             LazyColumn(
@@ -57,9 +64,24 @@ class PlaylistsFragment :
             }
         }
 
+        ScrollToTopEffect(listState)
+        ShowOnboardingEffect(uiState.showOnboarding)
+    }
+
+    @Composable
+    private fun ScrollToTopEffect(listState: LazyListState) {
         LaunchedEffect(listState) {
             scrollToTopSignal.collectLatest {
                 listState.animateScrollToItem(0)
+            }
+        }
+    }
+
+    @Composable
+    private fun ShowOnboardingEffect(show: Boolean) {
+        if (show) {
+            LaunchedEffect(show) {
+                PlaylistsOnboardingFragment().show(childFragmentManager, "playlists_onboarding")
             }
         }
     }
