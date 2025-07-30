7462ce762a129b565db4a996cf0bf4266941e6d0
Michał Sikora <me@mehow.io>
2025-07-30 10:19:32 +0200

    Add tests

--- modules/features/filters/build.gradle.kts
+++ modules/features/filters/build.gradle.kts
@@ -66,6 +66,8 @@ dependencies {
 
     testImplementation(libs.coroutines.test)
     testImplementation(libs.junit)
+    testImplementation(libs.mockito.core)
+    testImplementation(libs.mockito.kotlin)
     testImplementation(libs.turbine)
 
     testImplementation(projects.modules.services.sharedtest)
