341779638ce00986331ee839b74fab78a1fc57e3
Michał Sikora <me@mehow.io>
2025-07-31 17:10:28 +0200

    Control podcasts smart rule (#4299)

--- modules/features/filters/src/main/java/au/com/shiftyjelly/pocketcasts/playlists/create/CreatePlaylistViewModel.kt
+++ modules/features/filters/src/main/java/au/com/shiftyjelly/pocketcasts/playlists/create/CreatePlaylistViewModel.kt
@@ -3,41 +3,147 @@ package au.com.shiftyjelly.pocketcasts.playlists.create
 import androidx.compose.foundation.text.input.TextFieldState
 import androidx.compose.ui.text.TextRange
 import androidx.lifecycle.ViewModel
-import au.com.shiftyjelly.pocketcasts.repositories.playlist.SmartPlaylistDraft
+import androidx.lifecycle.viewModelScope
+import au.com.shiftyjelly.pocketcasts.models.entity.Podcast
+import au.com.shiftyjelly.pocketcasts.models.type.SmartRules.DownloadStatusRule
+import au.com.shiftyjelly.pocketcasts.models.type.SmartRules.EpisodeDurationRule
+import au.com.shiftyjelly.pocketcasts.models.type.SmartRules.EpisodeStatusRule
+import au.com.shiftyjelly.pocketcasts.models.type.SmartRules.MediaTypeRule
+import au.com.shiftyjelly.pocketcasts.models.type.SmartRules.PodcastsRule
+import au.com.shiftyjelly.pocketcasts.models.type.SmartRules.ReleaseDateRule
+import au.com.shiftyjelly.pocketcasts.models.type.SmartRules.StarredRule
+import au.com.shiftyjelly.pocketcasts.repositories.podcast.PodcastManager
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 
 @HiltViewModel(assistedFactory = CreatePlaylistViewModel.Factory::class)
 class CreatePlaylistViewModel @AssistedInject constructor(
-    @Assisted initialPlaylistName: String,
+    private val podcastManager: PodcastManager,
+    @Assisted initialPlaylistTitle: String,
 ) : ViewModel() {
     val playlistNameState = TextFieldState(
-        initialText = initialPlaylistName,
-        initialSelection = TextRange(0, initialPlaylistName.length),
+        initialText = initialPlaylistTitle,
+        initialSelection = TextRange(0, initialPlaylistTitle.length),
     )
 
-    private val _uiState = MutableStateFlow(
+    private val appliedRules = MutableStateFlow(AppliedRules.Empty)
+    private val rulesBuilder = MutableStateFlow(RulesBuilder.Empty)
+
+    val uiState = combine(
+        appliedRules,
+        rulesBuilder,
+        podcastManager.findSubscribedFlow(),
+    ) { appliedRules, rulesBuilder, followedPodcasts ->
         UiState(
-            smartPlaylistDraft = SmartPlaylistDraft(title = initialPlaylistName),
-        ),
-    )
-    val uiState = _uiState.asStateFlow()
+            appliedRules,
+            rulesBuilder,
+            followedPodcasts,
+        )
+    }.stateIn(viewModelScope, SharingStarted.Eagerly, initialValue = UiState.Empty)
 
-    fun updateDraftTitle() {
-        _uiState.update { state ->
-            val draft = state.smartPlaylistDraft.copy(title = playlistNameState.text.toString())
-            state.copy(smartPlaylistDraft = draft)
+    fun applyRule(type: RuleType) {
+        val rule = when (type) {
+            RuleType.Podcasts -> rulesBuilder.value.podcastsRule
+            RuleType.EpisodeStatus -> TODO()
+            RuleType.ReleaseDate -> TODO()
+            RuleType.EpisodeDuration -> TODO()
+            RuleType.DownloadStatus -> TODO()
+            RuleType.MediaType -> TODO()
+            RuleType.Starred -> TODO()
+        }
+        appliedRules.update { rules ->
+            rules.copy(podcasts = rule)
+        }
+    }
+
+    fun useAllPodcasts(use: Boolean) {
+        rulesBuilder.update { builder ->
+            builder.copy(useAllPodcasts = use)
+        }
+    }
+
+    fun selectPodcast(uuid: String) {
+        rulesBuilder.update { builder ->
+            val podcasts = builder.selectedPodcasts + uuid
+            builder.copy(selectedPodcasts = podcasts)
+        }
+    }
+
+    fun deselectPodcast(uuid: String) {
+        rulesBuilder.update { builder ->
+            val podcasts = builder.selectedPodcasts - uuid
+            builder.copy(selectedPodcasts = podcasts)
         }
     }
 
     data class UiState(
-        val smartPlaylistDraft: SmartPlaylistDraft,
-    )
+        val appliedRules: AppliedRules,
+        val rulesBuilder: RulesBuilder,
+        val followedPodcasts: List<Podcast>,
+    ) {
+        companion object {
+            val Empty = UiState(
+                appliedRules = AppliedRules.Empty,
+                rulesBuilder = RulesBuilder.Empty,
+                followedPodcasts = emptyList(),
+            )
+        }
+    }
+
+    data class AppliedRules(
+        val episodeStatus: EpisodeStatusRule?,
+        val downloadStatus: DownloadStatusRule?,
+        val mediaType: MediaTypeRule?,
+        val releaseDate: ReleaseDateRule?,
+        val starred: StarredRule?,
+        val podcasts: PodcastsRule?,
+        val episodeDuration: EpisodeDurationRule?,
+    ) {
+        val isAnyRuleApplied = episodeStatus != null ||
+            downloadStatus != null ||
+            mediaType != null ||
+            releaseDate != null ||
+            starred != null ||
+            podcasts != null ||
+            episodeDuration != null
+
+        companion object {
+            val Empty = AppliedRules(
+                episodeStatus = null,
+                downloadStatus = null,
+                mediaType = null,
+                releaseDate = null,
+                starred = null,
+                podcasts = null,
+                episodeDuration = null,
+            )
+        }
+    }
+
+    data class RulesBuilder(
+        val useAllPodcasts: Boolean,
+        val selectedPodcasts: Set<String>,
+    ) {
+        val podcastsRule get() = if (useAllPodcasts) {
+            PodcastsRule.Any
+        } else {
+            PodcastsRule.Selected(selectedPodcasts.toList())
+        }
+
+        companion object {
+            val Empty = RulesBuilder(
+                useAllPodcasts = true,
+                selectedPodcasts = emptySet(),
+            )
+        }
+    }
 
     @AssistedFactory
     interface Factory {
--- modules/features/filters/src/test/kotlin/au/com/shiftyjelly/pocketcasts/playlists/create/CreatePlaylistViewModelTest.kt
+++ modules/features/filters/src/test/kotlin/au/com/shiftyjelly/pocketcasts/playlists/create/CreatePlaylistViewModelTest.kt
@@ -1,22 +1,75 @@
 package au.com.shiftyjelly.pocketcasts.playlists.create
 
 import androidx.compose.ui.text.TextRange
+import app.cash.turbine.test
+import au.com.shiftyjelly.pocketcasts.models.type.SmartRules
+import au.com.shiftyjelly.pocketcasts.models.type.SmartRules.PodcastsRule
+import au.com.shiftyjelly.pocketcasts.playlists.create.CreatePlaylistViewModel.UiState
 import au.com.shiftyjelly.pocketcasts.sharedtest.MainCoroutineRule
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertNull
+import org.junit.Assert.assertTrue
 import org.junit.Rule
 import org.junit.Test
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
 
 class CreatePlaylistViewModelTest {
     @get:Rule
     val coroutineRule = MainCoroutineRule()
 
     private val viewModel = CreatePlaylistViewModel(
-        initialPlaylistName = "Playlist name",
+        initialPlaylistTitle = "Playlist name",
+        podcastManager = mock {
+            on { findSubscribedFlow() } doReturn flowOf(emptyList())
+        },
     )
 
     @Test
     fun `initial playlist name is highlighted`() {
         assertEquals("Playlist name", viewModel.playlistNameState.text)
         assertEquals(TextRange(0, 13), viewModel.playlistNameState.selection)
+    }
+
+    @Test
+    fun `manage podcasts rule`() = runTest {
+        viewModel.uiState.test {
+            var state = awaitItem()
+            assertEquals(UiState.Empty, state)
+
+            viewModel.useAllPodcasts(false)
+            state = awaitItem()
+            assertFalse(state.rulesBuilder.useAllPodcasts)
+            assertNull(state.appliedRules.podcasts)
+
+            viewModel.useAllPodcasts(true)
+            state = awaitItem()
+            assertTrue(state.rulesBuilder.useAllPodcasts)
+            assertNull(state.appliedRules.podcasts)
+
+            viewModel.applyRule(RuleType.Podcasts)
+            state = awaitItem()
+            assertEquals(PodcastsRule.Any, state.appliedRules.podcasts)
+
+            viewModel.useAllPodcasts(false)
+            skipItems(1)
+
+            viewModel.selectPodcast("id-1")
+            state = awaitItem()
+            assertEquals(setOf("id-1"), state.rulesBuilder.selectedPodcasts)
+            assertEquals(PodcastsRule.Any, state.appliedRules.podcasts)
+
+            viewModel.selectPodcast("id-2")
+            state = awaitItem()
+            assertEquals(setOf("id-1", "id-2"), state.rulesBuilder.selectedPodcasts)
+            assertEquals(PodcastsRule.Any, state.appliedRules.podcasts)
+
+            viewModel.applyRule(RuleType.Podcasts)
+            state = awaitItem()
+            assertEquals(PodcastsRule.Selected(listOf("id-1", "id-2")), state.appliedRules.podcasts)
+        }
     }
 }
