cdf9b08ee0cffc34b86f4a37009fd3ebaebc19f8
Michał Sikora <me@mehow.io>
2025-08-01 11:05:52 +0200

    Display created playlist

--- modules/features/filters/build.gradle.kts
+++ modules/features/filters/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.parcelize)
     alias(libs.plugins.ksp)
     alias(libs.plugins.hilt)
     alias(libs.plugins.compose.compiler)
