552f2eb252c1b9ff09ff058db25267923282840c
Michał Sikora <me@mehow.io>
2025-08-07 18:46:44 +0200

    Add artwork elevation

--- modules/features/filters/src/main/java/au/com/shiftyjelly/pocketcasts/playlists/PlaylistArtwork.kt
+++ modules/features/filters/src/main/java/au/com/shiftyjelly/pocketcasts/playlists/PlaylistArtwork.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
