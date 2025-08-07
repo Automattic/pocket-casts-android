a7e1c2433a1e8afca205c330b898b6ff8df53e8f
Michał Sikora <me@mehow.io>
2025-08-07 11:22:51 +0200

    Fix accessiblity

--- modules/features/filters/src/main/java/au/com/shiftyjelly/pocketcasts/playlists/rules/StarredRulePage.kt
+++ modules/features/filters/src/main/java/au/com/shiftyjelly/pocketcasts/playlists/rules/StarredRulePage.kt
@@ -25,6 +25,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -104,7 +105,8 @@ private fun StarredSwitchRow(
                 value = useStarredEpisodes,
                 onValueChange = onChangeUseStarredEpisodes,
             )
-            .padding(horizontal = 16.dp),
+            .padding(horizontal = 16.dp)
+            .semantics(mergeDescendants = true) {},
     ) {
         Column(
             modifier = Modifier
