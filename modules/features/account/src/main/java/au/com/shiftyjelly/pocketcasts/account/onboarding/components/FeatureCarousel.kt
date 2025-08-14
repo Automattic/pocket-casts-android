0f2260898484b354249e8ce7a5435f57da2906a1
Tamas Szelezsan <szelezsant@gmail.com>
2025-08-14 21:07:37 +0200

    Implement carousel skeleton

---
+++ modules/features/account/src/main/java/au/com/shiftyjelly/pocketcasts/account/onboarding/components/FeatureCarousel.kt
@@ -0,0 +1,110 @@
+package au.com.shiftyjelly.pocketcasts.account.onboarding.components
+
+import androidx.compose.animation.Crossfade
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.produceState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import au.com.shiftyjelly.pocketcasts.compose.AppThemeWithBackground
+import au.com.shiftyjelly.pocketcasts.ui.theme.Theme
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
+import kotlinx.coroutines.delay
+
+private const val CAROUSEL_ITEM_COUNT = 3
+
+@Composable
+fun FeatureCarousel(
+    modifier: Modifier = Modifier
+) {
+    val activeItemIndex by CyclingCounter(cycleRange = 0 until CAROUSEL_ITEM_COUNT)
+
+    Column(
+        modifier = modifier.padding(top = 11.dp)
+    ) {
+        CarouselActiveItemIndicatorBar(
+            itemCount = CAROUSEL_ITEM_COUNT,
+            activeItemIndex = activeItemIndex,
+        )
+        Crossfade(targetState = activeItemIndex, modifier = Modifier.fillMaxSize()) { index ->
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center,
+            ) {
+                Text(text = index.toString())
+            }
+        }
+    }
+}
+
+@Composable
+fun CyclingCounter(
+    cycleRange: IntRange,
+    delayBetweenCycles: Duration = 2.seconds,
+    isPerpetual: Boolean = true,
+): State<Int> {
+    return produceState(cycleRange.start) {
+        while (true) {
+            delay(delayBetweenCycles.inWholeMilliseconds)
+            val nextValue = (value + 1) % cycleRange.count()
+            if (nextValue == 0 && !isPerpetual) break
+            value = nextValue
+        }
+    }
+}
+
+@Composable
+private fun CarouselActiveItemIndicatorBar(
+    itemCount: Int,
+    activeItemIndex: Int,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.spacedBy(4.dp)
+    ) {
+        (0 until itemCount).forEach { index ->
+            CarouselActiveItemIndicator(
+                modifier = Modifier.weight(1f),
+                isActive = activeItemIndex == index,
+            )
+        }
+    }
+}
+
+@Composable
+private fun CarouselActiveItemIndicator(
+    isActive: Boolean,
+    modifier: Modifier = Modifier,
+    activeColor: Color = Color(0xff5B5B5B),
+    inactiveColor: Color = Color(0xffD9D9D9),
+) {
+    // TODO animate bg change
+    Box(
+        modifier = modifier
+            .height(4.dp)
+            .background(color = if (isActive) activeColor else inactiveColor)
+    )
+}
+
+@Preview
+@Composable
+private fun PreviewFeatureCarousel() = AppThemeWithBackground(Theme.ThemeType.LIGHT) {
+    FeatureCarousel(
+        modifier = Modifier.fillMaxSize()
+    )
+}
