9a0238843464154de645eed67b3c3449058fe249
Tamas Szelezsan <szelezsant@gmail.com>
2025-08-17 14:42:28 +0200

    Animate carousel page indicator

--- modules/features/account/src/main/java/au/com/shiftyjelly/pocketcasts/account/onboarding/components/FeatureCarousel.kt
+++ modules/features/account/src/main/java/au/com/shiftyjelly/pocketcasts/account/onboarding/components/FeatureCarousel.kt
@@ -1,6 +1,9 @@
 package au.com.shiftyjelly.pocketcasts.account.onboarding.components
 
+import androidx.compose.animation.Animatable
 import androidx.compose.animation.Crossfade
+import androidx.compose.animation.core.FastOutLinearInEasing
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -10,19 +13,20 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.produceState
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import au.com.shiftyjelly.pocketcasts.compose.AppThemeWithBackground
 import au.com.shiftyjelly.pocketcasts.ui.theme.Theme
-import kotlinx.coroutines.delay
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.seconds
+import kotlinx.coroutines.delay
 
 private const val CAROUSEL_ITEM_COUNT = 3
 
@@ -30,9 +34,10 @@ private const val CAROUSEL_ITEM_COUNT = 3
 fun FeatureCarousel(
     modifier: Modifier = Modifier,
 ) {
+    val delayBetweenCycles = 4.seconds
     val activeItemIndex by cyclicCounter(
         cycleRange = 0 until CAROUSEL_ITEM_COUNT,
-        delayBetweenCycles = 3.seconds,
+        delayBetweenCycles = delayBetweenCycles,
     )
 
     Column(
@@ -108,11 +113,18 @@ private fun CarouselActiveItemIndicator(
     activeColor: Color = Color(0xff5B5B5B),
     inactiveColor: Color = Color(0xffD9D9D9),
 ) {
-    // TODO animate bg change
+    val colorAnim = remember { Animatable(if (isActive) activeColor else inactiveColor) }
+    LaunchedEffect(isActive) {
+        colorAnim.animateTo(
+            targetValue = if (isActive) activeColor else inactiveColor,
+            animationSpec = tween(durationMillis = 200, easing = FastOutLinearInEasing)
+        )
+    }
+
     Box(
         modifier = modifier
             .height(4.dp)
-            .background(color = if (isActive) activeColor else inactiveColor),
+            .background(color = colorAnim.value),
     )
 }
 
