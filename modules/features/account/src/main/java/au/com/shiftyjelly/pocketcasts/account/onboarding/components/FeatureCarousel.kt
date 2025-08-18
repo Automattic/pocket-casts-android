5167e26d466e34272e8fc78ae19558de0138f946
Tamas Szelezsan <33399753+sztomek@users.noreply.github.com>
2025-08-18 09:51:02 +0200

    [Onboarding] Feature carousel skeleton (#4368)

---
+++ modules/features/account/src/main/java/au/com/shiftyjelly/pocketcasts/account/onboarding/components/FeatureCarousel.kt
@@ -0,0 +1,137 @@
+package au.com.shiftyjelly.pocketcasts.account.onboarding.components
+
+import androidx.compose.animation.Animatable
+import androidx.compose.animation.Crossfade
+import androidx.compose.animation.core.FastOutLinearInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.produceState
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import au.com.shiftyjelly.pocketcasts.compose.AppThemeWithBackground
+import au.com.shiftyjelly.pocketcasts.ui.theme.Theme
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
+import kotlinx.coroutines.delay
+
+private const val CAROUSEL_ITEM_COUNT = 3
+
+@Composable
+fun FeatureCarousel(
+    modifier: Modifier = Modifier,
+) {
+    val delayBetweenCycles = 4.seconds
+    val activeItemIndex by cyclicCounter(
+        cycleRange = 0 until CAROUSEL_ITEM_COUNT,
+        delayBetweenCycles = delayBetweenCycles,
+    )
+
+    Column(
+        modifier = modifier.padding(top = 11.dp),
+    ) {
+        CarouselActiveItemIndicatorBar(
+            itemCount = CAROUSEL_ITEM_COUNT,
+            activeItemIndex = activeItemIndex,
+        )
+        Crossfade(
+            targetState = activeItemIndex,
+            modifier = Modifier.fillMaxSize(),
+        ) { index ->
+            when (index) {
+                0 -> BestAppAnimation(
+                    modifier = Modifier.padding(top = 64.dp),
+                    itemDisplayDuration = delayBetweenCycles,
+                )
+
+                1 -> CustomizationIsInsaneAnimation(
+                    modifier = Modifier.padding(top = 24.dp),
+                    itemDisplayDuration = delayBetweenCycles,
+                )
+
+                2 -> OrganizingPodcastsAnimation(
+                    modifier = Modifier.padding(top = 24.dp),
+                    itemDisplayDuration = delayBetweenCycles,
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun cyclicCounter(
+    cycleRange: IntRange,
+    delayBetweenCycles: Duration = 3.seconds,
+    isPerpetual: Boolean = true,
+): State<Int> {
+    return produceState(cycleRange.start) {
+        while (true) {
+            delay(delayBetweenCycles.inWholeMilliseconds)
+            val nextValue = (value + 1) % cycleRange.count()
+            if (nextValue == 0 && !isPerpetual) break
+            value = nextValue
+        }
+    }
+}
+
+@Composable
+private fun CarouselActiveItemIndicatorBar(
+    itemCount: Int,
+    activeItemIndex: Int,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.spacedBy(4.dp),
+    ) {
+        (0 until itemCount).forEach { index ->
+            CarouselActiveItemIndicator(
+                modifier = Modifier.weight(1f),
+                isActive = activeItemIndex == index,
+            )
+        }
+    }
+}
+
+@Composable
+private fun CarouselActiveItemIndicator(
+    isActive: Boolean,
+    modifier: Modifier = Modifier,
+    activeColor: Color = Color(0xff5B5B5B),
+    inactiveColor: Color = Color(0xffD9D9D9),
+) {
+    val colorAnim = remember { Animatable(if (isActive) activeColor else inactiveColor) }
+    LaunchedEffect(isActive) {
+        colorAnim.animateTo(
+            targetValue = if (isActive) activeColor else inactiveColor,
+            animationSpec = tween(durationMillis = 200, easing = FastOutLinearInEasing),
+        )
+    }
+
+    Box(
+        modifier = modifier
+            .height(4.dp)
+            .background(color = colorAnim.value),
+    )
+}
+
+@Preview
+@Composable
+private fun PreviewFeatureCarousel() = AppThemeWithBackground(Theme.ThemeType.LIGHT) {
+    FeatureCarousel(
+        modifier = Modifier.fillMaxSize(),
+    )
+}
