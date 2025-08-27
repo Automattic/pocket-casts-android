2b2ac3a308544c307d3ad0fde2522900987df440
Michał Sikora <me@mehow.io>
2025-08-27 10:03:30 +0200

    Display playlist header

--- modules/features/filters/src/main/java/au/com/shiftyjelly/pocketcasts/playlists/manual/PlaylistFragment.kt
+++ modules/features/filters/src/main/java/au/com/shiftyjelly/pocketcasts/playlists/manual/PlaylistFragment.kt
@@ -3,19 +3,22 @@ package au.com.shiftyjelly.pocketcasts.playlists.manual
 import android.os.Bundle
 import android.os.Parcelable
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.foundation.text.input.TextFieldState
 import androidx.core.os.BundleCompat
 import androidx.core.os.bundleOf
-import au.com.shiftyjelly.pocketcasts.compose.AppThemeWithBackground
-import au.com.shiftyjelly.pocketcasts.compose.components.TextP40
-import au.com.shiftyjelly.pocketcasts.compose.extensions.contentWithoutConsumedInsets
+import au.com.shiftyjelly.pocketcasts.filters.databinding.PlaylistFragmentBinding
+import au.com.shiftyjelly.pocketcasts.playlists.component.PlaylistHeaderAdapter
+import au.com.shiftyjelly.pocketcasts.playlists.component.PlaylistHeaderButtonData
+import au.com.shiftyjelly.pocketcasts.playlists.component.PlaylistHeaderData
 import au.com.shiftyjelly.pocketcasts.views.fragments.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
+import kotlin.time.Duration
 import kotlinx.parcelize.Parcelize
+import timber.log.Timber
+import au.com.shiftyjelly.pocketcasts.images.R as IR
+import au.com.shiftyjelly.pocketcasts.localization.R as LR
 
 @AndroidEntryPoint
 class PlaylistFragment : BaseFragment() {
@@ -25,12 +28,38 @@ class PlaylistFragment : BaseFragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ) = contentWithoutConsumedInsets {
-        AppThemeWithBackground(theme.activeTheme) {
-            Box(modifier = Modifier.fillMaxSize()) {
-                TextP40("Manual playlist: ${args.playlistUuid}", modifier = Modifier.align(Alignment.Center))
-            }
-        }
+    ): View {
+        val binding = PlaylistFragmentBinding.inflate(inflater, container, false)
+        binding.setupContent()
+        return binding.root
+    }
+
+    private fun PlaylistFragmentBinding.setupContent() {
+        val headerAdapter = PlaylistHeaderAdapter(
+            themeType = theme.activeTheme,
+            leftButton = PlaylistHeaderButtonData(
+                iconId = IR.drawable.ic_playlist_add_episodes,
+                label = getString(LR.string.add_episodes),
+                onClick = { Timber.i("Add episodes") }
+            ),
+            rightButton = PlaylistHeaderButtonData(
+                iconId = IR.drawable.ic_playlist_play,
+                label = getString(LR.string.playlist_play_all),
+                onClick = { Timber.i("Play all") }
+            ),
+            searchState = TextFieldState(),
+            onChangeSearchFocus = { _, _ -> Timber.i("Scroll to content") }
+        )
+        headerAdapter.submitHeader(
+            PlaylistHeaderData(
+                title = "Playlist title",
+                totalEpisodeCount = 0,
+                displayedEpisodeCount = 0,
+                playbackDurationLeft = Duration.ZERO,
+                artworkPodcastUuids = emptyList(),
+            )
+        )
+        content.adapter = headerAdapter
     }
 
     @Parcelize
