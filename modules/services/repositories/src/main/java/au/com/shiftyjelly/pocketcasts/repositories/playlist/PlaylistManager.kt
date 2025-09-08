e13341f8ef73b8f5e574ee4961d274b97eee4b45
Michał Sikora <me@mehow.io>
2025-09-08 16:48:35 +0200

    Add query to delete episodes

--- modules/services/repositories/src/main/java/au/com/shiftyjelly/pocketcasts/repositories/playlist/PlaylistManager.kt
+++ modules/services/repositories/src/main/java/au/com/shiftyjelly/pocketcasts/repositories/playlist/PlaylistManager.kt
@@ -53,6 +53,8 @@ interface PlaylistManager {
     fun notAddedManualEpisodesFlow(playlistUuid: String, podcastUuid: String, searchTerm: String? = null): Flow<List<PodcastEpisode>>
 
     suspend fun addManualEpisode(playlistUuid: String, episodeUuid: String): Boolean
+
+    suspend fun deleteManualEpisodes(playlistUuid: String, episodeUuids: Collection<String>)
     // </editor-fold>
 
     companion object {
