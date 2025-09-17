e9c31738c89449429c08e4e3b3ed80ebe65a2be4
Michał Sikora <me@mehow.io>
2025-09-17 12:34:26 +0200

    Add episode playlist creation flow

--- modules/features/filters/src/main/java/au/com/shiftyjelly/pocketcasts/playlists/manual/AddToPlaylistFragment.kt
+++ modules/features/filters/src/main/java/au/com/shiftyjelly/pocketcasts/playlists/manual/AddToPlaylistFragment.kt
@@ -4,53 +4,78 @@ import android.os.Bundle
 import android.os.Parcelable
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.rememberNestedScrollInteropConnection
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 import androidx.core.os.BundleCompat
 import androidx.core.os.bundleOf
+import androidx.fragment.app.viewModels
 import androidx.fragment.compose.content
-import au.com.shiftyjelly.pocketcasts.compose.AppTheme
-import au.com.shiftyjelly.pocketcasts.compose.components.TextP40
+import androidx.navigation.compose.rememberNavController
+import au.com.shiftyjelly.pocketcasts.playlists.PlaylistFragment
+import au.com.shiftyjelly.pocketcasts.repositories.playlist.Playlist
+import au.com.shiftyjelly.pocketcasts.ui.helper.FragmentHostListener
 import au.com.shiftyjelly.pocketcasts.ui.theme.Theme
 import au.com.shiftyjelly.pocketcasts.views.fragments.BaseDialogFragment
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.lifecycle.withCreationCallback
 import kotlinx.parcelize.Parcelize
+import au.com.shiftyjelly.pocketcasts.localization.R as LR
+import au.com.shiftyjelly.pocketcasts.views.R as VR
 
 @AndroidEntryPoint
-class AddToPlaylistFragment : BaseDialogFragment() {
+internal class AddToPlaylistFragment : BaseDialogFragment() {
     private val args get() = requireNotNull(arguments?.let { BundleCompat.getParcelable(it, NEW_INSTANCE_ARGS, Args::class.java) })
+
+    private val viewModel by viewModels<AddToPlaylistViewModel>(
+        extrasProducer = {
+            defaultViewModelCreationExtras.withCreationCallback<AddToPlaylistViewModel.Factory> { factory ->
+                factory.create(args.episodeUuid, initialPlaylistTitle = getString(LR.string.new_playlist))
+            }
+        },
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ) = content {
+        val navController = rememberNavController()
+
+        OpenCreatedPlaylistEffect()
+
         DialogBox(
             themeType = args.customTheme ?: theme.activeTheme,
             modifier = Modifier.nestedScroll(rememberNestedScrollInteropConnection()),
         ) {
-            Column(
-                verticalArrangement = Arrangement.Center,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .verticalScroll(rememberScrollState())
-                    .padding(horizontal = 16.dp),
-            ) {
-                TextP40(
-                    text = "Add episode '${args.episodeUuid}'",
-                )
-            }
+            AddToPlaylistPage(
+                navController = navController,
+                newPlaylistNameState = viewModel.newPlaylistNameState,
+                onClickDoneButton = ::dismiss,
+                onClickCreatePlaylist = viewModel::createPlaylist,
+                onClickNavigationButton = {
+                    if (!navController.popBackStack()) {
+                        dismiss()
+                    }
+                },
+                modifier = Modifier.fillMaxSize(),
+            )
+        }
+    }
+
+    @Composable
+    private fun OpenCreatedPlaylistEffect() {
+        LaunchedEffect(Unit) {
+            val createdPlaylistUuid = viewModel.createdPlaylist.await()
+            val hostListener = requireActivity() as FragmentHostListener
+            val fragment = PlaylistFragment.newInstance(createdPlaylistUuid, Playlist.Type.Manual)
+            hostListener.openTab(VR.id.navigation_filters)
+            hostListener.addFragment(fragment)
+            hostListener.closePlayer()
+            dismiss()
         }
     }
 
