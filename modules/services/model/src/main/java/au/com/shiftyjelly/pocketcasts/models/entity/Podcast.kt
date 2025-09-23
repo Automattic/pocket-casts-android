f1403e7d6f069d732274d1f0a76b713d042e1f7e
Michał Sikora <me@mehow.io>
2025-09-23 09:28:52 +0200

    Misc cleanup

--- modules/services/model/src/main/java/au/com/shiftyjelly/pocketcasts/models/entity/Podcast.kt
+++ modules/services/model/src/main/java/au/com/shiftyjelly/pocketcasts/models/entity/Podcast.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.res.Resources
 import android.graphics.Color
 import android.text.format.DateUtils
+import androidx.core.graphics.toColorInt
 import androidx.room.ColumnInfo
 import androidx.room.Embedded
 import androidx.room.Entity
@@ -112,7 +113,7 @@ data class Podcast(
         ;
 
         companion object {
-            fun fromDatabaseInt(int: Int?) = values().firstOrNull { it.databaseInt == int }
+            fun fromDatabaseInt(int: Int?) = entries.firstOrNull { it.databaseInt == int }
         }
     }
 
@@ -136,15 +137,6 @@ data class Podcast(
 
     val isAutoDownloadNewEpisodes: Boolean
         get() = autoDownloadStatus == AUTO_DOWNLOAD_NEW_EPISODES
-
-    val isAutoAddToUpNextOff: Boolean
-        get() = autoAddToUpNext == AutoAddUpNext.OFF
-
-    val isAutoAddToUpNextPlayLast: Boolean
-        get() = autoAddToUpNext == AutoAddUpNext.PLAY_LAST
-
-    val isAutoAddToUpNextPlayNext: Boolean
-        get() = autoAddToUpNext == AutoAddUpNext.PLAY_NEXT
 
     val adapterId: Long
         get() = UUID.nameUUIDFromBytes(uuid.toByteArray()).mostSignificantBits
@@ -237,7 +229,7 @@ data class Podcast(
             } else {
                 host
             }
-        } catch (e: MalformedURLException) {
+        } catch (_: MalformedURLException) {
             ""
         }
     }
@@ -316,10 +308,10 @@ data class Podcast(
     }
 }
 
-private val DEFAULT_SERVER_LIGHT_TINT_COLOR = Color.parseColor("#F44336")
-private val DEFAULT_SERVER_DARK_TINT_COLOR = Color.parseColor("#C62828")
-private val DEFAULT_LIGHT_TINT = Color.parseColor("#1E1F1E")
-private val DEFAULT_DARK_TINT = Color.parseColor("#FFFFFF")
+private val DEFAULT_SERVER_LIGHT_TINT_COLOR = "#F44336".toColorInt()
+private val DEFAULT_SERVER_DARK_TINT_COLOR = "#C62828".toColorInt()
+private val DEFAULT_LIGHT_TINT = "#1E1F1E".toColorInt()
+private val DEFAULT_DARK_TINT = "#FFFFFF".toColorInt()
 
 private val KnownCategoryIds = mapOf(
     "arts" to 1,
