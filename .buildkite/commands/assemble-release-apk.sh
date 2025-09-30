aef58665cd1766215b1e7f40ecd8e09a31f3cb46
Olivier Halligon <olivier.halligon@automattic.com>
2025-09-30 14:07:38 +0200

    TO REVERT: Use fastlane to build bundle during regular PR build

--- .buildkite/commands/assemble-release-apk.sh
+++ .buildkite/commands/assemble-release-apk.sh
@@ -16,4 +16,7 @@ bundle exec fastlane run configure_apply
 
 echo "--- ⚙️ Building release variant"
 
-./gradlew "$1:assembleRelease" -PskipSentryProguardMappingUpload=true
+# ./gradlew "$1:assembleRelease" -PskipSentryProguardMappingUpload=true
+# TODO: Revert this, as this only builds the .aab not the .apk
+# We're just doing this temporarily to test the Sentry Mapping UUID annotation
+bundle exec fastlane build_bundle app:"$1"
