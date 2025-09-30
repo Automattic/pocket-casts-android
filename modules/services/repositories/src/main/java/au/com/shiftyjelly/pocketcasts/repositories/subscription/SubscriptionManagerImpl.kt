119454ba33fcd83877f2b3a5bbf3204ac66408a7
Michał Sikora <me@mehow.io>
2025-09-30 13:05:09 +0200

    Synchronize subscription fetch call

--- modules/services/repositories/src/main/java/au/com/shiftyjelly/pocketcasts/repositories/subscription/SubscriptionManagerImpl.kt
+++ modules/services/repositories/src/main/java/au/com/shiftyjelly/pocketcasts/repositories/subscription/SubscriptionManagerImpl.kt
@@ -4,17 +4,49 @@ import au.com.shiftyjelly.pocketcasts.models.type.Membership
 import au.com.shiftyjelly.pocketcasts.models.type.Subscription
 import au.com.shiftyjelly.pocketcasts.models.type.SubscriptionPlatform
 import au.com.shiftyjelly.pocketcasts.preferences.Settings
+import au.com.shiftyjelly.pocketcasts.repositories.di.ApplicationScope
 import au.com.shiftyjelly.pocketcasts.repositories.sync.SyncManager
 import au.com.shiftyjelly.pocketcasts.servers.sync.toMembership
 import javax.inject.Inject
 import javax.inject.Singleton
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 
 @Singleton
 class SubscriptionManagerImpl @Inject constructor(
+    @ApplicationScope private val scope: CoroutineScope,
     private val syncManager: SyncManager,
     private val settings: Settings,
 ) : SubscriptionManager {
+    private val fetchMutex = Mutex()
+    private var fetchCallsCount = 0
+    private var inFlightSubscription: Deferred<Subscription?>? = null
+
     override suspend fun fetchFreshSubscription(): Subscription? {
+        val deferred = fetchMutex.withLock {
+            val inFlight = inFlightSubscription ?: scope.async(start = CoroutineStart.LAZY) { fetchSubscription() }
+            inFlightSubscription = inFlight
+            fetchCallsCount++
+            inFlight
+        }
+
+        return try {
+            deferred.await()
+        } finally {
+            fetchMutex.withLock {
+                fetchCallsCount--
+                if (fetchCallsCount == 0) {
+                    inFlightSubscription = null
+                }
+            }
+        }
+    }
+
+    private suspend fun fetchSubscription(): Subscription? {
         return runCatching { syncManager.subscriptionStatus().toMembership() }
             .onSuccess { membership ->
                 val subscription = membership.subscription
