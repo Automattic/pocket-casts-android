79c569ecf676a2a26684f7a1de739ea50d18f782
Tamas Szelezsan <szelezsant@gmail.com>
2025-09-30 10:46:36 +0200

    Fix REST request type

--- modules/services/servers/src/main/java/au/com/shiftyjelly/pocketcasts/servers/search/SearchService.kt
+++ modules/services/servers/src/main/java/au/com/shiftyjelly/pocketcasts/servers/search/SearchService.kt
@@ -1,10 +1,10 @@
 package au.com.shiftyjelly.pocketcasts.servers.search
 
-import retrofit2.http.POST
+import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface SearchService {
-    @POST("/autocomplete/search")
+    @GET("/autocomplete/search")
     suspend fun autocompleteSearch(
         @Query("q") query: String,
         @Query("termsLimit") limit: Int,
