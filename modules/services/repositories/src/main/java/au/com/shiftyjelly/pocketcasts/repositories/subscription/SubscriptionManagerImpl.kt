9991b7ee16ce5009a8f82dfcb3e44187f64e3a83
Michał Sikora <me@mehow.io>
2025-10-03 17:04:45 +0200

    Minimize fetch subscription status call count (#4568)

--- modules/services/repositories/src/main/java/au/com/shiftyjelly/pocketcasts/repositories/subscription/SubscriptionManagerImpl.kt
+++ modules/services/repositories/src/main/java/au/com/shiftyjelly/pocketcasts/repositories/subscription/SubscriptionManagerImpl.kt
@@ -4,18 +4,23 @@ import au.com.shiftyjelly.pocketcasts.models.type.Membership
 import au.com.shiftyjelly.pocketcasts.models.type.Subscription
 import au.com.shiftyjelly.pocketcasts.models.type.SubscriptionPlatform
 import au.com.shiftyjelly.pocketcasts.preferences.Settings
+import au.com.shiftyjelly.pocketcasts.repositories.di.ApplicationScope
 import au.com.shiftyjelly.pocketcasts.repositories.sync.SyncManager
 import au.com.shiftyjelly.pocketcasts.servers.sync.toMembership
+import au.com.shiftyjelly.pocketcasts.utils.coroutines.SyncedAction
+import au.com.shiftyjelly.pocketcasts.utils.coroutines.run
 import javax.inject.Inject
 import javax.inject.Singleton
+import kotlinx.coroutines.CoroutineScope
 
 @Singleton
 class SubscriptionManagerImpl @Inject constructor(
+    @ApplicationScope private val scope: CoroutineScope,
     private val syncManager: SyncManager,
     private val settings: Settings,
 ) : SubscriptionManager {
-    override suspend fun fetchFreshSubscription(): Subscription? {
-        return runCatching { syncManager.subscriptionStatus().toMembership() }
+    private val fetchMembershipAction = SyncedAction<Unit, Membership?> {
+        runCatching { syncManager.subscriptionStatus().toMembership() }
             .onSuccess { membership ->
                 val subscription = membership.subscription
                 settings.cachedMembership.set(membership, updateModifiedAt = false)
@@ -23,8 +28,11 @@ class SubscriptionManagerImpl @Inject constructor(
                     settings.setTrialFinishedSeen(false)
                 }
             }
-            .map { membership -> membership.subscription }
             .getOrNull()
+    }
+
+    override suspend fun fetchFreshSubscription(): Subscription? {
+        return fetchMembershipAction.run(scope)?.subscription
     }
 
     override fun clearCachedMembership() {
