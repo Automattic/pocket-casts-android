c932d6531157595368885f69accd60a93483ec29
Wojtek Zieba <wojtek.zieba@automattic.com>
2025-10-08 12:51:17 +0200

    Bump Develocity dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,8 +17,8 @@ pluginManagement {
 }
 
 plugins {
-    id("com.gradle.develocity").version("3.19.2")
-    id("com.gradle.common-custom-user-data-gradle-plugin").version("2.2.1")
+    id("com.gradle.develocity").version("4.2.1")
+    id("com.gradle.common-custom-user-data-gradle-plugin").version("2.4.0")
 }
 
 dependencyResolutionManagement {
