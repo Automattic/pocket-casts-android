e38a76c566e750b904ad10878ad232307b5805ca
Tamas Szelezsan <szelezsant@gmail.com>
2025-10-23 09:26:37 +0200

    Re-add token exchange logic between phone and wear app

--- wear/src/main/kotlin/au/com/shiftyjelly/pocketcasts/wear/WearMainActivityViewModel.kt
+++ wear/src/main/kotlin/au/com/shiftyjelly/pocketcasts/wear/WearMainActivityViewModel.kt
@@ -3,12 +3,16 @@ package au.com.shiftyjelly.pocketcasts.wear
 import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import au.com.shiftyjelly.pocketcasts.account.watchsync.WatchSync
+import au.com.shiftyjelly.pocketcasts.account.watchsync.WatchSyncAuthData
 import au.com.shiftyjelly.pocketcasts.models.type.SignInState
 import au.com.shiftyjelly.pocketcasts.preferences.Settings
 import au.com.shiftyjelly.pocketcasts.repositories.playback.PlaybackManager
 import au.com.shiftyjelly.pocketcasts.repositories.podcast.PodcastManager
 import au.com.shiftyjelly.pocketcasts.repositories.refresh.RefreshPodcastsTask
+import au.com.shiftyjelly.pocketcasts.repositories.sync.LoginResult
 import au.com.shiftyjelly.pocketcasts.repositories.user.UserManager
+import com.google.android.horologist.auth.data.tokenshare.TokenBundleRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
@@ -28,6 +32,8 @@ class WearMainActivityViewModel @Inject constructor(
     private val userManager: UserManager,
     private val settings: Settings,
     @ApplicationContext private val context: Context,
+    private val tokenBundleRepository: TokenBundleRepository<WatchSyncAuthData?>,
+    private val watchSync: WatchSync,
 ) : ViewModel() {
 
     data class State(
@@ -40,12 +46,35 @@ class WearMainActivityViewModel @Inject constructor(
 
     init {
         viewModelScope.launch {
+            tokenBundleRepository.flow
+                .collect { watchSyncAuthData ->
+                    watchSync.processAuthDataChange(watchSyncAuthData) {
+                        onLoginFromPhoneResult(it)
+                    }
+                }
+        }
+
+        viewModelScope.launch {
             userManager
                 .getSignInState()
                 .asFlow()
                 .collect { signInState ->
                     _state.update { it.copy(signInState = signInState) }
                 }
+        }
+    }
+
+    private fun onLoginFromPhoneResult(loginResult: LoginResult) {
+        when (loginResult) {
+            is LoginResult.Failed -> { /* do nothing */ }
+            is LoginResult.Success -> {
+                viewModelScope.launch {
+                    podcastManager.refreshPodcastsAfterSignIn()
+                }
+                _state.update {
+                    it.copy(showLoggingInScreen = true)
+                }
+            }
         }
     }
 
