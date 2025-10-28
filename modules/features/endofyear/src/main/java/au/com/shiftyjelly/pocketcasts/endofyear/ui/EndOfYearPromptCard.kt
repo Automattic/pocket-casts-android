be3b336afe5989c2c2d6b6fb1ad08b440433bd35
Philip Simpson <philip.simpson@automattic.com>
2025-10-28 16:10:36 +1030

    New Playback entry point design for Profile tab

--- modules/features/endofyear/src/main/java/au/com/shiftyjelly/pocketcasts/endofyear/ui/EndOfYearPromptCard.kt
+++ modules/features/endofyear/src/main/java/au/com/shiftyjelly/pocketcasts/endofyear/ui/EndOfYearPromptCard.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -15,7 +17,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -24,6 +28,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import au.com.shiftyjelly.pocketcasts.compose.components.TextH30
 import au.com.shiftyjelly.pocketcasts.compose.components.TextH70
+import au.com.shiftyjelly.pocketcasts.compose.extensions.nonScaledSp
 import au.com.shiftyjelly.pocketcasts.images.R as IR
 import au.com.shiftyjelly.pocketcasts.localization.R as LR
 
@@ -32,42 +37,42 @@ fun EndOfYearPromptCard(
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
+    Box(
         modifier = modifier
-            .background(color = Color.Black, RoundedCornerShape(8.dp))
-            .clickable { onClick.invoke() }
-            .padding(start = 24.dp, top = 16.dp, bottom = 16.dp),
+            .clip(RoundedCornerShape(8.dp))
+            .background(color = Color(0xFF27486A))
+            .clickable { onClick.invoke() },
     ) {
-        Column(
-            modifier = Modifier.weight(1.1f),
-            verticalArrangement = Arrangement.SpaceBetween,
-        ) {
-            TextH30(
-                text = stringResource(LR.string.end_of_year_prompt_card_title),
-                color = Color.White,
-            )
-            Spacer(
-                modifier = Modifier.height(8.dp),
-            )
-            TextH70(
-                text = stringResource(LR.string.end_of_year_prompt_card_summary),
-                color = Color.White,
-                fontWeight = FontWeight.W600,
-                fontSize = 11.sp,
-                modifier = Modifier.alpha(0.7f),
-            )
-        }
-        Box(
-            contentAlignment = Alignment.CenterEnd,
+        Image(
+            modifier = Modifier.fillMaxWidth(),
+            painter = painterResource(IR.drawable.playback_banner),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+        )
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier
-                .weight(1f)
-                .padding(end = 16.dp),
+                .padding(start = 24.dp, top = 24.dp, bottom = 16.dp)
+                .fillMaxWidth(0.4f),
         ) {
-            Image(
-                painter = painterResource(IR.drawable.end_of_year_2024_pb24),
-                contentDescription = null,
-            )
+            Column(
+                verticalArrangement = Arrangement.SpaceBetween,
+            ) {
+                TextH30(
+                    text = stringResource(LR.string.end_of_year_prompt_card_title),
+                    color = Color.White,
+                    disableAutoScale = true,
+                )
+                Spacer(
+                    modifier = Modifier.height(12.dp),
+                )
+                TextH70(
+                    text = stringResource(LR.string.end_of_year_prompt_card_summary),
+                    color = Color.White,
+                    fontWeight = FontWeight.W600,
+                    disableAutoScale = true,
+                )
+            }
         }
     }
 }
