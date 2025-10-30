2f4863e0faec464017b901b7dc667d0c164182a8
Philip Simpson <philip.simpson@automattic.com>
2025-10-30 15:43:35 +1030

    Fix playback model radioactive theme

--- modules/features/endofyear/src/main/java/au/com/shiftyjelly/pocketcasts/endofyear/ui/EndOfYearLaunchBottomSheet.kt
+++ modules/features/endofyear/src/main/java/au/com/shiftyjelly/pocketcasts/endofyear/ui/EndOfYearLaunchBottomSheet.kt
@@ -1,17 +1,12 @@
 package au.com.shiftyjelly.pocketcasts.endofyear.ui
 
 import android.view.ViewGroup
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.ModalBottomSheetValue
 import androidx.compose.material.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
@@ -19,14 +14,12 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.clipToBounds
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import au.com.shiftyjelly.pocketcasts.compose.bottomsheet.BottomSheetContentState
 import au.com.shiftyjelly.pocketcasts.compose.bottomsheet.ModalBottomSheet
+import au.com.shiftyjelly.pocketcasts.compose.theme
 import kotlinx.coroutines.launch
 import au.com.shiftyjelly.pocketcasts.images.R as IR
 import au.com.shiftyjelly.pocketcasts.localization.R as LR
@@ -76,6 +69,7 @@ private fun ImageContent(modifier: Modifier = Modifier) {
         Image(
             painter = painterResource(IR.drawable.playback_launch_banner),
             contentDescription = null,
+            colorFilter = MaterialTheme.theme.imageColorFilter,
             modifier = Modifier.clip(RoundedCornerShape(16.dp)),
         )
     }
