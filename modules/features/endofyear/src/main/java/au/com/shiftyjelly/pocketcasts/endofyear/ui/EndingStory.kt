66211e75bfd177e35072ef74c810c5c00ef5e2cf
Tamas Szelezsan <33399753+sztomek@users.noreply.github.com>
2025-11-04 16:40:22 +0100

    [Playback] Thank you stroy (#4684)

--- modules/features/endofyear/src/main/java/au/com/shiftyjelly/pocketcasts/endofyear/ui/EndingStory.kt
+++ modules/features/endofyear/src/main/java/au/com/shiftyjelly/pocketcasts/endofyear/ui/EndingStory.kt
@@ -1,35 +1,44 @@
 package au.com.shiftyjelly.pocketcasts.endofyear.ui
 
+import androidx.compose.animation.core.FastOutLinearInEasing
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.animateDp
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import au.com.shiftyjelly.pocketcasts.compose.Devices
-import au.com.shiftyjelly.pocketcasts.compose.components.HorizontalDirection
-import au.com.shiftyjelly.pocketcasts.compose.components.ScrollingRow
 import au.com.shiftyjelly.pocketcasts.compose.components.TextH10
 import au.com.shiftyjelly.pocketcasts.compose.components.TextP40
-import au.com.shiftyjelly.pocketcasts.compose.extensions.nonScaledSp
 import au.com.shiftyjelly.pocketcasts.models.to.Story
+import kotlinx.coroutines.delay
 import au.com.shiftyjelly.pocketcasts.images.R as IR
 import au.com.shiftyjelly.pocketcasts.localization.R as LR
 import au.com.shiftyjelly.pocketcasts.ui.R as UR
@@ -45,114 +54,136 @@ internal fun EndingStory(
             .fillMaxSize()
             .background(story.backgroundColor)
             .padding(top = measurements.closeButtonBottomEdge),
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        ByeByeSection(
-            measurements = measurements,
-        )
-        EndingInfo(
-            story = story,
-            measurements = measurements,
-            onRestartPlayback = onRestartPlayback,
-        )
-    }
-}
+        val animations = screenAnimations()
 
-@Composable
-private fun ColumnScope.ByeByeSection(
-    measurements: EndOfYearMeasurements,
-) {
-    val textFactory = rememberHumaneTextFactory(
-        fontSize = 227.nonScaledSp * measurements.smallDeviceFactor,
-    )
-
-    Column(
-        verticalArrangement = Arrangement.Center,
-        modifier = Modifier
-            .weight(1f)
-            .requiredWidth(measurements.width * 1.5f),
-    ) {
-        ByeByeText(
-            scrollDirection = HorizontalDirection.Left,
-            textFactory = textFactory,
-        )
         Spacer(
-            modifier = Modifier.height(12.dp * measurements.smallDeviceFactor),
+            modifier = Modifier.weight(1f),
         )
-        ByeByeText(
-            scrollDirection = HorizontalDirection.Right,
-            textFactory = textFactory,
+        Image(
+            painter = painterResource(IR.drawable.navdrawer_logo),
+            contentDescription = null,
+            modifier = Modifier
+                .size(60.dp)
+                .offset {
+                    IntOffset(x = 0, y = animations.icon.offset.roundToPx())
+                }
+                .graphicsLayer {
+                    alpha = animations.icon.alpha
+                },
         )
-    }
-}
-
-@Composable
-private fun ByeByeText(
-    scrollDirection: HorizontalDirection,
-    textFactory: HumaneTextFactory,
-) {
-    ScrollingRow(
-        items = listOf("THANKS", "MERCI", "GRACIAS", "OBRIGADO", "GRATKI"),
-        scrollDirection = scrollDirection,
-    ) { text ->
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            textFactory.HumaneText(
-                text = text,
-                color = Color(0xFFEEB1F4),
-            )
-            Spacer(
-                modifier = Modifier.height(8.dp),
-            )
-            Image(
-                painter = painterResource(IR.drawable.eoy_heart_text_stop),
-                contentDescription = null,
-                colorFilter = ColorFilter.tint(Color(0xFFEEB1F4)),
-                modifier = Modifier.padding(horizontal = 8.dp),
-            )
-        }
-    }
-}
-
-@Composable
-private fun EndingInfo(
-    story: Story.Ending,
-    measurements: EndOfYearMeasurements,
-    onRestartPlayback: () -> Unit,
-) {
-    Column(
-        modifier = Modifier.background(
-            brush = Brush.verticalGradient(
-                0f to Color.Transparent,
-                0.1f to story.backgroundColor,
-            ),
-        ),
-    ) {
         Spacer(
             modifier = Modifier.height(16.dp),
         )
         TextH10(
-            text = stringResource(LR.string.end_of_year_story_epilogue_title, 2025),
+            text = stringResource(LR.string.end_of_year_story_epilogue_title),
             fontScale = measurements.smallDeviceFactor,
             disableAutoScale = true,
-            color = colorResource(UR.color.coolgrey_90),
-            modifier = Modifier.padding(horizontal = 24.dp),
+            fontSize = 25.sp,
+            color = colorResource(UR.color.white),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 24.dp)
+                .offset {
+                    IntOffset(x = 0, y = animations.title.offset.roundToPx())
+                }
+                .graphicsLayer {
+                    alpha = animations.title.alpha
+                },
+            textAlign = TextAlign.Center,
         )
         Spacer(
             modifier = Modifier.height(16.dp),
         )
         TextP40(
             text = stringResource(LR.string.end_of_year_story_epilogue_subtitle),
-            fontSize = 15.sp,
             disableAutoScale = true,
-            color = colorResource(UR.color.coolgrey_90),
-            modifier = Modifier.padding(horizontal = 24.dp),
+            color = colorResource(UR.color.white),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 24.dp)
+                .offset {
+                    IntOffset(x = 0, y = animations.subTitle.offset.roundToPx())
+                }
+                .graphicsLayer {
+                    alpha = animations.subTitle.alpha
+                },
+            textAlign = TextAlign.Center,
         )
-        OutlinedEoyButton(
+        Spacer(modifier = Modifier.weight(1f))
+        SolidEoyButton(
             text = stringResource(LR.string.end_of_year_replay),
             onClick = onRestartPlayback,
+            backgroundColor = colorResource(UR.color.white),
+            textColor = colorResource(UR.color.black),
         )
     }
+}
+
+private class ItemAnimation(
+    val alpha: Float,
+    val offset: Dp,
+)
+
+private class Animations(
+    val icon: ItemAnimation,
+    val title: ItemAnimation,
+    val subTitle: ItemAnimation,
+)
+
+@Composable
+private fun screenAnimations(): Animations {
+    var iconAnimationTrigger by remember { mutableStateOf(false) }
+    val iconAnimation = animateItem(start = iconAnimationTrigger, label = "icon animation")
+
+    var titleAnimationTrigger by remember { mutableStateOf(false) }
+    val titleAnimation = animateItem(start = titleAnimationTrigger, label = "title animation")
+    var subTitleAnimationTrigger by remember { mutableStateOf(false) }
+    val subTitleAnimation = animateItem(start = subTitleAnimationTrigger, label = "subTitle animation")
+
+    LaunchedEffect(Unit) {
+        iconAnimationTrigger = true
+        delay(75)
+        titleAnimationTrigger = true
+        delay(75)
+        subTitleAnimationTrigger = true
+    }
+
+    return Animations(
+        icon = iconAnimation,
+        title = titleAnimation,
+        subTitle = subTitleAnimation,
+    )
+}
+
+@Composable
+private fun animateItem(start: Boolean, label: String): ItemAnimation {
+    val transition = updateTransition(start, label)
+    val alpha by transition.animateFloat(
+        transitionSpec = {
+            tween(durationMillis = 400, easing = FastOutLinearInEasing)
+        },
+    ) {
+        if (it) {
+            1f
+        } else {
+            0f
+        }
+    }
+    val offset by transition.animateDp(
+        transitionSpec = {
+            tween(durationMillis = 400, easing = FastOutSlowInEasing)
+        },
+    ) {
+        if (it) {
+            0.dp
+        } else {
+            24.dp
+        }
+    }
+
+    return ItemAnimation(alpha = alpha, offset = offset)
 }
 
 @Preview(device = Devices.PORTRAIT_REGULAR)
