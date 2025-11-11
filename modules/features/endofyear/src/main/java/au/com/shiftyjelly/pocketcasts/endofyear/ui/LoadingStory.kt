ea8903a9392db9dee1e37dec27af2d3b9703d92d
Tamas Szelezsan <szelezsant@gmail.com>
2025-11-11 20:00:07 +0100

    Add loading story

---
+++ modules/features/endofyear/src/main/java/au/com/shiftyjelly/pocketcasts/endofyear/ui/LoadingStory.kt
@@ -0,0 +1,29 @@
+package au.com.shiftyjelly.pocketcasts.endofyear.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import au.com.shiftyjelly.pocketcasts.models.to.Story
+import au.com.shiftyjelly.pocketcasts.ui.R as UR
+
+@Composable
+internal fun LoadingStory(
+    story: Story.PlaceholderWhileLoading,
+    measurements: EndOfYearMeasurements
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(story.backgroundColor)
+            .padding(top = measurements.closeButtonBottomEdge),
+        contentAlignment = Alignment.Center,
+    ) {
+        CircularProgressIndicator(color = colorResource(UR.color.white))
+    }
+}
