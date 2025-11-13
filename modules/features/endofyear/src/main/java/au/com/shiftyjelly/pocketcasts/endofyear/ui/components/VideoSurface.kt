6c8a0d1d05d1cdbbf70a452aa2ad7e7631b668ef
Philip Simpson <philip.simpson@automattic.com>
2025-11-13 14:39:15 +1030

    Alignment fix for small phone

--- modules/features/endofyear/src/main/java/au/com/shiftyjelly/pocketcasts/endofyear/ui/components/VideoSurface.kt
+++ modules/features/endofyear/src/main/java/au/com/shiftyjelly/pocketcasts/endofyear/ui/components/VideoSurface.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -56,7 +57,10 @@ fun VideoSurface(
         }
     }
 
-    Box(modifier = modifier) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier,
+    ) {
         player?.let {
             PlayerSurface(
                 player = it,
