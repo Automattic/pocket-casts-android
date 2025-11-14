4b366d68ae8d4646aee4aed91e86cfec97f12ce7
Tamas Szelezsan <szelezsant@gmail.com>
2025-11-14 16:36:53 +0100

    Add lottie

--- modules/features/endofyear/src/main/java/au/com/shiftyjelly/pocketcasts/endofyear/ui/NumberOfShowsStory.kt
+++ modules/features/endofyear/src/main/java/au/com/shiftyjelly/pocketcasts/endofyear/ui/NumberOfShowsStory.kt
@@ -1,43 +1,42 @@
 package au.com.shiftyjelly.pocketcasts.endofyear.ui
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import au.com.shiftyjelly.pocketcasts.compose.Devices
-import au.com.shiftyjelly.pocketcasts.compose.components.HorizontalDirection
-import au.com.shiftyjelly.pocketcasts.compose.components.PodcastImageDeprecated
+import au.com.shiftyjelly.pocketcasts.compose.components.PodcastImage
 import au.com.shiftyjelly.pocketcasts.compose.components.ScrollSpeed
 import au.com.shiftyjelly.pocketcasts.compose.components.ScrollingRow
 import au.com.shiftyjelly.pocketcasts.compose.components.TextH10
-import au.com.shiftyjelly.pocketcasts.compose.components.TextP40
+import au.com.shiftyjelly.pocketcasts.endofyear.R
 import au.com.shiftyjelly.pocketcasts.endofyear.StoryCaptureController
-import au.com.shiftyjelly.pocketcasts.localization.R
 import au.com.shiftyjelly.pocketcasts.models.to.Story
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
 import dev.shreyaspatil.capturable.capturable
 import java.io.File
-import kotlin.math.tan
 import kotlin.time.Duration.Companion.seconds
+import au.com.shiftyjelly.pocketcasts.localization.R as LR
 import au.com.shiftyjelly.pocketcasts.ui.R as UR
 
 @OptIn(ExperimentalComposeUiApi::class)
@@ -48,102 +47,79 @@ internal fun NumberOfShowsStory(
     controller: StoryCaptureController,
     onShareStory: (File) -> Unit,
 ) {
-    val smallCoverSize = 160.dp * measurements.scale
-    val smallSpacingSize = smallCoverSize / 10
-    val largeCoverSize = 200.dp * measurements.scale
-    val largeSpacingSize = smallCoverSize / 10
-    val carouselRotationOffset = (measurements.width / 1.5f) * tan(STORY_ROTATION_RADIANS)
-
     Box(
         modifier = Modifier
             .capturable(controller.captureController(story))
             .fillMaxSize()
-            .background(story.backgroundColor),
+            .background(story.backgroundColor)
+            .padding(top = measurements.closeButtonBottomEdge),
     ) {
-        Column(
-            modifier = Modifier
-                .align(Alignment.TopCenter)
-                .rotate(STORY_ROTATION_DEGREES)
-                .offset(y = measurements.closeButtonBottomEdge + 8.dp - carouselRotationOffset)
-                .requiredWidth(measurements.width * 1.5f), // Increase the size to account for rotation
-        ) {
-            PodcastCoverCarousel(
-                podcastIds = story.topShowIds,
-                scrollDirection = HorizontalDirection.Left,
-                coverSize = smallCoverSize,
-                coverElevation = 0.dp,
-                spacingSize = smallSpacingSize,
-            )
-            PodcastCoverCarousel(
-                podcastIds = story.bottomShowIds,
-                scrollDirection = HorizontalDirection.Right,
-                coverSize = largeCoverSize,
-                coverElevation = 12.dp,
-                spacingSize = largeSpacingSize,
-                modifier = Modifier.offset(y = -smallCoverSize / 6),
-            )
-        }
+        TextH10(
+            text = stringResource(
+                LR.string.end_of_year_story_listened_to_numbers,
+                story.showCount,
+                story.epsiodeCount,
+            ),
+            fontSize = 25.sp,
+            lineHeight = 30.sp,
+            disableAutoScale = true,
+            textAlign = TextAlign.Center,
+            fontScale = measurements.smallDeviceFactor,
+            color = colorResource(UR.color.white),
+            modifier = Modifier.padding(horizontal = 24.dp),
+        )
 
-        Column(
+        val composition by rememberLottieComposition(
+            spec = LottieCompositionSpec.RawRes(R.raw.playback_number_of_shows_lottie),
+        )
+        val isPreview = LocalInspectionMode.current
+        val freezeAnimation = controller.isSharing || isPreview
+
+        val progress by animateLottieCompositionAsState(
+            composition = composition,
+            iterations = LottieConstants.IterateForever,
+            isPlaying = !freezeAnimation,
+        )
+
+        LottieAnimation(
+            composition = composition,
+            progress = { if (freezeAnimation) 1f else progress },
             modifier = Modifier
-                .align(Alignment.BottomCenter)
-                .background(
-                    brush = Brush.verticalGradient(
-                        0f to Color.Transparent,
-                        0.1f to story.backgroundColor,
-                    ),
-                ),
-        ) {
-            TextH10(
-                text = stringResource(
-                    R.string.end_of_year_story_listened_to_numbers,
-                    story.showCount,
-                    story.episodeCount,
-                ),
-                disableAutoScale = true,
-                fontScale = measurements.smallDeviceFactor,
-                color = colorResource(UR.color.coolgrey_90),
-                modifier = Modifier.padding(horizontal = 24.dp),
-            )
-            Spacer(
-                modifier = Modifier.height(16.dp),
-            )
-            TextP40(
-                text = stringResource(R.string.end_of_year_story_listened_to_numbers_subtitle),
-                fontSize = 15.sp,
-                disableAutoScale = true,
-                color = colorResource(UR.color.coolgrey_90),
-                modifier = Modifier.padding(horizontal = 24.dp),
-            )
-            ShareStoryButton(
-                story = story,
-                controller = controller,
-                onShare = onShareStory,
-            )
-        }
+                .matchParentSize()
+                .scale(1.2f),
+            contentScale = ContentScale.FillWidth,
+        )
+
+        PodcastCoverCarousel(
+            podcastIds = story.bottomShowIds,
+            coverSize = 260.dp,
+            modifier = Modifier.align(alignment = Alignment.Center),
+        )
+        ShareStoryButton(
+            modifier = Modifier
+                .padding(bottom = 18.dp)
+                .align(alignment = Alignment.BottomCenter),
+            story = story,
+            controller = controller,
+            onShare = onShareStory,
+        )
     }
 }
+
 
 @Composable
 private fun PodcastCoverCarousel(
     podcastIds: List<String>,
-    scrollDirection: HorizontalDirection,
     coverSize: Dp,
-    coverElevation: Dp,
-    spacingSize: Dp,
     modifier: Modifier = Modifier,
 ) {
     ScrollingRow(
         items = podcastIds,
-        scrollDirection = scrollDirection,
         scrollSpeed = ScrollSpeed(300.dp, 10.seconds),
-        horizontalArrangement = Arrangement.spacedBy(spacingSize),
         modifier = modifier,
     ) { podcastId ->
-        @Suppress("DEPRECATION")
-        PodcastImageDeprecated(
+        PodcastImage(
             uuid = podcastId,
-            elevation = coverElevation,
             cornerSize = 4.dp,
             modifier = Modifier.size(coverSize),
         )
