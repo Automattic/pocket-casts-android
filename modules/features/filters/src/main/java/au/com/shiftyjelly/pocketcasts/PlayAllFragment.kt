b4de1615f37285e59506000fbb6da65a4a696eaa
Michał Sikora <me@mehow.io>
2025-11-18 07:38:04 +0100

    Update Play all flow (#4744)

--- modules/features/filters/src/main/java/au/com/shiftyjelly/pocketcasts/PlayAllFragment.kt
+++ modules/features/filters/src/main/java/au/com/shiftyjelly/pocketcasts/PlayAllFragment.kt
@@ -4,34 +4,45 @@ import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.compose.animation.AnimatedContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.selection.toggleable
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.LocalRippleConfiguration
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.RippleConfiguration
+import androidx.compose.material.Switch
+import androidx.compose.material.SwitchDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.rememberNestedScrollInteropConnection
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
@@ -41,7 +52,6 @@ import androidx.fragment.app.viewModels
 import androidx.fragment.compose.content
 import au.com.shiftyjelly.pocketcasts.compose.AppThemeWithBackground
 import au.com.shiftyjelly.pocketcasts.compose.buttons.RowButton
-import au.com.shiftyjelly.pocketcasts.compose.buttons.RowOutlinedButton
 import au.com.shiftyjelly.pocketcasts.compose.components.TextH20
 import au.com.shiftyjelly.pocketcasts.compose.components.TextP40
 import au.com.shiftyjelly.pocketcasts.compose.preview.ThemePreviewParameterProvider
@@ -73,23 +83,12 @@ internal class PlayAllFragment : BaseDialogFragment() {
                 .navigationBarsPadding(),
         ) {
             PlayAllPage(
-                onSaveQueue = {
-                    viewModel.trackSaveUpNextTapped()
-                    viewModel.saveUpNextAsPlaylist(getString(LR.string.up_next))
-                    viewModel.playAll()
+                initialSaveUpNext = viewModel.saveUpNextDefaultValue,
+                onReplaceAndPlay = { saveUpNext ->
                     isFinalizingActionUsed = true
+                    viewModel.saveUpNextAsPlaylist(saveUpNext, getString(LR.string.up_next))
                     dismiss()
                 },
-                onReplaceAndPlay = {
-                    viewModel.trackReplaceAndPlayTapped()
-                },
-                onConfirmReplaceAndPlay = {
-                    viewModel.trackReplaceAndPlayConfirmTapped()
-                    viewModel.playAll()
-                    isFinalizingActionUsed = true
-                    dismiss()
-                },
-                onDismiss = ::dismiss,
             )
         }
     }
@@ -102,21 +101,12 @@ internal class PlayAllFragment : BaseDialogFragment() {
     }
 }
 
-private enum class PlayAllNavigationKey {
-    SaveQueue,
-    ReplaceAndPlay,
-}
-
 @Composable
 private fun PlayAllPage(
-    onSaveQueue: () -> Unit,
-    onReplaceAndPlay: () -> Unit,
-    onConfirmReplaceAndPlay: () -> Unit,
-    onDismiss: () -> Unit,
+    initialSaveUpNext: Boolean,
+    onReplaceAndPlay: (saveUpNext: Boolean) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    var navigationKey by rememberSaveable { mutableStateOf(PlayAllNavigationKey.SaveQueue) }
-
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier
@@ -129,87 +119,21 @@ private fun PlayAllPage(
                 .background(MaterialTheme.theme.colors.primaryUi05, CircleShape)
                 .size(56.dp, 4.dp),
         )
-        AnimatedContent(targetState = navigationKey) { state ->
-            when (state) {
-                PlayAllNavigationKey.SaveQueue -> SaveQueuePage(
-                    onSaveQueue = onSaveQueue,
-                    onReplaceAndPlay = {
-                        navigationKey = PlayAllNavigationKey.ReplaceAndPlay
-                        onReplaceAndPlay()
-                    },
-                )
-
-                PlayAllNavigationKey.ReplaceAndPlay -> ReplaceAndPlayPage(
-                    onReplaceAndPlay = onConfirmReplaceAndPlay,
-                    onDismiss = onDismiss,
-                )
-            }
-        }
-    }
-}
-
-@Composable
-private fun SaveQueuePage(
-    onSaveQueue: () -> Unit,
-    onReplaceAndPlay: () -> Unit,
-    modifier: Modifier = Modifier,
-) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier
-            .fillMaxWidth()
-            .padding(bottom = 8.dp, start = 20.dp, end = 20.dp),
-    ) {
-        Image(
-            painter = painterResource(IR.drawable.ic_upnext),
-            contentDescription = null,
-            colorFilter = ColorFilter.tint(MaterialTheme.theme.colors.primaryIcon01),
-            modifier = Modifier.size(36.dp),
-        )
-        Spacer(
-            modifier = Modifier.height(18.dp),
-        )
-        TextH20(
-            text = stringResource(LR.string.up_next_as_playlist_title),
-            textAlign = TextAlign.Center,
-            modifier = Modifier.padding(horizontal = 24.dp),
-        )
-        Spacer(
-            modifier = Modifier.height(8.dp),
-        )
-        TextP40(
-            text = stringResource(LR.string.up_next_as_playlist_body),
-            fontSize = 15.sp,
-            letterSpacing = 0.sp,
-            textAlign = TextAlign.Center,
-            color = MaterialTheme.theme.colors.primaryText02,
-            modifier = Modifier.padding(horizontal = 24.dp),
-        )
-        Spacer(
-            modifier = Modifier.height(24.dp),
-        )
-        RowButton(
-            text = stringResource(LR.string.up_next_as_playlist_button_primary),
-            includePadding = false,
-            onClick = onSaveQueue,
-        )
-        Spacer(
-            modifier = Modifier.height(16.dp),
-        )
-        RowOutlinedButton(
-            text = stringResource(LR.string.up_next_as_playlist_button_secondary),
-            includePadding = false,
-            onClick = onReplaceAndPlay,
+        ReplaceAndPlayPage(
+            initialSaveUpNext = initialSaveUpNext,
+            onReplaceAndPlay = onReplaceAndPlay,
         )
     }
 }
 
 @Composable
 private fun ReplaceAndPlayPage(
-    onReplaceAndPlay: () -> Unit,
-    onDismiss: () -> Unit,
+    initialSaveUpNext: Boolean,
+    onReplaceAndPlay: (saveUpNext: Boolean) -> Unit,
     modifier: Modifier = Modifier,
 ) {
+    var saveUpNext by rememberSaveable { mutableStateOf(initialSaveUpNext) }
+
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier
@@ -244,19 +168,48 @@ private fun ReplaceAndPlayPage(
         Spacer(
             modifier = Modifier.height(24.dp),
         )
+        CompositionLocalProvider(
+            LocalRippleConfiguration provides RippleConfiguration(color = MaterialTheme.theme.colors.primaryInteractive01),
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .clip(RoundedCornerShape(12.dp))
+                    .background(MaterialTheme.theme.colors.primaryUi02Active)
+                    .fillMaxWidth()
+                    .toggleable(
+                        value = saveUpNext,
+                        role = Role.Switch,
+                        onValueChange = { isChecked -> saveUpNext = isChecked },
+                    )
+                    .padding(16.dp)
+                    .semantics(mergeDescendants = true) { },
+            ) {
+                TextP40(
+                    text = stringResource(LR.string.up_next_as_playlist_replace_switch_description),
+                    color = MaterialTheme.theme.colors.primaryText02,
+                    modifier = Modifier.weight(1f),
+                )
+                Spacer(
+                    modifier = Modifier.width(16.dp),
+                )
+                Switch(
+                    checked = saveUpNext,
+                    onCheckedChange = null,
+                    colors = SwitchDefaults.colors(
+                        uncheckedThumbColor = Color.Gray,
+                        uncheckedTrackColor = Color.Gray,
+                    ),
+                )
+            }
+        }
+        Spacer(
+            modifier = Modifier.height(24.dp),
+        )
         RowButton(
             text = stringResource(LR.string.up_next_as_playlist_replace_button_primary),
             includePadding = false,
-            colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.theme.colors.support05),
-            onClick = onReplaceAndPlay,
-        )
-        Spacer(
-            modifier = Modifier.height(16.dp),
-        )
-        RowOutlinedButton(
-            text = stringResource(LR.string.up_next_as_playlist_replace_button_secondary),
-            includePadding = false,
-            onClick = onDismiss,
+            onClick = { onReplaceAndPlay(saveUpNext) },
         )
     }
 }
@@ -268,10 +221,8 @@ private fun PlayAllPagePreview(
 ) {
     AppThemeWithBackground(themeType) {
         PlayAllPage(
-            onSaveQueue = {},
+            initialSaveUpNext = true,
             onReplaceAndPlay = {},
-            onConfirmReplaceAndPlay = {},
-            onDismiss = {},
         )
     }
 }
