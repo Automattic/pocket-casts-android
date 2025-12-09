d0c70206a86b8535c7a8996a6669baff7b73ac14
Philip Simpson <philip.simpson@automattic.com>
2025-12-10 10:08:27 +1030

    Wear OS: Show archived or played episodes as greyed out

--- wear/src/main/kotlin/au/com/shiftyjelly/pocketcasts/wear/ui/component/EpisodeChip.kt
+++ wear/src/main/kotlin/au/com/shiftyjelly/pocketcasts/wear/ui/component/EpisodeChip.kt
@@ -20,13 +20,16 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.PlatformTextStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
@@ -37,9 +40,14 @@ import au.com.shiftyjelly.pocketcasts.compose.components.EpisodeImage
 import au.com.shiftyjelly.pocketcasts.images.R
 import au.com.shiftyjelly.pocketcasts.localization.helper.TimeHelper
 import au.com.shiftyjelly.pocketcasts.models.entity.BaseEpisode
+import au.com.shiftyjelly.pocketcasts.models.entity.PodcastEpisode
+import au.com.shiftyjelly.pocketcasts.models.type.EpisodePlayingStatus
+import au.com.shiftyjelly.pocketcasts.models.type.EpisodeStatusEnum
 import au.com.shiftyjelly.pocketcasts.repositories.playback.UpNextQueue
 import au.com.shiftyjelly.pocketcasts.utils.extensions.toLocalizedFormatPattern
+import au.com.shiftyjelly.pocketcasts.wear.theme.WearAppTheme
 import au.com.shiftyjelly.pocketcasts.wear.theme.WearColors
+import java.util.Date
 import au.com.shiftyjelly.pocketcasts.images.R as IR
 import au.com.shiftyjelly.pocketcasts.localization.R as LR
 
@@ -53,6 +61,32 @@ fun EpisodeChip(
     showImage: Boolean = true,
     viewModel: EpisodeChipViewModel = hiltViewModel(),
 ) {
+    // Make sure the episode is always up-to-date
+    val observedEpisode by viewModel
+        .observeByUuid(episode)
+        .collectAsState()
+    val queueState by viewModel.upNextQueue.collectAsState(UpNextQueue.State.Empty)
+    Content(
+        episode = observedEpisode,
+        queueState = queueState,
+        useEpisodeArtwork = useEpisodeArtwork,
+        onClick = onClick,
+        modifier = modifier,
+        useUpNextIcon = useUpNextIcon,
+        showImage = showImage,
+    )
+}
+
+@Composable
+private fun Content(
+    episode: BaseEpisode,
+    queueState: UpNextQueue.State,
+    useEpisodeArtwork: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    useUpNextIcon: Boolean = true,
+    showImage: Boolean = true,
+) {
     Box(
         contentAlignment = Alignment.Center,
         modifier = modifier
@@ -63,13 +97,6 @@ fun EpisodeChip(
             .fillMaxWidth()
             .padding(vertical = 10.dp),
     ) {
-        // Make sure the episode is always up-to-date
-        @Suppress("NAME_SHADOWING")
-        val episode by viewModel
-            .observeByUuid(episode)
-            .collectAsState()
-
-        val queueState by viewModel.upNextQueue.collectAsState(UpNextQueue.State.Empty)
         val upNextQueue = (queueState as? UpNextQueue.State.Loaded)
             ?.queue
             ?: emptyList()
@@ -89,7 +116,8 @@ fun EpisodeChip(
                         useEpisodeArtwork = useEpisodeArtwork,
                         modifier = Modifier
                             .size(30.dp)
-                            .clip(RoundedCornerShape(4.dp)),
+                            .clip(RoundedCornerShape(4.dp))
+                            .alphaIfArchivedOrPlayed(episode),
                     )
                     IconsRow(
                         showUpNextIcon,
@@ -118,6 +146,7 @@ fun EpisodeChip(
                         ),
                     ),
                     maxLines = 2,
+                    modifier = Modifier.alphaIfArchivedOrPlayed(episode),
                 )
                 val shortDate = episode.publishedDate.toLocalizedFormatPattern("dd MMM")
                 val timeLeft = TimeHelper.getTimeLeft(
@@ -141,6 +170,7 @@ fun EpisodeChip(
                         text = "$shortDate • $timeLeft",
                         color = MaterialTheme.colors.onSecondary,
                         style = MaterialTheme.typography.caption2,
+                        modifier = Modifier.alphaIfArchivedOrPlayed(episode),
                     )
                 }
                 episode.playErrorDetails?.let {
@@ -193,6 +223,79 @@ private fun IconsRow(
                 contentDescription = stringResource(LR.string.downloaded),
                 tint = WearColors.downloadedIcon,
                 modifier = Modifier.size(12.dp),
+            )
+        }
+    }
+}
+
+private const val ALPHA_ARCHIVED_OR_PLAYED = 0.6f
+
+private fun Modifier.alphaIfArchivedOrPlayed(episode: BaseEpisode) = if (episode.isArchived || episode.isFinished) alpha(ALPHA_ARCHIVED_OR_PLAYED) else this
+
+@Preview
+@Composable
+private fun EpisodeChipPreview() {
+    val episode = PodcastEpisode(
+        uuid = "preview-episode",
+        publishedDate = Date(System.currentTimeMillis() - 86400000L), // 1 day ago
+        title = "Sample Episode",
+        podcastUuid = "podcast-uuid",
+        episodeStatus = EpisodeStatusEnum.DOWNLOADED,
+        playedUpTo = 300.0,
+        duration = 1800.0,
+    )
+    val episodeLongTitle = episode.copy(
+        title = "Sample Episode: With a title that is quite long and wrap across multiple lines",
+    )
+    val playedEpisode = episode.copy(
+        title = "Played Episode",
+        playingStatus = EpisodePlayingStatus.COMPLETED,
+        playedUpTo = 1700.0,
+    )
+    val archivedEpisode = episode.copy(
+        title = "Archived Episode",
+        isArchived = true,
+    )
+
+    WearAppTheme {
+        Column(
+            verticalArrangement = spacedBy(8.dp),
+            modifier = Modifier.background(Color.Black).padding(8.dp),
+        ) {
+            Content(
+                episode = episode,
+                queueState = UpNextQueue.State.Empty,
+                useEpisodeArtwork = true,
+                onClick = {},
+                showImage = true,
+            )
+            Content(
+                episode = episodeLongTitle,
+                queueState = UpNextQueue.State.Empty,
+                useEpisodeArtwork = true,
+                onClick = {},
+                showImage = true,
+            )
+            Content(
+                episode = episode,
+                queueState = UpNextQueue.State.Empty,
+                useEpisodeArtwork = true,
+                onClick = {},
+                showImage = false,
+            )
+            Content(
+                episode = playedEpisode,
+                queueState = UpNextQueue.State.Empty,
+                useEpisodeArtwork = true,
+                onClick = {},
+                showImage = true,
+            )
+            Content(
+                episode = archivedEpisode,
+                queueState = UpNextQueue.State.Empty,
+                useEpisodeArtwork = true,
+                onClick = {},
+                showImage = true,
             )
         }
     }
