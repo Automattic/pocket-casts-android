acb486e0f3c4e6bd95c16723713612ff395eae3c
Philip Simpson <philip.simpson@automattic.com>
2025-12-09 14:46:08 +1030

    Organise imports

--- wear/src/main/kotlin/au/com/shiftyjelly/pocketcasts/wear/ui/component/NoContentScreen.kt
+++ wear/src/main/kotlin/au/com/shiftyjelly/pocketcasts/wear/ui/component/NoContentScreen.kt
@@ -1,11 +1,8 @@
 package au.com.shiftyjelly.pocketcasts.wear.ui.component
 
 import androidx.annotation.StringRes
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
