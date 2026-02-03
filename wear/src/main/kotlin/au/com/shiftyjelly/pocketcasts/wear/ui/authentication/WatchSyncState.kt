3bfe99238484f400d7773543c3061a72d721ab28
Tamas Szelezsan <szelezsant@gmail.com>
2026-02-03 20:55:55 +0100

    Add WatchSyncState sealed interface for watch login states

--- wear/src/main/kotlin/au/com/shiftyjelly/pocketcasts/wear/ui/authentication/WatchSyncState.kt
+++ wear/src/main/kotlin/au/com/shiftyjelly/pocketcasts/wear/ui/authentication/WatchSyncState.kt
@@ -1,6 +1,7 @@
 package au.com.shiftyjelly.pocketcasts.wear.ui.authentication
 
 sealed interface WatchSyncState {
+    data object Idle : WatchSyncState
     data object Syncing : WatchSyncState
     data object Success : WatchSyncState
     data class Failed(val error: WatchSyncError) : WatchSyncState
