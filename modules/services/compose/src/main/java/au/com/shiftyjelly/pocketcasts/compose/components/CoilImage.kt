a59c61b527276ee474ce58e6a1f78e93fcacc13a
Philip Simpson <philip.simpson@automattic.com>
2026-02-11 15:06:41 +1030

    Revert Compose changes

--- modules/services/compose/src/main/java/au/com/shiftyjelly/pocketcasts/compose/components/CoilImage.kt
+++ modules/services/compose/src/main/java/au/com/shiftyjelly/pocketcasts/compose/components/CoilImage.kt
@@ -3,16 +3,17 @@ package au.com.shiftyjelly.pocketcasts.compose.components
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImagePainter
@@ -28,7 +29,6 @@ internal fun CoilImage(
     contentScale: ContentScale = ContentScale.Crop,
     corners: Dp? = null,
     expandContentSize: Boolean = false,
-    uuid: String = "", // UUID for fallback color selection
 ) {
     val painter = rememberAsyncImagePainter(
         model = imageRequest,
@@ -48,11 +48,11 @@ internal fun CoilImage(
         )
         val state by painter.state.collectAsState()
         if (showTitle && state is AsyncImagePainter.State.Error) {
-            PodcastImageFallback(
-                uuid = uuid,
-                title = title,
-                imageSize = 120.dp, // Default size, will be constrained by parent
-                modifier = Modifier.fillMaxSize(),
+            TextP60(
+                text = title,
+                textAlign = TextAlign.Center,
+                maxLines = 4,
+                modifier = Modifier.padding(2.dp),
             )
         }
     }
