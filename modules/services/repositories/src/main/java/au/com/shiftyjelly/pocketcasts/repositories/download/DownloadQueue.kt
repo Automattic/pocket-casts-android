d2a78c5c83cc7505090b43746089c0997311dc2a
Michał Sikora <me@mehow.io>
2026-02-19 11:35:31 +0100

    Prepare downloads for analytics (#5003)

--- modules/services/repositories/src/main/java/au/com/shiftyjelly/pocketcasts/repositories/download/DownloadQueue.kt
+++ modules/services/repositories/src/main/java/au/com/shiftyjelly/pocketcasts/repositories/download/DownloadQueue.kt
@@ -1,15 +1,37 @@
 package au.com.shiftyjelly.pocketcasts.repositories.download
 
+import au.com.shiftyjelly.pocketcasts.analytics.SourceView
+
 interface DownloadQueue {
-    fun enqueue(episodeUuid: String, downloadType: DownloadType) = enqueueAll(setOf(episodeUuid), downloadType)
+    fun enqueue(
+        episodeUuid: String,
+        downloadType: DownloadType,
+        sourceView: SourceView,
+    ) = enqueueAll(setOf(episodeUuid), downloadType, sourceView)
 
-    fun enqueueAll(episodeUuids: Collection<String>, downloadType: DownloadType)
+    fun enqueueAll(
+        episodeUuids: Collection<String>,
+        downloadType: DownloadType,
+        sourceView: SourceView,
+    )
 
-    fun cancel(episodeUuid: String, disableAutoDownload: Boolean) = cancelAll(setOf(episodeUuid), disableAutoDownload)
+    fun cancel(
+        episodeUuid: String,
+        disableAutoDownload: Boolean,
+        sourceView: SourceView,
+    ) = cancelAll(setOf(episodeUuid), disableAutoDownload, sourceView)
 
-    fun cancelAll(episodeUuids: Collection<String>, disableAutoDownload: Boolean)
+    fun cancelAll(
+        episodeUuids: Collection<String>,
+        disableAutoDownload: Boolean,
+        sourceView: SourceView,
+    )
 
-    fun cancelAll(podcastUuid: String, disableAutoDownload: Boolean)
+    fun cancelAll(
+        podcastUuid: String,
+        disableAutoDownload: Boolean,
+        sourceView: SourceView,
+    )
 }
 
 enum class DownloadType {
