94228ffa278b35c43896db271dfd9d2af2ad33c8
Tamas Szelezsan <33399753+sztomek@users.noreply.github.com>
2026-02-25 08:23:01 +0100

    [Installmetns] Add and wire up experiment (#5029)

--- modules/services/analytics/src/main/java/au/com/shiftyjelly/pocketcasts/analytics/experiments/Experiment.kt
+++ modules/services/analytics/src/main/java/au/com/shiftyjelly/pocketcasts/analytics/experiments/Experiment.kt
@@ -7,6 +7,7 @@ interface ExperimentType {
 enum class Experiment(override val identifier: String) : ExperimentType {
     PaywallUpgradeABTest("pocketcasts_paywall_upgrade_android_ab_test"),
     NewOnboardingABTest("pocketcasts_android_onboarding_promote_free_trial"),
+    YearlyInstallments("pocketcasts_android_yearly_installments"),
     ;
 
     companion object {
