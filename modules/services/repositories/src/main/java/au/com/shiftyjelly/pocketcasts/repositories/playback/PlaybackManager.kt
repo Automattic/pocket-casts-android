8a57f10d6ba54658bfd9df3b1e3f4a986d952fd3
Michał Sikora <me@mehow.io>
2026-03-15 10:33:48 +0100

    Merge release/8.7 into main (#5129)

--- modules/services/repositories/src/main/java/au/com/shiftyjelly/pocketcasts/repositories/playback/PlaybackManager.kt
+++ modules/services/repositories/src/main/java/au/com/shiftyjelly/pocketcasts/repositories/playback/PlaybackManager.kt
@@ -1332,9 +1332,6 @@ open class PlaybackManager @Inject constructor(
             // remove from Up Next
             upNextQueue.removeEpisode(episode, shouldShuffleUpNext = settings.upNextShuffle.value)
 
-            // stop the downloads
-            downloadQueue.cancel(episode.uuid, SourceView.PLAYER)
-
             // mark as played
             episodeManager.updatePlayingStatusBlocking(episode, EpisodePlayingStatus.COMPLETED)
 
