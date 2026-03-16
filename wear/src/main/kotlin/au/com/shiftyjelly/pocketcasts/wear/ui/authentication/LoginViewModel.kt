e424190de3605f2a972fa8e0606be02d63a86b63
Michał Sikora <me@mehow.io>
2026-03-16 18:13:44 +0100

    Migrate wear events to Event Horizon (#5109)

--- wear/src/main/kotlin/au/com/shiftyjelly/pocketcasts/wear/ui/authentication/LoginViewModel.kt
+++ wear/src/main/kotlin/au/com/shiftyjelly/pocketcasts/wear/ui/authentication/LoginViewModel.kt
@@ -6,9 +6,12 @@ import androidx.credentials.CredentialManager
 import androidx.credentials.GetCredentialRequest
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import au.com.shiftyjelly.pocketcasts.analytics.AnalyticsEvent
-import au.com.shiftyjelly.pocketcasts.analytics.AnalyticsTracker
 import au.com.shiftyjelly.pocketcasts.preferences.Settings
+import com.automattic.eventhorizon.EventHorizon
+import com.automattic.eventhorizon.WearSigninEmailTappedEvent
+import com.automattic.eventhorizon.WearSigninGoogleTappedEvent
+import com.automattic.eventhorizon.WearSigninPhoneTappedEvent
+import com.automattic.eventhorizon.WearSigninShownEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
@@ -16,18 +19,18 @@ import kotlinx.coroutines.launch
 
 @HiltViewModel
 class LoginViewModel @Inject constructor(
-    private val analyticsTracker: AnalyticsTracker,
+    private val eventHorizon: EventHorizon,
     private val credentialRequest: GetCredentialRequest,
     @ApplicationContext context: Context,
 ) : ViewModel() {
     private val credentialManager: CredentialManager by lazy { CredentialManager.create(context) }
 
     fun onShown() {
-        analyticsTracker.track(AnalyticsEvent.WEAR_SIGNIN_SHOWN)
+        eventHorizon.track(WearSigninShownEvent)
     }
 
     fun onGoogleLoginClicked(onFinish: () -> Unit) {
-        analyticsTracker.track(AnalyticsEvent.WEAR_SIGNIN_GOOGLE_TAPPED)
+        eventHorizon.track(WearSigninGoogleTappedEvent)
         viewModelScope.launch {
             val isGoogleSignInAvailable = Settings.GOOGLE_SIGN_IN_SERVER_CLIENT_ID.isNotEmpty()
 
@@ -43,10 +46,10 @@ class LoginViewModel @Inject constructor(
     }
 
     fun onPhoneLoginClicked() {
-        analyticsTracker.track(AnalyticsEvent.WEAR_SIGNIN_PHONE_TAPPED)
+        eventHorizon.track(WearSigninPhoneTappedEvent)
     }
 
     fun onEmailLoginClicked() {
-        analyticsTracker.track(AnalyticsEvent.WEAR_SIGNIN_EMAIL_TAPPED)
+        eventHorizon.track(WearSigninEmailTappedEvent)
     }
 }
