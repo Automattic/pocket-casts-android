e22fa24ae5bbcd2df83ed7e096f90699786b1ae1
Tamas Szelezsan <szelezsant@gmail.com>
2026-03-25 19:21:56 +0100

    Add foreground detection

--- modules/features/shared/src/main/java/au/com/shiftyjelly/pocketcasts/shared/di/SharedModule.kt
+++ modules/features/shared/src/main/java/au/com/shiftyjelly/pocketcasts/shared/di/SharedModule.kt
@@ -1,7 +1,9 @@
 package au.com.shiftyjelly.pocketcasts.shared.di
 
 import au.com.shiftyjelly.pocketcasts.crashlogging.ConnectionStatusProvider
+import au.com.shiftyjelly.pocketcasts.repositories.playback.AppLifecycleProvider
 import au.com.shiftyjelly.pocketcasts.repositories.playback.NetworkConnectionWatcher
+import au.com.shiftyjelly.pocketcasts.shared.AppLifecycleProviderImpl
 import au.com.shiftyjelly.pocketcasts.shared.NetworkConnectionStatusProvider
 import au.com.shiftyjelly.pocketcasts.shared.NetworkConnectionWatcherImpl
 import dagger.Binds
@@ -17,6 +19,10 @@ abstract class SharedModule {
     @Binds
     @Singleton
     abstract fun provideNetworkConnectionWatcher(networkWatcherImpl: NetworkConnectionWatcherImpl): NetworkConnectionWatcher
+
+    @Binds
+    @Singleton
+    abstract fun bindAppLifecycleProvider(impl: AppLifecycleProviderImpl): AppLifecycleProvider
 
     @Binds
     abstract fun bindConnectionStatusProvider(
