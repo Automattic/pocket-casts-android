65a147cb3bd320ba09e75e020cc5a045ef127bd3
Philip Simpson <philip.simpson@automattic.com>
2026-05-04 14:56:20 +0930

    Start adding Blogs experiment

--- modules/features/profile/src/main/java/au/com/shiftyjelly/pocketcasts/profile/blogs/BlogsFragment.kt
+++ modules/features/profile/src/main/java/au/com/shiftyjelly/pocketcasts/profile/blogs/BlogsFragment.kt
@@ -3,17 +3,21 @@ package au.com.shiftyjelly.pocketcasts.profile.blogs
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import au.com.shiftyjelly.pocketcasts.compose.AppTheme
+import au.com.shiftyjelly.pocketcasts.compose.bars.ThemedTopAppBar
 import au.com.shiftyjelly.pocketcasts.compose.extensions.contentWithoutConsumedInsets
 import au.com.shiftyjelly.pocketcasts.views.fragments.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
-
-private object BlogsRoutes {
-    const val EMPTY = "empty"
-}
+import au.com.shiftyjelly.pocketcasts.localization.R as LR
 
 @AndroidEntryPoint
 class BlogsFragment : BaseFragment() {
@@ -24,14 +28,17 @@ class BlogsFragment : BaseFragment() {
         savedInstanceState: Bundle?,
     ) = contentWithoutConsumedInsets {
         AppTheme(themeType = theme.activeTheme) {
-            val navController = rememberNavController()
-            NavHost(navController = navController, startDestination = BlogsRoutes.EMPTY) {
-                composable(BlogsRoutes.EMPTY) {
-                    EmptyBlogsPage(
-                        onBackPress = { activity?.onBackPressedDispatcher?.onBackPressed() },
-                        onAddBlogClick = { },
-                    )
-                }
+            Column {
+                ThemedTopAppBar(
+                    title = stringResource(LR.string.profile_navigation_blogs),
+                    onNavigationClick = { activity?.onBackPressedDispatcher?.onBackPressed() },
+                )
+                Spacer(Modifier.height(12.dp))
+                Text(
+                    text = "Coming soon",
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center,
+                )
             }
         }
     }
