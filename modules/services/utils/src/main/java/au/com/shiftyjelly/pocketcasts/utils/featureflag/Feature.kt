21a71acaf362e40f40de0aecd77310793d0a9f9f
Tamas Szelezsan <szelezsant@gmail.com>
2026-05-08 23:29:15 +0200

    Add SYNCED_TRANSCRIPTS feature flag

--- modules/services/utils/src/main/java/au/com/shiftyjelly/pocketcasts/utils/featureflag/Feature.kt
+++ modules/services/utils/src/main/java/au/com/shiftyjelly/pocketcasts/utils/featureflag/Feature.kt
@@ -288,6 +288,14 @@ enum class Feature(
         hasFirebaseRemoteFlag = true,
         hasDevToggle = true,
     ),
+    SYNCED_TRANSCRIPTS(
+        key = "synced_transcripts",
+        title = "Synced transcripts with playback timing",
+        defaultValue = isDebugOrPrototypeBuild,
+        tier = FeatureTier.Free,
+        hasFirebaseRemoteFlag = true,
+        hasDevToggle = true,
+    ),
 }
 
 sealed class FeatureTier {
