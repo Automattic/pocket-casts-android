18508a0fa59f3b6b3bee668c625c6e87f8be1bf7
Philip Simpson <philz.the.gecko@gmail.com>
2026-05-18 21:12:29 +0930

    Fix web feed creation polling and improve subscribed blog sorting (#5306)

--- modules/services/model/src/main/java/au/com/shiftyjelly/pocketcasts/models/db/dao/PodcastDao.kt
+++ modules/services/model/src/main/java/au/com/shiftyjelly/pocketcasts/models/db/dao/PodcastDao.kt
@@ -71,7 +71,7 @@ abstract class PodcastDao {
     abstract suspend fun findSubscribedNoOrder(): List<Podcast>
 
     @Transaction
-    @Query("SELECT * FROM podcasts WHERE subscribed = 1 AND web_feed = 1 ORDER BY CASE WHEN latest_episode_date IS NULL THEN 1 ELSE 0 END, latest_episode_date DESC, LOWER(title) ASC")
+    @Query("SELECT * FROM podcasts WHERE subscribed = 1 AND web_feed = 1 ORDER BY CASE WHEN latest_episode_date IS NULL THEN 0 ELSE 1 END, latest_episode_date DESC, LOWER(title) ASC")
     abstract fun observeSubscribedWebFeedPodcasts(): Flow<List<Podcast>>
 
     @Transaction
