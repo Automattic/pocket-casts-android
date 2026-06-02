ae214335efb4c97fb7030ccb7be918f3cbe6f5d4
Tamas Szelezsan <szelezsant@gmail.com>
2026-06-02 17:57:47 +0200

    Add TvScaffoldViewModel and TvTabPlaceholder for tab content

---
+++ tv/src/main/java/au/com/shiftyjelly/pocketcasts/TvTabPlaceholder.kt
@@ -0,0 +1,40 @@
+package au.com.shiftyjelly.pocketcasts
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.tv.material3.MaterialTheme
+import androidx.tv.material3.Text
+
+@Composable
+fun TvTabPlaceholder(
+    tab: TvTab,
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.surface),
+        contentAlignment = Alignment.Center,
+    ) {
+        Text(
+            text = stringResource(tab.labelRes),
+            style = MaterialTheme.typography.displayMedium,
+            color = MaterialTheme.colorScheme.onSurface,
+        )
+    }
+}
+
+@Preview(device = Devices.TV_1080p)
+@Composable
+private fun TvTabPlaceholderPreview() {
+    MaterialTheme {
+        TvTabPlaceholder(tab = TvTab.Podcasts)
+    }
+}
