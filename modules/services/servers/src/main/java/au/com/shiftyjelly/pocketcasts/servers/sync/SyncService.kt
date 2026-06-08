058dfe7804a46c2b9788859ae4f1ba8728670fe7
Tamas Szelezsan <szelezsant@gmail.com>
2026-06-08 16:22:12 +0200

    Add device authorization API models and endpoints

--- modules/services/servers/src/main/java/au/com/shiftyjelly/pocketcasts/servers/sync/SyncService.kt
+++ modules/services/servers/src/main/java/au/com/shiftyjelly/pocketcasts/servers/sync/SyncService.kt
@@ -6,6 +6,9 @@ import au.com.shiftyjelly.pocketcasts.servers.sync.forgotpassword.ForgotPassword
 import au.com.shiftyjelly.pocketcasts.servers.sync.forgotpassword.ForgotPasswordResponse
 import au.com.shiftyjelly.pocketcasts.servers.sync.history.HistoryYearResponse
 import au.com.shiftyjelly.pocketcasts.servers.sync.history.HistoryYearSyncRequest
+import au.com.shiftyjelly.pocketcasts.servers.sync.login.DeviceAuthorizeRequest
+import au.com.shiftyjelly.pocketcasts.servers.sync.login.DeviceAuthorizeResponse
+import au.com.shiftyjelly.pocketcasts.servers.sync.login.DeviceTokenRequest
 import au.com.shiftyjelly.pocketcasts.servers.sync.login.ExchangeSonosResponse
 import au.com.shiftyjelly.pocketcasts.servers.sync.login.LoginGoogleRequest
 import au.com.shiftyjelly.pocketcasts.servers.sync.login.LoginPocketCastsRequest
@@ -62,6 +65,12 @@ interface SyncService {
 
     @POST("/user/token")
     suspend fun loginToken(@Body request: LoginTokenRequest): LoginTokenResponse
+
+    @POST("/device/authorize")
+    suspend fun deviceAuthorize(@Body request: DeviceAuthorizeRequest): DeviceAuthorizeResponse
+
+    @POST("/user/token")
+    suspend fun deviceToken(@Body request: DeviceTokenRequest): LoginTokenResponse
 
     @POST("/user/register_pocket_casts")
     suspend fun register(@Body request: RegisterRequest): LoginTokenResponse
