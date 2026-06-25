c43a3d7ba4568d8c3ea5bebe9c0a9e8e97d9a0ee
Tamas Szelezsan <szelezsant@gmail.com>
2026-06-25 17:04:12 +0200

    Route server HLS detection through BaseEpisode.isHlsMimeType

--- modules/services/servers/src/main/java/au/com/shiftyjelly/pocketcasts/servers/AlternateEnclosures.kt
+++ modules/services/servers/src/main/java/au/com/shiftyjelly/pocketcasts/servers/AlternateEnclosures.kt
@@ -1,16 +1,13 @@
 package au.com.shiftyjelly.pocketcasts.servers
 
-import androidx.media3.common.MimeTypes
+import au.com.shiftyjelly.pocketcasts.models.entity.BaseEpisode
 import au.com.shiftyjelly.pocketcasts.models.entity.EpisodeAlternateEnclosure
-
-// HLS is advertised as either of these MIME types (matches BaseEpisode.isHlsMimeType).
-private val HLS_MIME_TYPES = setOf(MimeTypes.APPLICATION_M3U8.lowercase(), "application/vnd.apple.mpegurl")
 
 /** First HLS enclosure's first http(s) source URI, or null if none can be streamed. */
 internal fun List<EpisodeAlternateEnclosure>?.firstHlsStreamUrl(): String? {
     val enclosures = this ?: return null
     return enclosures
-        .firstOrNull { it.type?.lowercase() in HLS_MIME_TYPES }
+        .firstOrNull { BaseEpisode.isHlsMimeType(it.type) }
         ?.sources
         ?.firstOrNull { it.uri.isPlayableHttpUri() }
         ?.uri
