688d8000b5207c6fc1e0324471ea6be86ff5f6c1
Tamas Szelezsan <szelezsant@gmail.com>
2026-07-27 12:46:20 +0200

    Remove the recommendations feature flag

--- modules/services/repositories/src/main/java/au/com/shiftyjelly/pocketcasts/repositories/lists/ListRepository.kt
+++ modules/services/repositories/src/main/java/au/com/shiftyjelly/pocketcasts/repositories/lists/ListRepository.kt
@@ -6,8 +6,6 @@ import au.com.shiftyjelly.pocketcasts.servers.model.Discover
 import au.com.shiftyjelly.pocketcasts.servers.model.DiscoverCategory
 import au.com.shiftyjelly.pocketcasts.servers.model.ListFeed
 import au.com.shiftyjelly.pocketcasts.servers.server.ListWebService
-import au.com.shiftyjelly.pocketcasts.utils.featureflag.Feature
-import au.com.shiftyjelly.pocketcasts.utils.featureflag.FeatureFlag
 import timber.log.Timber
 
 class ListRepository(
@@ -17,8 +15,7 @@ class ListRepository(
 ) {
 
     suspend fun getDiscoverFeed(): Discover {
-        val version = if (platform == PLATFORM_TV || FeatureFlag.isEnabled(Feature.RECOMMENDATIONS)) 3 else 2
-        return listWebService.getDiscoverFeed(platform = platform, version = version)
+        return listWebService.getDiscoverFeed(platform = platform, version = 3)
     }
 
     suspend fun getListFeed(url: String, authenticated: Boolean? = false): ListFeed? {
