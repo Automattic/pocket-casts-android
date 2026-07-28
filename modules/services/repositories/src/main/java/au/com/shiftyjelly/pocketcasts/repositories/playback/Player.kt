2b663f4fbc06e6fb5e55f30b0df3bd21fb1463f0
Tamas Szelezsan <33399753+sztomek@users.noreply.github.com>
2026-07-28 10:36:13 +0200

    Cherry-pick HLS video toggle (#5620) and progressive audio-only (#5627) to 8.17 (#5659)

--- modules/services/repositories/src/main/java/au/com/shiftyjelly/pocketcasts/repositories/playback/Player.kt
+++ modules/services/repositories/src/main/java/au/com/shiftyjelly/pocketcasts/repositories/playback/Player.kt
@@ -20,7 +20,7 @@ sealed interface EpisodeLocation {
     ) : EpisodeLocation
 
     companion object {
-        fun create(episode: BaseEpisode) = if (episode.isDownloaded) {
+        fun create(episode: BaseEpisode, preferStream: Boolean = false) = if (episode.isDownloaded && !(preferStream && episode.isStreamUrlHls)) {
             EpisodeLocation.Downloaded(episode, episode.downloadedFilePath)
         } else {
             EpisodeLocation.Stream(episode, episode.streamUrl, episode.isStreamUrlHls)
@@ -40,6 +40,15 @@ enum class StreamVideoState {
     Unknown,
     HasVideo,
     AudioOnly,
+    ;
+
+    companion object {
+        fun initialFor(episode: BaseEpisode, audioOnly: Boolean, playingHlsStream: Boolean) = when {
+            audioOnly && (episode.isVideo || playingHlsStream) -> AudioOnly
+            playingHlsStream -> Unknown
+            else -> NotVideo
+        }
+    }
 }
 
 interface Player {
@@ -71,5 +80,5 @@ interface Player {
     fun supportsVideo(): Boolean
     fun setVolume(volume: Float)
     fun setPodcast(podcast: Podcast?)
-    fun setEpisode(episode: BaseEpisode)
+    fun setEpisode(episode: BaseEpisode, preferStream: Boolean = false)
 }
