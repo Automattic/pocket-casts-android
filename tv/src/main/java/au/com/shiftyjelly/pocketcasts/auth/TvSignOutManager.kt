c4391238c6a5f5e5be61ef5d6656b882d3d943f0
Tamas Szelezsan <szelezsant@gmail.com>
2026-07-28 16:25:28 +0200

    Add a TV sign out manager that wipes local data

---
+++ tv/src/main/java/au/com/shiftyjelly/pocketcasts/auth/TvSignOutManager.kt
@@ -0,0 +1,60 @@
+package au.com.shiftyjelly.pocketcasts.auth
+
+import au.com.shiftyjelly.pocketcasts.coroutines.di.ApplicationScope
+import au.com.shiftyjelly.pocketcasts.preferences.Settings
+import au.com.shiftyjelly.pocketcasts.repositories.di.IoDispatcher
+import au.com.shiftyjelly.pocketcasts.repositories.file.FileStorage
+import au.com.shiftyjelly.pocketcasts.repositories.playback.PlaybackManager
+import au.com.shiftyjelly.pocketcasts.repositories.playback.UpNextQueue
+import au.com.shiftyjelly.pocketcasts.repositories.podcast.EpisodeManager
+import au.com.shiftyjelly.pocketcasts.repositories.podcast.FolderManager
+import au.com.shiftyjelly.pocketcasts.repositories.searchhistory.SearchHistoryManager
+import au.com.shiftyjelly.pocketcasts.repositories.user.UserManager
+import au.com.shiftyjelly.pocketcasts.ui.images.CoilManager
+import au.com.shiftyjelly.pocketcasts.utils.FileUtil
+import dagger.Lazy
+import javax.inject.Inject
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+class TvSignOutManager @Inject constructor(
+    private val userManager: UserManager,
+    private val playbackManager: Lazy<PlaybackManager>,
+    private val upNextQueue: Lazy<UpNextQueue>,
+    private val folderManager: Lazy<FolderManager>,
+    private val searchHistoryManager: Lazy<SearchHistoryManager>,
+    private val episodeManager: Lazy<EpisodeManager>,
+    private val fileStorage: FileStorage,
+    private val coilManager: CoilManager,
+    private val settings: Settings,
+    @ApplicationScope private val applicationScope: CoroutineScope,
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher,
+) {
+    fun signOutAndWipeData() {
+        applicationScope.launch(ioDispatcher) {
+            userManager.signOutAndClearData(
+                playbackManager = playbackManager.get(),
+                upNextQueue = upNextQueue.get(),
+                folderManager = folderManager.get(),
+                searchHistoryManager = searchHistoryManager.get(),
+                episodeManager = episodeManager.get(),
+                wasInitiatedByUser = true,
+            )
+            deleteDownloadedFiles()
+            coilManager.clearAll()
+            settings.clearUserPreferences()
+            settings.setFullySignedOut(true)
+        }
+    }
+
+    private fun deleteDownloadedFiles() {
+        val dirs = listOfNotNull(
+            fileStorage.getOrCreateEpisodesDir(),
+            fileStorage.getOrCreateCloudDir(),
+            fileStorage.getOrCreateNetworkImagesDir(),
+            fileStorage.getOrCreateEpisodesTempDir(),
+        )
+        dirs.forEach { dir -> FileUtil.deleteDirContents(dir.absolutePath) }
+    }
+}
---
+++ tv/src/test/java/au/com/shiftyjelly/pocketcasts/auth/TvSignOutManagerTest.kt
@@ -0,0 +1,113 @@
+package au.com.shiftyjelly.pocketcasts.auth
+
+import au.com.shiftyjelly.pocketcasts.preferences.Settings
+import au.com.shiftyjelly.pocketcasts.repositories.file.FileStorage
+import au.com.shiftyjelly.pocketcasts.repositories.playback.PlaybackManager
+import au.com.shiftyjelly.pocketcasts.repositories.playback.UpNextQueue
+import au.com.shiftyjelly.pocketcasts.repositories.podcast.EpisodeManager
+import au.com.shiftyjelly.pocketcasts.repositories.podcast.FolderManager
+import au.com.shiftyjelly.pocketcasts.repositories.searchhistory.SearchHistoryManager
+import au.com.shiftyjelly.pocketcasts.repositories.user.UserManager
+import au.com.shiftyjelly.pocketcasts.sharedtest.MainCoroutineRule
+import au.com.shiftyjelly.pocketcasts.ui.images.CoilManager
+import java.io.File
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.eq
+import org.mockito.kotlin.inOrder
+import org.mockito.kotlin.mock
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class TvSignOutManagerTest {
+
+    @get:Rule
+    val coroutineRule = MainCoroutineRule()
+
+    @get:Rule
+    val temporaryFolder = TemporaryFolder()
+
+    private val userManager = mock<UserManager>()
+    private val playbackManager = mock<PlaybackManager>()
+    private val upNextQueue = mock<UpNextQueue>()
+    private val folderManager = mock<FolderManager>()
+    private val searchHistoryManager = mock<SearchHistoryManager>()
+    private val episodeManager = mock<EpisodeManager>()
+    private val coilManager = mock<CoilManager>()
+    private val settings = mock<Settings>()
+
+    @Test
+    fun `sign out clears the account data and caches in order`() = runTest {
+        val manager = createManager(fileStorage = mock<FileStorage>())
+
+        manager.signOutAndWipeData()
+        advanceUntilIdle()
+
+        inOrder(userManager, coilManager, settings) {
+            verify(userManager).signOutAndClearData(
+                playbackManager = eq(playbackManager),
+                upNextQueue = eq(upNextQueue),
+                folderManager = eq(folderManager),
+                searchHistoryManager = eq(searchHistoryManager),
+                episodeManager = eq(episodeManager),
+                wasInitiatedByUser = eq(true),
+            )
+            verify(coilManager).clearAll()
+            verify(settings).clearUserPreferences()
+            verify(settings).setFullySignedOut(true)
+        }
+    }
+
+    @Test
+    fun `sign out deletes the downloaded files`() = runTest {
+        val episodesDir = temporaryFolder.newFolder("episodes")
+        val cloudDir = temporaryFolder.newFolder("cloud_files")
+        val downloadedEpisode = File(episodesDir, "episode.mp3").apply { writeText("audio") }
+        val cloudFile = File(cloudDir, "file.mp3").apply { writeText("audio") }
+        val fileStorage = mock<FileStorage> {
+            on { getOrCreateEpisodesDir() } doReturn episodesDir
+            on { getOrCreateCloudDir() } doReturn cloudDir
+        }
+        val manager = createManager(fileStorage = fileStorage)
+
+        manager.signOutAndWipeData()
+        advanceUntilIdle()
+
+        assertFalse(downloadedEpisode.exists())
+        assertFalse(cloudFile.exists())
+        assertTrue(episodesDir.exists())
+    }
+
+    @Test
+    fun `sign out ignores unavailable storage directories`() = runTest {
+        val manager = createManager(fileStorage = mock<FileStorage>())
+
+        manager.signOutAndWipeData()
+        advanceUntilIdle()
+
+        inOrder(settings) {
+            verify(settings).clearUserPreferences()
+            verify(settings).setFullySignedOut(true)
+        }
+    }
+
+    private fun kotlinx.coroutines.test.TestScope.createManager(fileStorage: FileStorage) = TvSignOutManager(
+        userManager = userManager,
+        playbackManager = { playbackManager },
+        upNextQueue = { upNextQueue },
+        folderManager = { folderManager },
+        searchHistoryManager = { searchHistoryManager },
+        episodeManager = { episodeManager },
+        fileStorage = fileStorage,
+        coilManager = coilManager,
+        settings = settings,
+        applicationScope = this,
+        ioDispatcher = coroutineRule.testDispatcher,
+    )
+}
