0b16674819b3ed9bacab09f7ad10e7c405c26982
Tamas Szelezsan <szelezsant@gmail.com>
2026-07-31 16:50:37 +0200

    Host TV toast overlay above the navigation graph

--- tv/src/main/java/au/com/shiftyjelly/pocketcasts/onboarding/TvOnboardingNavHost.kt
+++ tv/src/main/java/au/com/shiftyjelly/pocketcasts/onboarding/TvOnboardingNavHost.kt
@@ -1,11 +1,22 @@
 package au.com.shiftyjelly.pocketcasts.onboarding
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.tv.material3.MaterialTheme
+import au.com.shiftyjelly.pocketcasts.component.LocalTvToastHostState
+import au.com.shiftyjelly.pocketcasts.component.TvToastHost
+import au.com.shiftyjelly.pocketcasts.component.TvToastHostState
 import au.com.shiftyjelly.pocketcasts.compose.AppTheme
 import au.com.shiftyjelly.pocketcasts.home.TvScaffold
 import au.com.shiftyjelly.pocketcasts.onboarding.createaccount.TvCreateAccountScreen
@@ -21,10 +32,13 @@ fun TvOnboardingNavHost(
     AppTheme(themeType = Theme.ThemeType.EXTRA_DARK) {
         MaterialTheme {
             val navController = rememberNavController()
-            NavHost(
-                navController = navController,
-                startDestination = viewModel.startDestination,
-            ) {
+            val toastHostState = remember { TvToastHostState() }
+            CompositionLocalProvider(LocalTvToastHostState provides toastHostState) {
+                Box(modifier = Modifier.fillMaxSize()) {
+                    NavHost(
+                        navController = navController,
+                        startDestination = viewModel.startDestination,
+                    ) {
                 composable(TvOnboardingRoutes.LANDING) {
                     TvWelcomeScreen(
                         onSignIn = { navController.navigate(TvOnboardingRoutes.SIGN_IN) },
@@ -65,6 +79,14 @@ fun TvOnboardingNavHost(
                     TvScaffold(
                         onLogIn = { navController.navigate(TvOnboardingRoutes.SIGN_IN) },
                         onCreateAccount = { navController.navigate(TvOnboardingRoutes.CREATE_ACCOUNT) },
+                    )
+                }
+                    }
+                    TvToastHost(
+                        state = toastHostState,
+                        modifier = Modifier
+                            .align(Alignment.TopEnd)
+                            .padding(top = 48.dp, end = 48.dp),
                     )
                 }
             }
