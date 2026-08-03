4eeeaf7a71572a36bbf21079ce8420951798c4b3
Tamas Szelezsan <33399753+sztomek@users.noreply.github.com>
2026-08-03 13:40:41 +0200

    [TV] Show playlists on the Playlists tab (#5657)

--- tv/src/main/java/au/com/shiftyjelly/pocketcasts/theme/TvColors.kt
+++ tv/src/main/java/au/com/shiftyjelly/pocketcasts/theme/TvColors.kt
@@ -6,9 +6,12 @@ object TvColors {
     val Dark = Color(0xFF161718)
     val DarkGray = Color(0xFF292B2E)
     val Gray = Color(0xFF3C3E42)
+    val LightGray = Color(0xFFD4D6DB)
     val TextPrimary = Color(0xFFFBFBFC)
     val TextPrimary20 = Color(0x33FBFBFC)
     val TextSecondary = Color(0xFFB0B3B8)
+    val TextSecondaryActive = Color(0xFF3D4044)
     val BgActive = Color(0xFFD4D6DB)
+    val BgActive20 = Color(0x33FBFBFC)
     val Divider = Color(0xFF4A4D51)
 }
