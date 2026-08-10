b6307ce0dda0dc343bfa68af43db1304c2d69764
Tamas Szelezsan <szelezsant@gmail.com>
2026-08-10 14:32:06 +0200

    Test TV playlist details play-all

--- tv/src/test/java/au/com/shiftyjelly/pocketcasts/playlists/details/TvPlaylistDetailsViewModelTest.kt
+++ tv/src/test/java/au/com/shiftyjelly/pocketcasts/playlists/details/TvPlaylistDetailsViewModelTest.kt
@@ -5,6 +5,8 @@ import au.com.shiftyjelly.pocketcasts.models.entity.ManualPlaylistEpisode
 import au.com.shiftyjelly.pocketcasts.models.entity.PodcastEpisode
 import au.com.shiftyjelly.pocketcasts.models.to.PlaylistEpisode
 import au.com.shiftyjelly.pocketcasts.preferences.TvPreferences
+import au.com.shiftyjelly.pocketcasts.repositories.playback.PlayAllHandler
+import au.com.shiftyjelly.pocketcasts.repositories.playback.PlayAllResponse
 import au.com.shiftyjelly.pocketcasts.repositories.playlist.ManualPlaylist
 import au.com.shiftyjelly.pocketcasts.repositories.playlist.Playlist
 import au.com.shiftyjelly.pocketcasts.repositories.playlist.PlaylistManager
@@ -20,8 +22,11 @@ import org.mockito.kotlin.any
 import org.mockito.kotlin.anyOrNull
 import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.eq
+import org.mockito.kotlin.inOrder
 import org.mockito.kotlin.mock
+import org.mockito.kotlin.never
 import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class TvPlaylistDetailsViewModelTest {
@@ -34,6 +39,11 @@ class TvPlaylistDetailsViewModelTest {
         on { manualPlaylistFlow(any(), anyOrNull(), any()) } doReturn playlists
     }
     private val preferences = mock<TvPreferences>()
+
+    private val playAllHandler = mock<PlayAllHandler>()
+    private val playAllHandlerFactory = mock<PlayAllHandler.Factory> {
+        on { create(any()) } doReturn playAllHandler
+    }
 
     private val availableEpisode = episode(uuid = "episode-1", isArchived = false)
     private val archivedEpisode = episode(uuid = "episode-2", isArchived = true)
@@ -130,11 +140,74 @@ class TvPlaylistDetailsViewModelTest {
         }
     }
 
+    @Test
+    fun `play all opens now playing when the queue does not need replacing`() = runTest {
+        whenever(playAllHandler.handlePlayAllEpisodes(any())) doReturn PlayAllResponse.DoNothing
+        val viewModel = createViewModel()
+
+        viewModel.uiState.test {
+            assertEquals(TvPlaylistDetailsUiState.Loading, awaitItem())
+            playlists.emit(playlist(availableEpisode))
+            assertEquals(listOf(availableEpisode), (awaitItem() as TvPlaylistDetailsUiState.Loaded).episodes)
+
+            viewModel.events.test {
+                viewModel.playAll()
+                assertEquals(TvPlaylistDetailsEvent.OpenNowPlaying, awaitItem())
+            }
+        }
+    }
+
+    @Test
+    fun `play all asks for confirmation when the queue would be replaced`() = runTest {
+        whenever(playAllHandler.handlePlayAllEpisodes(any())) doReturn PlayAllResponse.ShowWarning
+        val viewModel = createViewModel()
+
+        viewModel.uiState.test {
+            assertEquals(TvPlaylistDetailsUiState.Loading, awaitItem())
+            playlists.emit(playlist(availableEpisode))
+            assertEquals(listOf(availableEpisode), (awaitItem() as TvPlaylistDetailsUiState.Loaded).episodes)
+
+            viewModel.events.test {
+                viewModel.playAll()
+                assertEquals(TvPlaylistDetailsEvent.ShowReplaceUpNextConfirmation, awaitItem())
+            }
+        }
+    }
+
+    @Test
+    fun `replacing without saving plays the pending episodes and opens now playing`() = runTest {
+        val viewModel = createViewModel()
+
+        viewModel.events.test {
+            viewModel.replaceUpNextAndPlay(saveUpNext = false, upNextName = "Up Next")
+            assertEquals(TvPlaylistDetailsEvent.OpenNowPlaying, awaitItem())
+        }
+
+        verify(playAllHandler, never()).saveUpNextAsPlaylist(any())
+        verify(playAllHandler).playAllPendingEpisodes()
+    }
+
+    @Test
+    fun `replacing with saving saves the queue before playing`() = runTest {
+        val viewModel = createViewModel()
+
+        viewModel.events.test {
+            viewModel.replaceUpNextAndPlay(saveUpNext = true, upNextName = "Up Next")
+            assertEquals(TvPlaylistDetailsEvent.OpenNowPlaying, awaitItem())
+        }
+
+        inOrder(playAllHandler) {
+            verify(playAllHandler).saveUpNextAsPlaylist("Up Next")
+            verify(playAllHandler).playAllPendingEpisodes()
+        }
+    }
+
     private fun createViewModel(prefs: TvPreferences = preferences) = TvPlaylistDetailsViewModel(
         playlistUuid = "playlist-uuid",
         playlistType = Playlist.Type.Manual,
         playlistManager = playlistManager,
         preferences = prefs,
+        playAllHandlerFactory = playAllHandlerFactory,
     )
 
     private fun playlist(vararg episodes: PodcastEpisode) = ManualPlaylist(
