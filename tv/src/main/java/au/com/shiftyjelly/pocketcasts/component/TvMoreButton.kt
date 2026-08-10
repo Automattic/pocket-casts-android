78566ccadd40995d286ca5fcff3587b6c3b9a49a
Tamas Szelezsan <szelezsant@gmail.com>
2026-08-10 15:37:26 +0200

    Allow TvMoreButton size to be customised by callers

--- tv/src/main/java/au/com/shiftyjelly/pocketcasts/component/TvMoreButton.kt
+++ tv/src/main/java/au/com/shiftyjelly/pocketcasts/component/TvMoreButton.kt
@@ -6,6 +6,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.tv.material3.Icon
 import androidx.tv.material3.IconButton
@@ -18,16 +19,18 @@ import au.com.shiftyjelly.pocketcasts.localization.R as LR
 fun TvMoreButton(
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
+    buttonSize: Dp = 48.dp,
+    iconSize: Dp = 20.dp,
 ) {
     IconButton(
         onClick = onClick,
         colors = TvButtonDefaults.iconButtonColors(),
-        modifier = Modifier.size(48.dp).then(modifier),
+        modifier = Modifier.size(buttonSize).then(modifier),
     ) {
         Icon(
             painter = painterResource(IR.drawable.ic_ellipsis_horizontal),
             contentDescription = stringResource(LR.string.more_options),
-            modifier = Modifier.size(20.dp),
+            modifier = Modifier.size(iconSize),
         )
     }
 }
