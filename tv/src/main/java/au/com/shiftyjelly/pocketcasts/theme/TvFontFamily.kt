0059c763529c1f955d7a2c519f64c9222b462c53
Tamas Szelezsan <szelezsant@gmail.com>
2026-08-11 15:20:14 +0200

    Apply Google Sans to the TV type ramp

---
+++ tv/src/main/java/au/com/shiftyjelly/pocketcasts/theme/TvFontFamily.kt
@@ -0,0 +1,21 @@
+package au.com.shiftyjelly.pocketcasts.theme
+
+import androidx.compose.ui.text.ExperimentalTextApi
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontVariation
+import androidx.compose.ui.text.font.FontWeight
+import au.com.shiftyjelly.pocketcasts.R
+
+@OptIn(ExperimentalTextApi::class)
+private fun googleSans(weight: Int) = Font(
+    resId = R.font.google_sans,
+    weight = FontWeight(weight),
+    variationSettings = FontVariation.Settings(FontVariation.weight(weight)),
+)
+
+val GoogleSansFontFamily = FontFamily(
+    googleSans(400),
+    googleSans(510),
+    googleSans(700),
+)
