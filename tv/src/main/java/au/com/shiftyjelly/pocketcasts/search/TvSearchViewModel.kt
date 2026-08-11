2ed9432cee0cc531f1a57502211884d75a3161cb
Tamas Szelezsan <szelezsant@gmail.com>
2026-08-11 18:10:32 +0200

    Load the discover feed in the TV search view model

--- tv/src/main/java/au/com/shiftyjelly/pocketcasts/search/TvSearchViewModel.kt
+++ tv/src/main/java/au/com/shiftyjelly/pocketcasts/search/TvSearchViewModel.kt
@@ -2,8 +2,11 @@ package au.com.shiftyjelly.pocketcasts.search
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import au.com.shiftyjelly.pocketcasts.discover.TvDiscoverFeedLoader
+import au.com.shiftyjelly.pocketcasts.discover.TvDiscoverRow
 import au.com.shiftyjelly.pocketcasts.preferences.Settings
 import au.com.shiftyjelly.pocketcasts.repositories.lists.ListRepository
+import au.com.shiftyjelly.pocketcasts.repositories.sync.SyncManager
 import au.com.shiftyjelly.pocketcasts.servers.model.DiscoverCategory
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -17,10 +20,15 @@ import timber.log.Timber
 @HiltViewModel
 class TvSearchViewModel @Inject constructor(
     private val listRepository: ListRepository,
+    private val discoverFeedLoader: TvDiscoverFeedLoader,
+    private val syncManager: SyncManager,
 ) : ViewModel() {
 
     private val _categories = MutableStateFlow<List<DiscoverCategory>>(emptyList())
     val categories: StateFlow<List<DiscoverCategory>> = _categories.asStateFlow()
+
+    private val _discoverRows = MutableStateFlow<List<TvDiscoverRow>>(emptyList())
+    val discoverRows: StateFlow<List<TvDiscoverRow>> = _discoverRows.asStateFlow()
 
     init {
         viewModelScope.launch {
@@ -30,6 +38,16 @@ class TvSearchViewModel @Inject constructor(
                 throw exception
             } catch (exception: Exception) {
                 Timber.e(exception, "Failed to load TV browse categories")
+                emptyList()
+            }
+        }
+        viewModelScope.launch {
+            _discoverRows.value = try {
+                discoverFeedLoader.load(syncManager.isLoggedIn())
+            } catch (exception: CancellationException) {
+                throw exception
+            } catch (exception: Exception) {
+                Timber.e(exception, "Failed to load TV search discover feed")
                 emptyList()
             }
         }
