e51cf4f42200024069458bbb15c60289a256fc0c
Tamas Szelezsan <szelezsant@gmail.com>
2026-08-11 18:07:37 +0200

    Extract shared TV discover row rendering

--- tv/src/main/java/au/com/shiftyjelly/pocketcasts/home/TvHomeScreen.kt
+++ tv/src/main/java/au/com/shiftyjelly/pocketcasts/home/TvHomeScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -34,16 +33,12 @@ import androidx.tv.material3.Text
 import au.com.shiftyjelly.pocketcasts.component.LocalOpenNowPlaying
 import au.com.shiftyjelly.pocketcasts.component.LocalTvToastHostState
 import au.com.shiftyjelly.pocketcasts.component.TvDetailOverlay
-import au.com.shiftyjelly.pocketcasts.component.TvFeaturedTile
-import au.com.shiftyjelly.pocketcasts.component.TvPodcastTile
-import au.com.shiftyjelly.pocketcasts.component.TvPodcastTileDefaults
-import au.com.shiftyjelly.pocketcasts.component.TvRow
-import au.com.shiftyjelly.pocketcasts.component.TvVideoTile
 import au.com.shiftyjelly.pocketcasts.component.tvFocusInactiveWhen
 import au.com.shiftyjelly.pocketcasts.compose.loading.LoadingView
 import au.com.shiftyjelly.pocketcasts.discover.TvDiscoverEpisode
 import au.com.shiftyjelly.pocketcasts.discover.TvDiscoverPodcast
 import au.com.shiftyjelly.pocketcasts.discover.TvDiscoverRow
+import au.com.shiftyjelly.pocketcasts.discover.tvDiscoverRow
 import au.com.shiftyjelly.pocketcasts.podcasts.TvPodcastDetailsScreen
 import au.com.shiftyjelly.pocketcasts.theme.TvTheme
 import au.com.shiftyjelly.pocketcasts.theme.TvTopBarHeight
@@ -184,84 +179,13 @@ private fun TvHomeRows(
                 }
             }
             val rowFocusRequester = rowFocusRequesters[rowIndex]
-            when (row) {
-                is TvDiscoverRow.FeaturedPodcasts -> item(key = row.id) {
-                    TvRow(
-                        title = row.title,
-                        items = row.podcasts,
-                        itemSpacing = 32.dp,
-                        key = TvDiscoverPodcast::uuid,
-                        focusRequester = rowFocusRequester,
-                        modifier = rowModifier,
-                    ) { podcast ->
-                        TvFeaturedTile(
-                            artworkUrl = podcast.artworkUrl,
-                            isSponsored = podcast.isSponsored,
-                            title = podcast.title,
-                            description = podcast.description,
-                            onGoToPodcast = { onOpenPodcast(podcast.uuid) },
-                            onPlayLastEpisode = {},
-                        )
-                    }
-                }
-
-                is TvDiscoverRow.SinglePodcast -> item(key = row.id) {
-                    TvRow(
-                        title = row.title,
-                        items = row.podcasts,
-                        itemSpacing = 32.dp,
-                        key = TvDiscoverPodcast::uuid,
-                        focusRequester = rowFocusRequester,
-                        modifier = rowModifier,
-                    ) { podcast ->
-                        TvFeaturedTile(
-                            artworkUrl = podcast.artworkUrl,
-                            isSponsored = podcast.isSponsored,
-                            title = podcast.title,
-                            description = podcast.description,
-                            onGoToPodcast = { onOpenPodcast(podcast.uuid) },
-                            onPlayLastEpisode = {},
-                        )
-                    }
-                }
-
-                is TvDiscoverRow.Episodes -> item(key = row.id) {
-                    TvRow(
-                        title = row.title,
-                        items = row.episodes,
-                        itemSpacing = 32.dp,
-                        key = TvDiscoverEpisode::episodeUuid,
-                        focusRequester = rowFocusRequester,
-                        modifier = rowModifier,
-                    ) { episode ->
-                        TvVideoTile(
-                            thumbnailUrl = episode.thumbnailUrl,
-                            podcastArtworkUrl = episode.podcastArtworkUrl,
-                            podcastTitle = episode.podcastTitle,
-                            episodeTitle = episode.episodeTitle,
-                            onPlayEpisode = { onPlayEpisode(episode) },
-                            onGoToPodcast = { onOpenPodcast(episode.podcastUuid) },
-                        )
-                    }
-                }
-
-                is TvDiscoverRow.Podcasts -> item(key = row.id) {
-                    TvRow(
-                        title = row.title,
-                        items = row.podcasts,
-                        key = TvDiscoverPodcast::uuid,
-                        focusRequester = rowFocusRequester,
-                        modifier = rowModifier,
-                    ) { podcast ->
-                        TvPodcastTile(
-                            artworkUrl = podcast.artworkUrl,
-                            podcastTitle = podcast.title,
-                            onClick = { onOpenPodcast(podcast.uuid) },
-                            imageModifier = Modifier.width(TvPodcastTileDefaults.RowImageWidth),
-                        )
-                    }
-                }
-            }
+            tvDiscoverRow(
+                row = row,
+                onOpenPodcast = onOpenPodcast,
+                onPlayEpisode = onPlayEpisode,
+                modifier = rowModifier,
+                focusRequester = rowFocusRequester,
+            )
         }
 
         item { Spacer(modifier = Modifier.height(8.dp)) }
