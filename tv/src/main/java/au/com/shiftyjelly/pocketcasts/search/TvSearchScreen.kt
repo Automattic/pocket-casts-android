626f2ccc89d4082b55c2c49d73edb33f633798e7
Tamas Szelezsan <szelezsant@gmail.com>
2026-08-25 19:16:41 +0200

    Make the search field full width so the top bar focuses it when moving down

--- tv/src/main/java/au/com/shiftyjelly/pocketcasts/search/TvSearchScreen.kt
+++ tv/src/main/java/au/com/shiftyjelly/pocketcasts/search/TvSearchScreen.kt
@@ -233,7 +233,9 @@ private fun TvSearchContent(
                     }
                     isEditing = editing
                 },
-                modifier = Modifier.focusRequester(searchFieldFocusRequester),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .focusRequester(searchFieldFocusRequester),
             )
             Spacer(modifier = Modifier.height(24.dp))
         }
