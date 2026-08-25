5571cb5e301553aada0ed30cca848b8b8b3e8088
Tamas Szelezsan <szelezsant@gmail.com>
2026-08-25 20:10:22 +0200

    Add Welcome onboarding analytics to Android TV

---
+++ tv/src/main/java/au/com/shiftyjelly/pocketcasts/onboarding/welcome/TvWelcomeViewModel.kt
@@ -0,0 +1,43 @@
+package au.com.shiftyjelly.pocketcasts.onboarding.welcome
+
+import androidx.lifecycle.ViewModel
+import com.automattic.eventhorizon.BrowseNoAccountTappedEvent
+import com.automattic.eventhorizon.EventHorizon
+import com.automattic.eventhorizon.OnboardingFlowType
+import com.automattic.eventhorizon.SetupAccountButtonTappedEvent
+import com.automattic.eventhorizon.SetupAccountButtonType
+import com.automattic.eventhorizon.SetupAccountShownEvent
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class TvWelcomeViewModel @Inject constructor(
+    private val eventHorizon: EventHorizon,
+) : ViewModel() {
+
+    fun trackShown() {
+        eventHorizon.track(SetupAccountShownEvent(flow = OnboardingFlowType.Unknown))
+    }
+
+    fun trackSignInClicked() {
+        eventHorizon.track(
+            SetupAccountButtonTappedEvent(
+                flow = OnboardingFlowType.Unknown,
+                button = SetupAccountButtonType.SignIn,
+            ),
+        )
+    }
+
+    fun trackCreateAccountClicked() {
+        eventHorizon.track(
+            SetupAccountButtonTappedEvent(
+                flow = OnboardingFlowType.Unknown,
+                button = SetupAccountButtonType.CreateAccount,
+            ),
+        )
+    }
+
+    fun trackBrowseWithoutAccountClicked() {
+        eventHorizon.track(BrowseNoAccountTappedEvent)
+    }
+}
