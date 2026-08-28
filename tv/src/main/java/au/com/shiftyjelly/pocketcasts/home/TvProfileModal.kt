4093e01f27d7c9ccd37e2cca7d96bb0cda0ee878
Tamas Szelezsan <33399753+sztomek@users.noreply.github.com>
2026-08-28 14:14:35 +0200

    [TV] Confirm before logging out from the profile modal (#5796)

--- tv/src/main/java/au/com/shiftyjelly/pocketcasts/home/TvProfileModal.kt
+++ tv/src/main/java/au/com/shiftyjelly/pocketcasts/home/TvProfileModal.kt
@@ -1,5 +1,6 @@
 package au.com.shiftyjelly.pocketcasts.home
 
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.ColumnScope
@@ -8,7 +9,10 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -23,6 +27,7 @@ import androidx.tv.material3.Icon
 import androidx.tv.material3.MaterialTheme
 import androidx.tv.material3.Text
 import au.com.shiftyjelly.pocketcasts.BuildConfig
+import au.com.shiftyjelly.pocketcasts.component.TvConfirmationContent
 import au.com.shiftyjelly.pocketcasts.component.TvModal
 import au.com.shiftyjelly.pocketcasts.component.TvModalButton
 import au.com.shiftyjelly.pocketcasts.component.TvModalSurface
@@ -71,9 +76,29 @@ private fun ColumnScope.TvProfileModalContent(
     onSettings: () -> Unit,
     onLogOut: () -> Unit,
 ) {
+    var isShowingLogoutConfirmation by remember { mutableStateOf(false) }
+    var returnFocusToLogOut by remember { mutableStateOf(false) }
     val focusRequester = remember { FocusRequester() }
-    LaunchedEffect(profile is TvProfileState.SignedIn) {
-        focusRequester.requestFocus()
+    LaunchedEffect(profile is TvProfileState.SignedIn, isShowingLogoutConfirmation) {
+        if (!isShowingLogoutConfirmation) {
+            focusRequester.requestFocus()
+        }
+    }
+
+    if (isShowingLogoutConfirmation) {
+        fun cancel() {
+            returnFocusToLogOut = true
+            isShowingLogoutConfirmation = false
+        }
+        BackHandler(onBack = ::cancel)
+        TvConfirmationContent(
+            title = stringResource(LR.string.tv_profile_log_out_confirmation_title),
+            message = stringResource(LR.string.tv_profile_log_out_confirmation_message),
+            confirmLabel = stringResource(LR.string.log_out),
+            onConfirm = onLogOut,
+            onCancel = ::cancel,
+        )
+        return
     }
 
     when (profile) {
@@ -90,7 +115,7 @@ private fun ColumnScope.TvProfileModalContent(
             TvModalButton(
                 text = stringResource(LR.string.tv_profile_starred_episodes),
                 onClick = onStarredEpisodes,
-                modifier = Modifier.focusRequester(focusRequester),
+                modifier = if (returnFocusToLogOut) Modifier else Modifier.focusRequester(focusRequester),
             )
             TvModalButton(
                 text = stringResource(LR.string.profile_navigation_listening_history),
@@ -102,7 +127,8 @@ private fun ColumnScope.TvProfileModalContent(
             )
             TvModalButton(
                 text = stringResource(LR.string.log_out),
-                onClick = onLogOut,
+                onClick = { isShowingLogoutConfirmation = true },
+                modifier = if (returnFocusToLogOut) Modifier.focusRequester(focusRequester) else Modifier,
             )
         }
 
