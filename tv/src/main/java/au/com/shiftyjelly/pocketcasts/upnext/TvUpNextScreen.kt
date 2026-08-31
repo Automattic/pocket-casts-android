30958d15f9eb47b464ea4aed4cc6b51bd31c87c1
Tamas Szelezsan <szelezsant@gmail.com>
2026-08-31 22:01:31 +0200

    Apply Figma density to TV up next, starred and history

--- tv/src/main/java/au/com/shiftyjelly/pocketcasts/upnext/TvUpNextScreen.kt
+++ tv/src/main/java/au/com/shiftyjelly/pocketcasts/upnext/TvUpNextScreen.kt
@@ -143,13 +143,13 @@ private fun UpNextList(
         modifier = modifier
             .fillMaxHeight()
             .fillMaxWidth(ROW_WIDTH_FRACTION)
-            .padding(start = 32.dp, top = 8.dp),
+            .padding(start = 42.dp, top = 40.dp),
     ) {
         UpNextHeader(episodes = episodes)
-        Spacer(Modifier.height(26.dp))
+        Spacer(Modifier.height(16.dp))
         LazyColumn(
             state = listState,
-            verticalArrangement = Arrangement.spacedBy(12.dp),
+            verticalArrangement = Arrangement.spacedBy(6.dp),
             contentPadding = PaddingValues(bottom = 32.dp),
             modifier = Modifier.weight(1f),
         ) {
@@ -203,7 +203,7 @@ private fun UpNextHeader(
     ) {
         Text(
             text = stringResource(LR.string.up_next),
-            style = MaterialTheme.tvTypography.title3,
+            style = MaterialTheme.tvTypography.title2,
             color = MaterialTheme.tvColors.textPrimary,
         )
         Text(
@@ -239,7 +239,7 @@ private fun UpNextEmpty(
     )
 }
 
-private const val ROW_WIDTH_FRACTION = 0.75f
+private const val ROW_WIDTH_FRACTION = 0.65f
 
 @Preview(device = Devices.TV_1080p)
 @Composable
