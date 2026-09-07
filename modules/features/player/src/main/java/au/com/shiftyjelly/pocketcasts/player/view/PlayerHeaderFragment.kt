e1a2671a097d598b9a8a4e6ac0f3cf937935c905
Tamas Szelezsan <33399753+sztomek@users.noreply.github.com>
2026-09-07 17:44:03 +0200

    Fix background OOM kills by pausing player recomposition when backgrounded (#5843)

--- modules/features/player/src/main/java/au/com/shiftyjelly/pocketcasts/player/view/PlayerHeaderFragment.kt
+++ modules/features/player/src/main/java/au/com/shiftyjelly/pocketcasts/player/view/PlayerHeaderFragment.kt
@@ -76,6 +76,7 @@ import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.asFlow
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import au.com.shiftyjelly.pocketcasts.ads.AdReportFragment
@@ -223,15 +224,16 @@ class PlayerHeaderFragment :
         val isPortraitPlayer = isPortraitConfiguration || windowSize.isAtLeastMediumHeight()
         val maxWidthFraction = if (isPortraitConfiguration && windowSize.isAtLeastMediumWidth()) 0.8f else 1f
 
-        val podcastColors by remember { podcastColorsFlow() }.collectAsState(PodcastColors.ForUserEpisode)
-        val headerData by remember { playerHeaderFlow() }.collectAsState(PlayerViewModel.PlayerHeader())
+        val podcastColors by remember { podcastColorsFlow() }.collectAsStateWithLifecycle(PodcastColors.ForUserEpisode)
+        val headerData by remember { playerHeaderFlow() }.collectAsStateWithLifecycle(PlayerViewModel.PlayerHeader())
+        // Not lifecycle-gated: carries the video Player/surface and re-emitting it on resume can freeze the frame.
         val artworkOrVideoState by remember { playerVisualsStateFlow() }.collectAsState(ArtworkOrVideoState.NoContent)
-        val activeAd by viewModel.activeAd.collectAsState()
-        val playbackNotice by viewModel.playbackNotice.collectAsState()
+        val activeAd by viewModel.activeAd.collectAsStateWithLifecycle()
+        val playbackNotice by viewModel.playbackNotice.collectAsStateWithLifecycle()
 
-        val isPlayerOpen by isPlayerOpenFlow().collectAsState(false)
-        val isTranscriptOpen by shelfSharedViewModel.isTranscriptOpen.collectAsState()
-        val transcriptUiState by transcriptViewModel.uiState.collectAsState()
+        val isPlayerOpen by remember { isPlayerOpenFlow() }.collectAsStateWithLifecycle(false)
+        val isTranscriptOpen by shelfSharedViewModel.isTranscriptOpen.collectAsStateWithLifecycle()
+        val transcriptUiState by transcriptViewModel.uiState.collectAsStateWithLifecycle()
 
         val transitionData = updateTranscriptTransitionData(
             isTranscriptOpen = isTranscriptOpen,
@@ -635,6 +637,7 @@ class PlayerHeaderFragment :
             override fun onSlide(bottomSheet: View, slideOffset: Float) = Unit
         }
         val hostListener = (requireActivity() as FragmentHostListener)
+        send(hostListener.getPlayerBottomSheetState() == BottomSheetBehavior.STATE_EXPANDED)
         hostListener.addPlayerBottomSheetCallback(callback)
         awaitClose {
             hostListener.removePlayerBottomSheetCallback(callback)
