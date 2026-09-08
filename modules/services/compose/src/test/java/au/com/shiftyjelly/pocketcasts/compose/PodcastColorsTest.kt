0a693b9bf362d43efdfee7afe555904a526fa8e8
Tamas Szelezsan <szelezsant@gmail.com>
2026-09-08 12:36:28 +0200

    Add a test that the player tint stays visible for a podcast with no extracted colors

---
+++ modules/services/compose/src/test/java/au/com/shiftyjelly/pocketcasts/compose/PodcastColorsTest.kt
@@ -0,0 +1,30 @@
+package au.com.shiftyjelly.pocketcasts.compose
+
+import androidx.compose.ui.graphics.Color
+import au.com.shiftyjelly.pocketcasts.models.entity.Podcast
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+@Config(manifest = Config.NONE)
+@RunWith(RobolectricTestRunner::class)
+class PodcastColorsTest {
+    @Test
+    fun `player tint stays opaque when the podcast has no extracted colors`() {
+        val podcastColors = PodcastColors(Podcast(uuid = "uuid"))
+
+        assertEquals(1f, podcastColors.playerTint.alpha, 0f)
+        assertEquals(Color.White, podcastColors.playerTint)
+    }
+
+    @Test
+    fun `player tint uses the podcast dark tint when it is set`() {
+        val tint = 0xFF3366FF.toInt()
+
+        val podcastColors = PodcastColors(Podcast(uuid = "uuid", tintColorForDarkBg = tint))
+
+        assertEquals(Color(tint), podcastColors.playerTint)
+    }
+}
