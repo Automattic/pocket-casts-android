ad7ce9e9a44162e8d9d0b7a9f76baca97fb1855f
Gio Lodi <gio@mokacoding.com>
2026-09-08 17:36:50 +1000

    Write multi-param fastlane call across multiple line

--- .buildkite/commands/update-rollouts.sh
+++ .buildkite/commands/update-rollouts.sh
@@ -24,4 +24,8 @@ echo '--- 🔐 Access Secrets'
 bundle exec fastlane run configure_apply
 
 echo '--- 🚀 Update Rollouts'
-bundle exec fastlane update_rollouts track:"$TRACK" percent:"$ROLLOUT_PERCENT" version:"$RELEASE_VERSION" milestone:"${MILESTONE:-}"
+bundle exec fastlane update_rollouts \
+  track:"$TRACK" \
+  percent:"$ROLLOUT_PERCENT" \
+  version:"$RELEASE_VERSION" \
+  milestone:"${MILESTONE:-}"
