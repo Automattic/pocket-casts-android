94ba7ff4f782a3c160d189c60cdeb3f72e6cb192
Tamas Szelezsan <szelezsant@gmail.com>
2026-09-10 16:08:17 +0200

    Keep the episode list from jumping when the more button gains focus

--- tv/src/main/java/au/com/shiftyjelly/pocketcasts/component/TvEpisodeListItem.kt
+++ tv/src/main/java/au/com/shiftyjelly/pocketcasts/component/TvEpisodeListItem.kt
@@ -4,13 +4,11 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -18,26 +16,17 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusProperties
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.focus.onFocusChanged
-import androidx.compose.ui.input.key.Key
-import androidx.compose.ui.input.key.KeyEventType
-import androidx.compose.ui.input.key.key
-import androidx.compose.ui.input.key.onPreviewKeyEvent
-import androidx.compose.ui.input.key.type
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.layout.LayoutCoordinates
+import androidx.compose.ui.node.ModifierNodeElement
+import androidx.compose.ui.relocation.BringIntoViewModifierNode
 import androidx.compose.ui.unit.dp
-import androidx.tv.material3.Icon
-import androidx.tv.material3.MaterialTheme
 import au.com.shiftyjelly.pocketcasts.localization.helper.RelativeDateFormatter
 import au.com.shiftyjelly.pocketcasts.models.entity.PodcastEpisode
-import au.com.shiftyjelly.pocketcasts.theme.tvColors
-import au.com.shiftyjelly.pocketcasts.images.R as IR
-import au.com.shiftyjelly.pocketcasts.localization.R as LR
 
 @Composable
 fun TvEpisodeListItem(
@@ -50,6 +39,7 @@ fun TvEpisodeListItem(
     leftFocusRequester: FocusRequester? = null,
 ) {
     TvEpisodeListItemContainer(
+        onOpenActions = onOpenActions,
         modifier = modifier,
     ) { rowModifier ->
         TvEpisodeRow(
@@ -58,21 +48,14 @@ fun TvEpisodeListItem(
             dateFormatter = dateFormatter,
             modifier = rowModifier
                 .then(if (leftFocusRequester != null) Modifier.focusProperties { left = leftFocusRequester } else Modifier)
-                .then(if (episodeFocusRequester != null) Modifier.focusRequester(episodeFocusRequester) else Modifier)
-                .onPreviewKeyEvent { event ->
-                    if (event.type == KeyEventType.KeyDown && event.key == Key.DirectionRight) {
-                        onOpenActions()
-                        true
-                    } else {
-                        false
-                    }
-                },
+                .then(if (episodeFocusRequester != null) Modifier.focusRequester(episodeFocusRequester) else Modifier),
         )
     }
 }
 
 @Composable
 fun TvEpisodeListItemContainer(
+    onOpenActions: () -> Unit,
     modifier: Modifier = Modifier,
     content: @Composable (Modifier) -> Unit,
 ) {
@@ -84,39 +67,49 @@ fun TvEpisodeListItemContainer(
             .onFocusChanged { isItemFocused = it.hasFocus },
     ) {
         content(Modifier.weight(1f))
-        MoreButtonHint(visible = isItemFocused)
+        MoreButtonSlot(
+            visible = isItemFocused,
+            onClick = onOpenActions,
+        )
     }
 }
 
 @Composable
-private fun MoreButtonHint(visible: Boolean) {
+private fun MoreButtonSlot(
+    visible: Boolean,
+    onClick: () -> Unit,
+) {
     Box(
         contentAlignment = Alignment.Center,
         modifier = Modifier
             .padding(start = 12.dp)
-            .size(TvMoreButtonSize),
+            .size(TvMoreButtonSize)
+            .then(ConsumeBringIntoViewElement),
     ) {
         AnimatedVisibility(
             visible = visible,
             enter = fadeIn(tween(MORE_BUTTON_ANIMATION_DURATION_MS)),
             exit = fadeOut(tween(MORE_BUTTON_ANIMATION_DURATION_MS)),
         ) {
-            Box(
-                contentAlignment = Alignment.Center,
-                modifier = Modifier
-                    .size(TvMoreButtonSize)
-                    .clip(CircleShape)
-                    .background(MaterialTheme.tvColors.backgroundActive20),
-            ) {
-                Icon(
-                    painter = painterResource(IR.drawable.ic_ellipsis_horizontal),
-                    contentDescription = stringResource(LR.string.more_options),
-                    tint = MaterialTheme.tvColors.textPrimary,
-                    modifier = Modifier.size(15.dp),
-                )
-            }
+            TvMoreButton(onClick = onClick)
         }
     }
 }
 
 private const val MORE_BUTTON_ANIMATION_DURATION_MS = 200
+
+private object ConsumeBringIntoViewElement : ModifierNodeElement<ConsumeBringIntoViewNode>() {
+    override fun create() = ConsumeBringIntoViewNode()
+
+    override fun update(node: ConsumeBringIntoViewNode) = Unit
+
+    override fun hashCode() = 0
+
+    override fun equals(other: Any?) = other is ConsumeBringIntoViewElement
+}
+
+private class ConsumeBringIntoViewNode :
+    Modifier.Node(),
+    BringIntoViewModifierNode {
+    override suspend fun bringIntoView(childCoordinates: LayoutCoordinates, boundsProvider: () -> Rect?) = Unit
+}
