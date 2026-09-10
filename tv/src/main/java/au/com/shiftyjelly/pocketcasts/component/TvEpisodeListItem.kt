56c6d21ec613708304e32b82b641295fb665ea9a
Tamas Szelezsan <szelezsant@gmail.com>
2026-09-10 14:42:49 +0200

    Open episode actions with D-pad right so the row no longer jumps

--- tv/src/main/java/au/com/shiftyjelly/pocketcasts/component/TvEpisodeListItem.kt
+++ tv/src/main/java/au/com/shiftyjelly/pocketcasts/component/TvEpisodeListItem.kt
@@ -4,11 +4,13 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -16,13 +18,26 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusProperties
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.KeyEventType
+import androidx.compose.ui.input.key.key
+import androidx.compose.ui.input.key.onPreviewKeyEvent
+import androidx.compose.ui.input.key.type
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.tv.material3.Icon
+import androidx.tv.material3.MaterialTheme
 import au.com.shiftyjelly.pocketcasts.localization.helper.RelativeDateFormatter
 import au.com.shiftyjelly.pocketcasts.models.entity.PodcastEpisode
+import au.com.shiftyjelly.pocketcasts.theme.tvColors
+import au.com.shiftyjelly.pocketcasts.images.R as IR
+import au.com.shiftyjelly.pocketcasts.localization.R as LR
 
 @Composable
 fun TvEpisodeListItem(
@@ -35,7 +50,6 @@ fun TvEpisodeListItem(
     leftFocusRequester: FocusRequester? = null,
 ) {
     TvEpisodeListItemContainer(
-        onOpenActions = onOpenActions,
         modifier = modifier,
     ) { rowModifier ->
         TvEpisodeRow(
@@ -44,14 +58,21 @@ fun TvEpisodeListItem(
             dateFormatter = dateFormatter,
             modifier = rowModifier
                 .then(if (leftFocusRequester != null) Modifier.focusProperties { left = leftFocusRequester } else Modifier)
-                .then(if (episodeFocusRequester != null) Modifier.focusRequester(episodeFocusRequester) else Modifier),
+                .then(if (episodeFocusRequester != null) Modifier.focusRequester(episodeFocusRequester) else Modifier)
+                .onPreviewKeyEvent { event ->
+                    if (event.type == KeyEventType.KeyDown && event.key == Key.DirectionRight) {
+                        onOpenActions()
+                        true
+                    } else {
+                        false
+                    }
+                },
         )
     }
 }
 
 @Composable
 fun TvEpisodeListItemContainer(
-    onOpenActions: () -> Unit,
     modifier: Modifier = Modifier,
     content: @Composable (Modifier) -> Unit,
 ) {
@@ -63,18 +84,12 @@ fun TvEpisodeListItemContainer(
             .onFocusChanged { isItemFocused = it.hasFocus },
     ) {
         content(Modifier.weight(1f))
-        MoreButtonSlot(
-            visible = isItemFocused,
-            onClick = onOpenActions,
-        )
+        MoreButtonHint(visible = isItemFocused)
     }
 }
 
 @Composable
-private fun MoreButtonSlot(
-    visible: Boolean,
-    onClick: () -> Unit,
-) {
+private fun MoreButtonHint(visible: Boolean) {
     Box(
         contentAlignment = Alignment.Center,
         modifier = Modifier
@@ -86,7 +101,20 @@ private fun MoreButtonSlot(
             enter = fadeIn(tween(MORE_BUTTON_ANIMATION_DURATION_MS)),
             exit = fadeOut(tween(MORE_BUTTON_ANIMATION_DURATION_MS)),
         ) {
-            TvMoreButton(onClick = onClick)
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .size(TvMoreButtonSize)
+                    .clip(CircleShape)
+                    .background(MaterialTheme.tvColors.backgroundActive20),
+            ) {
+                Icon(
+                    painter = painterResource(IR.drawable.ic_ellipsis_horizontal),
+                    contentDescription = stringResource(LR.string.more_options),
+                    tint = MaterialTheme.tvColors.textPrimary,
+                    modifier = Modifier.size(15.dp),
+                )
+            }
         }
     }
 }
