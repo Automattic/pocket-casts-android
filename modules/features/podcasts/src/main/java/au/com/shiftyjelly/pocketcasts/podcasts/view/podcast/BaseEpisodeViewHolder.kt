6a2e86fbf268ed6a4ce1ee77dcfe7bb5d3416144
Philip Simpson <philz.the.gecko@gmail.com>
2026-09-14 10:07:39 +0930

    Re-observe episode row data when the flow completes (#5936)

--- modules/features/podcasts/src/main/java/au/com/shiftyjelly/pocketcasts/podcasts/view/podcast/BaseEpisodeViewHolder.kt
+++ modules/features/podcasts/src/main/java/au/com/shiftyjelly/pocketcasts/podcasts/view/podcast/BaseEpisodeViewHolder.kt
@@ -69,8 +69,6 @@ abstract class BaseEpisodeViewHolder<T : Any>(
 
     private var streamByDefault = false
 
-    private var isObservingRowData = false
-
     private var hasHlsAlternateEnclosure = false
 
     @Suppress("UNCHECKED_CAST")
@@ -122,8 +120,8 @@ abstract class BaseEpisodeViewHolder<T : Any>(
         val previousUuid = boundItem?.let(::toPodcastEpisode)?.uuid
         setupInitialState(item, tint, isMultiSelectEnabled, streamByDefault)
 
-        val isNewEpisode = previousUuid != episode.uuid || !isObservingRowData
-        if (isNewEpisode) {
+        val shouldObserveRowData = previousUuid != episode.uuid || rowDataJob?.isActive != true
+        if (shouldObserveRowData) {
             observeRowData()
         }
         bindArtwork(useEpisodeArtwork)
@@ -163,14 +161,12 @@ abstract class BaseEpisodeViewHolder<T : Any>(
     fun unbind() {
         rowDataJob?.cancel()
         rowDataJob = null
-        isObservingRowData = false
         binding.episodeRow.handler?.removeCallbacksAndMessages(null)
     }
 
     private fun observeRowData() {
         rowDataJob?.cancel()
         hasHlsAlternateEnclosure = false
-        isObservingRowData = true
         rowDataJob = holderScope.launch {
             rowDataProvider.episodeRowDataFlow(episode.uuid).collect { data ->
                 hasHlsAlternateEnclosure = data.hasHlsAlternateEnclosure
