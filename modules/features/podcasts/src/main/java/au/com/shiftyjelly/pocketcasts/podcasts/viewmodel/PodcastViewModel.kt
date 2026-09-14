5e13af0fc0f5d3e70a8c7c90ddf8b13130275d74
Tamas Szelezsan <szelezsant@gmail.com>
2026-09-14 23:48:48 +0200

    Address PR comments

--- modules/features/podcasts/src/main/java/au/com/shiftyjelly/pocketcasts/podcasts/viewmodel/PodcastViewModel.kt
+++ modules/features/podcasts/src/main/java/au/com/shiftyjelly/pocketcasts/podcasts/viewmodel/PodcastViewModel.kt
@@ -89,6 +89,7 @@ import kotlinx.coroutines.NonCancellable
 import kotlinx.coroutines.withContext
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.flatMapLatest
@@ -476,8 +477,11 @@ class PodcastViewModel @Inject constructor(
         )
     }
 
+    private var playJob: Job? = null
+
     fun play(bookmark: Bookmark) {
-        launch {
+        playJob?.cancel()
+        playJob = launch {
             val bookmarkEpisode = resolveEpisode(bookmark) ?: return@launch
             val hasReferenceTime = bookmark.referenceTime != null
             val isPlayingBookmarkEpisode = playbackManager.isPlaying() &&
