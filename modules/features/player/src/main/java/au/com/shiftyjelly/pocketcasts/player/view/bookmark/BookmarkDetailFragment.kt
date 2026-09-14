466f9535d34d56de3437c2d9812081e14bbfb404
Tamas Szelezsan <szelezsant@gmail.com>
2026-09-14 16:21:01 +0200

    player: keep the bookmark details open after playing

--- modules/features/player/src/main/java/au/com/shiftyjelly/pocketcasts/player/view/bookmark/BookmarkDetailFragment.kt
+++ modules/features/player/src/main/java/au/com/shiftyjelly/pocketcasts/player/view/bookmark/BookmarkDetailFragment.kt
@@ -208,7 +208,6 @@ class BookmarkDetailFragment : BaseDialogFragment() {
                     podcastUuid = args.podcastUuid,
                 ),
             )
-            dismiss()
         }
     }
 }
