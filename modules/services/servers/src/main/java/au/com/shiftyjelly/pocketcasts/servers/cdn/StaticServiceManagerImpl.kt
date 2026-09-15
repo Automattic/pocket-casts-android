e3f643a856600568b55e132b3c48a689ec194644
Philip Simpson <philip.simpson@automattic.com>
2026-09-15 21:41:18 +0930

    Replace StaticServiceManager.getColorsSingle with the suspend getColors
    The Rx Single was a thin rxSingle wrapper over the suspend getColors. Its two
    callers, SubscribeManager and the Discover carousel in DiscoverAdapter, now
    bridge from getColors themselves, so the servers cdn package is Rx-free.

--- modules/services/servers/src/main/java/au/com/shiftyjelly/pocketcasts/servers/cdn/StaticServiceManagerImpl.kt
+++ modules/services/servers/src/main/java/au/com/shiftyjelly/pocketcasts/servers/cdn/StaticServiceManagerImpl.kt
@@ -1,19 +1,11 @@
 package au.com.shiftyjelly.pocketcasts.servers.cdn
 
 import au.com.shiftyjelly.pocketcasts.models.entity.BlazeAd
-import au.com.shiftyjelly.pocketcasts.utils.Optional
-import io.reactivex.Single
 import javax.inject.Inject
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.rx2.rxSingle
 
 class StaticServiceManagerImpl @Inject constructor(
     private val service: StaticService,
 ) : StaticServiceManager {
-    override fun getColorsSingle(podcastUuid: String): Single<Optional<ArtworkColors>> {
-        return rxSingle(Dispatchers.IO) { Optional.of(getColors(podcastUuid)) }
-    }
-
     override suspend fun getColors(podcastUuid: String): ArtworkColors? {
         return service.getColors(podcastUuid)?.toArtworkColors()
     }
