82407a5bfed98955d97e52ef5a1a8cfbc894e7f6
Tamas Szelezsan <szelezsant@gmail.com>
2026-09-15 17:11:53 +0200

    Emit bookmark edit-sheet analytics for suggestions, passage edits, and submitted passages

--- modules/features/player/src/main/java/au/com/shiftyjelly/pocketcasts/player/view/bookmark/BookmarkFragment.kt
+++ modules/features/player/src/main/java/au/com/shiftyjelly/pocketcasts/player/view/bookmark/BookmarkFragment.kt
@@ -49,13 +49,13 @@ class BookmarkFragment : BaseFragment() {
     private val args get() = requireArguments().requireParcelable<BookmarkArguments>(NEW_INSTANCE_KEY)
 
     private val editTranscriptLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-        if (result.resultCode == Activity.RESULT_OK) {
-            val data = result.data
-            val passage = data?.getStringExtra(BookmarkTranscriptEditActivity.RESULT_PASSAGE)
-            val passageLocation = data?.getIntExtra(BookmarkTranscriptEditActivity.RESULT_PASSAGE_LOCATION, -1) ?: -1
-            if (passage != null && passageLocation >= 0) {
-                viewModel.onPassageEdited(passage, passageLocation)
-            }
+        val data = result.data
+        val passage = data?.getStringExtra(BookmarkTranscriptEditActivity.RESULT_PASSAGE)?.takeIf { result.resultCode == Activity.RESULT_OK }
+        val passageLocation = data?.getIntExtra(BookmarkTranscriptEditActivity.RESULT_PASSAGE_LOCATION, -1) ?: -1
+        val editedPassage = passage?.takeIf { passageLocation >= 0 }
+        viewModel.onPassageEditorDismissed(editedPassage)
+        if (editedPassage != null) {
+            viewModel.onPassageEdited(editedPassage, passageLocation)
         }
     }
 
@@ -95,7 +95,7 @@ class BookmarkFragment : BaseFragment() {
                     isCapturingPassage = uiState.isCapturingPassage,
                     onEditTranscript = ::editTranscript,
                     titleSuggestion = uiState.titleSuggestion,
-                    onApplySuggestion = { viewModel.applySuggestion(it) },
+                    onApplySuggestion = { viewModel.onSuggestionTapped(it) },
                     modifier = Modifier
                         .background(playerColors.background01)
                         .windowInsetsPadding(WindowInsets.ime.union(WindowInsets.systemBars)),
@@ -139,6 +139,7 @@ class BookmarkFragment : BaseFragment() {
                 podcastColors = args.podcastColors,
             ),
         )
+        viewModel.onPassageEditorShown()
         editTranscriptLauncher.launch(intent)
     }
 
