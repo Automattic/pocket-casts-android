b163ee92e095cad2ce4c90fd3c2dd0d5df84cc49
Tamas Szelezsan <szelezsant@gmail.com>
2026-09-15 20:46:10 +0200

    Address PR comments

--- modules/features/player/src/main/java/au/com/shiftyjelly/pocketcasts/player/view/bookmark/BookmarkViewModel.kt
+++ modules/features/player/src/main/java/au/com/shiftyjelly/pocketcasts/player/view/bookmark/BookmarkViewModel.kt
@@ -5,6 +5,7 @@ import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import au.com.shiftyjelly.pocketcasts.analytics.SourceView
 import au.com.shiftyjelly.pocketcasts.models.entity.Bookmark
 import au.com.shiftyjelly.pocketcasts.models.entity.PodcastEpisode
 import au.com.shiftyjelly.pocketcasts.repositories.bookmark.BookmarkGenerationAnalytics
@@ -69,6 +70,7 @@ class BookmarkViewModel
     companion object {
         private const val DEFAULT_TITLE = "Bookmark"
         private val SUGGESTION_TIMEOUT = 10.seconds
+        private val WORD_SEPARATOR = Regex("\\s+")
 
         private fun buildSelectedTextFieldValue(text: String): TextFieldValue {
             return TextFieldValue(text = text, selection = TextRange(0, text.length))
@@ -272,10 +274,10 @@ class BookmarkViewModel
         }
     }
 
-    fun onShown(isNewBookmark: Boolean) {
+    fun onShown(isNewBookmark: Boolean, source: SourceView) {
         eventHorizon.track(
             BookmarkEditFormShownEvent(
-                source = analyticsSource,
+                source = source.analyticsValue,
                 isNewBookmark = isNewBookmark,
             ),
         )
@@ -324,6 +326,7 @@ class BookmarkViewModel
     }
 
     fun onPassageEditorDismissed(newPassage: String?) {
+        if (!::arguments.isInitialized) return
         val currentPassage = uiState.value.passage
         val counted = newPassage ?: currentPassage
         eventHorizon.track(
@@ -337,5 +340,5 @@ class BookmarkViewModel
         )
     }
 
-    private fun countWords(text: String) = text.split(Regex("\\s+")).count { it.isNotBlank() }
+    private fun countWords(text: String) = text.split(WORD_SEPARATOR).count { it.isNotBlank() }
 }
