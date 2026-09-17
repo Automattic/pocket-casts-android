d4b969a088872d7bc2a3d525eb0b4a1fe30b4e2b
Philip Simpson <philz.the.gecko@gmail.com>
2026-09-17 16:34:02 +0930

    Group options in the Developer page (#6008)

--- modules/features/settings/src/main/java/au/com/shiftyjelly/pocketcasts/settings/developer/DeveloperViewModel.kt
+++ modules/features/settings/src/main/java/au/com/shiftyjelly/pocketcasts/settings/developer/DeveloperViewModel.kt
@@ -190,6 +190,11 @@ class DeveloperViewModel
         settings.showReferralsTooltip.set(true, updateModifiedAt = false)
     }
 
+    fun showPlaylistTooltips() {
+        settings.showPremadePlaylistsTooltip.set(true, updateModifiedAt = false)
+        settings.showRearrangePlaylistsTooltip.set(true, updateModifiedAt = false)
+    }
+
     fun resetNotificationsPrompt() {
         settings.notificationsPromptAcknowledged.set(false, updateModifiedAt = false)
     }
