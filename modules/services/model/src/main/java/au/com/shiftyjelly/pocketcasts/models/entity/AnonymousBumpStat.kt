43282eb89579791874a60585c0117beb1b7df0b1
Tamas Szelezsan <33399753+sztomek@users.noreply.github.com>
2026-09-18 09:39:01 +0200

    Fix Turkish locale breaking sponsored Discover analytics events (#6009)

--- modules/services/model/src/main/java/au/com/shiftyjelly/pocketcasts/models/entity/AnonymousBumpStat.kt
+++ modules/services/model/src/main/java/au/com/shiftyjelly/pocketcasts/models/entity/AnonymousBumpStat.kt
@@ -8,6 +8,7 @@ import com.squareup.moshi.FromJson
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.ToJson
 import com.squareup.moshi.Types
+import java.util.Locale
 import timber.log.Timber
 
 @Entity(tableName = "bump_stats", primaryKeys = ["name", "event_time", "custom_event_props"])
@@ -28,7 +29,7 @@ data class AnonymousBumpStat(
     }
 
     fun withBumpName(): AnonymousBumpStat {
-        val bumpName = "pcandroid_${name}_bump"
+        val bumpName = "pcandroid_${name.lowercase(Locale.ROOT)}_bump"
         return copy(name = bumpName)
     }
 
