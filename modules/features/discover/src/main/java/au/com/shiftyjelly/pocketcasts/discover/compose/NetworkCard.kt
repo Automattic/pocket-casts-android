472efeea051d95f370c113429610f375b8546a84
Philip Simpson <philip.simpson@automattic.com>
2026-09-18 14:18:29 +0930

    Improve the search results network image

--- modules/features/discover/src/main/java/au/com/shiftyjelly/pocketcasts/discover/compose/NetworkCard.kt
+++ modules/features/discover/src/main/java/au/com/shiftyjelly/pocketcasts/discover/compose/NetworkCard.kt
@@ -1,43 +1,30 @@
 package au.com.shiftyjelly.pocketcasts.discover.compose
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.dropShadow
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.shadow.Shadow
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import au.com.shiftyjelly.pocketcasts.compose.AppThemeWithBackground
+import au.com.shiftyjelly.pocketcasts.compose.components.NetworkImage
 import au.com.shiftyjelly.pocketcasts.compose.components.TextH40
 import au.com.shiftyjelly.pocketcasts.compose.components.TextP50
 import au.com.shiftyjelly.pocketcasts.compose.preview.ThemePreviewParameterProvider
 import au.com.shiftyjelly.pocketcasts.compose.theme
-import au.com.shiftyjelly.pocketcasts.repositories.images.PocketCastsImageRequestFactory
 import au.com.shiftyjelly.pocketcasts.servers.model.DiscoverListSummary
-import au.com.shiftyjelly.pocketcasts.ui.extensions.themed
 import au.com.shiftyjelly.pocketcasts.ui.theme.Theme
-import coil3.compose.rememberAsyncImagePainter
 
 internal val NetworkCardWidth = 168.dp
 
@@ -51,7 +38,11 @@ internal fun NetworkCard(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier.clickable(onClick = onClick),
     ) {
-        NetworkImage(imageUrl = network.collectionImage)
+        NetworkImage(
+            imageUrl = network.collectionImage,
+            imageRequestSize = NetworkCardWidth,
+            modifier = Modifier.fillMaxWidth(),
+        )
         Spacer(Modifier.height(TitleSpacing))
         TextH40(
             text = network.title.orEmpty(),
@@ -71,38 +62,6 @@ internal fun NetworkCard(
     }
 }
 
-@Composable
-private fun NetworkImage(
-    imageUrl: String?,
-    modifier: Modifier = Modifier,
-) {
-    val context = LocalContext.current
-    val imageRequest = remember(imageUrl, context) {
-        PocketCastsImageRequestFactory(
-            context = context,
-            placeholderType = PocketCastsImageRequestFactory.PlaceholderType.Small,
-            size = NetworkCardWidth.value.toInt(),
-        ).themed().createForFileOrUrl(imageUrl.orEmpty())
-    }
-    Image(
-        painter = rememberAsyncImagePainter(imageRequest, contentScale = ContentScale.Crop),
-        contentScale = ContentScale.Crop,
-        contentDescription = null,
-        modifier = modifier
-            .fillMaxWidth()
-            .aspectRatio(1f)
-            .dropShadow(CircleShape, NetworkImageShadow)
-            .clip(CircleShape),
-    )
-}
-
-private val NetworkImageShadow = Shadow(
-    radius = 8.dp,
-    color = Color.Black,
-    spread = 0.dp,
-    offset = DpOffset(x = 0.dp, y = 2.dp),
-    alpha = 0.15f,
-)
 private val TitleSpacing: Dp = 10.dp
 private val DescriptionSpacing: Dp = 2.dp
 private val DescriptionHeight: Dp = 40.dp
