12b6a60db9a1199b9a178e0d1bde511b05c06637
Tamas Szelezsan <szelezsant@gmail.com>
2026-09-22 22:51:00 +0200

    Add a reusable swipe row component

---
+++ modules/services/compose/src/main/java/au/com/shiftyjelly/pocketcasts/compose/swipe/SwipeRowState.kt
@@ -0,0 +1,37 @@
+package au.com.shiftyjelly.pocketcasts.compose.swipe
+
+import androidx.compose.foundation.gestures.AnchoredDraggableState
+import androidx.compose.foundation.gestures.animateTo
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+@Stable
+class SwipeRowState internal constructor(
+    internal val draggableState: AnchoredDraggableState<SwipeRowAnchor>,
+    private val scope: CoroutineScope,
+) {
+    val isOpen get() = draggableState.targetValue != SwipeRowAnchor.Resting
+
+    fun settle() {
+        scope.launch { settleAndWait() }
+    }
+
+    internal suspend fun settleAndWait() {
+        draggableState.animateTo(SwipeRowAnchor.Resting)
+    }
+}
+
+@Composable
+fun rememberSwipeRowState(): SwipeRowState {
+    val scope = rememberCoroutineScope()
+    return remember(scope) {
+        SwipeRowState(
+            draggableState = AnchoredDraggableState(initialValue = SwipeRowAnchor.Resting),
+            scope = scope,
+        )
+    }
+}
