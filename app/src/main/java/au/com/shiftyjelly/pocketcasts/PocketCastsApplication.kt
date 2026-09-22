772eef59448345421698edbfca0a7863fea4fa31
Tamas Szelezsan <szelezsant@gmail.com>
2026-09-22 22:59:57 +0200

    whats new: refresh the catalog when the app comes to the foreground

--- app/src/main/java/au/com/shiftyjelly/pocketcasts/PocketCastsApplication.kt
+++ app/src/main/java/au/com/shiftyjelly/pocketcasts/PocketCastsApplication.kt
@@ -39,6 +39,7 @@ import au.com.shiftyjelly.pocketcasts.repositories.stats.PlaybackStatsSyncWorker
 import au.com.shiftyjelly.pocketcasts.repositories.support.DatabaseExportHelper
 import au.com.shiftyjelly.pocketcasts.repositories.user.StatsManager
 import au.com.shiftyjelly.pocketcasts.repositories.user.UserManager
+import au.com.shiftyjelly.pocketcasts.repositories.whatsnew.WhatsNewManager
 import au.com.shiftyjelly.pocketcasts.shared.AppLifecycleObserver
 import au.com.shiftyjelly.pocketcasts.shared.DownloadStatisticsReporter
 import au.com.shiftyjelly.pocketcasts.ui.helper.AppIcon
@@ -149,6 +150,8 @@ class PocketCastsApplication :
 
     @Inject lateinit var appReviewExceptionHandler: AppReviewExceptionHandler
 
+    @Inject lateinit var whatsNewManager: WhatsNewManager
+
     override fun onCreate() {
         if (BuildConfig.DEBUG) {
             StrictMode.setThreadPolicy(
@@ -210,6 +213,16 @@ class PocketCastsApplication :
             .setJobSchedulerJobIdRange(1000, 20000)
             .build()
 
+    private fun refreshWhatsNewWhenForegrounded() {
+        processLifecycleOwner.lifecycle.addObserver(
+            object : DefaultLifecycleObserver {
+                override fun onStart(owner: LifecycleOwner) {
+                    applicationScope.launch { whatsNewManager.refreshIfNeeded() }
+                }
+            },
+        )
+    }
+
     private fun applySelectedAppIconWhenBackgrounded() {
         processLifecycleOwner.lifecycle.addObserver(
             object : DefaultLifecycleObserver {
@@ -235,6 +248,7 @@ class PocketCastsApplication :
 
         runBlocking {
             applySelectedAppIconWhenBackgrounded()
+            refreshWhatsNewWhenForegrounded()
 
             notificationHelper.setupNotificationChannels()
             notificationManager.setupOnboardingNotifications()
