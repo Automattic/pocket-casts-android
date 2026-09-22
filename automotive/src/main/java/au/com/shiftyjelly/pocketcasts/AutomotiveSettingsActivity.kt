4609ee7bcaad878220072d72a00bf345a3bb362c
Tamas Szelezsan <szelezsant@gmail.com>
2026-09-22 13:57:38 +0200

    Remove the Automotive settings back callback that was never enabled

--- automotive/src/main/java/au/com/shiftyjelly/pocketcasts/AutomotiveSettingsActivity.kt
+++ automotive/src/main/java/au/com/shiftyjelly/pocketcasts/AutomotiveSettingsActivity.kt
@@ -3,7 +3,6 @@ package au.com.shiftyjelly.pocketcasts
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
-import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
@@ -24,15 +23,6 @@ class AutomotiveSettingsActivity :
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_automotive_settings)
-
-        onBackPressedDispatcher.addCallback(
-            this,
-            object : OnBackPressedCallback(false) {
-                override fun handleOnBackPressed() {
-                    handleBackPressed()
-                }
-            },
-        )
 
         if (savedInstanceState == null) {
             val settingsFragment = AutomotiveSettingsFragment()
