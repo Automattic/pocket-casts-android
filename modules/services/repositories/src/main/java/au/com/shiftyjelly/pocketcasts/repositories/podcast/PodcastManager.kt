85aaa6a493213bd8ec61891f211564b38fe85a04
Philip Simpson <philip.simpson@automattic.com>
2026-09-24 10:31:47 +0930

    Fix comment

--- modules/services/repositories/src/main/java/au/com/shiftyjelly/pocketcasts/repositories/podcast/PodcastManager.kt
+++ modules/services/repositories/src/main/java/au/com/shiftyjelly/pocketcasts/repositories/podcast/PodcastManager.kt
@@ -58,8 +58,6 @@ interface PodcastManager {
     fun subscribeToPodcast(podcastUuid: String, sync: Boolean, shouldAutoDownload: Boolean = true)
 
     suspend fun subscribeToPodcastOrThrow(podcastUuid: String, sync: Boolean = false, shouldAutoDownload: Boolean = true): Podcast
-
-    /** If the podcast isn't already in the database add it as unsubscribed. */
     suspend fun findOrDownloadPodcast(podcastUuid: String, waitForSubscribe: Boolean = false): Podcast
     fun isSubscribingToPodcasts(): Boolean
     fun isSubscribingToPodcast(podcastUuid: String): Boolean
